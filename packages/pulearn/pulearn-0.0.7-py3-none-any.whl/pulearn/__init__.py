7187f0bd1d1616764d6b6289df1cd2667f66e29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package pulearn/0.0.7/pulearn-0.0.7-py3-none-any.whl

---
+++ packages/pulearn/pulearn-0.0.7-py3-none-any.whl/pulearn/__init__.py
@@ -0,0 +1,18 @@
+"""
+The `pulearn` Python package provide a collection of scikit-learn wrappers to
+several positive-unlabled learning (PU-learning) methods.
+
+.. include:: ./documentation.md
+"""
+
+from .elkanoto import (  # noqa: F401
+    ElkanotoPuClassifier,
+    WeightedElkanotoPuClassifier,
+)
+from .bagging import (  # noqa: F401
+    BaggingPuClassifier,
+)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
