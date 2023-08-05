73ff453e99048f5969e12ea694bca557a457a814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:03 +0000

    Add package epitran/1.9/epitran-1.9-py2.py3-none-any.whl

---
+++ packages/epitran/epitran-1.9-py2.py3-none-any.whl/epitran-1.9.data/scripts/decompose.py
@@ -0,0 +1,13 @@
+#!python
+
+import unicodedata
+import sys
+
+
+def main(fn):
+    with open(fn, encoding='utf-8') as f:
+        print(unicodedata.normalize('NFD', f.read()))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
