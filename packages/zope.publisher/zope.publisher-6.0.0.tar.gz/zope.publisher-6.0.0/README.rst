246f71c7d72e08bd56cdb4996a7a28db6b7ef455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:39 +0000

    Add package zope.publisher/6.0.0/zope.publisher-6.0.0.tar.gz

---
+++ packages/zope.publisher/zope.publisher-6.0.0.tar.gz/zope.publisher-6.0.0/README.rst
@@ -0,0 +1,19 @@
+================
+ zope.publisher
+================
+
+.. image:: https://travis-ci.com/zopefoundation/zope.publisher.svg?branch=master
+        :target: https://travis-ci.com/zopefoundation/zope.publisher
+
+.. image:: https://readthedocs.org/projects/zopepublisher/badge/?version=latest
+         :target: http://zopepublisher.readthedocs.io/en/latest/?badge=latest
+         :alt: Documentation Status
+
+This package allows you to publish Python objects on the web.  It
+has support for plain HTTP/WebDAV clients, web browsers as well as
+XML-RPC and FTP clients.  Input and output streams are represented by
+request and response objects which allow for easy client interaction
+from Python.  The behaviour of the publisher is geared towards WSGI
+compatibility.
+
+Documentation is hosted at https://zopepublisher.readthedocs.io/
---
+++ packages/zope.publisher/zope.publisher-6.0.0.tar.gz/zope.publisher-6.0.0/setup.py
@@ -0,0 +1,109 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+entry_points = '''
+[paste.app_factory]
+main = zope.publisher.paste:Application
+
+[zope.publisher.publication_factory]
+sample = zope.publisher.tests.test_paste:SamplePublication
+'''
+
+tests_require = [
+    'zope.testing',
+    'zope.testrunner',
+]
+
+setup(
+    name='zope.publisher',
+    version='6.0.0',
+    url='https://github.com/zopefoundation/zope.publisher',
+    license='ZPL 2.1',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='The Zope publisher publishes Python objects on the web.',
+    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope'],
+    install_requires=[
+        'multipart<0.2; python_version < "3"',
+        'multipart; python_version >= "3"',
+        'setuptools',
+        'six',
+        'zope.browser',
+        'zope.component',
+        'zope.configuration',
+        'zope.contenttype>=4.0.0',
+        'zope.event',
+        'zope.exceptions',
+        'zope.i18n>=4.0.0',
+        'zope.interface>=4.0.1',
+        'zope.location',
+        'zope.proxy',
+        'zope.security>=4.0.0',
+    ],
+    extras_require={
+        'test': tests_require,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    tests_require=tests_require,
+    entry_points=entry_points,
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=', '.join([
+        '>=2.7',
+        '!=3.0.*',
+        '!=3.1.*',
+        '!=3.2.*',
+        '!=3.3.*',
+        '!=3.4.*',
+    ]),
+)
