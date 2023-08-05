27c92e095e2aff3e9883d6ae82f303feedbafe0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package pygments-style-heringer/0.1/pygments-style-heringer-0.1.tar.gz

---
+++ packages/pygments-style-heringer/pygments-style-heringer-0.1.tar.gz/pygments-style-heringer-0.1/setup.py
@@ -0,0 +1,34 @@
+﻿# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pygments-style-heringer',
+    version='0.1',
+    description='Default Pygments theme with different colors.',
+    keywords='pygments style',
+    license='BSD',
+
+    author='Matthias Lüken',
+
+    url='https://github.com/matthiaslueken/pygments-style-heringer',
+
+    packages=find_packages(),
+    install_requires=['pygments >= 1.4'],
+
+    entry_points='''[pygments.styles]
+                    heringer=pygments_style_heringer:HeringerStyle''',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
