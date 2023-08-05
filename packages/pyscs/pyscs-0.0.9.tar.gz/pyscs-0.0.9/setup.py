01a2ff4fcd992a7d5cc0de75b5a61e432f38f270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package pyscs/0.0.9/pyscs-0.0.9.tar.gz

---
+++ packages/pyscs/pyscs-0.0.9.tar.gz/pyscs-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import io
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = ""
+    
+setup(
+    name="pyscs",
+    packages=find_packages(),
+    version='0.0.9',
+    description="support scs >=2.4.1 client lib for python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="libbyandhelen",
+    author_email='libbyandhelen@163.com',
+    url="https://github.com/username/reponame",
+    install_requires=[
+        'requests',
+    ]
+)
