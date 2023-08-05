037573ccc65b5d527be6b661af99640e76552d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:41 +0000

    Add package fireant/7.7.3/fireant-7.7.3.tar.gz

---
+++ packages/fireant/fireant-7.7.3.tar.gz/fireant-7.7.3/fireant/database/mysql.py
@@ -0,0 +1,236 @@
+from pypika import (
+    Dialects,
+    MySQLQuery,
+    Table,
+    enums,
+    functions as fn,
+    terms,
+)
+from pypika.terms import CustomFunction, Interval, Parameter
+
+from . import sql_types
+from .base import Database
+from .type_engine import TypeEngine
+
+_DateFormat = CustomFunction('DATE_FORMAT', ['field', 'date_format'])
+_DateSub = CustomFunction('DATE_SUB', ['field', 'interval'])
+_MakeDate = CustomFunction('MAKEDATE', ['year', 'day_of_year'])
+_Weekday = CustomFunction('WEEKDAY', ['arg'])
+_Year = CustomFunction('YEAR', ['arg'])
+_Quarter = CustomFunction('QUARTER', ['arg'])
+_Timestamp = CustomFunction('TIMESTAMP', ['arg'])
+
+
+class DateAdd(terms.Function):
+    """
+    Override for the MySQL specific DateAdd function which expects an interval instead of the date part and interval
+    unit e.g. DATE_ADD("date", INTERVAL 1 YEAR)
+    """
+
+    def __init__(self, field, interval_term, alias=None):
+        super(DateAdd, self).__init__('DATE_ADD', field, interval_term, alias=alias)
+
+
+class _CustomMySQLInterval(terms.Function):
+    """
+    The PyPika Interval function generates Interval SQL based on the provided time unit arguments.
+    Fireant needs a more flexible Interval query string generator than this
+    to mimicking the Vertica TRUNC function, so this is defined locally here.
+    """
+
+    def __init__(self, field, unit):
+        super().__init__('INTERVAL', field, unit)
+        self.field = field
+        self.unit = unit
+
+    def get_sql(self, **kwargs):
+        field_sql = self.field.get_sql(**kwargs)
+        return f'INTERVAL {field_sql} {self.unit}'
+
+
+class MySQLDatabase(Database):
+    """
+    MySQL client that uses the PyMySQL module.
+    """
+
+    # The pypika query class to use for constructing queries
+    query_cls = MySQLQuery
+
+    def __init__(
+        self, host='localhost', port=3306, database=None, user=None, password=None, charset='utf8mb4', **kwags
+    ):
+        super(MySQLDatabase, self).__init__(host, port, database, **kwags)
+        self.user = user
+        self.password = password
+        self.charset = charset
+        self.type_engine = MySQLTypeEngine()
+
+    def _get_connection_class(self):
+        # Nesting inside a function so the import does not cause issues if users have not installed the 'mysql' extra
+        # when installing
+        import pymysql
+
+        class MySQLConnection(pymysql.connections.Connection):
+            """
+            PyMySQL has deprecated context managers in the connection class.
+            To make the functionality consistent with other database drivers, we override
+            the context manager to return the connection instead of the cursor object.
+
+            This also fixes an issue where connections were not being closed in the current PyMySQL
+            context manager implementation!
+            https://github.com/PyMySQL/PyMySQL/issues/735
+            """
+
+            def __enter__(self):
+                return self
+
+            def __exit__(self, exc, value, traceback):
+                self.close()
+
+        return MySQLConnection
+
+    def connect(self):
+        """
+        Returns a MySQL connection
+
+        :return: pymysql Connection class
+        """
+        import pymysql
+
+        connection_class = self._get_connection_class()
+        return connection_class(
+            host=self.host,
+            port=self.port,
+            db=self.database,
+            user=self.user,
+            password=self.password,
+            charset=self.charset,
+            cursorclass=pymysql.cursors.Cursor,
+        )
+
+    def trunc_date(self, field, interval):
+        if interval == 'hour':
+            return _DateFormat(field, '%Y-%m-%d %H:00:00')
+        elif interval == 'day':
+            return _DateFormat(field, '%Y-%m-%d 00:00:00')
+        elif interval == 'week':
+            return _DateFormat(_DateSub(field, _CustomMySQLInterval(_Weekday(field), 'DAY')), '%Y-%m-%d 00:00:00')
+        elif interval == 'month':
+            return _DateFormat(field, '%Y-%m-01')
+        elif interval == 'quarter':
+            return (
+                _MakeDate(_Year(_Timestamp(field)), 1)
+                + _CustomMySQLInterval(_Quarter(_Timestamp(field)), 'QUARTER')
+                - Interval(quarters=1, dialect=Dialects.MYSQL)
+            )
+        elif interval == 'year':
+            return _DateFormat(field, '%Y-01-01')
+        else:
+            raise ValueError(f'Invalid interval provided to trunc_date method: {interval}')
+
+    def to_char(self, definition):
+        return fn.Cast(definition, enums.SqlTypes.CHAR)
+
+    def date_add(self, field, date_part, interval):
+        # adding an extra 's' as MySQL's interval doesn't work with 'year', 'week' etc, it expects a plural
+        interval_term = terms.Interval(**{'{}s'.format(str(date_part)): interval, 'dialect': Dialects.MYSQL})
+        return DateAdd(field, interval_term)
+
+    def get_column_definitions(self, schema, table, connection=None):
+        columns = Table('columns', schema='INFORMATION_SCHEMA')
+
+        columns_query = (
+            MySQLQuery.from_(columns)
+            .select(columns.column_name, columns.column_type)
+            .where(columns.table_schema == Parameter('%(schema)s'))
+            .where(columns.field('table_name') == Parameter('%(table)s'))
+            .distinct()
+            .orderby(columns.column_name)
+        )
+
+        return self.fetch(str(columns_query), parameters=dict(schema=schema, table=table), connection=connection)
+
+    def import_csv(self, table, file_path, connection=None):
+        """
+        Execute a query to import a file into a table using the provided connection.
+
+        :param table: The name of a table to import data into.
+        :param file_path: The path of the file to be imported.
+        :param connection: (Optional) The connection to execute this query with.
+        """
+        import_query = MySQLQuery.load(file_path).into(table)
+
+        self.execute(str(import_query), connection=connection)
+
+    def create_temporary_table_from_columns(self, table, columns, connection=None):
+        """
+        Creates a temporary table from a list of columns.
+
+        :param table: The name of the new temporary table.
+        :param columns: The columns of the new temporary table.
+        :param connection: (Optional) The connection to execute this query with.
+        """
+        create_query = MySQLQuery.create_table(table).temporary().columns(*columns)
+
+        self.execute(str(create_query), connection=connection)
+
+    def create_temporary_table_from_select(self, table, select_query, connection=None):
+        """
+        Creates a temporary table from a SELECT query.
+
+        :param table: The name of the new temporary table.
+        :param select_query: The query to be used for selecting data of an existing table for the new temporary table.
+        :param connection: (Optional) The connection to execute this query with.
+        """
+        create_query = MySQLQuery.create_table(table).temporary().as_select(select_query)
+
+        self.execute(str(create_query), connection=connection)
+
+
+class MySQLTypeEngine(TypeEngine):
+    mysql_to_ansi_mapper = {
+        'bit': sql_types.Char,
+        'char': sql_types.Char,
+        'nchar': sql_types.Char,
+        'varchar': sql_types.VarChar,
+        'nvarchar': sql_types.VarChar,
+        'text': sql_types.Text,
+        'boolean': sql_types.Boolean,
+        'int': sql_types.Integer,
+        'integer': sql_types.Integer,
+        'year': sql_types.Integer,
+        'smallint': sql_types.SmallInt,
+        'tinyint': sql_types.SmallInt,
+        'bigint': sql_types.BigInt,
+        'decimal': sql_types.Decimal,
+        'fixed': sql_types.Decimal,
+        'numeric': sql_types.Numeric,
+        'float': sql_types.Float,
+        'real': sql_types.DoublePrecision,
+        'double': sql_types.DoublePrecision,
+        'date': sql_types.Date,
+        'time': sql_types.Time,
+        'datetime': sql_types.DateTime,
+        'timestamp': sql_types.Timestamp,
+    }
+
+    ansi_to_mysql_mapper = {
+        'CHAR': 'char',
+        'VARCHAR': 'varchar',
+        'TEXT': 'text',
+        'BOOLEAN': 'boolean',
+        'INTEGER': 'integer',
+        'SMALLINT': 'smallint',
+        'BIGINT': 'bigint',
+        'DECIMAL': 'decimal',
+        'NUMERIC': 'numeric',
+        'FLOAT': 'float',
+        'REAL': 'real',
+        'DOUBLEPRECISION': 'real',
+        'DATE': 'date',
+        'TIME': 'time',
+        'TIMESTAMP': 'timestamp',
+    }
+
+    def __init__(self):
+        super(MySQLTypeEngine, self).__init__(self.mysql_to_ansi_mapper, self.ansi_to_mysql_mapper)
---
+++ packages/fireant/fireant-7.7.3.tar.gz/fireant-7.7.3/fireant/tests/database/test_postgresql.py
@@ -0,0 +1,107 @@
+from unittest import TestCase
+from unittest.mock import (
+    Mock,
+    patch,
+)
+
+from pypika import Field
+
+from fireant.database import PostgreSQLDatabase
+
+
+class TestPostgreSQL(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.database = PostgreSQLDatabase()
+
+    def test_defaults(self):
+        self.assertEqual('localhost', self.database.host)
+        self.assertEqual(5432, self.database.port)
+        self.assertIsNone(self.database.database)
+        self.assertIsNone(self.database.password)
+
+    def test_connect(self):
+        mock_postgresql = Mock()
+        with patch.dict('sys.modules', psycopg2=mock_postgresql):
+            mock_postgresql.connect.return_value = 'OK'
+
+            postgresql = PostgreSQLDatabase('test_host', 1234, 'test_database', 'test_user', 'password')
+            result = postgresql.connect()
+
+        self.assertEqual('OK', result)
+        mock_postgresql.connect.assert_called_once_with(
+            host='test_host',
+            port=1234,
+            dbname='test_database',
+            user='test_user',
+            password='password',
+        )
+
+    def test_trunc_hour(self):
+        result = self.database.trunc_date(Field('date'), 'hour')
+
+        self.assertEqual('DATE_TRUNC(\'hour\',"date")', str(result))
+
+    def test_trunc_day(self):
+        result = self.database.trunc_date(Field('date'), 'day')
+
+        self.assertEqual('DATE_TRUNC(\'day\',"date")', str(result))
+
+    def test_trunc_week(self):
+        result = self.database.trunc_date(Field('date'), 'week')
+
+        self.assertEqual('DATE_TRUNC(\'week\',"date")', str(result))
+
+    def test_trunc_quarter(self):
+        result = self.database.trunc_date(Field('date'), 'quarter')
+
+        self.assertEqual('DATE_TRUNC(\'quarter\',"date")', str(result))
+
+    def test_trunc_year(self):
+        result = self.database.trunc_date(Field('date'), 'year')
+
+        self.assertEqual('DATE_TRUNC(\'year\',"date")', str(result))
+
+    def test_date_add_hour(self):
+        result = self.database.date_add(Field('date'), 'hour', 1)
+
+        self.assertEqual('DATE_ADD(\'hour\',1,"date")', str(result))
+
+    def test_date_add_day(self):
+        result = self.database.date_add(Field('date'), 'day', 1)
+
+        self.assertEqual('DATE_ADD(\'day\',1,"date")', str(result))
+
+    def test_date_add_week(self):
+        result = self.database.date_add(Field('date'), 'week', 1)
+
+        self.assertEqual('DATE_ADD(\'week\',1,"date")', str(result))
+
+    def test_date_add_month(self):
+        result = self.database.date_add(Field('date'), 'month', 1)
+
+        self.assertEqual('DATE_ADD(\'month\',1,"date")', str(result))
+
+    def test_date_add_quarter(self):
+        result = self.database.date_add(Field('date'), 'quarter', 1)
+
+        self.assertEqual('DATE_ADD(\'quarter\',1,"date")', str(result))
+
+    def test_date_add_year(self):
+        result = self.database.date_add(Field('date'), 'year', 1)
+
+        self.assertEqual('DATE_ADD(\'year\',1,"date")', str(result))
+
+    # noinspection SqlDialectInspection,SqlNoDataSourceInspection
+    @patch.object(PostgreSQLDatabase, 'fetch')
+    def test_get_column_definitions(self, mock_fetch):
+        PostgreSQLDatabase().get_column_definitions('test_schema', 'test_table')
+
+        mock_fetch.assert_called_once_with(
+            'SELECT DISTINCT "column_name","data_type" '
+            'FROM "information_schema"."columns" '
+            'WHERE "table_schema"=%(schema)s AND "table_name"=%(table)s '
+            'ORDER BY "column_name"',
+            connection=None,
+            parameters={'schema': 'test_schema', 'table': 'test_table'},
+        )
