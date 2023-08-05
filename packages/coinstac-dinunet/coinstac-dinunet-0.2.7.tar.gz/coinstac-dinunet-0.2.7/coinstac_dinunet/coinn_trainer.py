9fd3872a22e15d3272bf4c5fe7b429a2acb40646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:06 +0000

    Add package coinstac-dinunet/0.2.7/coinstac-dinunet-0.2.7.tar.gz

---
+++ packages/coinstac-dinunet/coinstac-dinunet-0.2.7.tar.gz/coinstac-dinunet-0.2.7/coinstac_dinunet/coinn_trainer.py
@@ -0,0 +1,337 @@
+"""
+@author: Aashis Khanal
+@email: sraashis@gmail.com
+@ref: https://github.com/sraashis/easytorch
+"""
+
+import json as _json
+import random as _rd
+from collections import OrderedDict as _ODict
+from os import sep as _sep
+
+import torch as _torch
+
+import coinstac_dinunet.config as _conf
+import coinstac_dinunet.data as _data
+import coinstac_dinunet.metrics as _base_metrics
+import coinstac_dinunet.utils.tensorutils as _tu
+from coinstac_dinunet.config.status import *
+from coinstac_dinunet.data import COINNDataLoader as _COINNDLoader
+from coinstac_dinunet.utils import FrozenDict as _FrozenDict
+
+
+class COINNTrainer:
+    def __init__(self, cache: dict = None, input: dict = None, state: dict = None, **kw):
+        self.cache = cache
+        self.input = _FrozenDict(input)
+        self.state = _FrozenDict(state)
+        self.nn = _ODict()
+        self.device = _ODict()
+        self.optimizer = _ODict()
+
+    def _init_nn_model(self):
+        r"""
+        User cam override and initialize required models in self.nn dict.
+        """
+        raise NotImplementedError('Must be implemented in child class.')
+
+    def _init_nn_weights(self, **kw):
+        r"""
+        By default, will initialize network with Kaimming initialization.
+        If path to pretrained weights are given, it will be used instead.
+        """
+        if self.cache.get('pretrained_path') is not None:
+            self.load_checkpoint(self.cache['pretrained_path'])
+        elif self.cache['mode'] == Mode.TRAIN:
+            _torch.manual_seed(self.cache['seed'])
+            for mk in self.nn:
+                _tu.initialize_weights(self.nn[mk])
+
+    def _init_optimizer(self):
+        r"""
+        Initialize required optimizers here. Default is Adam,
+        """
+        first_model = list(self.nn.keys())[0]
+        self.optimizer['adam'] = _torch.optim.Adam(self.nn[first_model].parameters(),
+                                                   lr=self.cache['learning_rate'])
+
+    def init_nn(self, init_weights=False):
+        self._init_nn_model()
+        self._init_optimizer()
+        if init_weights:
+            self._init_nn_weights(init_weights=init_weights)
+        self._set_gpus()
+
+    def _set_gpus(self):
+        self.device['gpu'] = _torch.device("cpu")
+        if len(self.cache.get('gpus', [])) > 0:
+            if _conf.gpu_available:
+                self.device['gpu'] = _torch.device(f"cuda:{self.cache['gpus'][0]}")
+            else:
+                raise Exception(f'*** GPU not detected in {self.state["clientId"]}. ***')
+        for model_key in self.nn:
+            self.nn[model_key] = self.nn[model_key].to(self.device['gpu'])
+
+    def load_checkpoint_from_key(self, key='checkpoint'):
+        self.load_checkpoint(self.cache['log_dir'] + _sep + self.cache[key])
+
+    def load_checkpoint(self, full_path):
+        try:
+            chk = _torch.load(full_path)
+        except:
+            chk = _torch.load(full_path, map_location='cpu')
+
+        if chk.get('source', 'Unknown').lower() == 'coinstac':
+            for m in chk['models']:
+                try:
+                    self.nn[m].module.load_state_dict(chk['models'][m])
+                except:
+                    self.nn[m].load_state_dict(chk['models'][m])
+
+            for m in chk['optimizers']:
+                try:
+                    self.optimizer[m].module.load_state_dict(chk['optimizers'][m])
+                except:
+                    self.optimizer[m].load_state_dict(chk['optimizers'][m])
+        else:
+            mkey = list(self.nn.keys())[0]
+            try:
+                self.nn[mkey].module.load_state_dict(chk)
+            except:
+                self.nn[mkey].load_state_dict(chk)
+
+    def backward(self, it):
+        out = {}
+        it['loss'].backward()
+        out['grads_file'] = _conf.grads_file
+        first_model = list(self.nn.keys())[0]
+        grads = _tu.extract_grads(self.nn[first_model])
+        _tu.save_grads(self.state['transferDirectory'] + _sep + out['grads_file'], grads)
+        return out
+
+    def step(self):
+        grads = _tu.load_grads(self.state['baseDirectory'] + _sep + self.input['avg_grads_file'])
+
+        first_model = list(self.nn.keys())[0]
+        for i, param in enumerate(self.nn[first_model].parameters()):
+            param.grad = _torch.tensor(grads[i], dtype=_torch.float32).to(self.device['gpu'])
+
+        first_optim = list(self.optimizer.keys())[0]
+        self.optimizer[first_optim].step()
+
+    def save_checkpoint(self, file_name, src='coinstac'):
+        checkpoint = {'source': src}
+        for k in self.nn:
+            checkpoint['models'] = {}
+            try:
+                checkpoint['models'][k] = self.nn[k].module.state_dict()
+            except:
+                checkpoint['models'][k] = self.nn[k].state_dict()
+        for k in self.optimizer:
+            checkpoint['optimizers'] = {}
+            try:
+                checkpoint['optimizers'][k] = self.optimizer[k].module.state_dict()
+            except:
+                checkpoint['optimizers'][k] = self.optimizer[k].state_dict()
+        _torch.save(checkpoint, self.cache['log_dir'] + _sep + file_name)
+
+    def evaluation(self, dataset_list=None, save_pred=False):
+        for k in self.nn:
+            self.nn[k].eval()
+
+        eval_avg = self.new_averages()
+        eval_metrics = self.new_metrics()
+        eval_loaders = [_data.COINNDataLoader.new(dataset=d, **self.cache) for d in dataset_list]
+        with _torch.no_grad():
+            for loader in eval_loaders:
+                its = []
+                metrics = self.new_metrics()
+                avg = self.new_averages()
+                for i, batch in enumerate(loader):
+                    it = self.iteration(batch)
+                    if not it.get('metrics'):
+                        it['metrics'] = _base_metrics.COINNMetrics()
+                    metrics.accumulate(it['metrics'])
+                    avg.accumulate(it['averages'])
+                    if save_pred:
+                        its.append(it)
+                eval_metrics.accumulate(metrics)
+                eval_avg.accumulate(avg)
+                if save_pred:
+                    self.save_predictions(loader.dataset, its)
+        return eval_avg, eval_metrics
+
+    def iteration(self, batch):
+        r"""
+        Left for user to implement one mini-bath iteration:
+        Example:{
+                    inputs = batch['input'].to(self.device['gpu']).float()
+                    labels = batch['label'].to(self.device['gpu']).long()
+                    out = self.nn['model'](inputs)
+                    loss = F.cross_entropy(out, labels)
+                    out = F.softmax(out, 1)
+                    _, pred = torch.max(out, 1)
+                    sc = self.new_metrics()
+                    sc.add(pred, labels)
+                    avg = self.new_averages()
+                    avg.add(loss.item(), len(inputs))
+                    return {'loss': loss, 'averages': avg, 'output': out, 'metrics': sc, 'predictions': pred}
+                }
+        Note: loss, averages, and metrics are required, whereas others are optional
+            -we will have to do backward on loss
+            -we need to keep track of loss
+            -we need to keep track of metrics
+        """
+        return {'metrics': _base_metrics.COINNMetrics(), 'averages': _base_metrics.COINNAverages(num_averages=1)}
+
+    def save_predictions(self, dataset, its):
+        pass
+
+    def train(self, dataset_cls):
+        out = {}
+
+        for k in self.nn:
+            self.nn[k].train()
+        for k in self.optimizer:
+            self.optimizer[k].zero_grad()
+
+        itr_avgs, itr_metrics = self.new_averages(), self.new_metrics()
+
+        its = []
+        for _ in range(self.cache.get('local_iterations', 1)):
+            it = self.iteration(self.next_batch(dataset_cls, mode='train'))
+            out.update(**self.backward(it))
+            out.update(**self.next_iter())
+            itr_avgs.accumulate(it['averages'])
+            itr_metrics.accumulate(it['metrics'])
+            its.append([it])
+        self.cache['train_log'].append([vars(itr_avgs), vars(itr_metrics)])
+
+        if self.input.get('avg_grads_file'):
+            self.step()
+            self.save_checkpoint(file_name=self.cache['current_nn_state'])
+
+        out.update(**self._on_iteration_end(0, self.cache['epoch'], its))
+        return out
+
+    def validation(self, dataset_cls):
+        out = {}
+        avg, scores = self.evaluation(self._get_validation_dataset(dataset_cls), save_pred=False)
+        out['validation_log'] = [vars(avg), vars(scores)]
+        out.update(**self.next_epoch())
+        out.update(**self._on_epoch_end(self.cache['epoch'], None, None, avg, scores))
+        return out
+
+    def test(self, dataset_cls):
+        out = {}
+        self.load_checkpoint_from_key(key='best_nn_state')
+        avg, scores = self.evaluation(self._get_test_dataset(dataset_cls), save_pred=True)
+        out['test_log'] = [vars(avg), vars(scores)]
+        return out
+
+    def load_data_indices(self, dataset_cls, split_key='train'):
+        """
+        Parse and load dataset and save to cache:
+        so that in next global iteration we dont have to do that again.
+        The data IO depends on use case-For a instance, if your data can fit in RAM, you can load
+         and save the entire dataset in cache. But, in general,
+         it is better to save indices in cache, and load only a mini-batch at a time
+         (logic in __nextitem__) of the data loader.
+        """
+        dataset = dataset_cls(mode='eval', limit=self.cache.get('load_limit', float('inf')))
+        file = self.cache['split_dir'] + _sep + self.cache['split_file']
+        with open(file) as split_file:
+            split = _json.loads(split_file.read())
+            dataset.add(files=split[split_key], cache=self.cache, state=self.state)
+
+        self.cache['data_indices'] = dataset.indices
+        if len(dataset) % self.cache['batch_size'] >= self.cache.get('min_batch_size', 4):
+            self.cache['data_len'] = len(dataset)
+        else:
+            self.cache['data_len'] = (len(dataset) // self.cache['batch_size']) * self.cache['batch_size']
+
+    def _get_validation_dataset(self, dataset_cls):
+        r"""
+        Load the validation data from current fold/split.
+        """
+        val_dataset_list = []
+        file = self.cache['split_dir'] + _sep + self.cache['split_file']
+        with open(file) as split_file:
+            split = _json.loads(split_file.read())
+            val_dataset = dataset_cls(mode='eval', limit=self.cache.get('load_limit', _conf.data_load_lim))
+            val_dataset.add(files=split['validation'], cache=self.cache, state=self.state)
+            val_dataset_list.append(val_dataset)
+        return val_dataset_list
+
+    def _get_test_dataset(self, dataset_cls):
+        r"""
+        Load the test data from current fold/split.
+        """
+        test_dataset_list = []
+        file = self.cache['split_dir'] + _sep + self.cache['split_file']
+        with open(file) as split_file:
+            split = _json.loads(split_file.read())
+            if self.cache.get('load_sparse'):
+                for f in split.get('test', []):
+                    test_dataset = dataset_cls(mode='eval', limit=self.cache.get('load_limit', _conf.data_load_lim))
+                    test_dataset.add(files=[f], cache=self.cache, state=self.state)
+                    test_dataset_list.append(test_dataset)
+            else:
+                test_dataset = dataset_cls(mode='eval', limit=self.cache.get('load_limit', _conf.data_load_lim))
+                test_dataset.add(files=split['test'], cache=self.cache, state=self.state)
+                test_dataset_list.append(test_dataset)
+        return test_dataset_list
+
+    def new_metrics(self):
+        return _base_metrics.Prf1a()
+
+    def new_averages(self):
+        return _base_metrics.COINNAverages(num_averages=1)
+
+    def next_iter(self):
+        out = {}
+        self.cache['cursor'] += self.cache['batch_size']
+        if self.cache['cursor'] >= self.cache['data_len']:
+            out['mode'] = Mode.VALIDATION_WAITING
+            self.cache['cursor'] = 0
+            _rd.shuffle(self.cache['data_indices'])
+        return out
+
+    def next_batch(self, dataset_cls, mode='train'):
+        dataset = dataset_cls(mode=mode, limit=self.cache.get('load_limit', _conf.data_load_lim))
+        dataset.indices = self.cache['data_indices'][self.cache['cursor']:]
+        dataset.add(files=[], cache=self.cache, state=self.state)
+        loader = _COINNDLoader.new(dataset=dataset, **self.cache)
+        return next(loader.__iter__())
+
+    def next_epoch(self):
+        """
+        Transition to next epoch after validation.
+        It will set 'train_waiting' status if we need more training
+        Else it will set 'test' status
+        """
+        out = {}
+        self.cache['epoch'] += 1
+        if self.cache['epoch'] - self.cache.get('best_epoch', self.cache['epoch']) \
+                >= self.cache['patience'] or self.cache['epoch'] >= self.cache['epochs']:
+            out['mode'] = Mode.TEST
+        else:
+            self.cache['cursor'] = 0
+            out['mode'] = Mode.TRAIN_WAITING
+            _rd.shuffle(self.cache['data_indices'])
+
+        out['train_log'] = self.cache['train_log']
+        self.cache['train_log'] = []
+        return out
+
+    def _on_epoch_end(self, ep, ep_loss, ep_metrics, val_loss, val_metrics):
+        r"""
+        Any logic to run after an epoch ends.
+        """
+        return {}
+
+    def _on_iteration_end(self, i, ep, it):
+        r"""
+        Any logic to run after an iteration ends.
+        """
+        return {}
---
+++ packages/coinstac-dinunet/coinstac-dinunet-0.2.7.tar.gz/coinstac-dinunet-0.2.7/coinstac_dinunet/nodes/remote.py
@@ -0,0 +1,247 @@
+"""
+@author: Aashis Khanal
+@email: sraashis@gmail.com
+@ref: https://github.com/sraashis/easytorch
+"""
+
+import datetime as _datetime
+import json as _json
+import os as _os
+import random as _random
+import shutil as _shutil
+import sys as _sys
+from typing import Callable as _Callable
+
+import numpy as _np
+import torch as _torch
+
+import coinstac_dinunet.config as _conf
+import coinstac_dinunet.metrics as _metric
+import coinstac_dinunet.utils as _utils
+import coinstac_dinunet.utils.tensorutils as _tu
+from coinstac_dinunet.config.status import *
+from coinstac_dinunet.vision import plotter as _plot
+
+
+def average_sites_gradients(cache, input, state):
+    """
+    Average each sites gradients and pass it to all sites.
+    """
+    out = {'avg_grads_file': _conf.avg_grads_file}
+    grads = []
+    for site, site_vars in input.items():
+        grads_file = state['baseDirectory'] + _os.sep + site + _os.sep + site_vars['grads_file']
+        grads.append(_tu.load_grads(grads_file))
+
+    avg_grads = []
+    for layer_grad in zip(*grads):
+        avg_grads.append(_np.array(layer_grad).mean(0))
+    _tu.save_grads(state['transferDirectory'] + _os.sep + out['avg_grads_file'], avg_grads)
+    return out
+
+
+class COINNRemote:
+    def __init__(self, cache: dict = None, input: dict = None, state: dict = None,
+                 sites_reducer: _Callable = average_sites_gradients, **kw):
+        self.out = {}
+        self.cache = cache
+        self.input = _utils.FrozenDict(input)
+        self.state = _utils.FrozenDict(state)
+        self.sites_reducer = sites_reducer
+
+    def _init_runs(self):
+        self.cache.update(computation_id=[v['computation_id'] for _, v in self.input.items()][0])
+        self.cache.update(num_folds=[v['num_folds'] for _, v in self.input.items()][0])
+        self.cache.update(seed=[v.get('seed') for _, v in self.input.items()][0])
+        self.cache.update(seed=_random.randint(0, int(1e6)) if self.cache['seed'] is None else self.cache['seed'])
+        self.cache['folds'] = []
+        for fold in range(self.cache['num_folds']):
+            self.cache['folds'].append({'split_ix': fold, 'seed': self.cache['seed']})
+
+        self.cache['folds'] = self.cache['folds'][::-1]
+
+    def _next_run(self):
+        """
+        This function pops a new fold, lock parameters, and forward init_nn signal to all sites
+        """
+        self.cache['fold'] = self.cache['folds'].pop()
+        self.cache.update(
+            log_dir=self.state['outputDirectory'] + _os.sep + self.cache[
+                'computation_id'] + _os.sep + f"fold_{self.cache['fold']['split_ix']}")
+        _os.makedirs(self.cache['log_dir'], exist_ok=True)
+
+        metric_direction = self._monitor_metric()[1]
+        self.cache.update(best_val_score=0 if metric_direction == 'maximize' else 1e11)
+        self.cache.update(train_log=[], validation_log=[], test_log=[])
+
+        """**** Parameter Lock ******"""
+        out = {}
+        for site, site_vars in self.input.items():
+            out[site] = self.cache['fold']
+        return out
+
+    def _get_log_headers(self):
+        return 'Loss,Precision,Recall,F1,Accuracy'
+
+    @staticmethod
+    def _check(logic, k, v, kw):
+        phases = []
+        for site_vars in kw.values():
+            phases.append(site_vars.get(k) == v)
+        return logic(phases)
+
+    def _new_metrics(self):
+        return _metric.Prf1a()
+
+    def _new_averages(self):
+        return _metric.COINNAverages()
+
+    def _monitor_metric(self):
+        return 'f1', 'maximize'
+
+    def _on_epoch_end(self):
+        """
+        #############################
+        Entry status: "train_waiting"
+        Exit status: "train"
+        ############################
+
+        This function runs once an epoch of training is done and all sites
+            run the validation step i.e. all sites in "train_waiting" status.
+        We accumulate training/validation loss and scores of the last epoch.
+        We also send a save current model as best signal to all sites if the global validation score is better than the previously saved one.
+        """
+        out = {}
+        train_scores = self._new_metrics()
+        train_loss = self._new_averages()
+        val_scores = self._new_metrics()
+        val_loss = self._new_averages()
+        for site, site_vars in self.input.items():
+            for ta, tm in site_vars['train_log']:
+                train_loss.update(**ta)
+                train_scores.update(**tm)
+            va, vm = site_vars['validation_log']
+            val_loss.update(**va)
+            val_scores.update(**vm)
+
+        self.cache['train_log'].append([*train_loss.get(), *train_scores.get()])
+        self.cache['validation_log'].append([*val_loss.get(), *val_scores.get()])
+        self._save_if_better(val_scores)
+        _plot.plot_progress(self.cache, self.cache['log_dir'], plot_keys=['train_log', 'validation_log'])
+        return out
+
+    def _save_if_better(self, metrics):
+        r"""
+        Save the current model as best if it has better validation scores.
+        """
+        monitor_metric, direction = self._monitor_metric()
+        sc = getattr(metrics, monitor_metric)
+        if callable(sc):
+            sc = sc()
+
+        if (direction == 'maximize' and sc >= self.cache['best_val_score']) or (
+                direction == 'minimize' and sc <= self.cache['best_val_score']):
+            self.out['save_current_as_best'] = True
+            self.cache['best_val_score'] = sc
+        else:
+            self.out['save_current_as_best'] = False
+
+    def _save_test_scores(self):
+        """
+        ########################
+        Entry: phase "next_run_waiting"
+        Exit: continue on next fold if folds left, else success and stop
+        #######################
+        This function saves test score of last fold.
+        """
+        test_scores = self._new_metrics()
+        test_loss = self._new_averages()
+        for site, site_vars in self.input.items():
+            va, vm = site_vars['test_log']
+            test_loss.update(**va)
+            test_scores.update(**vm)
+        self.cache['test_log'].append([*test_loss.get(), *test_scores.get()])
+        self.cache['test_scores'] = _json.dumps(vars(test_scores))
+        self.cache['global_test_score'].append(vars(test_scores))
+        _plot.plot_progress(self.cache, self.cache['log_dir'], plot_keys=['train_log', 'validation_log'])
+        _utils.save_scores(self.cache, self.cache['log_dir'], file_keys=['test_log', 'test_scores'])
+        _utils.save_cache(self.cache, self.state['log_dir'])
+
+    def _send_global_scores(self):
+        out = {}
+        score = self._new_metrics()
+        for sc in self.cache['global_test_score']:
+            score.update(**sc)
+        self.cache['global_test_score'] = ['Precision,Recall,F1,Accuracy']
+        self.cache['global_test_score'].append(score.get())
+        _utils.save_scores(self.cache, self.state['outputDirectory'] + _os.sep + self.cache['computation_id'],
+                           file_keys=['global_test_score'])
+
+        out['results_zip'] = f"{self.cache['computation_id']}_" + '_'.join(str(_datetime.datetime.now()).split(' '))
+        _shutil.make_archive(f"{self.state['transferDirectory']}{_os.sep}{out['results_zip']}",
+                             'zip', self.cache['log_dir'])
+        return out
+
+    def _set_mode(self, mode=None):
+        out = {}
+        for site, site_vars in self.input.items():
+            out[site] = mode if mode else site_vars['mode']
+        return out
+
+    def compute(self):
+
+        nxt_phase = self.input.get('phase', Phase.INIT_RUNS)
+        if self._check(all, 'phase', Phase.INIT_RUNS, self.input):
+            """
+            Initialize all folds and loggers
+            """
+            self.cache['global_test_score'] = []
+            self._init_runs()
+            self.out['run'] = self._next_run()
+            self.out['global_modes'] = self._set_mode()
+            nxt_phase = Phase.INIT_NN
+
+        if self._check(all, 'phase', Phase.COMPUTATION, self.input):
+            """
+            Main computation phase where we aggregate sites information
+            We also handle train/validation/test stages of local sites by sending corresponding signals from here
+            """
+            nxt_phase = Phase.COMPUTATION
+            self.out['global_modes'] = self._set_mode()
+            if self._check(all, 'grads_file', _conf.grads_file, self.input):
+                self.out.update(**self.sites_reducer(self.cache, self.input, self.state))
+
+            if self._check(all, 'mode', Mode.VALIDATION_WAITING, self.input):
+                self.out['global_modes'] = self._set_mode(mode=Mode.VALIDATION)
+
+            if self._check(all, 'mode', Mode.TRAIN_WAITING, self.input):
+                self.out.update(**self._on_epoch_end())
+                self.out['global_modes'] = self._set_mode(mode=Mode.TRAIN)
+
+            if self._check(all, 'mode', Mode.TEST, self.input):
+                self.out.update(**self._on_epoch_end())
+                self.out['global_modes'] = self._set_mode(mode=Mode.TEST)
+
+        if self._check(all, 'mode', Phase.NEXT_RUN_WAITING, self.input):
+            """
+            This block runs when a fold has completed all train, test, validation phase.
+            We save all the scores and plot the results.
+            We transition to new fold if there is any left, else we stop the distributed computation with a success signal.
+            """
+            self._save_test_scores()
+            if len(self.cache['folds']) > 0:
+                self.out['nn'] = {}
+                self.out['run'] = self._next_run()
+                self.out['global_modes'] = self._set_mode()
+                nxt_phase = Phase.INIT_NN
+            else:
+                self.out.update(**self._send_global_scores())
+                nxt_phase = Phase.SUCCESS
+
+        self.out['phase'] = nxt_phase
+
+    def send(self):
+        output = _json.dumps(
+            {'output': self.out, 'cache': self.cache,
+             'success': self._check(all, 'phase', Phase.SUCCESS, self.input)})
+        _sys.stdout.write(output)
