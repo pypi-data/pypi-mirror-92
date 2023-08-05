acd1a469f76dc72cfc19f6676396b0bbc35024bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:15 +0000

    Add package wagtail-seo/0.0.3/wagtail-seo-0.0.3.tar.gz

---
+++ packages/wagtail-seo/wagtail-seo-0.0.3.tar.gz/wagtail-seo-0.0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+from wagtailseo import __version__
+
+with open("README.md", encoding="utf8") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="wagtail-seo",
+    version=__version__,
+    author="CodeRed LLC",
+    author_email="info@coderedcorp.com",
+    url="https://github.com/coderedcorp/wagtail-seo",
+    description="Search engine and social media optimization for Wagtail.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="BSD license",
+    include_package_data=True,
+    packages=["wagtailseo"],
+    python_requires=">=3.6",
+    install_requires=["wagtail>=2.0"],
+    classifiers=[
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Wagtail",
+        "Framework :: Wagtail :: 2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+    ],
+)
