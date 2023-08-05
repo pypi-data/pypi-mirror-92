4ac2518f4180a99cdbc8bc6350167f72f4031bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:57 +0000

    Add package perception/0.5.0/perception-0.5.0-cp36-cp36m-win_amd64.whl

---
+++ packages/perception/perception-0.5.0-cp36-cp36m-win_amd64.whl/tests/test_benchmarking.py
@@ -0,0 +1,194 @@
+# pylint: disable=protected-access,invalid-name
+import tempfile
+import shutil
+import os
+
+from imgaug import augmenters as iaa
+from scipy import spatial
+import numpy as np
+import pytest
+
+from perception import benchmarking, hashers, testing
+
+files = testing.DEFAULT_TEST_IMAGES
+dataset = benchmarking.BenchmarkImageDataset.from_tuples(
+    [(fn, i % 2) for i, fn in enumerate(files)])
+
+
+def test_deduplicate():
+    tempdir = tempfile.TemporaryDirectory()
+    new_file = os.path.join(tempdir.name, 'dup_file.jpg')
+    shutil.copy(files[0], new_file)
+    duplicated_files = files + [new_file]
+    deduplicated, duplicates = benchmarking.BenchmarkImageDataset.from_tuples(
+        [(fn, i % 2) for i, fn in enumerate(duplicated_files)]).deduplicate(
+            hasher=hashers.AverageHash(), threshold=1e-2)
+    assert len(duplicates) == 1
+    assert len(deduplicated._df) == len(files)
+
+
+def test_bad_dataset():
+    bad_files = files + ['tests/images/nonexistent.jpg']
+    bad_dataset = benchmarking.BenchmarkImageDataset.from_tuples(
+        [(fn, i % 2) for i, fn in enumerate(bad_files)])
+    transforms = {
+        'blur0.05': iaa.GaussianBlur(0.05),
+        'noop': iaa.Resize(size=(256, 256))
+    }
+    with pytest.raises(Exception):
+        transformed = bad_dataset.transform(
+            transforms=transforms,
+            storage_dir='/tmp/transforms',
+            errors='raise')
+    with pytest.warns(UserWarning, match='occurred reading'):
+        transformed = bad_dataset.transform(
+            transforms=transforms,
+            storage_dir='/tmp/transforms',
+            errors='warn')
+    assert len(transformed._df) == len(files) * 2
+
+
+def test_benchmark_dataset():
+    assert len(dataset._df) == len(files)
+    assert len(dataset.filter(category=[0])._df) == len(files) / 2
+    with pytest.warns(UserWarning, match='Did not find'):
+        assert len(dataset.filter(category=[3])._df) == 0  # pylint: disable=len-as-condition
+
+    dataset.save('/tmp/dataset.zip')
+    dataset.save('/tmp/dataset_folder')
+    o1 = benchmarking.BenchmarkImageDataset.load('/tmp/dataset.zip')
+    o2 = benchmarking.BenchmarkImageDataset.load('/tmp/dataset_folder')
+    o3 = benchmarking.BenchmarkImageDataset.load('/tmp/dataset.zip')
+
+    for opened in [o1, o2, o3]:
+        assert (opened._df['filepath'].apply(
+            os.path.basename) == dataset._df['filepath'].apply(
+                os.path.basename)).all()
+
+
+def test_benchmark_transforms():
+    transformed = dataset.transform(
+        transforms={
+            'blur0.05': iaa.GaussianBlur(0.05),
+            'noop': iaa.Resize(size=(256, 256))
+        },
+        storage_dir='/tmp/transforms')
+
+    assert len(transformed._df) == len(files) * 2
+
+    hashes = transformed.compute_hashes(hashers={'pdna': hashers.PHash()})
+    tr = hashes.compute_threshold_recall().reset_index()
+
+    hashes._metrics = None
+    hashes._df.at[0, 'hash'] = None
+    with pytest.warns(UserWarning, match='invalid / empty hashes'):
+        hashes.compute_threshold_recall()
+
+    assert (tr[tr['transform_name'] == 'noop']['recall'] == 100.0).all()
+
+    # This is a charting function but we execute it just to make sure
+    # it runs without error.
+    hashes.show_histograms()
+
+
+def test_video_benchmark_dataset():
+    video_dataset = benchmarking.BenchmarkVideoDataset.from_tuples(
+        files=[('perception/testing/videos/v1.m4v',
+                'category1'), ('perception/testing/videos/v2.m4v',
+                               'category1'),
+               ('perception/testing/videos/v1.m4v',
+                'category2'), ('perception/testing/videos/v2.m4v',
+                               'category2')])
+    transforms = {
+        'noop':
+        benchmarking.video_transforms.get_simple_transform(
+            width=128, sar='1/1'),
+        'gif':
+        benchmarking.video_transforms.get_simple_transform(
+            codec='gif', output_ext='.gif'),
+        'clip1s':
+        benchmarking.video_transforms.get_simple_transform(clip_s=(1, None)),
+        'blackpad':
+        benchmarking.video_transforms.get_black_frame_padding_transform(
+            duration_s=1),
+        'slideshow':
+        benchmarking.video_transforms.get_slideshow_transform(
+            frame_input_rate=1, frame_output_rate=1),
+    }
+    transformed = video_dataset.transform(
+        storage_dir=tempfile.TemporaryDirectory().name, transforms=transforms)
+    assert len(transformed._df) == len(transforms) * len(video_dataset._df)
+    assert transformed._df['filepath'].isnull().sum() == 0
+
+    # We will compute hashes for each of the transformed
+    # videos and check the results for correctness.
+    phash_framewise_hasher = hashers.FramewiseHasher(
+        frame_hasher=hashers.PHash(),
+        interframe_threshold=-1,
+        frames_per_second=2)
+    hashes = transformed.compute_hashes(
+        hashers={'phashframewise': phash_framewise_hasher})
+
+    guid = hashes._df.guid.iloc[0]
+    df = hashes._df[hashes._df['guid'] == guid]
+    clip1s = df[(df.transform_name == 'clip1s')]
+    noop = df[(df.transform_name == 'noop')]
+    blackpad = df[(df.transform_name == 'blackpad')]
+    slideshow = df[(df.transform_name == 'slideshow')]
+
+    # We should have dropped two hashes from the beginning
+    # on the clipped video.
+    assert len(clip1s) == len(noop) - 2
+
+    # The first hash from the clipped video should be the
+    # same as the third hash from the original
+    assert clip1s.hash.iloc[0] == noop.hash.iloc[2]
+
+    # The black padding adds four hashes (two on either side).
+    assert len(blackpad) == len(noop) + 4
+
+    # A black frame should yield all zeros for PHash
+    assert phash_framewise_hasher.string_to_vector(
+        blackpad.iloc[0].hash).sum() == 0
+
+    # The slideshow hashes should be the same as the noop
+    # hashes for every other hash.
+    assert (noop.hash.values[::2] == slideshow.hash.values[::2]).all()
+
+    # Every second hash in the slideshow should be the same as the
+    # previous one.
+    for n in range(0, 10, 2):
+        assert slideshow.hash.values[n] == slideshow.hash.values[n + 1]
+
+
+def test_euclidean_extension():
+
+    # This function plainly inplements the process of computing
+    # the closest positive and negative examples and their indexes.
+    def compute_euclidean_metrics_py(X_noop, X_transformed, mask):
+        distance_matrix = spatial.distance.cdist(
+            XA=X_transformed, XB=X_noop, metric='euclidean')
+        pos = np.ma.masked_array(distance_matrix, np.logical_not(mask))
+        neg = np.ma.masked_array(distance_matrix, mask)
+        distances = np.concatenate([
+            neg.min(axis=1).data[np.newaxis],
+            pos.min(axis=1).data[np.newaxis]
+        ],
+                                   axis=0).T
+        indexes = np.concatenate(
+            [neg.argmin(axis=1)[np.newaxis],
+             pos.argmin(axis=1)[np.newaxis]]).T
+        return distances, indexes
+
+    X_noop = np.random.uniform(low=0, high=255, size=(5, 144)).astype('int32')
+    X_trans = np.random.uniform(
+        low=0, high=255, size=(10, 144)).astype('int32')
+    mask = np.array([True, False] * 5 * 5).reshape(10, 5)
+
+    distances, indexes = benchmarking.common.extensions.compute_euclidean_metrics(
+        X_noop, X_trans, mask)
+    distances_py, indexes_py = compute_euclidean_metrics_py(
+        X_noop, X_trans, mask)
+
+    assert (indexes_py == indexes).all()
+    np.testing.assert_allclose(distances, distances_py)
---
+++ packages/perception/perception-0.5.0-cp36-cp36m-win_amd64.whl/tests/test_tools.py
@@ -0,0 +1,210 @@
+# pylint: disable=invalid-name
+import tempfile
+import shutil
+import os
+
+import numpy as np
+import pytest
+
+from perception import hashers, tools, testing
+
+
+def test_deduplicate():
+    directory = tempfile.TemporaryDirectory()
+    original = testing.DEFAULT_TEST_IMAGES[0]
+    duplicate = os.path.join(directory.name, 'image1.jpg')
+    shutil.copy(original, duplicate)
+    pairs = tools.deduplicate(
+        files=[
+            testing.DEFAULT_TEST_IMAGES[0], testing.DEFAULT_TEST_IMAGES[1],
+            duplicate
+        ],
+        hashers=[(hashers.PHash(hash_size=16), 0.25)])
+    assert len(pairs) == 1
+    file1, file2 = pairs[0]
+    assert ((file1 == duplicate) and
+            (file2 == original)) or ((file1 == original) and
+                                     (file2 == duplicate))
+
+
+def test_deduplicate_u8():
+    # This test verifies that extensions.compute_euclidean_pairwise_duplicates
+    # works properly.
+    directory = tempfile.TemporaryDirectory()
+    original = testing.DEFAULT_TEST_IMAGES[0]
+    duplicate = os.path.join(directory.name, 'image1.jpg')
+    shutil.copy(original, duplicate)
+    pairs = tools.deduplicate(
+        files=[
+            testing.DEFAULT_TEST_IMAGES[0], testing.DEFAULT_TEST_IMAGES[1],
+            duplicate
+        ],
+        hashers=[(hashers.PHashU8(hash_size=16), 10)])
+    assert len(pairs) == 1
+    file1, file2 = pairs[0]
+    assert ((file1 == duplicate) and
+            (file2 == original)) or ((file1 == original) and
+                                     (file2 == duplicate))
+
+
+def test_deduplicate_hashes_multiple():
+    # This test verifies that deduplicate_hashes functions properly
+    # when there is more than one hash for a file.
+    directory = tempfile.TemporaryDirectory()
+    original = testing.DEFAULT_TEST_IMAGES[0]
+    duplicate = os.path.join(directory.name, 'image1.jpg')
+    hasher = hashers.PHashU8(hash_size=16)
+    shutil.copy(original, duplicate)
+    hashes = [
+        (0, hasher.compute(original)),
+        (1, hasher.compute(duplicate)),
+        (1, hasher.compute(duplicate)),
+        (1, hasher.compute(duplicate)),
+        (2, hasher.compute(testing.DEFAULT_TEST_IMAGES[1])),
+    ]
+    pairs = tools.deduplicate_hashes(
+        hashes=hashes,
+        threshold=10,
+        hash_format='base64',
+        hash_length=hasher.hash_length,
+        distance_metric='euclidean',
+        hash_dtype='uint8')
+    assert len(pairs) == 1
+    file1, file2 = pairs[0]
+    assert ((file1 == 0) and (file2 == 1)) or ((file1 == 1) and (file2 == 0))
+
+
+def test_compute_euclidean_pairwise_duplicates():
+    # The purpose of this test is to verify that the handling of
+    # deduplication with files that have multiple hashes works
+    # properly. This is particularly important for video where
+    # we are likely to have many hashes.
+    X = np.array([
+        # File 1
+        [0, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2],
+        # File 2
+        [1, 1, 1],
+        [2, 2, 2],
+        [3, 3, 3],
+        # File 3
+        [3, 3, 3],
+        [4, 4, 4],
+        # File 4
+        [5, 5, 5],
+        [6, 6, 6]
+    ])
+
+    # Use grouped files.
+    counts = np.array([3, 3, 2, 2])
+    expected = np.array([[2 / 3, 2 / 3], [0, 0], [0, 0], [1 / 3, 1 / 2],
+                         [0, 0], [0, 0]])
+    actual = tools.extensions.compute_euclidean_pairwise_duplicates(
+        X=X.astype('int32'),
+        threshold=1,
+        counts=counts.astype('uint32'),
+        compute_overlap=True)
+    assert (expected == actual).all()
+
+    # Use without computing overlap.
+    expected = np.array([[2, 2], [0, 0], [0, 0], [1, 1], [0, 0], [0, 0]])
+    actual = tools.extensions.compute_euclidean_pairwise_duplicates(
+        X=X.astype('int32'),
+        threshold=1,
+        counts=counts.astype('uint32'),
+        compute_overlap=False)
+    assert (expected == actual).all()
+
+    # Use ungrouped files.
+    X = np.array([
+        # File 1
+        [0, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2],
+        [1, 1, 1],
+    ])
+    expected = np.array([[0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [0, 0]])
+    actual = tools.extensions.compute_euclidean_pairwise_duplicates(
+        X=X.astype('int32'), threshold=1, compute_overlap=True)
+    assert (expected == actual).all()
+
+
+def test_api_is_over_https():
+    matcher_https = tools.SaferMatcher(
+        api_key='foo', url='https://www.example.com/')
+    assert matcher_https
+
+    if 'SAFER_MATCHING_SERVICE_DEV_ALLOW_HTTP' in os.environ:
+        del os.environ['SAFER_MATCHING_SERVICE_DEV_ALLOW_HTTP']
+    with pytest.raises(ValueError):
+        tools.SaferMatcher(api_key='foo', url='http://www.example.com/')
+
+    os.environ['SAFER_MATCHING_SERVICE_DEV_ALLOW_HTTP'] = '1'
+    matcher_http_with_escape_hatch = tools.SaferMatcher(
+        api_key='foo', url='http://www.example.com/')
+    assert matcher_http_with_escape_hatch
+
+
+def test_unletterbox():
+    image = hashers.tools.read(testing.DEFAULT_TEST_IMAGES[0])
+    padded = np.zeros((image.shape[0] + 100, image.shape[1] + 50, 3),
+                      dtype='uint8')
+    padded[50:50 + image.shape[0], 25:25 + image.shape[1]] = image
+    (x1, x2), (y1, y2) = hashers.tools.unletterbox(padded)
+    assert y1 == 50
+    assert y2 == 50 + image.shape[0]
+    assert x1 == 25
+    assert x2 == 25 + image.shape[1]
+
+
+def test_unletterbox_aspect_ratio():
+    """Test the value of .1 in unletterbox()."""
+    image = hashers.tools.read(testing.DEFAULT_TEST_IMAGES[0])
+    h, w, z = image.shape
+
+    # make tall skinny images with non-trivial content just below and
+    # above 10% threshold
+    base = int(4.5 * h)  # 2 * base + h = 100%
+    h_fail, h_pass = base + 10, base - 10
+
+    padded = np.r_[np.zeros((h_fail, w, 3)), image, np.zeros((h_fail, w, 3))]
+    assert None is hashers.tools.unletterbox(padded)
+
+    padded = np.r_[np.zeros((h_pass, w, 3)), image, np.zeros((h_pass, w, 3))]
+    (x1, x2), (y1, y2) = hashers.tools.unletterbox(padded)
+
+    assert y1 == h_pass
+    assert y2 == h_pass + image.shape[0]
+    assert x1 == 0
+    assert x2 == image.shape[1]
+
+
+def test_unletterbox_noblackbars():
+    image = hashers.tools.read(testing.DEFAULT_TEST_IMAGES[0])
+    (x1, x2), (y1, y2) = hashers.tools.unletterbox(image)
+    assert x1 == 0
+    assert y1 == 0
+    assert x2 == image.shape[1]
+    assert y2 == image.shape[0]
+
+
+def test_ffmpeg_video():
+    """Check that the FFMPEG video parsing code provides substantially similar
+    results to the OpenCV approach (which also uses FFMPEG under the hood but
+    also has different frame selection logic)."""
+    frames_per_second = 2.3
+    for filepath in testing.DEFAULT_TEST_VIDEOS:
+        filename = os.path.basename(filepath)
+        for (frame1, index1, timestamp1), (frame2, index2, timestamp2) in zip(
+                hashers.tools.read_video_to_generator_ffmpeg(
+                    filepath, frames_per_second=frames_per_second),
+                hashers.tools.read_video_to_generator(
+                    filepath, frames_per_second=frames_per_second)):
+            diff = np.abs(frame1.astype("int32") -
+                          frame2.astype("int32")).flatten()
+            assert index1 == index2, f"Index mismatch for {filename}"
+            np.testing.assert_allclose(
+                timestamp1, timestamp2), f"Timestamp mismatch for {filename}"
+            assert np.percentile(diff,
+                                 75) < 25, f"Frame mismatch for {filename}"
