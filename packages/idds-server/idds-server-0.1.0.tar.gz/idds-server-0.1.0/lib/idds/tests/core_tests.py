406a223308bac9d060e869b7ddee3f1a5271c4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:24 +0000

    Add package idds-server/0.1.0/idds-server-0.1.0.tar.gz

---
+++ packages/idds-server/idds-server-0.1.0.tar.gz/idds-server-0.1.0/lib/idds/tests/core_tests.py
@@ -0,0 +1,34 @@
+from idds.common.utils import json_dumps
+from idds.core.requests import get_requests
+from idds.core.messages import retrieve_messages
+from idds.core.transforms import get_transforms
+from idds.core.workprogress import get_workprogresses
+
+
+reqs = get_requests()
+# print(len(reqs))
+for req in reqs:
+    # print(req)
+    # print(req['request_metadata']['workflow'].to_dict())
+    print(json_dumps(req, sort_keys=True, indent=4))
+    pass
+
+tfs = get_transforms()
+for tf in tfs:
+    if tf['transform_id'] == 104:
+        # print(tf)
+        # print(tf['transform_metadata']['work'].to_dict())
+        # print(json_dumps(tf, sort_keys=True, indent=4))
+        pass
+
+msgs = retrieve_messages()
+for msg in msgs:
+    # if msg['msg_id'] in [1161]:
+    #     print(msg)
+    pass
+
+wps = get_workprogresses()
+for wp in wps:
+    if wp['workprogress_id'] == 79:
+        # print(json_dumps(wp, sort_keys=True, indent=4))
+        pass
