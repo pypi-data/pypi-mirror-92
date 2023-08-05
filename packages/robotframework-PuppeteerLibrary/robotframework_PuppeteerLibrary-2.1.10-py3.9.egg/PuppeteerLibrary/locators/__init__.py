5e327e5fc4b2c5db59f2031aaacfcae50619b0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:57 +0000

    Add package robotframework-PuppeteerLibrary/2.1.10/robotframework_PuppeteerLibrary-2.1.10-py3.9.egg

---
+++ packages/robotframework-PuppeteerLibrary/robotframework_PuppeteerLibrary-2.1.10-py3.9.egg/PuppeteerLibrary/locators/__init__.py
@@ -0,0 +1,10 @@
+# Import parser
+from .CssLocatorParser import CssLocatorParser
+from .IdLocatorParser import IdLocatorParser
+from .XPathLocatorParser import XPathLocatorParser
+from .LinkLocatorParser import LinkLocatorParser
+from .PartialLinkLocatorParser import PartialLinkLocatorParser
+
+# Import other
+from .LocatorParserImplementation import LocatorParserImplementation
+from .SelectorAbstraction import SelectorAbstraction
