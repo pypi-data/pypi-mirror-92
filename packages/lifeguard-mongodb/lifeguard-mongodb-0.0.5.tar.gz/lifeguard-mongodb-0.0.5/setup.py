c3069160416307e3eed089fd1738038088fe76cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:45 +0000

    Add package lifeguard-mongodb/0.0.5/lifeguard-mongodb-0.0.5.tar.gz

---
+++ packages/lifeguard-mongodb/lifeguard-mongodb-0.0.5.tar.gz/lifeguard-mongodb-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lifeguard-mongodb",
+    version="0.0.5",
+    url="https://github.com/LifeguardSystem/lifeguard-mongodb",
+    author="Diego Rubin",
+    author_email="contact@diegorubin.dev",
+    license="GPL2",
+    scripts=[],
+    include_package_data=True,
+    description="Lifeguard integration with MongoDB",
+    install_requires=["lifeguard", "pymongo"],
+    classifiers=["Development Status :: 3 - Alpha"],
+    packages=find_packages(),
+)
