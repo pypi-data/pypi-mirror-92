4fb661b13552527a0417aadad814c8ba1cd69b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:07 +0000

    Add package mpl-tools/0.2.21/mpl-tools-0.2.21.tar.gz

---
+++ packages/mpl-tools/mpl-tools-0.2.21.tar.gz/mpl-tools-0.2.21/mpl_tools/__init__.py
@@ -0,0 +1,17 @@
+"""Documentation for mpl-tools.
+
+## Dev guide
+Also see `dev_guide`
+
+## API reference
+The rendered docstrings can be browsed
+through the following links, which are also available in the left sidebar.
+"""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
+
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+__version__ = importlib_metadata.version(__name__)
