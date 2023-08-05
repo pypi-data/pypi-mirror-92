054a6bd25c94369e326eb5988b46ce5327e0630e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package dkist-processing-core/0.0.4rc2/dkist-processing-core-0.0.4rc2.tar.gz

---
+++ packages/dkist-processing-core/dkist-processing-core-0.0.4rc2.tar.gz/dkist-processing-core-0.0.4rc2/dkist_processing_core/tests/task_example.py
@@ -0,0 +1,19 @@
+"""
+Example task subclass used in the tests
+"""
+from dkist_processing_core import TaskBase
+
+
+class Task(TaskBase):
+    log_url = "http://localhost:8080/log?execution_date=2021-01-07T18%3A19%3A38.214767%2B00%3A00&task_id=task_a&dag_id=test_dag"
+
+    def __init__(self, *args, **kwargs):
+        self.run_was_called = False
+        self.record_provenance_was_called = False
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        self.run_was_called = True
+
+    def record_provenance(self):
+        self.record_provenance_was_called = True
---
+++ packages/dkist-processing-core/dkist-processing-core-0.0.4rc2.tar.gz/dkist-processing-core-0.0.4rc2/dkist_processing_core/tests/test_build_utils.py
@@ -0,0 +1,15 @@
+"""
+Tests for the build utils
+"""
+import pytest
+
+from dkist_processing_core.build_utils import export
+from dkist_processing_core.build_utils import validate_workflows
+
+
+def test_validate_workflow():
+    pass  # TODO
+
+
+def test_export():
+    pass  # TODO
