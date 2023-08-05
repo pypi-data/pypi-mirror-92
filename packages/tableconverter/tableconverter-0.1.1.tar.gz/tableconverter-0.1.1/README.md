61d4491961481c91a8e55718700ae761c646dbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:27 +0000

    Add package tableconverter/0.1.1/tableconverter-0.1.1.tar.gz

---
+++ packages/tableconverter/tableconverter-0.1.1.tar.gz/tableconverter-0.1.1/README.md
@@ -0,0 +1,27 @@
+# PyTableConverter
+Tools for converting tables in different formats.
+
+[![](https://img.shields.io/pypi/v/tableconverter.svg)](https://pypi.python.org/pypi/tableconverter)
+
+## Installation
+
+```bash
+pip install --upgrade tableconverter
+```
+
+## Quick start
+
+```bash
+tableconverter demo.csv -d html
+python -m tableconverter demo.csv -d html
+# or you may use Heredoc
+```
+
+## Supported formats
+* CSV
+* Markdown
+* HTML
+* JSON
+
+## Lisence
+PyTableConverter is released under the terms of the MIT license.
---
+++ packages/tableconverter/tableconverter-0.1.1.tar.gz/tableconverter-0.1.1/tableconverter/autogen/JSONTableLexer.py
@@ -0,0 +1,109 @@
+# Generated from JSONTable.g4 by ANTLR 4.8
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\17")
+        buf.write("\u008d\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\3\2\3\2\3\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7")
+        buf.write("\3\b\3\b\3\b\3\b\3\b\3\t\3\t\3\t\3\t\3\t\3\t\3\n\3\n\3")
+        buf.write("\n\3\n\3\n\3\13\3\13\3\13\7\13G\n\13\f\13\16\13J\13\13")
+        buf.write("\3\13\3\13\3\f\3\f\3\f\5\fQ\n\f\3\r\3\r\3\r\3\r\3\r\3")
+        buf.write("\r\3\16\3\16\3\17\3\17\3\20\5\20^\n\20\3\20\3\20\3\20")
+        buf.write("\6\20c\n\20\r\20\16\20d\3\20\5\20h\n\20\3\20\3\20\3\20")
+        buf.write("\6\20m\n\20\r\20\16\20n\5\20q\n\20\3\20\3\20\5\20u\n\20")
+        buf.write("\3\21\3\21\3\21\7\21z\n\21\f\21\16\21}\13\21\5\21\177")
+        buf.write("\n\21\3\22\3\22\5\22\u0083\n\22\3\22\3\22\3\23\6\23\u0088")
+        buf.write("\n\23\r\23\16\23\u0089\3\23\3\23\2\2\24\3\3\5\4\7\5\t")
+        buf.write("\6\13\7\r\b\17\t\21\n\23\13\25\f\27\2\31\2\33\2\35\2\37")
+        buf.write("\r!\16#\2%\17\3\2\n\n\2$$\61\61^^ddhhppttvv\5\2\62;CH")
+        buf.write("ch\5\2\2!$$^^\3\2\62;\3\2\63;\4\2GGgg\4\2--//\5\2\13\f")
+        buf.write("\17\17\"\"\2\u0094\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2")
+        buf.write("\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21")
+        buf.write("\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\37\3\2\2\2\2!\3\2")
+        buf.write("\2\2\2%\3\2\2\2\3\'\3\2\2\2\5)\3\2\2\2\7+\3\2\2\2\t-\3")
+        buf.write("\2\2\2\13/\3\2\2\2\r\61\3\2\2\2\17\63\3\2\2\2\218\3\2")
+        buf.write("\2\2\23>\3\2\2\2\25C\3\2\2\2\27M\3\2\2\2\31R\3\2\2\2\33")
+        buf.write("X\3\2\2\2\35Z\3\2\2\2\37t\3\2\2\2!~\3\2\2\2#\u0080\3\2")
+        buf.write("\2\2%\u0087\3\2\2\2\'(\7]\2\2(\4\3\2\2\2)*\7.\2\2*\6\3")
+        buf.write("\2\2\2+,\7_\2\2,\b\3\2\2\2-.\7}\2\2.\n\3\2\2\2/\60\7\177")
+        buf.write("\2\2\60\f\3\2\2\2\61\62\7<\2\2\62\16\3\2\2\2\63\64\7v")
+        buf.write("\2\2\64\65\7t\2\2\65\66\7w\2\2\66\67\7g\2\2\67\20\3\2")
+        buf.write("\2\289\7h\2\29:\7c\2\2:;\7n\2\2;<\7u\2\2<=\7g\2\2=\22")
+        buf.write("\3\2\2\2>?\7p\2\2?@\7w\2\2@A\7n\2\2AB\7n\2\2B\24\3\2\2")
+        buf.write("\2CH\7$\2\2DG\5\27\f\2EG\5\35\17\2FD\3\2\2\2FE\3\2\2\2")
+        buf.write("GJ\3\2\2\2HF\3\2\2\2HI\3\2\2\2IK\3\2\2\2JH\3\2\2\2KL\7")
+        buf.write("$\2\2L\26\3\2\2\2MP\7^\2\2NQ\t\2\2\2OQ\5\31\r\2PN\3\2")
+        buf.write("\2\2PO\3\2\2\2Q\30\3\2\2\2RS\7w\2\2ST\5\33\16\2TU\5\33")
+        buf.write("\16\2UV\5\33\16\2VW\5\33\16\2W\32\3\2\2\2XY\t\3\2\2Y\34")
+        buf.write("\3\2\2\2Z[\n\4\2\2[\36\3\2\2\2\\^\7/\2\2]\\\3\2\2\2]^")
+        buf.write("\3\2\2\2^_\3\2\2\2_`\5!\21\2`b\7\60\2\2ac\t\5\2\2ba\3")
+        buf.write("\2\2\2cd\3\2\2\2db\3\2\2\2de\3\2\2\2eu\3\2\2\2fh\7/\2")
+        buf.write("\2gf\3\2\2\2gh\3\2\2\2hi\3\2\2\2ip\5!\21\2jl\7\60\2\2")
+        buf.write("km\t\5\2\2lk\3\2\2\2mn\3\2\2\2nl\3\2\2\2no\3\2\2\2oq\3")
+        buf.write("\2\2\2pj\3\2\2\2pq\3\2\2\2qr\3\2\2\2rs\5#\22\2su\3\2\2")
+        buf.write("\2t]\3\2\2\2tg\3\2\2\2u \3\2\2\2v\177\7\62\2\2w{\t\6\2")
+        buf.write("\2xz\t\5\2\2yx\3\2\2\2z}\3\2\2\2{y\3\2\2\2{|\3\2\2\2|")
+        buf.write("\177\3\2\2\2}{\3\2\2\2~v\3\2\2\2~w\3\2\2\2\177\"\3\2\2")
+        buf.write("\2\u0080\u0082\t\7\2\2\u0081\u0083\t\b\2\2\u0082\u0081")
+        buf.write("\3\2\2\2\u0082\u0083\3\2\2\2\u0083\u0084\3\2\2\2\u0084")
+        buf.write("\u0085\5!\21\2\u0085$\3\2\2\2\u0086\u0088\t\t\2\2\u0087")
+        buf.write("\u0086\3\2\2\2\u0088\u0089\3\2\2\2\u0089\u0087\3\2\2\2")
+        buf.write("\u0089\u008a\3\2\2\2\u008a\u008b\3\2\2\2\u008b\u008c\b")
+        buf.write("\23\2\2\u008c&\3\2\2\2\20\2FHP]dgnpt{~\u0082\u0089\3\b")
+        buf.write("\2\2")
+        return buf.getvalue()
+
+
+class JSONTableLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    T__3 = 4
+    T__4 = 5
+    T__5 = 6
+    TRUE = 7
+    FALSE = 8
+    NULL = 9
+    STRING = 10
+    FLOAT = 11
+    INT = 12
+    WS = 13
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'['", "','", "']'", "'{'", "'}'", "':'", "'true'", "'false'", 
+            "'null'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "TRUE", "FALSE", "NULL", "STRING", "FLOAT", "INT", "WS" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "TRUE", 
+                  "FALSE", "NULL", "STRING", "ESC", "UNICODE", "HEX", "SAFECODEPOINT", 
+                  "FLOAT", "INT", "EXP", "WS" ]
+
+    grammarFileName = "JSONTable.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.8")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
