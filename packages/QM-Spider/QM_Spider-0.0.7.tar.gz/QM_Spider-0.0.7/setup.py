96d01a84e92d1b03b86180b21124a6868a7b61e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:39 +0000

    Add package QM-Spider/0.0.7/QM_Spider-0.0.7.tar.gz

---
+++ packages/QM-Spider/QM_Spider-0.0.7.tar.gz/QM_Spider-0.0.7/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="QM_Spider",
+    version="0.0.7",
+    author="@道长",
+    author_email="ctrlf4@yeah.net",
+    description="QiMai Crawler library",
+    long_description="QiMai Crawler library",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShellMonster/QM_Spider",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
+
+install_requires=[
+    'requests',
+    'datetime'
+    'pandas',
+    'numpy',
+    'scipy'
+]
+
