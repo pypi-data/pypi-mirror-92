5978d7fa3abb0ecffb99d3cc37b53afaa66bab98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:36 +0000

    Add package timemachines/0.1.11/timemachines-0.1.11-py3-none-any.whl

---
+++ packages/timemachines/timemachines-0.1.11-py3-none-any.whl/timemachines/skaters/allskaters.py
@@ -0,0 +1,5 @@
+from timemachines.skaters.pmd import pmd_auto
+from timemachines.skaters.dlmseasonal import dlm_seasonal
+from timemachines.skaters.dlmauto import dlm_auto
+
+SKATERS = [ pmd_auto, dlm_auto, dlm_seasonal ]
