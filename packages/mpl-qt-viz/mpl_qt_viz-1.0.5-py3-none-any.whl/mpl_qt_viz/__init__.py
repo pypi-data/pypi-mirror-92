280ddfda303109ae9909041f19444404a6812286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:02 +0000

    Add package mpl-qt-viz/1.0.5/mpl_qt_viz-1.0.5-py3-none-any.whl

---
+++ packages/mpl-qt-viz/mpl_qt_viz-1.0.5-py3-none-any.whl/mpl_qt_viz/__init__.py
@@ -0,0 +1,17 @@
+"""
+A Python package providing enhanced data visualization and ROI selection built on top of Matplotlib and PyQt5.
+
+Subpackages
+------------
+.. autosummary::
+   :toctree: generated/
+
+   roiSelection
+   visualizers
+
+"""
+from . import version
+
+__author__ = 'Nick Anthony'
+
+__version__ = version.version
