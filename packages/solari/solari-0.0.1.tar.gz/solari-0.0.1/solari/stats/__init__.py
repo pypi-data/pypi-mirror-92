bb6fceb6316d065029553bba755819e1622fbf1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:29 +0000

    Add package solari/0.0.1/solari-0.0.1.tar.gz

---
+++ packages/solari/solari-0.0.1.tar.gz/solari-0.0.1/solari/stats/__init__.py
@@ -0,0 +1,25 @@
+from .champions_rate_stats import (
+    ChampionPickrate,
+    ChampionPickCount,
+    ChampionWinrate,
+    ChampionBanrate,
+    ChampionBanCount,
+    ChampionPresenceRate
+)
+
+from .champions_stats import (
+    ChampionGeneric,
+    ChampionGenericPerMin,
+    ChampionKDA,
+    ChampionKillParticipation
+)
+
+from .items_rate_stats import (
+    ItemPickrate,
+    ItemWinrate
+)
+
+from .player_rates import (
+    PlayerPickrate,
+    PlayerWinrate
+)
