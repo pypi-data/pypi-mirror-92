595d37f67385c6e17d5dd1bf9984de3dd3ea2e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package pdpp/0.4.2/pdpp-0.4.2-py3-none-any.whl

---
+++ packages/pdpp/pdpp-0.4.2-py3-none-any.whl/pdpp/languages/runners.py
@@ -0,0 +1,18 @@
+from os import name
+from subprocess import run
+from posixpath import join
+from pdpp.tasks.base_task import BaseTask
+
+def python_runner(script_name: str, task: BaseTask):
+    if name == 'posix':
+        python_caller = 'python3'
+    else:
+        python_caller = 'python'
+    run([python_caller, script_name], check=True, cwd=join(task.target_dir, task.SRC_DIR))
+
+def r_runner(script_name, target_dir, src_dir):
+    run(['Rscript', script_name], check=True, cwd=join(target_dir, src_dir))
+
+# TODO: Fix the project runner and bring it in line with the other runners
+def project_runner(task: BaseTask):
+    run(["doit"], check=True, cwd=task.target_dir)
---
+++ packages/pdpp/pdpp-0.4.2-py3-none-any.whl/pdpp/templates/populate_new_project.py
@@ -0,0 +1,17 @@
+from pdpp.templates.create_gitignore import create_gitignore
+from pdpp.templates.dodo_template import create_dodo_template
+from pdpp.tasks.export_task import ExportTask
+from pdpp.tasks.import_task import ImportTask
+
+
+def populate_new_project(_ = None):
+    """
+    This is run whenever `pdpp new` and `pdpp sub` is invoked. 
+    It checks to see whether the necessary support structure is in place.
+    This is also run in a new pdpp subproject. 
+    """
+
+    ExportTask().initialize_task()
+    ImportTask().initialize_task()
+    create_dodo_template()
+    create_gitignore()
