74da3775e3ed1b35591615dff0a2818eed43794f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:41 +0000

    Add package docker-devbox-ddb/1.6.0/docker_devbox_ddb-1.6.0-py3-none-any.whl

---
+++ packages/docker-devbox-ddb/docker_devbox_ddb-1.6.0-py3-none-any.whl/ddb/feature/cookiecutter/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from typing import ClassVar, Iterable
+
+from ddb.action import Action
+from ddb.feature import Feature
+from .actions import CookiecutterAction
+from .schema import CookiecutterFeatureSchema
+
+
+class CookiecutterFeature(Feature):
+    """
+    Generate code from a cookiecutter template available on github (https://cookiecutter.readthedocs.io).
+    """
+
+    @property
+    def name(self) -> str:
+        return "cookiecutter"
+
+    @property
+    def dependencies(self) -> Iterable[str]:
+        return ["core"]
+
+    @property
+    def schema(self) -> ClassVar[CookiecutterFeatureSchema]:
+        return CookiecutterFeatureSchema
+
+    @property
+    def actions(self) -> Iterable[Action]:
+        return (
+            CookiecutterAction(),
+        )
