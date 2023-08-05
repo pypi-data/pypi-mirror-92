e0e97b9069ae18ee8f643a9c928e275c596e9470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:43 +0000

    Add package statick-md/0.0.6/statick-md-0.0.6.tar.gz

---
+++ packages/statick-md/statick-md-0.0.6.tar.gz/statick-md-0.0.6/src/statick_md/plugins/md_discovery_plugins/markdown_discovery_plugin.py
@@ -0,0 +1,38 @@
+"""Discover Markdown files to analyze."""
+
+import logging
+from collections import OrderedDict
+
+from statick_tool.discovery_plugin import DiscoveryPlugin
+
+
+class MarkdownDiscoveryPlugin(DiscoveryPlugin):
+    """Discover Markdown files to analyze."""
+
+    def get_name(self):
+        """Get name of discovery type."""
+        return "markdown"
+
+    def scan(self, package, level, exceptions=None):
+        """Scan package looking for Markdown files."""
+        src_files = []
+
+        self.find_files(package)
+
+        for file_dict in package.files.values():
+            if file_dict["name"].endswith(".md"):
+                src_files.append(file_dict["path"])
+
+        src_files = list(OrderedDict.fromkeys(src_files))
+
+        logging.info("  %d markdown files found.", len(src_files))
+        if exceptions:
+            original_file_count = len(src_files)
+            src_files = exceptions.filter_file_exceptions_early(package, src_files)
+            if original_file_count > len(src_files):
+                logging.info(
+                    "  After filtering, %d markdown files will be scanned.",
+                    len(src_files),
+                )
+
+        package["md_src"] = src_files
