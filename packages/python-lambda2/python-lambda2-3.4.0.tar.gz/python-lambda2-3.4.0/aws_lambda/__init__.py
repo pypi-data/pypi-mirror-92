052ae7f3d7967b82a73a5e23558c9bb01f340cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:57 +0000

    Add package python-lambda2/3.4.0/python-lambda2-3.4.0.tar.gz

---
+++ packages/python-lambda2/python-lambda2-3.4.0.tar.gz/python-lambda2-3.4.0/aws_lambda/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+__author__ = 'Wassim Salib '
+__email__ = 'wassim87@gmail.com'
+__version__ = '3.4.0'
+
+from .aws_lambda import deploy, deploy_s3, invoke, init, build, upload, cleanup_old_versions
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
