2aa9fc520846d8689604caebef17eec43cb5d023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:33 +0000

    Add package MeUtils/2021.1.19.20.52.39/MeUtils-2021.1.19.20.52.39.tar.gz

---
+++ packages/MeUtils/MeUtils-2021.1.19.20.52.39.tar.gz/MeUtils-2021.1.19.20.52.39/meutils/email_utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : email_utils
+# @Time         : 2020/12/3 9:17 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
