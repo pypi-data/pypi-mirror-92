9da3669a8a61817e92775fb92e8202e5ed5f6264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:12 +0000

    Add package gambaterm/0.1.0/gambaterm-0.1.0.tar.gz

---
+++ packages/gambaterm/gambaterm-0.1.0.tar.gz/gambaterm-0.1.0/setup.py
@@ -0,0 +1,61 @@
+import os
+import glob
+import numpy
+from setuptools import Extension, setup
+
+# List libgambatte sources, excluding `file_zip.cpp`
+libgambatte_sources = [
+    path
+    for path in glob.glob("libgambatte/**/*.cpp", recursive=True)
+    if not path.endswith("file_zip.cpp")
+]
+
+# List all directories containing `.h` files
+libgambatte_include_dirs = list(
+    set(
+        os.path.dirname(path)
+        for path in glob.glob("libgambatte/**/*.h", recursive=True)
+    )
+)
+
+# The gambatte extension, including libgambatte with the Cython wrapper
+gambatte_extension = Extension(
+    "gambaterm._gambatte",
+    language="c++",
+    include_dirs=libgambatte_include_dirs + [numpy.get_include()],
+    extra_compile_args=["-DHAVE_STDINT_H"],
+    sources=libgambatte_sources + ["ext/_gambatte.pyx"],
+)
+
+
+setup(
+    name="gambaterm",
+    version="0.1.0",
+    packages=["gambaterm"],
+    setup_requires=["setuptools", "cython", "numpy"],
+    ext_modules=[gambatte_extension],
+    install_requires=[
+        "numpy",
+        "asyncssh",
+        "sounddevice",
+        "samplerate",
+        "prompt_toolkit",
+        "xlib; sys_platform == 'linux'",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "gambaterm = gambaterm:main",
+            "gambaterm-ssh = gambaterm.ssh:main",
+        ],
+    },
+    description="A terminal frontend for gambatte game boy color emulator ",
+    url="https://github.com/vxgmichel/gambatte-terminal",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    author="Vincent Michel",
+    author_email="vxgmichel@gmail.com",
+)
