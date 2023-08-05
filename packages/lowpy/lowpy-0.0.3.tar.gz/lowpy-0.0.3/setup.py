1d9591d9e5ec4243c0e60d4e33b9609419671729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:35 +0000

    Add package lowpy/0.0.3/lowpy-0.0.3.tar.gz

---
+++ packages/lowpy/lowpy-0.0.3.tar.gz/lowpy-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lowpy", # Replace with your own username
+    version="0.0.3",
+    author="Andrew Ford",
+    author_email="author@example.com",
+    description="High level GPU simulations of low level device characteristics in ML algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fordaj/lowpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
