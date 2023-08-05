0697bff6ce9b4f1b1660f8567ba2837f3536b201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:58 +0000

    Add package mibexx-gym-minesweeper/0.0.3/mibexx_gym_minesweeper-0.0.3.tar.gz

---
+++ packages/mibexx-gym-minesweeper/mibexx_gym_minesweeper-0.0.3.tar.gz/mibexx_gym_minesweeper-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mibexx_gym_minesweeper',
+    version='0.0.3',
+    license='MIT',
+    install_requires=['gym', 'numpy'],
+    author='Mike Bertram',
+    author_email='contact@mibexx.de',
+    description='Gym Environment for Minesweeper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords = ['GYM', 'ENVIRONMENT', 'MINESWEEPER', 'REINFORCEMENT LEARNING']
+)
