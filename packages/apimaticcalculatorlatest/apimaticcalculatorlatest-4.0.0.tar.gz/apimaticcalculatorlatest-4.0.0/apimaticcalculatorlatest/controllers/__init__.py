87a9841632448f768389fee3fec4b1c5223fe656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package apimaticcalculatorlatest/4.0.0/apimaticcalculatorlatest-4.0.0.tar.gz

---
+++ packages/apimaticcalculatorlatest/apimaticcalculatorlatest-4.0.0.tar.gz/apimaticcalculatorlatest-4.0.0/apimaticcalculatorlatest/controllers/__init__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    'base_controller',
+    'product_controller',
+    'los_pricing_controller',
+    'messaging_controller',
+    'property_managers_controller',
+    'request_to_book_controller',
+    'images_controller',
+    'fee_and_tax_mandatory_at_the_property_level_controller',
+    'yields_controller',
+    'fee_and_tax_controller',
+    'validation_controller',
+    'push_notification_controller',
+    'rates_and_availability_controller',
+    'reservation_notifications_controller',
+    'testing_of_message_api_calls_controller',
+    'authorization_controller',
+]
