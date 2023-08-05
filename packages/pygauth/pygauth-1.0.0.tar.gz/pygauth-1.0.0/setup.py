97f6c9953bef49e669aa30b9be2f3e613c7a879f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:05 +0000

    Add package pygauth/1.0.0/pygauth-1.0.0.tar.gz

---
+++ packages/pygauth/pygauth-1.0.0.tar.gz/pygauth-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.readlines()
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pygauth',
+    version='1.0.0',
+    install_requires=install_requires,
+    author='Squili',
+    description='A Python helper library for Google Authentication',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://git.squi.live/squili/pygauth',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5'
+)
