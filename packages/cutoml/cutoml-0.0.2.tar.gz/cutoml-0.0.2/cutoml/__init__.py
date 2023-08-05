211348c526c9372ae133a820ac0df0f079058f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package cutoml/0.0.2/cutoml-0.0.2.tar.gz

---
+++ packages/cutoml/cutoml-0.0.2.tar.gz/cutoml-0.0.2/cutoml/__init__.py
@@ -0,0 +1,5 @@
+from cutoml.cutoml import CutoClassifier
+from cutoml.cutoml import CutoRegressor
+from cutoml.config import classifiers
+from cutoml.config import regressors
+from cutoml.utils import timer
