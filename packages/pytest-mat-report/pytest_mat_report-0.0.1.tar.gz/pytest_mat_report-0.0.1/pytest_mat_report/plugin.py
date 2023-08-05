4c01ad300356e20f376a52015b5c0b4569027836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package pytest-mat-report/0.0.1/pytest_mat_report-0.0.1.tar.gz

---
+++ packages/pytest-mat-report/pytest_mat_report-0.0.1.tar.gz/pytest_mat_report-0.0.1/pytest_mat_report/plugin.py
@@ -0,0 +1,50 @@
+import pytest
+import requests
+import jsonpath
+import json
+import os
+import sys
+from _pytest.reports import TestReport
+
+from py.io import TerminalWriter
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--mat_report",
+        action="store",
+        default="on",
+        help="'Default 'off' for change, option: on or off"
+    )
+
+def pytest_runtest_makereport(self, item, call):
+    out = yield
+    w = TerminalWriter()
+    print("asdasd")
+    print(item)
+    print(call)
+    print(out.get_result())
+
+    report = out.get_result()
+    if report.when == "call" and report.outcome != 'passed':
+        nodeid = item.nodeid
+        num = int(item.name.replace(item.originalname,'').replace('params','').strip('[').strip(']'))
+        used_fixtures = sorted(getattr(item, "fixturenames", []))
+        f = {}
+        if used_fixtures:
+            for i in used_fixtures:
+                f[i] = item.funcargs[i]
+        print('\n')
+        length = w.fullwidth
+        name = item.nodeid.center(length,"_")
+        color = 'red'
+        w.write(name, **{color: True})
+        print(('\n\n运行结果: %s' % report.outcome))
+        print('\n ---  \033[32mfixtrue数据为\033[0m：' + str(f))
+        case = item.module.report[item.originalname][num]
+        print('\n ---  \033[33m接口请求数据为\033[0m：' + str(case['request']))
+        print('\n ---  \033[36m接口返回数据为\033[0m：' + str(case['respone']))
+        if 'assert' in report.longreprtext:
+            index = report.longreprtext.index('assert')
+            print('\n ---  \033[31m断言结果为\033[0m：\n     ' + str(report.longreprtext[index:]))
+            print('\n')
+
---
+++ packages/pytest-mat-report/pytest_mat_report-0.0.1.tar.gz/pytest_mat_report-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytest_mat_report']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{u'pytest11': ['mat_report = pytest_mat_report.plugin']}
+
+setup_kwargs = {
+    'name': 'pytest-mat-report',
+    'version': '0.0.1',
+    'description': 'this is report',
+    'long_description': None,
+    'author': '\xe8\xa7\xa3\xe9\x87\x8a',
+    'author_email': 'xieshi@forchange.tech',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
