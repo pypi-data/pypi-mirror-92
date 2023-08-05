0686a5193e4983c1a6d192b587e5366767786bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:40 +0000

    Add package ground/2.1.0/ground-2.1.0.tar.gz

---
+++ packages/ground/ground-2.1.0.tar.gz/ground-2.1.0/ground/core/centroidal/robust/contour.py
@@ -0,0 +1,30 @@
+from typing import (Sequence,
+                    Type)
+
+from ground.core.hints import UnaryCoordinateOperation
+from ground.core.shewchuk import (scale_expansion,
+                                  sum_expansions,
+                                  to_cross_product)
+from ground.hints import Point
+
+
+def centroid(inverse: UnaryCoordinateOperation,
+             point_cls: Type[Point],
+             vertices: Sequence[Point]) -> Point:
+    double_area = x_numerator = y_numerator = (0,)
+    prev = vertices[-1]
+    prev_x, prev_y = prev.x, prev.y
+    for vertex in vertices:
+        x, y = vertex.x, vertex.y
+        area_component = to_cross_product(prev_x, prev_y, x, y)
+        double_area = sum_expansions(double_area, area_component)
+        x_numerator = sum_expansions(x_numerator,
+                                     scale_expansion(area_component,
+                                                     prev_x + x))
+        y_numerator = sum_expansions(y_numerator,
+                                     scale_expansion(area_component,
+                                                     prev_y + y))
+        prev_x, prev_y = x, y
+    inverted_denominator = inverse(3 * double_area[-1])
+    return point_cls(x_numerator[-1] * inverted_denominator,
+                     y_numerator[-1] * inverted_denominator)
