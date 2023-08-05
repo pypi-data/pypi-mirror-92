24807339b0dbf1d776d67c6f8aaa67b4da46a961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:37 +0000

    Add package tls.messaging/0.6.1/tls.messaging-0.6.1.tar.gz

---
+++ packages/tls.messaging/tls.messaging-0.6.1.tar.gz/tls.messaging-0.6.1/tls/messaging/types.py
@@ -0,0 +1,14 @@
+"""Shared types for messaging."""
+
+TTo = str
+TFrom = str
+TNotifyUrl = str
+TMessageId = str
+TBody = str
+TSentTimestamp = str
+TValidity = int
+TScheduledDelivery = int
+TReplyRequest = bool
+TPriority = bool
+TReceiptOff = bool
+TUsrMsgRef = str
