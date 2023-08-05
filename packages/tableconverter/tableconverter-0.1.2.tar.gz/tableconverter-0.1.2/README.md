f575af02edb9a7d0ab2aa02a1a8b0aeb582ee705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:00 +0000

    Add package tableconverter/0.1.2/tableconverter-0.1.2.tar.gz

---
+++ packages/tableconverter/tableconverter-0.1.2.tar.gz/tableconverter-0.1.2/README.md
@@ -0,0 +1,27 @@
+# PyTableConverter
+A command line tool to convert tables in various formats.
+
+[![](https://img.shields.io/pypi/v/tableconverter.svg)](https://pypi.python.org/pypi/tableconverter)
+
+## Quick start
+
+### Installation
+
+```bash
+pip install --upgrade tableconverter
+```
+
+### Examples
+```bash
+tableconverter demos/assets/example.csv -d html
+
+# or you may use Heredoc
+```
+## Supported formats
+* CSV
+* Markdown
+* HTML
+* JSON
+
+## Lisence
+PyTableConverter is released under the terms of the MIT license.
---
+++ packages/tableconverter/tableconverter-0.1.2.tar.gz/tableconverter-0.1.2/tableconverter/autogen/HTMLTableListener.py
@@ -0,0 +1,102 @@
+# Generated from HTMLTable.g4 by ANTLR 4.8
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .HTMLTable import HTMLTable
+else:
+    from HTMLTable import HTMLTable
+
+# This class defines a complete listener for a parse tree produced by HTMLTable.
+class HTMLTableListener(ParseTreeListener):
+
+    # Enter a parse tree produced by HTMLTable#table.
+    def enterTable(self, ctx:HTMLTable.TableContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#table.
+    def exitTable(self, ctx:HTMLTable.TableContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#NormalTag.
+    def enterNormalTag(self, ctx:HTMLTable.NormalTagContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#NormalTag.
+    def exitNormalTag(self, ctx:HTMLTable.NormalTagContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#Ignored.
+    def enterIgnored(self, ctx:HTMLTable.IgnoredContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#Ignored.
+    def exitIgnored(self, ctx:HTMLTable.IgnoredContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#htmlContent.
+    def enterHtmlContent(self, ctx:HTMLTable.HtmlContentContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#htmlContent.
+    def exitHtmlContent(self, ctx:HTMLTable.HtmlContentContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#htmlAttribute.
+    def enterHtmlAttribute(self, ctx:HTMLTable.HtmlAttributeContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#htmlAttribute.
+    def exitHtmlAttribute(self, ctx:HTMLTable.HtmlAttributeContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#htmlChardata.
+    def enterHtmlChardata(self, ctx:HTMLTable.HtmlChardataContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#htmlChardata.
+    def exitHtmlChardata(self, ctx:HTMLTable.HtmlChardataContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#htmlMisc.
+    def enterHtmlMisc(self, ctx:HTMLTable.HtmlMiscContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#htmlMisc.
+    def exitHtmlMisc(self, ctx:HTMLTable.HtmlMiscContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#htmlComment.
+    def enterHtmlComment(self, ctx:HTMLTable.HtmlCommentContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#htmlComment.
+    def exitHtmlComment(self, ctx:HTMLTable.HtmlCommentContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#script.
+    def enterScript(self, ctx:HTMLTable.ScriptContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#script.
+    def exitScript(self, ctx:HTMLTable.ScriptContext):
+        pass
+
+
+    # Enter a parse tree produced by HTMLTable#style.
+    def enterStyle(self, ctx:HTMLTable.StyleContext):
+        pass
+
+    # Exit a parse tree produced by HTMLTable#style.
+    def exitStyle(self, ctx:HTMLTable.StyleContext):
+        pass
+
+
+
+del HTMLTable
