37ceaeab8105e076971a7056a50df7346e898992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:34 +0000

    Add package deep-daze/0.2.6/deep-daze-0.2.6.tar.gz

---
+++ packages/deep-daze/deep-daze-0.2.6.tar.gz/deep-daze-0.2.6/setup.py
@@ -0,0 +1,45 @@
+import sys
+from setuptools import setup, find_packages
+
+sys.path[0:0] = ['deep_daze']
+from version import __version__
+
+setup(
+  name = 'deep-daze',
+  packages = find_packages(),
+  include_package_data = True,
+  entry_points={
+    'console_scripts': [
+      'imagine = deep_daze.cli:main',
+    ],
+  },
+  version = __version__,
+  license='MIT',
+  description = 'Deep Daze',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/deep-daze',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'implicit neural representations',
+    'text to image'
+  ],
+  install_requires=[
+    'torch>=1.7.1',
+    'einops>=0.3',
+    'fire',
+    'ftfy',
+    'siren-pytorch>=0.0.7',
+    'tqdm',
+    'regex'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
