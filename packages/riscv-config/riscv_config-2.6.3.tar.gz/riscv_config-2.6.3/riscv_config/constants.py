734c53c8236a1a6ef9f63059a7b43a1301d80f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package riscv-config/2.6.3/riscv_config-2.6.3.tar.gz

---
+++ packages/riscv-config/riscv_config-2.6.3.tar.gz/riscv_config-2.6.3/riscv_config/constants.py
@@ -0,0 +1,7 @@
+import os
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+isa_schema = os.path.join(root, 'schemas/schema_isa.yaml')
+platform_schema = os.path.join(root, 'schemas/schema_platform.yaml')
+
