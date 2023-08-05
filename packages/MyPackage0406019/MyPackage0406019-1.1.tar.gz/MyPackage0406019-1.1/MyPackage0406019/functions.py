52acd63c590331ef8411d20172b1ae2edbc9da95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:47 +0000

    Add package MyPackage0406019/1.1/MyPackage0406019-1.1.tar.gz

---
+++ packages/MyPackage0406019/MyPackage0406019-1.1.tar.gz/MyPackage0406019-1.1/MyPackage0406019/functions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def sum(x,y):
+    return x+y
+
+def average(x,y):
+    return (x+y)/2
+
+def power(x,y):
+    return x**y
+
---
+++ packages/MyPackage0406019/MyPackage0406019-1.1.tar.gz/MyPackage0406019-1.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+setup(name='MyPackage0406019',
+      version='1.1',
+      description='Math functions',
+      packages=['MyPackage0406019'],
+      zip_safe=False
+)
+
