699c92f851872d6473c16472d682be272690c179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package openkongqi/0.2.3/openkongqi-0.2.3-py2.py3-none-any.whl

---
+++ packages/openkongqi/openkongqi-0.2.3-py2.py3-none-any.whl/openkongqi/stations.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+from copy import deepcopy
+
+from .conf import settings
+from .utils import get_uuid, load_tree, passthrough_loader, SEP, WILDCARD
+
+
+_STATIONS_MAP = load_tree(settings['STATIONS_MAP_DIR'],
+                          data_loader=passthrough_loader)
+
+
+def get_station_map(uuid=None):
+    """Get the entire station map given a UUID, or get the concatenated station
+    map from multiple cities/regions given a UIID with a wildcard "*"
+
+    Usage::
+
+        >>> from openkongqi.stations import get_station_map
+        >>> get_station_map('cn:guangdong:guangzhou') # get the station map from Guangzhou, Guangdong
+        >>> get_station_map('cn:guangdong:*') # get the concatenated station map from all cities in Guangdong province
+        >>> get_station_map('cn:*') # get the concatenated station map from all cities and provinces in China
+
+    :param uuid: a UUID key
+    :type uuid: str
+    """
+    if uuid.endswith(SEP + WILDCARD) is True:
+        id_map = {}
+        uuid_without_wildcard = uuid[:-1]
+        for k in _STATIONS_MAP:
+            if k.startswith(uuid_without_wildcard):
+                prefix = k.replace(uuid_without_wildcard, "", 1)
+                new_map = _inject_uuid_prefix(prefix, _STATIONS_MAP[k])
+                id_map.update(new_map)
+
+    else:
+        id_map = _STATIONS_MAP.get(uuid, {})
+    return id_map
+
+
+def get_all_uuids():
+    """Return a sorted list of complete station UUIDs."""
+    res = []
+    for country, station_map in _STATIONS_MAP.items():
+        res.extend([get_uuid(country, s) for s in station_map.values()])
+    return sorted(res)
+
+
+def _inject_uuid_prefix(prefix, station_map):
+    map_copy = deepcopy(station_map)
+    for station in map_copy:
+        map_copy[station]["uuid"] = get_uuid(prefix, map_copy[station]["uuid"])
+    return map_copy
