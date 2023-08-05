8d9ac46c2ec03b2d16c767a2534c66e792a2ed3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:37 +0000

    Add package ocrd-validators/2.22.0b4/ocrd_validators-2.22.0b4.tar.gz

---
+++ packages/ocrd-validators/ocrd_validators-2.22.0b4.tar.gz/ocrd_validators-2.22.0b4/ocrd_validators/__init__.py
@@ -0,0 +1,26 @@
+"""
+Validators for various OCR-D related data structures.
+"""
+__all__ = [
+    'ParameterValidator',
+    'WorkspaceValidator',
+    'PageValidator',
+    'OcrdToolValidator',
+    'OcrdConfigValidator',
+    'OcrdResourceListValidator',
+    'OcrdZipValidator',
+    'XsdValidator',
+    'XsdMetsValidator',
+    'XsdPageValidator',
+]
+
+from .parameter_validator import ParameterValidator
+from .workspace_validator import WorkspaceValidator
+from .page_validator import PageValidator
+from .ocrd_tool_validator import OcrdToolValidator
+from .ocrd_config_validator import OcrdConfigValidator
+from .resource_list_validator import OcrdResourceListValidator
+from .ocrd_zip_validator import OcrdZipValidator
+from .xsd_validator import XsdValidator
+from .xsd_mets_validator import XsdMetsValidator
+from .xsd_page_validator import XsdPageValidator
