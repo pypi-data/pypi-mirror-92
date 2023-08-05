2d7731771b741375f58dc9aadbe95e447c349f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:46 +0000

    Add package flexiv-consul-service/1.6/flexiv_consul_service-1.6-py3-none-any.whl

---
+++ packages/flexiv-consul-service/flexiv_consul_service-1.6-py3-none-any.whl/flexiv_consul_service/conf/settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Author: shenglong
+# Date: 2021/1/12
+
+CONSUL_IP = "10.24.13.15"
+
+CONSUL_PORT = 8900
+
+CONSUL_TOKEN = None
+
+CONSUL_SERVICE_HEALTH_CHECK = "30s"
---
+++ packages/flexiv-consul-service/flexiv_consul_service-1.6-py3-none-any.whl/flexiv_consul_service/src/client.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Author: shenglong
+# Date: 2021/1/12
+import requests
+import json
+
+from flexiv_consul_service.src.base_service import ConsulBaseService
+
+
+def register_service(ip, port, service_list):
+    if not isinstance(service_list, list):
+        raise TypeError("service list must be list params")
+    if not service_list:
+        raise Exception("service_list not allow empty")
+
+    consul_client = ConsulBaseService()
+    for service in service_list:
+        service_id = service + ip + ':' + str(port)
+        consul_client.register(service, service_id=service_id, address=ip, port=port, tags=['master'])
+
+
+def get_service_info(service_name: str, token: str, method="get", data={}):
+    if not service_name:
+        raise Exception("not allow service empty")
+    if not token:
+        raise Exception("not allow token empty")
+
+    consul_client = ConsulBaseService()
+    ip, port = consul_client.get_service(service_name)
+
+    headers = {"Authorization": f"jwt {token}", 'content-type': "application/json"}
+
+    if not hasattr(requests, method):
+        raise AttributeError(f'requests has no attribute {method} ')
+    func = getattr(requests, method)
+    service_res = func(f"http://{ip}:{port}/{service_name}",
+                       json=json.dumps(data),
+                       headers=headers,
+                       verify=False, timeout=2.0)
+
+    service_res_content = service_res.json()
+
+    return service_res_content
+
+
+
+
+
+
+
+
+
+
+
+
+
