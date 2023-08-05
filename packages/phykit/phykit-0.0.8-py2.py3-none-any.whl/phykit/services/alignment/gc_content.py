c1eb176a0b0c8bfc42cef945e3bae747da2077aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:33 +0000

    Add package phykit/0.0.8/phykit-0.0.8-py2.py3-none-any.whl

---
+++ packages/phykit/phykit-0.0.8-py2.py3-none-any.whl/phykit/services/alignment/gc_content.py
@@ -0,0 +1,63 @@
+from enum import Enum
+import re
+import sys
+
+from Bio import SeqIO
+
+from .base import Alignment
+
+from ...helpers.files import get_alignment_and_format
+
+class FileFormat(Enum):
+    fasta = "fasta"
+    clustal = "clustal"
+    maf = "maf"
+    mauve = "mauve"
+    phylip = "phylip"
+    phylip_seq = "phylip-sequential"
+    stockholm = "stockholm"
+
+class GCContent(Alignment):
+    def __init__(self, args) -> None:
+        super().__init__(**self.process_args(args))
+
+    def run(self):
+        # create biopython object of sequences
+        records, _ = get_alignment_and_format(self.fasta)
+        
+        # initialize and populate dict for
+        # holding the entry sequences
+        entry_and_seq = {}
+        for record in records:
+            entry_and_seq[record.id] = str(record.seq)
+
+        if self.verbose:
+            for entry, seq in entry_and_seq.items():
+                seq, matches = self.find_matches_and_remove_gaps(seq)
+                print(f"{entry}\t{round(len(matches)/len(seq), 4)}")
+        else:
+            all_seqs = []
+            for entry, seq in entry_and_seq.items():
+                all_seqs.append(seq)
+            all_seqs = ''.join(all_seqs)
+            all_seqs, matches = self.find_matches_and_remove_gaps(all_seqs)
+            try:
+                gc_content = round(len(matches)/len(all_seqs), 4)
+            except ZeroDivisionError:
+                print("Input file has an unacceptable format. Please check input file argument.")
+                sys.exit()
+            print(gc_content)
+            
+
+    def process_args(self, args):
+        return dict(
+            fasta=args.fasta, 
+            verbose=args.verbose
+        )
+
+    def find_matches_and_remove_gaps(self, seq: str):
+        regex_pattern = re.compile('[GgCc]')
+        seq = seq.replace('-', '')
+        seq = seq.replace('?', '')
+        matches = regex_pattern.findall(seq)
+        return seq, matches
