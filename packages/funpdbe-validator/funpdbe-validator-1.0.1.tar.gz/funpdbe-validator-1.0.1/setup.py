200acdc7b4381e0faed0a35851116d437eca673d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:58 +0000

    Add package funpdbe-validator/1.0.1/funpdbe-validator-1.0.1.tar.gz

---
+++ packages/funpdbe-validator/funpdbe-validator-1.0.1.tar.gz/funpdbe-validator-1.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+# def license():
+#     with open('LICENSE') as f:
+#         return f.read()
+
+
+setup(
+    name="funpdbe-validator",
+    version="1.0.1",
+    description="Validate PDBe-KB JSONs by FunPDBe Schema",
+    long_description_content_type='text/markdown',
+    long_description=readme(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    keywords="json validator",
+    url="https://github.com/PDBe-KB/funpdbe-validator",
+    author="Mihaly Varadi",
+    author_email="mvaradi@ebi.ac.uk",
+    license_file="LICENSE",
+    packages=["validator"],
+    install_requires=["jsonschema", "requests"],
+    test_suite="tests",
+    tests_require=["pytest", "pytest-cov"],
+    include_package_data=True,
+    zip_safe=True,
+    python_requires='>=3.6',
+)
