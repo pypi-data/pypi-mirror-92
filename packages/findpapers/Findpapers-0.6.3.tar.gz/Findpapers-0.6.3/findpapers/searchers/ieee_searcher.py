7a18e3b99d404fa8aaff2d97a4e637f7807fc55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:03 +0000

    Add package findpapers/0.6.3/Findpapers-0.6.3.tar.gz

---
+++ packages/findpapers/Findpapers-0.6.3.tar.gz/Findpapers-0.6.3/findpapers/searchers/ieee_searcher.py
@@ -0,0 +1,260 @@
+import requests
+import datetime
+import logging
+import re
+import math
+from lxml import html
+from typing import Optional
+import findpapers.utils.common_util as common_util
+import findpapers.utils.query_util as query_util
+from findpapers.models.search import Search
+from findpapers.models.paper import Paper
+from findpapers.models.publication import Publication
+from findpapers.utils.requests_util import DefaultSession
+
+
+DATABASE_LABEL = 'IEEE'
+BASE_URL = 'http://ieeexploreapi.ieee.org'
+MAX_ENTRIES_PER_PAGE = 200
+
+
+def _get_search_url(search: Search, api_token: str, start_record: Optional[int] = 1) -> str:
+    """
+    This method return the URL to be used to retrieve data from IEEE database
+    See https://developer.ieee.org/docs/read/Metadata_API_details for query tips
+
+    Parameters
+    ----------
+    search : Search
+        A search instance
+    api_token : str
+        The API key used to fetch data from IEEE database,
+    start_record : str
+        Sequence number of first record to fetch, by default 1
+
+    Returns
+    -------
+    str
+        a URL to be used to retrieve data from IEEE database
+    """
+
+    query = search.query.replace(' AND NOT ', ' NOT ')
+    query = query_util.replace_search_term_enclosures(query, '"Abstract":"', '"')
+
+    url = f'{BASE_URL}/api/v1/search/articles?querytext=({query})&format=json&apikey={api_token}&max_records={MAX_ENTRIES_PER_PAGE}'
+
+    if search.since is not None:
+        url += f'&start_year={search.since.year}'
+
+    if search.until is not None:
+        url += f'&end_year={search.until.year}'
+
+    if start_record is not None:
+        url += f'&start_record={start_record}'
+
+    if search.publication_types is not None:
+
+        content_types = set()
+
+        if 'conference proceedings' in search.publication_types:
+            content_types.add('Conferences')
+        if 'journal' in search.publication_types:
+            content_types.add('Journals')
+        if 'book' in search.publication_types:
+            content_types.add('Books')
+        if 'other' in search.publication_types:
+            content_types.add('Courses')
+            content_types.add('Early Access')
+            content_types.add('Magazines')
+            content_types.add('Standards')
+            
+        url += f'&content_type={",".join(content_types)}'
+
+    return url
+
+
+def _get_api_result(search: Search, api_token: str, start_record: Optional[int] = 1) -> dict:  # pragma: no cover
+    """
+    This method return results from IEEE database using the provided search parameters
+
+    Parameters
+    ----------
+    search : Search
+        A search instance
+    api_token : str
+        The API key used to fetch data from IEEE database,
+    start_record : str
+        Sequence number of first record to fetch, by default 1
+
+    Returns
+    -------
+    dict
+        a result from IEEE database
+    """
+
+    url = _get_search_url(search, api_token, start_record)
+
+    return common_util.try_success(lambda: DefaultSession().get(url).json(), 2)
+
+
+def _get_publication(paper_entry: dict) -> Publication:
+    """
+    Using a paper entry provided, this method builds a publication instance
+
+    Parameters
+    ----------
+    paper_entry : dict
+        A paper entry retrieved from IEEE API
+
+    Returns
+    -------
+    Publication
+        A publication instance or None
+    """
+
+    publication_title = paper_entry.get('publication_title', None)
+
+    if publication_title is None or len(publication_title) == 0:
+        return None
+
+    publication_isbn = paper_entry.get('isbn', None)
+    publication_issn = paper_entry.get('issn', None)
+    publication_publisher = paper_entry.get('publisher', None)
+    publication_category = paper_entry.get('content_type', None)
+
+    publication = Publication(publication_title, publication_isbn,
+                              publication_issn, publication_publisher, publication_category)
+
+    return publication
+
+
+def _get_paper(paper_entry: dict, publication: Publication) -> Paper:
+    """
+    Using a paper entry provided, this method builds a paper instance
+
+    Parameters
+    ----------
+    paper_entry : dict
+        A paper entry retrieved from IEEE API
+    publication : Publication
+        A publication instance that will be associated with the paper
+
+    Returns
+    -------
+    Paper
+        A paper instance or None
+    """
+
+    paper_title = paper_entry.get('title', None)
+
+    if paper_title is None or len(paper_title) == 0:
+        return None
+
+    paper_publication_date = paper_entry.get('publication_date', None)
+    paper_doi = paper_entry.get('doi', None)
+    paper_citations = paper_entry.get('citing_paper_count', None)
+    paper_abstract = paper_entry.get('abstract', None)
+    paper_urls = {paper_entry.get('pdf_url')}
+    paper_pages = None
+    paper_number_of_pages = None
+
+    try:
+        paper_keywords = set([ x.strip() for x in paper_entry.get(
+            'index_terms').get('author_terms').get('terms')])
+    except Exception as e:
+        paper_keywords = set()
+
+    if paper_publication_date is not None:
+        try:
+            paper_publication_date_split = paper_publication_date.split(' ')
+            day = int(paper_publication_date_split[0].split('-')[0])
+            month = int(common_util.get_numeric_month_by_string(
+                paper_publication_date_split[1]))
+            year = int(paper_publication_date_split[2])
+
+            paper_publication_date = datetime.date(year, month, day)
+        except Exception as e:
+            pass
+
+    if not isinstance(paper_publication_date, datetime.date):
+        paper_publication_date = datetime.date(
+            paper_entry.get('publication_year'), 1, 1)
+
+    if paper_publication_date is None:
+        return None
+
+    paper_authors = []
+    for author in paper_entry.get('authors').get('authors'):
+        paper_authors.append(author.get('full_name'))
+
+    start_page = paper_entry.get('start_page', None)
+    end_page = paper_entry.get('end_page', None)
+    
+
+    if start_page is not None and end_page is not None:
+        try:
+            paper_pages = f"{paper_entry.get('start_page')}-{paper_entry.get('end_page')}"
+            paper_number_of_pages = abs(
+                int(paper_entry.get('start_page'))-int(paper_entry.get('end_page')))+1
+        except Exception:  # pragma: no cover
+            pass
+
+    paper = Paper(paper_title, paper_abstract, paper_authors, publication,
+                  paper_publication_date, paper_urls, paper_doi, paper_citations, 
+                  paper_keywords, None, paper_number_of_pages, paper_pages)
+
+    return paper
+
+
+def run(search: Search, api_token: str):
+    """
+    This method fetch papers from IEEE database using the provided search parameters
+    After fetch the data from IEEE, the collected papers are added to the provided search instance
+
+    Parameters
+    ----------
+    search : Search
+        A search instance
+    api_token : str
+        The API key used to fetch data from IEEE database,
+
+    Raises
+    ------
+    AttributeError
+        - The API token cannot be null
+    """
+
+    if api_token is None or len(api_token.strip()) == 0:
+        raise AttributeError('The API token cannot be null')
+
+    papers_count = 0
+    result = _get_api_result(search, api_token)
+    total_papers = result.get('total_records')
+
+    logging.info(f'IEEE: {total_papers} papers to fetch')
+
+    while(papers_count < total_papers and not search.reached_its_limit(DATABASE_LABEL)):
+
+        for paper_entry in result.get('articles'):
+
+            if papers_count >= total_papers or search.reached_its_limit(DATABASE_LABEL):
+                break
+            
+            papers_count += 1
+
+            try:
+
+                logging.info(f'({papers_count}/{total_papers}) Fetching IEEE paper: {paper_entry.get("title")}')
+
+                publication = _get_publication(paper_entry)
+                paper = _get_paper(paper_entry, publication)
+
+                if paper is not None:
+                    paper.add_database(DATABASE_LABEL)
+                    search.add_paper(paper)
+
+            except Exception as e:  # pragma: no cover
+                logging.debug(e, exc_info=True)
+
+        if papers_count < total_papers and not search.reached_its_limit(DATABASE_LABEL):
+            result = _get_api_result(search, api_token, papers_count+1)
---
+++ packages/findpapers/Findpapers-0.6.3.tar.gz/Findpapers-0.6.3/findpapers/tools/bibtex_generator_tool.py
@@ -0,0 +1,112 @@
+import logging
+import datetime
+from typing import Optional
+import findpapers.utils.persistence_util as persistence_util
+import findpapers.utils.common_util as common_util
+
+
+def generate_bibtex(search_path: str, outputpath: str, only_selected_papers: Optional[bool] = False,
+                    categories_filter: Optional[dict] = None, add_findpapers_citation: Optional[bool] = False):
+    """
+    Method used to generate a BibTeX file from a search result
+
+    Parameters
+    ----------
+    search_path : str
+        A valid file path containing a JSON representation of the search results
+    outputpath : str
+        A valid file path for the BibTeX output file
+    only_selected_papers : bool, optional
+        If you only want to generate a BibTeX file for selected papers, by default False
+    categories_filter : dict, None by default
+        A dict of categories to be used to filter which papers will be downloaded
+    add_findpapers_citation : bool, optional
+        If you want to add an entry for Findpapers in your BibTeX output file, by default False
+    """
+
+    search = persistence_util.load(search_path)
+    common_util.check_write_access(outputpath)
+
+    default_tab = ' ' * 4
+    bibtex_output = ''
+
+    today = datetime.datetime.now().strftime('%Y/%m/%d')
+    year = datetime.datetime.now().year
+
+    if add_findpapers_citation:
+        bibtex_output = '\n'.join([
+            '@misc{findpapers',
+            '\ttitle = {Findpapers},',
+            '\tauthor = {Jonatas Grosman},',
+            '\thowpublished = {Available at https://gitlab.com/jonatasgrosman/findpapers ('+today+')},',
+            f'\tyear = {{{year}}}',
+            '}\n\n'
+        ])
+
+    for paper in search.papers:
+
+        if (only_selected_papers and not paper.selected) or \
+        (categories_filter is not None and (paper.categories is None or not paper.has_category_match(categories_filter))):
+            continue
+
+        logging.info(f'Exporting bibtex for: {paper.title}')
+
+        try:
+
+            citation_type = '@unpublished'
+            if paper.publication is not None:
+                if paper.publication.category == 'Journal':
+                    citation_type = '@article'
+                elif paper.publication.category == 'Conference Proceedings':
+                    citation_type = '@inproceedings'
+                elif paper.publication.category == 'Book':
+                    citation_type = '@book'
+                else:
+                    citation_type = '@misc'
+
+            bibtex_output += f'{citation_type}{"{"}{paper.get_citation_key()},\n'
+
+            bibtex_output += f'{default_tab}title = {{{paper.title}}},\n'
+
+            if len(paper.authors) > 0:
+                authors = ' and '.join(paper.authors)
+                bibtex_output += f'{default_tab}author = {{{authors}}},\n'
+
+            if citation_type == '@unpublished':
+                note = ''
+                if len(paper.urls) > 0:
+                    note += f'Available at {list(paper.urls)[0]}'
+                if paper.publication_date is not None:
+                    note += f' ({paper.publication_date.strftime("%Y/%m/%d")})'
+                if paper.comments is not None:
+                    note += paper.comments if len(
+                        note) == 0 else f' | {paper.comments}'
+                bibtex_output += f'{default_tab}note = {{{note}}},\n'
+            elif citation_type == '@article':
+                bibtex_output += f'{default_tab}journal = {{{paper.publication.title}}},\n'
+            elif citation_type == '@inproceedings':
+                bibtex_output += f'{default_tab}booktitle = {{{paper.publication.title}}},\n'
+            elif citation_type == '@misc' and len(paper.urls) > 0 and paper.publication_date is not None:
+                date = paper.publication_date.strftime('%Y/%m/%d')
+                url = list(paper.urls)[0]
+                bibtex_output += f'{default_tab}howpublished = {{Available at {url} ({date})}},\n'
+
+            if paper.publication is not None and paper.publication.publisher is not None:
+                bibtex_output += f'{default_tab}publisher = {{{paper.publication.publisher}}},\n'
+
+            if paper.publication_date is not None:
+                bibtex_output += f'{default_tab}year = {{{paper.publication_date.year}}},\n'
+
+            if paper.pages is not None:
+                bibtex_output += f'{default_tab}pages = {{{paper.pages}}},\n'
+
+            bibtex_output = bibtex_output.rstrip(
+                ',\n') + '\n'  # removing last comma
+
+            bibtex_output += '}\n\n'
+
+        except Exception as e:
+            logging.debug(e, exc_info=True)
+
+    with open(outputpath, 'w') as fp:
+        fp.write(bibtex_output)
