8e1cd97b0401d1366622e9260e2041a32df62d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:57 +0000

    Add package awesome-pattern-matching/0.17.0/awesome-pattern-matching-0.17.0.tar.gz

---
+++ packages/awesome-pattern-matching/awesome-pattern-matching-0.17.0.tar.gz/awesome-pattern-matching-0.17.0/apm/__init__.py
@@ -0,0 +1,87 @@
+from .__pkginfo__ import __version__
+from .case_of import case
+from .core import \
+    AllOf, \
+    Capture, \
+    Either, \
+    MatchContext, \
+    MatchResult, \
+    Not, \
+    OneOf, \
+    Pattern, \
+    Remaining, \
+    Some, \
+    Strict, \
+    String, \
+    StringPattern, \
+    Value, \
+    _
+from .error import MatchError
+from .match import match
+from .overload import case_distinction, Match
+from .patterns import \
+    Arguments, \
+    At, \
+    Between, \
+    Check, \
+    Contains, \
+    Each, \
+    EachItem, \
+    InstanceOf, \
+    IsNumber, \
+    IsString, \
+    IsTruish, \
+    Length, \
+    Maybe, \
+    NoneOf, \
+    Object, \
+    Regex, \
+    Returns, \
+    Transformed
+from .try_match import Case, Default
+
+__all__ = [
+    'AllOf',
+    'Capture',
+    'Either',
+    'MatchContext',
+    'MatchResult',
+    'Not',
+    'OneOf',
+    'Pattern',
+    'Remaining',
+    'Some',
+    'Strict',
+    'String',
+    'StringPattern',
+    'Value',
+
+    '_',
+
+    'case',
+    'match',
+    'case_distinction',
+    'Match',
+    'MatchError',
+    'Case',
+    'Default',
+
+    'Arguments',
+    'At',
+    'Between',
+    'Check',
+    'Contains',
+    'Each',
+    'EachItem',
+    'InstanceOf',
+    'IsNumber',
+    'IsString',
+    'IsTruish',
+    'Length',
+    'Maybe',
+    'NoneOf',
+    'Object',
+    'Regex',
+    'Returns',
+    'Transformed',
+]
