7cfab80f56eb69be61f2cb0c251f0952f08a209b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:45 +0000

    Add package compath-resources/0.1.0/compath_resources-0.1.0-py3-none-any.whl

---
+++ packages/compath-resources/compath_resources-0.1.0-py3-none-any.whl/compath_resources/constants.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""Constants for ComPath's resources."""
+
+import os
+from pathlib import Path
+
+import pystow
+
+COMPATH_HOME = pystow.get('compath')
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+ROOT = Path(os.path.abspath(os.path.join(HERE, os.pardir, os.pardir)))
+RESOURCES = Path(HERE) / 'resources'
+DOCS_DIRECTORY = ROOT / 'docs'
+IMG_DIRECTORY = DOCS_DIRECTORY / 'img'
+
+_BASE_URL = 'https://raw.githubusercontent.com/ComPath/resources/master/mappings'
+
+# Inter-database mappings URLs
+
+KEGG_WIKIPATHWAYS_URL = f'{_BASE_URL}/kegg_wikipathways.csv'
+KEGG_REACTOME_URL = f'{_BASE_URL}/kegg_reactome.csv'
+WIKIPATHWAYS_REACTOME_URL = f'{_BASE_URL}/wikipathways_reactome.csv'
+PATHBANK_KEGG_URL = f'{_BASE_URL}/pathbank_kegg.csv'
+PATHBANK_REACTOME_URL = f'{_BASE_URL}/pathbank_reactome.csv'
+PATHBANK_WIKIPATHWAYS_URL = f'{_BASE_URL}/pathbank_wikipathways.csv'
+
+# Intra-database mappings URLS
+
+SPECIAL_MAPPINGS_URL = f'{_BASE_URL}/special_mappings.csv'
+REACTOME_HIERARCHICAL_MAPPINGS_URL = f'{_BASE_URL}/reactome_hierarchy.tsv'
