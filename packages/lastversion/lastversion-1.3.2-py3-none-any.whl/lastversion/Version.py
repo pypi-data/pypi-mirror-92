7f307638760272b2a5549e70cc7e409e018db0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:44 +0000

    Add package lastversion/1.3.2/lastversion-1.3.2-py3-none-any.whl

---
+++ packages/lastversion/lastversion-1.3.2-py3-none-any.whl/lastversion/Version.py
@@ -0,0 +1,111 @@
+import re
+
+from packaging.version import Version as PackagingVersion
+
+
+class Version(PackagingVersion):
+
+    def fix_letter_post_release(self, match):
+        self.fixed_letter_post_release = True
+        return match.group(1) + '.post' + str(ord(match.group(2)))
+
+    def __init__(self, version):
+
+        self.fixed_letter_post_release = False
+
+        # many times they would tag foo-1.2.3 which would parse to LegacyVersion
+        # we can avoid this, by reassigning to what comes after the dash:
+        parts = version.split('-', 1)
+
+        if len(parts) == 2 and parts[0].isalpha():
+            version = parts[1]
+
+        # remove *any* non-digits which appear at the beginning of the version string
+        # e.g. Rhino1_7_13_Release does not even bother to put a delimiter...
+        # such string at the beginning typically do not convey stability level
+        # so we are fine to remove them (unlike the ones in the tail)
+        version = re.sub('^[^0-9]+', '', version, 1)
+
+        # help devel releases to be correctly identified
+        # https://www.python.org/dev/peps/pep-0440/#developmental-releases
+        version = re.sub('-devel$', '.dev0', version, 1)
+        version = re.sub('-test$', '.dev0', version, 1)
+        # help post (patch) releases to be correctly identified (e.g. Magento 2.3.4-p2)
+        version = re.sub('-p(\\d+)$', '.post\\1', version, 1)
+        version = re.sub('(\\d)([a-z])$', self.fix_letter_post_release, version, 1)
+        # release-3_0_2 is often seen on Mercurial holders
+        # note that above code removes "release-" already so we are left with "3_0_2"
+        if re.search(r'^(?:\d+_)+(?:\d+)', version):
+            version = version.replace('_', '.')
+        # finally, split by dot "delimiter", see if there are common words which are definitely removable
+        parts = version.split('.')
+        version = []
+        for p in parts:
+            if p.lower() in ['release']:
+                continue
+            version.append(p)
+        version = '.'.join(version)
+        super(Version, self).__init__(version)
+
+    @property
+    def epoch(self):
+        # type: () -> int
+        _epoch = self._version.epoch  # type: int
+        return _epoch
+
+    @property
+    def release(self):
+        _release = self._version.release
+        return _release
+
+    @property
+    def pre(self):
+        _pre = self._version.pre
+        return _pre
+
+    @property
+    def post(self):
+        return self._version.post[1] if self._version.post else None
+
+    @property
+    def dev(self):
+        return self._version.dev[1] if self._version.dev else None
+
+    @property
+    def local(self):
+        if self._version.local:
+            return ".".join(str(x) for x in self._version.local)
+        else:
+            return None
+
+    def __str__(self):
+        # type: () -> str
+        parts = []
+
+        # Epoch
+        if self.epoch != 0:
+            parts.append("{0}!".format(self.epoch))
+
+        # Release segment
+        parts.append(".".join(str(x) for x in self.release))
+
+        # Pre-release
+        if self.pre is not None:
+            parts.append("".join(str(x) for x in self.pre))
+
+        # Post-release
+        if self.post is not None:
+            if self.fixed_letter_post_release:
+                parts.append("{0}".format(chr(self.post)))
+            else:
+                parts.append(".post{0}".format(self.post))
+
+        # Development release
+        if self.dev is not None:
+            parts.append(".dev{0}".format(self.dev))
+
+        # Local version segment
+        if self.local is not None:
+            parts.append("+{0}".format(self.local))
+
+        return "".join(parts)
