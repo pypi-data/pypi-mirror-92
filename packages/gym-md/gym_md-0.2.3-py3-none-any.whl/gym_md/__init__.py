50e6369a9ec98af5a016533e067dcf06549acb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:32 +0000

    Add package gym-md/0.2.3/gym_md-0.2.3-py3-none-any.whl

---
+++ packages/gym-md/gym_md-0.2.3-py3-none-any.whl/gym_md/__init__.py
@@ -0,0 +1,21 @@
+"""gym-md init module."""
+__version__ = "0.2.3"
+
+from logging import NullHandler, getLogger
+
+from gym.envs.registration import register
+
+getLogger(__name__).addHandler(NullHandler())
+
+register(
+    id="md-base-v0",
+    entry_point="gym_md.envs:MdEnvBase",
+)
+register(
+    id="md-test-v0",
+    entry_point="gym_md.envs:TestMdEnv",
+)
+register(
+    id="md-edge-v0",
+    entry_point="gym_md.envs:EdgeMdEnv",
+)
