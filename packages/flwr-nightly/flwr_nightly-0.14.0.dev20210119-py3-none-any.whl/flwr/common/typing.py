ea06a8a044e268238920e648c8ac17be9b7ab921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:30 +0000

    Add package flwr-nightly/0.14.0.dev20210119/flwr_nightly-0.14.0.dev20210119-py3-none-any.whl

---
+++ packages/flwr-nightly/flwr_nightly-0.14.0.dev20210119-py3-none-any.whl/flwr/common/typing.py
@@ -0,0 +1,93 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Flower type definitions."""
+
+
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Union
+
+import numpy as np
+
+Weights = List[np.ndarray]
+Scalar = Union[bool, bytes, float, int, str]
+Metrics = Dict[str, Scalar]
+
+
+@dataclass
+class Parameters:
+    """Model parameters."""
+
+    tensors: List[bytes]
+    tensor_type: str
+
+
+@dataclass
+class ParametersRes:
+    """Response when asked to return parameters."""
+
+    parameters: Parameters
+
+
+@dataclass
+class FitIns:
+    """Fit instructions for a client."""
+
+    parameters: Parameters
+    config: Dict[str, str]
+    # metrics: Metrics
+
+
+@dataclass
+class FitRes:
+    """Fit response from a client."""
+
+    parameters: Parameters
+    num_examples: int
+    num_examples_ceil: int  # Deprecated
+    fit_duration: float  # Deprecated
+    # metrics: Metrics
+
+
+@dataclass
+class EvaluateIns:
+    """Evaluate instructions for a client."""
+
+    parameters: Parameters
+    config: Dict[str, str]
+    # metrics: Metrics
+
+
+@dataclass
+class EvaluateRes:
+    """Evaluate response from a client."""
+
+    num_examples: int
+    loss: float
+    accuracy: float  # Deprecated
+    metrics: Optional[Metrics] = None
+
+
+@dataclass
+class Reconnect:
+    """Reconnect message from server to client."""
+
+    seconds: Optional[int]
+
+
+@dataclass
+class Disconnect:
+    """Disconnect message from client to server."""
+
+    reason: str
