e016d3503834acbec5a51e66514a7737ada37814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package django-email-verification/0.1.0/django-email-verification-0.1.0.tar.gz

---
+++ packages/django-email-verification/django-email-verification-0.1.0.tar.gz/django-email-verification-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-email-verification",
+    version="0.1.0",
+    author="Leone Bacciu",
+    author_email="leonebacciu@gmail.com",
+    description="Email confirmation app for django",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeoneBacciu/django-email-verification",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.1",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
