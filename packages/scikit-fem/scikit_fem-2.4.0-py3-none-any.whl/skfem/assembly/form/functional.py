0f5a05bab5ee7775635170974cc53eb6855b6c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:20 +0000

    Add package scikit-fem/2.4.0/scikit_fem-2.4.0-py3-none-any.whl

---
+++ packages/scikit-fem/scikit_fem-2.4.0-py3-none-any.whl/skfem/assembly/form/functional.py
@@ -0,0 +1,37 @@
+from typing import Dict, Optional
+
+from numpy import ndarray
+
+from .form import Form, FormDict
+from ..basis import Basis
+from ...element import DiscreteField
+
+
+class Functional(Form):
+    """A functional for finite element assembly.
+
+    Used similarly as :class:`~skfem.assembly.BilinearForm` with the expection
+    that forms take one parameter `w`.
+
+    """
+
+    def _kernel(self,
+                w: Dict[str, DiscreteField],
+                dx: ndarray) -> ndarray:
+        if self.form is None:
+            raise Exception("Form function handle not defined.")
+        return (self.form(w) * dx).sum(-1)
+
+    def elemental(self,
+                  v: Basis,
+                  **kwargs) -> ndarray:
+        w = FormDict({**v.default_parameters(), **self.dictify(kwargs)})
+        return self._kernel(w, v.dx)
+
+    def assemble(self,
+                 ubasis: Basis,
+                 vbasis: Optional[Basis] = None,
+                 **kwargs) -> float:
+        assert vbasis is None
+        vbasis = ubasis
+        return self.elemental(vbasis, **kwargs).sum(-1)
