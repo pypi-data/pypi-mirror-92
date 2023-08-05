5670d7e5a73eaf1b213d5cf89a8715ad5378e08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:33 +0000

    Add package PythonExtensions/1.0.30/PythonExtensions-1.0.30.tar.gz

---
+++ packages/PythonExtensions/PythonExtensions-1.0.30.tar.gz/PythonExtensions-1.0.30/Tests/base.py
@@ -0,0 +1,48 @@
+from PythonExtensions.Json import *
+from PythonExtensions.Logging import *
+
+from PythonExtensions.debug import *
+
+
+
+
+def Test_base():
+    start = Point.Create(0, 0)
+    end = Point.Create(150, 150)
+    pic_pos = Point.Create(10, 10)
+    img_size = Size.Create(200, 200)
+
+    print(CropBox.BoxSize(start, end, pic_pos, img_size))
+
+
+
+def TestLogging():
+    class Test(object): pass
+
+
+
+    class Other(object): pass
+
+
+
+    m = LoggingManager.FromTypes(Test, Other, app_name='app', root_path='.')
+
+    PrettyPrint(m.paths.Test)
+    PrettyPrint(m.paths.Test_errors)
+
+    PrettyPrint(m.paths.Other)
+    PrettyPrint(m.paths.Other_errors)
+
+    PrettyPrint('m.paths.logs', m.paths.logs)
+
+    Print(m.CreateLogger(Test(), debug=True))
+    Print(m.CreateLogger(Other(), debug=True))
+
+    try: m.CreateLogger(Other, debug=True)
+    except Exception as e: print_exception(e)
+
+
+
+if __name__ == '__main__':
+    Test_base()
+    TestLogging()
