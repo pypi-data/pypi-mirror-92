bf4f8aeda0f46c99a743328aa84fb2944b5422e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:58 +0000

    Add package builelib/1.0.6/builelib-1.0.6.tar.gz

---
+++ packages/builelib/builelib-1.0.6.tar.gz/builelib-1.0.6/builelib/elevetor.py
@@ -0,0 +1,164 @@
+import json
+import numpy as np
+import os
+
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import commons as bc
+
+def calc_energy(inputdata, DEBUG = False):
+
+
+    #----------------------------------------------------------------------------------
+    # 計算結果を格納する変数
+    #----------------------------------------------------------------------------------
+    resultJson = {
+        "E_elevetor": 0,
+        "Es_elevetor": 0,
+        "BEI_EV": 0,
+        "Elevators": {},
+        "for_CGS":{
+            "Edesign_MWh_day": np.zeros(365)
+        }
+    }
+
+    #----------------------------------------------------------------------------------
+    # 解説書 6.2 速度制御方式に応じて定められる係数
+    #----------------------------------------------------------------------------------
+
+    for room_name in inputdata["Elevators"]:
+        for unit_id, unit_configure in enumerate(inputdata["Elevators"][room_name]["Elevator"]):
+            
+            if unit_configure["ControlType"] ==  "交流帰還制御":
+                inputdata["Elevators"][room_name]["Elevator"][unit_id]["ControlTypeCoefficient"] = 1/20
+
+            elif unit_configure["ControlType"] ==  "VVVF(電力回生なし)" or unit_configure["ControlType"] ==  "VVVF（電力回生なし）":
+                inputdata["Elevators"][room_name]["Elevator"][unit_id]["ControlTypeCoefficient"] = 1/40
+
+            elif unit_configure["ControlType"] ==  "VVVF(電力回生あり)" or unit_configure["ControlType"] ==  "VVVF（電力回生あり）":
+                inputdata["Elevators"][room_name]["Elevator"][unit_id]["ControlTypeCoefficient"] = 1/45
+
+            elif unit_configure["ControlType"] ==  "VVVF(電力回生なし、ギアレス)" or unit_configure["ControlType"] ==  "VVVF（電力回生なし、ギアレス）":
+                inputdata["Elevators"][room_name]["Elevator"][unit_id]["ControlTypeCoefficient"] = 1/45
+
+            elif unit_configure["ControlType"] ==  "VVVF(電力回生あり、ギアレス)" or unit_configure["ControlType"] ==  "VVVF（電力回生あり、ギアレス）":
+                inputdata["Elevators"][room_name]["Elevator"][unit_id]["ControlTypeCoefficient"] = 1/50
+            
+            else:
+                raise Exception("速度制御方式 が不正です。")
+
+    #----------------------------------------------------------------------------------
+    # 解説書 6.3 昇降機系統に属する昇降機1台あたりの年間電力消費量
+    #----------------------------------------------------------------------------------
+
+    for room_name in inputdata["Elevators"]:
+
+        # 建物用途、室用途、室面積の取得
+        buildingType = inputdata["Rooms"][room_name]["buildingType"]
+        roomType     = inputdata["Rooms"][room_name]["roomType"]
+
+        # 年間照明点灯時間 [時間] 
+        if buildingType == "共同住宅":
+            inputdata["Elevators"][room_name]["operation_time"] = 5480
+        else:
+            inputdata["Elevators"][room_name]["operation_time"] = bc.RoomUsageSchedule[buildingType][roomType]["年間照明点灯時間"]
+        
+        if DEBUG:
+            print(f'室 {room_name} に設置された昇降機')
+            print(f'  - 昇降機運転時間 {inputdata["Elevators"][room_name]["operation_time"]}')
+
+        # 時刻別スケジュール
+        if buildingType == "共同住宅":
+            inputdata["Elevators"][room_name]["operation_schedule_hourly"] = 5480/8760 * np.ones((365,24))
+        else:
+            inputdata["Elevators"][room_name]["operation_schedule_hourly"] = bc.get_dailyOpeSchedule_lighting(buildingType, roomType)
+
+
+    # エネルギー消費量計算 [kWh/年]
+    Edesign_MWh_hour = np.zeros((365,24))
+    for room_name in inputdata["Elevators"]:
+        for unit_id, unit_configure in enumerate(inputdata["Elevators"][room_name]["Elevator"]):
+
+            inputdata["Elevators"][room_name]["Elevator"][unit_id]["energy_consumption"] = \
+                unit_configure["Number"] * \
+                unit_configure["Velocity"] * unit_configure["LoadLimit"] * unit_configure["ControlTypeCoefficient"] * \
+                inputdata["Elevators"][room_name]["operation_time"] / 860 
+
+            if DEBUG:
+                print(f'室 {room_name} に設置された {unit_id+1} 台目の昇降機')
+                print(f'　- 台数  {unit_configure["Number"]}')
+                print(f'　- 速度  {unit_configure["Velocity"]}')
+                print(f'　- 積載量  {unit_configure["LoadLimit"]}')
+                print(f'　- 速度制御方式による係数  {unit_configure["ControlTypeCoefficient"]}')
+                print(f'　- エネルギー消費量 kWh/年 {inputdata["Elevators"][room_name]["Elevator"][unit_id]["energy_consumption"]}')
+
+            # 時刻別エネルギー消費量 [MWh]
+            Edesign_MWh_hour += \
+                unit_configure["Number"] * \
+                unit_configure["Velocity"] * unit_configure["LoadLimit"] * unit_configure["ControlTypeCoefficient"] * \
+                inputdata["Elevators"][room_name]["operation_schedule_hourly"] / 860 /1000
+
+
+    #----------------------------------------------------------------------------------
+    # 解説書 6.4 昇降機の設計一次エネルギー消費量
+    #----------------------------------------------------------------------------------
+
+    # 設計一次エネルギー消費量計算 [MJ/年]
+    for room_name in inputdata["Elevators"]:
+        for unit_id, unit_configure in enumerate(inputdata["Elevators"][room_name]["Elevator"]):
+
+            resultJson["E_elevetor"] += unit_configure["energy_consumption"] * 9760 / 1000
+
+    if DEBUG:
+        print(f'昇降機の設計一次エネルギー消費量  {resultJson["E_elevetor"]}  MJ/年')
+
+
+    #----------------------------------------------------------------------------------
+    # 解説書 10.5 昇降機の基準一次エネルギー消費量
+    #----------------------------------------------------------------------------------
+
+    # エネルギー消費量計算 [kWh/年]
+    for room_name in inputdata["Elevators"]:
+        for unit_id, unit_configure in enumerate(inputdata["Elevators"][room_name]["Elevator"]):
+
+            inputdata["Elevators"][room_name]["Elevator"][unit_id]["Es"] = \
+                unit_configure["Number"] * \
+                unit_configure["Velocity"] * unit_configure["LoadLimit"] * (1/40) * \
+                unit_configure["TransportCapacityFactor"] * \
+                inputdata["Elevators"][room_name]["operation_time"] / 860 
+
+            # 基準一次エネルギー消費量計算 [MJ/年]
+            resultJson["Es_elevetor"] += inputdata["Elevators"][room_name]["Elevator"][unit_id]["Es"] * 9760 / 1000
+
+    if DEBUG:
+        print(f'昇降機の基準一次エネルギー消費量  {resultJson["Es_elevetor"]}  MJ/年')
+
+
+    # 入力データも出力
+    resultJson["Elevetors"] = inputdata["Elevators"]
+
+    # BEI/Vの計算
+    resultJson["BEI_EV"] = resultJson["E_elevetor"] / resultJson["Es_elevetor"]
+
+    # 日積算値
+    resultJson["for_CGS"]["Edesign_MWh_day"] = np.sum(Edesign_MWh_hour,1)
+
+    return resultJson
+
+
+if __name__ == '__main__':
+
+    print('----- elevetor.py -----')
+    # filename = './sample/sample01_WEBPRO_inputSheet_for_Ver2.5.json'
+    filename = './tests/cogeneration/Case_hospital_00.json'
+
+    # 入力ファイルの読み込み
+    with open(filename, 'r') as f:
+        inputdata = json.load(f)
+
+    resultJson = calc_energy(inputdata, DEBUG = True)
+
+    with open("resultJson_EV.json",'w') as fw:
+        json.dump(resultJson, fw, indent=4, ensure_ascii=False, cls = bc.MyEncoder)
+
---
+++ packages/builelib/builelib-1.0.6.tar.gz/builelib-1.0.6/builelib/lighting.py
@@ -0,0 +1,183 @@
+import json
+import numpy as np
+import os
+
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import commons as bc
+
+# データベースファイルの保存場所
+database_directory =  os.path.dirname(os.path.abspath(__file__)) + "/database/"
+
+
+def set_roomIndexCoeff(roomIndex):
+    '''
+    室の形状に応じて定められる係数（仕様書4.4）
+    '''
+
+    if roomIndex == None:
+        roomIndexCoeff = 1
+    else:
+        if roomIndex < 0:
+            roomIndexCoeff = 1
+        elif roomIndex < 0.75:
+            roomIndexCoeff = 0.50
+        elif roomIndex < 0.95:
+            roomIndexCoeff = 0.60
+        elif roomIndex < 1.25:
+            roomIndexCoeff = 0.70
+        elif roomIndex < 1.75:
+            roomIndexCoeff = 0.80
+        elif roomIndex < 2.50:
+            roomIndexCoeff = 0.90
+        elif roomIndex >= 2.50:
+            roomIndexCoeff = 1.00
+    
+    return roomIndexCoeff
+
+
+def calc_energy(inputdata, DEBUG = False):
+
+    # データベースjsonの読み込み
+    with open( database_directory + 'lightingControl.json', 'r') as f:
+        lightingCtrl = json.load(f)
+
+    # 計算結果を格納する変数
+    resultJson = {
+        "E_lighting": None,
+        "Es_lighting": None,
+        "BEI_L": None,
+        "E_lighting_hourly": None,
+        "lighting":{
+        },
+        "for_CGS":{
+            "Edesign_MWh_day": np.zeros(365)
+        }
+    }
+
+    # 変数初期化
+    E_lighting = 0    # 設計一次エネルギー消費量 [GJ]
+    E_lighting_hourly = np.zeros((365,24))  # 設計一次エネルギー消費量（時刻別） [GJ]
+    Es_lighting = 0   # 基準一次エネルギー消費量 [GJ]
+
+
+    # 室毎（照明系統毎）のループ
+    for ikey, isys in inputdata["LightingSystems"].items():
+
+        # 建物用途、室用途、室面積の取得
+        buildingType = inputdata["Rooms"][ikey]["buildingType"]
+        roomType     = inputdata["Rooms"][ikey]["roomType"]
+        roomArea     = inputdata["Rooms"][ikey]["roomArea"]
+
+        # 年間照明点灯時間 [時間] ← 計算には使用しない。
+        opeTime = bc.RoomUsageSchedule[buildingType][roomType]["年間照明点灯時間"]
+        # 時刻別スケジュールの読み込み
+        opePattern_hourly_light = bc.get_dailyOpeSchedule_lighting(buildingType, roomType)
+
+        ## 室の形状に応じて定められる係数（仕様書4.4）
+        # 室指数
+        if isys["roomIndex"] != None:
+            roomIndex = isys["roomIndex"]
+        elif isys["roomWidth"] != None and isys["roomDepth"] != None and isys["unitHeight"] != None:
+            if isys["roomWidth"] > 0 and isys["roomDepth"] > 0 and isys["unitHeight"] > 0:
+                roomIndex = (isys["roomWidth"] * isys["roomDepth"]) / ( (isys["roomWidth"] + isys["roomDepth"]) * isys["unitHeight"] )
+            else:
+                roomIndex = None
+        else:
+            roomIndex = None
+        
+        # 補正係数
+        roomIndexCoeff = set_roomIndexCoeff(roomIndex)
+
+        ## 器具毎のループ
+        unitPower = 0
+        for _, iunit in isys["lightingUnit"].items():
+        
+            # 室指数による補正
+            rmIx = 1
+
+            # 制御による効果
+            ctrl = (
+                lightingCtrl["OccupantSensingCTRL"][iunit["OccupantSensingCTRL"]] *
+                lightingCtrl["IlluminanceSensingCTRL"][iunit["IlluminanceSensingCTRL"]] *
+                lightingCtrl["TimeScheduleCTRL"][iunit["TimeScheduleCTRL"]] *
+                lightingCtrl["InitialIlluminationCorrectionCTRL"][iunit["InitialIlluminationCorrectionCTRL"]]
+            )
+
+            # 照明器具の消費電力（制御込み） [W]
+            unitPower += iunit["RatedPower"] * iunit["Number"] * ctrl
+
+
+        # 時刻別の設計一次エネルギー消費量 [MJ]
+        E_room_hourly = opePattern_hourly_light * unitPower * roomIndexCoeff * bc.fprime * 10**(-6)
+        # 各室の年間エネルギー消費量 [MJ]
+        E_room = E_room_hourly.sum()
+
+        # 出力用に積算
+        E_lighting += E_room  
+        E_lighting_hourly += E_room_hourly
+
+        # 床面積あたりの設計一次エネルギー消費量 [MJ/m2]
+        if roomArea <= 0:
+            PrimaryEnergyPerArea = None
+        else:
+            PrimaryEnergyPerArea = E_room / roomArea
+
+
+        # 基準一次エネルギー消費量 [MJ]
+        Es_room = bc.RoomStandardValue[buildingType][roomType]["照明"] * roomArea
+        Es_lighting += Es_room  # 出力用に積算
+
+
+        # 各室の計算結果を格納
+        resultJson["lighting"][ikey] = {
+                "buildingType": buildingType,
+                "roomType": roomType,
+                "roomArea": roomArea,
+                "opelationTime": opeTime,
+                "roomIndex": roomIndex,
+                "roomIndexCoeff": roomIndexCoeff,
+                "unitPower": unitPower,
+                "PrimaryEnergy": E_room,
+                "PrimaryEnergyPerArea": PrimaryEnergyPerArea,
+                "StandardEnergy": Es_room
+            }
+
+        if DEBUG:
+            print( f'室名称　{ikey}')
+            print( f'　- 設計一次エネルギー消費量  {E_room} MJ')
+            print( f'　- 基準一次エネルギー消費量  {Es_room} MJ')
+            
+    # BEI/L [-]
+    if Es_lighting <= 0:
+        BEI_L = None
+    else:
+        BEI_L = E_lighting / Es_lighting
+        
+    # 建物全体の計算結果
+    resultJson["E_lighting"] = E_lighting
+    resultJson["Es_lighting"] = Es_lighting
+    resultJson["BEI_L"] = BEI_L
+    resultJson["E_lighting_hourly"] = E_lighting_hourly
+
+    # 日積算値
+    resultJson["for_CGS"]["Edesign_MWh_day"] = np.sum(E_lighting_hourly/9760,1)
+
+    return resultJson
+
+
+if __name__ == '__main__':
+
+    print('----- lighting.py -----')
+    filename = './sample/sample03_WEBPRO_inputSheet_for_Ver3.0.json'
+    # filename = './tests/cogeneration/Case_hotel_00.json'
+
+    # テンプレートjsonの読み込み
+    with open(filename, 'r') as f:
+        inputdata = json.load(f)
+
+    resultJson = calc_energy(inputdata, DEBUG = True)
+
+    with open("resultJson_L.json",'w') as fw:
+        json.dump(resultJson, fw, indent=4, ensure_ascii=False, cls = bc.MyEncoder)
