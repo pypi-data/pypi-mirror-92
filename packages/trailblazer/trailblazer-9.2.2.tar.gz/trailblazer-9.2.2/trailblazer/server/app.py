0bc3730531afa8745f50b0ff9983cadcf6cee344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:53 +0000

    Add package trailblazer/9.2.2/trailblazer-9.2.2.tar.gz

---
+++ packages/trailblazer/trailblazer-9.2.2.tar.gz/trailblazer-9.2.2/trailblazer/server/app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+
+import coloredlogs
+from flask import Flask
+from flask_cors import CORS
+from flask_reverse_proxy import FlaskReverseProxied
+
+from trailblazer.server import api, ext
+
+coloredlogs.install(level="INFO")
+app = Flask(__name__)
+
+SECRET_KEY = "unsafe!!!"
+TEMPLATES_AUTO_RELOAD = True
+SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+SQLALCHEMY_POOL_RECYCLE = 7200
+SQLALCHEMY_TRACK_MODIFICATIONS = "FLASK_DEBUG" in os.environ
+
+app.config.from_object(__name__)
+
+# register blueprints
+app.register_blueprint(api.blueprint)
+
+# configure extensions
+FlaskReverseProxied(app)
+ext.store.init_app(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+
+
+@app.route("/")
+def index():
+    return "Welcome to Trailblazer REST API"
