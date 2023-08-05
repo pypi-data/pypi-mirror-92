9cf8ef5d24b1f2541b816277435a2478c16941d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:44 +0000

    Add package dpt-runtime/1.0.4/dpt_runtime-1.0.4-py3-none-any.whl

---
+++ packages/dpt-runtime/dpt_runtime-1.0.4-py3-none-any.whl/dpt_runtime/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+direct Python Toolbox
+All-in-one toolbox to encapsulate Python runtime variants
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?dpt;runtime
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;mpl2
+----------------------------------------------------------------------------
+v1.0.4
+dpt_runtime/__init__.py
+"""
+
+__all__ = [ "binary",
+            "byte_buffer",
+            "charset",
+            "descriptor_selector",
+            "environment",
+            "exception_log_trap",
+            "file_like_copy_mixin",
+            "input_filter",
+            "io_exception",
+            "iterator",
+            "not_implemented_class",
+            "not_implemented_exception",
+            "operation_not_supported_exception",
+            "socket_reader",
+            "stacked_dict",
+            "supports_mixin",
+            "traced_exception",
+            "type_exception",
+            "value_exception"
+          ]
