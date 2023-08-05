ec885e9d1f76cf1a5d9cdacc9d1dd67cda2675fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package tfds-nightly/4.2.0.dev202101190107/tfds_nightly-4.2.0.dev202101190107-py3-none-any.whl

---
+++ packages/tfds-nightly/tfds_nightly-4.2.0.dev202101190107-py3-none-any.whl/tensorflow_datasets/core/load_test.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# Copyright 2021 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for tensorflow_datasets.core.load.
+
+Note: `load.py` code was previously in `registered.py`, so some of the tests
+are still on `registered_test.py`.
+"""
+
+
+def test_dummy():
+  assert True  # Pytest raise error if no tests are detected
