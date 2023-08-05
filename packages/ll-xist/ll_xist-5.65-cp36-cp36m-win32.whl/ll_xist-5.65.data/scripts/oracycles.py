caabf7aa98382d30882b8c1fb7488f0f86b94d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package ll-xist/5.65/ll_xist-5.65-cp36-cp36m-win32.whl

---
+++ packages/ll-xist/ll_xist-5.65-cp36-cp36m-win32.whl/ll_xist-5.65.data/scripts/oracycles.py
@@ -0,0 +1,11 @@
+#!C:\Python36_32\python.exe
+# -*- coding: utf-8 -*-
+# cython: language_level=3, always_allow_keywords=True
+
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.orasql.scripts import oracycles
+	sys.exit(oracycles.main())
