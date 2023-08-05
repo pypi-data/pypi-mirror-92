2bdcf652538a9408c1fdd40756825e275068106f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:36 +0000

    Add package edc-registration/0.2.50/edc_registration-0.2.50-py3-none-any.whl

---
+++ packages/edc-registration/edc_registration-0.2.50-py3-none-any.whl/edc_registration/admin_site.py
@@ -0,0 +1,12 @@
+from django.contrib.admin import AdminSite
+
+
+class EdcRegistrationAdminSite(AdminSite):
+    site_header = "Registration"
+    site_title = "Registration"
+    index_title = "Registration Administration"
+    site_url = "/administration/"
+
+
+edc_registration_admin = EdcRegistrationAdminSite(name="edc_registration_admin")
+edc_registration_admin.disable_action("delete_selected")
