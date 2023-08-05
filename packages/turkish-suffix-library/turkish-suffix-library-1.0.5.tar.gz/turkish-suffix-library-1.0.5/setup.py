afdc05de6539894edd6a8ed982f0058f0dabc0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:46 +0000

    Add package turkish-suffix-library/1.0.5/turkish-suffix-library-1.0.5.tar.gz

---
+++ packages/turkish-suffix-library/turkish-suffix-library-1.0.5.tar.gz/turkish-suffix-library-1.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="turkish-suffix-library",
+    version="1.0.5",
+    author="Cem Yildiz",
+    author_email="cem.yildiz@ya.ru",
+    description="Turkish suffix library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/miklagard/Turkish-Suffix-Library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
