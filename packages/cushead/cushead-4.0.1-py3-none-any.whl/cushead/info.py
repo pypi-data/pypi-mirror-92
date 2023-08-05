6ef48ced2aa9124ff4d4dbe89b21baadb0733ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:01 +0000

    Add package cushead/4.0.1/cushead-4.0.1-py3-none-any.whl

---
+++ packages/cushead/cushead-4.0.1-py3-none-any.whl/cushead/info.py
@@ -0,0 +1,21 @@
+"""
+Store package information.
+"""
+PACKAGE_NAME = "cushead"
+PACKAGE_VERSION = "4.0.1"
+PYTHON_MIN_VERSION = (3, 8)
+SOURCE = f"https://github.com/lucasvazq/{PACKAGE_NAME}"
+DOCUMENTATION = f"https://github.com/lucasvazq/{PACKAGE_NAME}/blob/master/README.md"
+REQUIRED_PACKAGES = (
+    "colorama==0.4.4",
+    "Jinja2==2.11.2",
+    "Pillow==8.1.0",
+    "python-resize-image==1.1.19",
+    "schema==0.7.3",
+)
+AUTHOR = "Lucas Vazquez"
+EMAIL = "lucas5zvazquez@gmail.com"
+DESCRIPTION = "CLI that help you to improve the SEO and UX of your websites."
+PACKAGE_LICENSE = "MIT"
+KEYWORDS = "SEO, meta-tags, UX, PWA, front-end, favicon, manifest, robots, browserconfig, sitemap, opensearch"
+AUTHOR_PAGE = "https://github.com/lucasvazq"
