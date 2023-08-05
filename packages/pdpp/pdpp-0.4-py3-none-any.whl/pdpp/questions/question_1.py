c3effadd2b3f30c307bf54cfecce9959a6f0c5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package pdpp/0.4/pdpp-0.4-py3-none-any.whl

---
+++ packages/pdpp/pdpp-0.4-py3-none-any.whl/pdpp/questions/question_1.py
@@ -0,0 +1,53 @@
+from questionary import prompt, Choice
+from click import clear as click_clear
+from pdpp.styles.prompt_style import custom_style_fancy 
+from pdpp.tasks.base_task import BaseTask
+from typing import List
+
+
+def q1(dep_tasks: List[BaseTask], task: BaseTask) -> List[BaseTask]:
+    """
+    This question is used to determine which other tasks in the project structure are dependencies of the current task. 
+    """
+
+    click_clear()
+
+    choice_list = []
+
+    '''
+    First, add all the project subdirectories (riggable_subdirectories) returned from Question 0. 
+    When this process encounters the task being rigged (target_dir), add it to the list as a disabled entry. 
+    '''
+
+    for dep_task in dep_tasks:
+        if dep_task.target_dir == task.target_dir:
+            choice_list.append(
+                Choice(
+                    title=dep_task.target_dir,
+                    value=dep_task,
+                    disabled='This is the selected task'
+                )
+            )
+
+        else:
+            choice_list.append(
+                Choice(
+                    title=dep_task.target_dir,
+                    value=dep_task,
+                    checked=dep_task.target_dir in task.dep_files
+                )
+            )
+            
+    if len(choice_list) < 1:
+        return []
+
+    questions_1 = [
+        {
+            'type': 'checkbox',
+            'message': 'Select tasks which contain dependencies for "{}"'.format(task.target_dir),
+            'name': 'dep_tasks',
+            'choices': choice_list,
+        }
+    ]
+
+    return prompt(questions_1, style=custom_style_fancy)['dep_tasks']
---
+++ packages/pdpp/pdpp-0.4-py3-none-any.whl/pdpp/utils/yaml_task.py
@@ -0,0 +1,15 @@
+import yaml
+
+def dump_self(task):
+    with open(type(task).FILENAME, 'w') as stream:
+        yaml.dump(task, stream, default_flow_style=False)
+
+
+def load_task(task_path):
+    from pdpp.tasks.custom_task import CustomTask
+    from pdpp.tasks.export_task import ExportTask
+    from pdpp.tasks.import_task import ImportTask
+    from pdpp.tasks.standard_task import StandardTask
+    from pdpp.tasks.sub_task import SubTask
+    with open(task_path, 'r') as stream:
+        return yaml.load(stream, Loader=yaml.FullLoader)
