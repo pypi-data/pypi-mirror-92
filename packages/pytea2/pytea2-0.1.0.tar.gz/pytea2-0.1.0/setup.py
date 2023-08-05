693d90384b0bf8caa6fc9a1b276b55318b371978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package pytea2/0.1.0/pytea2-0.1.0.tar.gz

---
+++ packages/pytea2/pytea2-0.1.0.tar.gz/pytea2-0.1.0/setup.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from collections import defaultdict
+
+from setuptools import Extension, setup, find_packages
+from setuptools.command.build_ext import build_ext
+from Cython.Build import cythonize
+
+BUILD_ARGS = defaultdict(lambda: ['-O3', '-g0'])
+
+for compiler, args in [
+    ('msvc', ['/EHsc', '/DHUNSPELL_STATIC', r"\utf-8"]),
+    ('gcc', ['-O3', '-g0'])]:
+    BUILD_ARGS[compiler] = args
+
+
+class build_ext_compiler_check(build_ext):
+    def build_extensions(self):
+        compiler = self.compiler.compiler_type
+        args = BUILD_ARGS[compiler]
+        for ext in self.extensions:
+            ext.extra_compile_args = args
+        super().build_extensions()
+
+
+ext_modules = [
+    Extension("pytea.tea",
+              sources=["pytea/tea.pyx", "pytea/src/tea.c"], library_dirs=["pytea/src", "pytea"])
+]
+
+
+def get_version() -> str:
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "pytea", "__init__.py")
+    with open(path, "r", encoding="utf-8") as f:
+        data = f.read()
+    result = re.findall(r"(?<=__version__ = \")\S+(?=\")", data)
+    return result[0]
+
+
+def get_dis():
+    with open("README.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+packages = find_packages(exclude=('test', 'tests.*', "test*"))
+
+
+def main():
+    version: str = get_version()
+
+    dis = get_dis()
+    setup(
+        name="pytea2",
+        version=version,
+        url="https://github.com/synodriver/pytea",
+        packages=packages,
+        keywords=["tea", "encrypt", "decrypt"],
+        description="tea encrypt and decrypt",
+        long_description_content_type="text/markdown",
+        long_description=dis,
+        author="synodriver",
+        author_email="diguohuangjiajinweijun@gmail.com",
+        python_requires=">=3.6",
+        install_requires=["cython"],
+        license='GPLv3',
+        ext_modules=cythonize(ext_modules),
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Operating System :: OS Independent",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Programming Language :: C",
+            "Programming Language :: Cython",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: Implementation :: CPython"
+        ],
+        include_package_data=True
+    )
+
+
+if __name__ == "__main__":
+    main()
