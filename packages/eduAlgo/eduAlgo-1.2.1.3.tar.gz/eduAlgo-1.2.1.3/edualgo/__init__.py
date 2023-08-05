8282d5b6d57c2835fd7044e2c20be8826f51305b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package eduAlgo/1.2.1.3/eduAlgo-1.2.1.3.tar.gz

---
+++ packages/eduAlgo/eduAlgo-1.2.1.3.tar.gz/eduAlgo-1.2.1.3/edualgo/__init__.py
@@ -0,0 +1,21 @@
+def print_msg_box(msg, indent=1, width=None, title=None):
+    """Print message-box with optional title."""
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    if title:
+        box += f'║{space}{title:<{width}}{space}║\n'  # title
+        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
+    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
+    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
+    print(box)
+
+def file_output(msg,file_name):
+    # opening a file, if it doen't exist then creating the file
+    file = open(file_name, 'w')
+    # writing a string to the "edualgo.txt" file
+    file.write(msg)
+    # closing the opened file
+    file.close()
