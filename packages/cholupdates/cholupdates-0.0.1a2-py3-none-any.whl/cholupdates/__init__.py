ac18dee427caef87e210eb3afb66c56ee910ecd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package cholupdates/0.0.1a2/cholupdates-0.0.1a2-py3-none-any.whl

---
+++ packages/cholupdates/cholupdates-0.0.1a2-py3-none-any.whl/cholupdates/__init__.py
@@ -0,0 +1,7 @@
+""" This package provides algorithms which efficiently compute the Cholesky
+factorization of low-rank up-/downdates to a matrix with a known Cholesky factorization.
+"""
+
+from . import rank_1
+
+__version__ = "0.0.1a2"
