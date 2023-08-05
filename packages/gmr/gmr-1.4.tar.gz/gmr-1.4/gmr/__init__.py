687faea631f27f9731a1dc7756d0d41d832d42a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:06 +0000

    Add package gmr/1.4/gmr-1.4.tar.gz

---
+++ packages/gmr/gmr-1.4.tar.gz/gmr-1.4/gmr/__init__.py
@@ -0,0 +1,16 @@
+"""
+gmr
+===
+
+Gaussian Mixture Models (GMMs) for clustering and regression in Python.
+"""
+
+__version__ = "1.4"
+
+from . import gmm, mvn, utils
+
+__all__ = ['gmm', 'mvn', 'utils']
+
+from .mvn import MVN, plot_error_ellipse
+from .gmm import (GMM, plot_error_ellipses, kmeansplusplus_initialization,
+                  covariance_initialization)
---
+++ packages/gmr/gmr-1.4.tar.gz/gmr-1.4/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+# Author: Alexander Fabisch <afabisch@googlemail.com>
+# License: BSD 3 clause
+
+from setuptools import setup
+import gmr
+VERSION = gmr.__version__
+
+def setup_package():
+    setup(
+        name="gmr",
+        version=VERSION,
+        author="Alexander Fabisch",
+        author_email="afabisch@googlemail.com",
+        url="https://github.com/AlexanderFabisch/gmr",
+        description="Gaussian Mixture Regression",
+        long_description=open("README.rst").read(),
+        license="new BSD",
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+        ],
+        packages=["gmr"],
+        requires=["numpy", "scipy"],
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
