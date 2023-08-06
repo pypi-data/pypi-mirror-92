b55ed0cb76a1170f63872b08b596947ed80f17b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:45 +0000

    Add package phoenix-ml8/0.1/phoenix_ml8-0.1.tar.gz

---
+++ packages/phoenix-ml8/phoenix_ml8-0.1.tar.gz/phoenix_ml8-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='phoenix_ml8',
+      version='0.1',
+      description='Machine Learning FrameWork',
+      packages=['phoenix_ml8'],
+      zip_safe=False)
+
+
