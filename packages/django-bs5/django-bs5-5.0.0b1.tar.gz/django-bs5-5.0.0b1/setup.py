3331ac9d42bbe71271c222646da1312f79755d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:26 +0000

    Add package django-bs5/5.0.0b1/django-bs5-5.0.0b1.tar.gz

---
+++ packages/django-bs5/django-bs5-5.0.0b1.tar.gz/django-bs5-5.0.0b1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='django-bs5',
+    description='simple bootstrap5 support for django',
+    long_description=open('README.rst').read(),
+    version='5.0.0b1',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+)
