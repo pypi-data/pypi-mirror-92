89164deef4f37cbe715ae56e3d11727cf364923b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:13 +0000

    Add package anduin/4.0.2/anduin-4.0.2.tar.gz

---
+++ packages/anduin/anduin-4.0.2.tar.gz/anduin-4.0.2/anduin/__init__.py
@@ -0,0 +1,45 @@
+'''Anduin: A light python mysql connector.
+
+Copyright (c) 2020-2024 Campanula<421248329@qq.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.'''
+
+# from .server import Data
+from functools import wraps
+from .server import *
+
+__version__='4.0.2'
+
+def func_time(f):
+    """
+    简单记录执行时间
+    :param f:
+    :return:
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = f(*args, **kwargs)
+        end = time.time()
+        info = str(f.__name__) + ' took ' + str(end - start) + ' seconds '
+        print(info)
+        return result
+
+    return wrapper
