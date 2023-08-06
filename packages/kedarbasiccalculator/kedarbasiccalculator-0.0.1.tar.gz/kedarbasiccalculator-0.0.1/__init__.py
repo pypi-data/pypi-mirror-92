9f2af9077c1a33d8702b053b2bd0bb993132ed1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:04 +0000

    Add package kedarbasiccalculator/0.0.1/kedarbasiccalculator-0.0.1.tar.gz

---
+++ packages/kedarbasiccalculator/kedarbasiccalculator-0.0.1.tar.gz/kedarbasiccalculator-0.0.1/__init__.py
@@ -0,0 +1,13 @@
+
+def add_numbers(num1,num2)
+    return num1 + num2
+
+def sub_numbers(num1,num2)
+    return num1 - num2
+
+
+def multi_numbers(num1,num2)
+    return num1 * num2
+
+def div_numbers(num1,num2)
+    return num1 / num2
