ac660e8710b38b4ef548fc1611d7af2daa089528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:16 +0000

    Add package PyCPP/0.0.6/PyCPP-0.0.6.tar.gz

---
+++ packages/PyCPP/PyCPP-0.0.6.tar.gz/PyCPP-0.0.6/pycpp/__init__.py
@@ -0,0 +1,44 @@
+"""PyCPP is a simple tool for wrapping and using C++ functions and libraries."""
+import ctypes
+import os
+from shutil import copy
+try:
+  from . import FuncParser
+except ImportError:
+  import FuncParser
+
+
+def loadDLL(dll):
+  c_lib = ctypes.CDLL(os.getcwd() + "/" + dll + ".dll")
+  funcs = FuncParser.parse(os.getcwd() + "/" + '/'.join(dll.split("/")[:-1]) + "/src/" + dll.split("/")[-1] + ".cpp")
+  return c_lib, funcs
+
+
+def wrap(dll, out):
+  c_lib, funcs = loadDLL(dll)
+  code = []
+  for ff in funcs:
+    f_code = "def {{name}}({{*arg}}):\n\tc_lib.{{name}}.restype = {{res}}\n\treturn c_lib.cmult({{*arg_}})"
+    star_arg = ""
+    star_arg_ = ""
+    i = 0
+    f_code = f_code.replace("{{name}}", ff.name).replace("{{res}}", "ctypes." + str(ff.restype))
+    for key in ff.args:
+      star_arg += key + ", "
+      star_arg_ += "ctypes." + str(ff.args[key]) + "(" + key + "), "
+      i += 1
+    f_code = f_code.replace("{{*arg}}", star_arg[:-2]).replace("{{*arg_}}", star_arg_[:-2])
+    code.append(f_code)
+  if not os.path.exists(os.getcwd() + "/" + out + "/" + dll.split("/")[-1]):
+    os.makedirs(os.getcwd() + "/" + out + "/" + dll.split("/")[-1])
+  file = open(os.getcwd() + "/" +out + "/" + dll.split("/")[-1] + "/__init__.py", "w")
+  file.write("")
+  file.close()
+  file = open(os.getcwd() + "/" + out + "/" + dll.split("/")[-1] + "/__init__.py", "a")
+  file.write('import ctypes,os\nlib_name = os.path.realpath(__file__).replace("__init__.py", "dllmain.dll")\nc_lib = ctypes.CDLL(lib_name)\n')
+  for d in code:
+    file.write(d)
+    # exec(d, globals())
+  file.close()
+  copy(os.getcwd() + "/" + dll + ".dll", os.getcwd() + "/" +out + "/" + dll.split("/")[-1] + "/" + dll.split("/")[-1] + ".dll")
+  return code
---
+++ packages/PyCPP/PyCPP-0.0.6.tar.gz/PyCPP-0.0.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="PyCPP",
+  version="0.0.6",
+  author="Commander07",
+  author_email="author@example.com",
+  description="PyCPP is a simple tool for wrapping and using C++ functions and libraries.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/commander07/PyCPP",
+  packages=setuptools.find_packages(),
+  python_requires='>=3.6',
+  scripts=['bin/pycpp.py']
+)
