9813d397e06f1313480af4158a0a1b14505bceb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:50 +0000

    Add package genocide/23/genocide-23.tar.gz

---
+++ packages/genocide/genocide-23.tar.gz/genocide-23/genocide/ver.py
@@ -0,0 +1,8 @@
+# GENOCIDE - OTP-CR-117/19 | otp.informationdesk@icc-cpi.int
+#
+# this file is placed in the public domain
+
+__version__ = 22
+
+def ver(event):
+    event.reply("GENOCIDE %s | OTP-CR-117/19 | otp.informationdesk@icc-cpi.int | http://genocide.rtfd.io" % __version__)
