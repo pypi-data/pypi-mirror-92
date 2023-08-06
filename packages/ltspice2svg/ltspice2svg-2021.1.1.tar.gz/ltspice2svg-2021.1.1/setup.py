77be94492225b2bd2283a4bdc82575da2d64ae2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:21 +0000

    Add package ltspice2svg/2021.1.1/ltspice2svg-2021.1.1.tar.gz

---
+++ packages/ltspice2svg/ltspice2svg-2021.1.1.tar.gz/ltspice2svg-2021.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md",'r') as fh:
+    long_description=fh.read()
+
+setuptools.setup(
+  name = 'ltspice2svg',
+  packages = ['ltspice2svg'],
+  version = '2021.01.01',
+  license='MIT',
+  description = 'Converting LTspice file to SVG',
+  author = 'Harsh Agarwal',
+  author_email = 'harshvinay752@gmail.com',
+  url = 'https://github.com/harshvinay752',
+  download_url = 'https://github.com/harshvinay752/ltspice2svg/archive/2021.01.tar.gz',
+  keywords = ['LTspice','spice','SVG','vector','image','convert'],
+  install_requires=['svgwrite'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
