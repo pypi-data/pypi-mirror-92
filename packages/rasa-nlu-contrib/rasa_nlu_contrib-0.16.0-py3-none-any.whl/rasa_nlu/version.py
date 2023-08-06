34284a615215886a3b5f08d35043040cdb65a2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:10 +0000

    Add package rasa-nlu-contrib/0.16.0/rasa_nlu_contrib-0.16.0-py3-none-any.whl

---
+++ packages/rasa-nlu-contrib/rasa_nlu_contrib-0.16.0-py3-none-any.whl/rasa_nlu/version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# rasa_nlu.version
+'''
+:author:    madkote
+:contact:   madkote(at)bluewin.ch
+:copyright: Copyright 2021, madkote RES
+
+rasa_nlu.version
+----------------
+Version
+'''
+
+from __future__ import absolute_import
+
+VERSION = (0, 16, 0)
+
+__all__ = []
+__author__ = 'madkote <madkote(at)bluewin.ch>'
+__version__ = '.'.join(str(x) for x in VERSION)
+__copyright__ = 'Copyright 2021, madkote RES'
