1d2365465c1262b21484db6f34369562ece2838f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:23 +0000

    Add package tlp-classifier/0.1.5/tlp-classifier-0.1.5.tar.gz

---
+++ packages/tlp-classifier/tlp-classifier-0.1.5.tar.gz/tlp-classifier-0.1.5/tlp_classifier/__init__.py
@@ -0,0 +1,12 @@
+from .file_help import *
+from .complexity import *
+from .two_labels_classifier import *
+from .problem_set import *
+from .problem import *
+from .classifier import *
+from .generator import *
+from .algorithms import *
+from .api import *
+
+# Package version
+__version__ = '0.1.5'
