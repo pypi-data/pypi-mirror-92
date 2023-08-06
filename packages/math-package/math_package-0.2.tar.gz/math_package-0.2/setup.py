33a762f94031828a573bd7fd4dd7d6632d9e37e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:35 +0000

    Add package math-package/0.2/math_package-0.2.tar.gz

---
+++ packages/math-package/math_package-0.2.tar.gz/math_package-0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(name='math_package',
+                 version='0.2',
+                 description='Addition, subtraction, multiplication and division',
+                 url='https://gitlab.com/yooyaa741/math-package',
+                 author='yooyaa741',
+                 author_email='yooyaa741@gmail.com',
+                 license='MIT',
+                 packages=setuptools.find_packages('math_package'),
+                 zip_safe=False)
+
