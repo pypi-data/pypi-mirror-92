6a596d18f5d171cb33ef440ff2475f1abd3990c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:53 +0000

    Add package EasyTransformer/1.1.0/EasyTransformer-1.1.0.tar.gz

---
+++ packages/EasyTransformer/EasyTransformer-1.1.0.tar.gz/EasyTransformer-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='EasyTransformer',
+    version='1.1.0',
+    author='kevinpro',
+    author_email='3121416933@qq.com',
+    url='https://github.com/Ricardokevins/EasyTransformer',
+    description=('Simple implement of BERT and Transformer extracted from other repo'),
+    packages=['EasyTransformer'],
+    install_requires=['numpy','torch'],
+)
+#python setup.py sdist build
+#twine upload dist/*
