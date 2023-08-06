e78f098105465ed156b2121512bd9995aa2650c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:18 +0000

    Add package pyinfraformat/21.1.22/pyinfraformat-21.1.22-py3-none-any.whl

---
+++ packages/pyinfraformat/pyinfraformat-21.1.22-py3-none-any.whl/pyinfraformat/__init__.py
@@ -0,0 +1,21 @@
+# pylint: disable=wildcard-import
+"""pyinfraformat, Python library for Finnish infraformat files."""
+import logging
+
+from .core import *
+from .exceptions import *
+from .plots import *
+
+logger = logging.getLogger("pyinfraformat")
+
+if not logging.root.handlers:
+    # overwrite the first handler
+    # which is always on WARNING -level
+    logger.addHandler(logging.NullHandler())
+    handler = logging.StreamHandler()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+
+__version__ = "21.1.22"
+__all__ = ["Holes", "from_infraformat", "to_infraformat", "print_info"]
