12a9055680e235cf6dd0ec334f5478b089bb41f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:22 +0000

    Add package availabledatasource/1.0.0/availabledatasource-1.0.0.tar.gz

---
+++ packages/availabledatasource/availabledatasource-1.0.0.tar.gz/availabledatasource-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+setup(name='availabledatasource',
+version='1.0.0',
+description='Testing installation of Package',
+url='https://infygithub.ad.infosys.com/veena-devipalamuri/JupyterIntegration.git',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=find_packages(),
+
+zip_safe=False)
