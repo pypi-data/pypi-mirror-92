d00bd9d88b8b72ea9e53effa77c80f7501972246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:28 +0000

    Add package pros-cli/3.1.5a18/pros_cli-3.1.5a18-py3-none-any.whl

---
+++ packages/pros-cli/pros_cli-3.1.5a18-py3-none-any.whl/pros/cli/terminal.py
@@ -0,0 +1,82 @@
+import os
+import signal
+import time
+
+import click
+
+import pros.conductor as c
+import pros.serial.devices as devices
+import pros.serial.ports as ports
+from pros.common.utils import logger
+from .common import default_options, resolve_v5_port, resolve_cortex_port, pros_root
+
+
+@pros_root
+def terminal_cli():
+    pass
+
+
+@terminal_cli.command()
+@default_options
+@click.argument('port', default='default')
+@click.option('--backend', type=click.Choice(['share', 'solo']), default='solo',
+              help='Backend port of the terminal. See above for details')
+@click.option('--raw', is_flag=True, default=False,
+              help='Don\'t process the data.')
+@click.option('--hex', is_flag=True, default=False, help="Display data as hexadecimal values. Unaffected by --raw")
+@click.option('--ports', nargs=2, type=int, default=(None, None),
+              help='Specify 2 ports for the "share" backend. The default option deterministically selects ports '
+                   'based on the serial port name')
+@click.option('--banner/--no-banner', 'request_banner', default=True)
+def terminal(port: str, backend: str, **kwargs):
+    """
+    Open a terminal to a serial port
+
+    There are two possible backends for the terminal: "share" or "solo". In "share" mode, a server/bridge is created
+    so that multiple PROS processes (such as another terminal or flash command) may communicate with the device. In the
+    simpler solo mode, only one PROS process may communicate with the device. The default mode is "share", but "solo"
+    may be preferred when "share" doesn't perform adequately.
+
+    Note: share backend is not yet implemented.
+    """
+    from pros.serial.devices.vex.v5_user_device import V5UserDevice
+    from pros.serial.terminal import Terminal
+    if port == 'default':
+        project_path = c.Project.find_project(os.getcwd())
+        if project_path is None:
+            v5_port = resolve_v5_port(None, 'user', quiet=True)
+            cortex_port = resolve_cortex_port(None, quiet=True)
+            if ((v5_port is None) ^ (cortex_port is None)) or (v5_port is not None and v5_port == cortex_port):
+                port = v5_port or cortex_port
+            else:
+                raise click.UsageError('You must be in a PROS project directory to enable default port selecting')
+        else:
+            project = c.Project(project_path)
+            port = project.target
+
+    if port == 'v5':
+        port = None
+        port = resolve_v5_port(port, 'user')
+    elif port == 'cortex':
+        port = None
+        port = resolve_cortex_port(port)
+        kwargs['raw'] = True
+    if not port:
+        return -1
+
+    if backend == 'share':
+        ser = ports.SerialSharePort(port)
+    else:
+        ser = ports.DirectPort(port)
+    if kwargs.get('raw', False):
+        device = devices.RawStreamDevice(ser)
+    else:
+        device = V5UserDevice(ser)
+    term = Terminal(device, request_banner=kwargs.pop('request_banner', True))
+
+    signal.signal(signal.SIGINT, term.stop)
+    term.start()
+    while not term.alive.is_set():
+        time.sleep(0.005)
+    term.join()
+    logger(__name__).info('CLI Main Thread Dying')
---
+++ packages/pros-cli/pros_cli-3.1.5a18-py3-none-any.whl/pros/serial/devices/vex/__init__.py
@@ -0,0 +1,4 @@
+from .comm_error import VEXCommError
+from .cortex_device import CortexDevice, find_cortex_ports
+from .v5_device import V5Device, find_v5_ports
+from .vex_device import VEXDevice
