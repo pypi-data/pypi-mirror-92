17ca20053fa5bda71f97e5708aa1aa3abf2c57f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package pdfToJson/0.0.2/pdfToJson-0.0.2.tar.gz

---
+++ packages/pdfToJson/pdfToJson-0.0.2.tar.gz/pdfToJson-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+with open('README.md','r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name ='pdfToJson',
+    version ='0.0.2',
+    author ='xjw',
+    author_email = 'XiongJingWen_0629@163.com',
+    description = 'This is a package which converts pdf to json using GROBID',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages = setuptools.find_packages()
+)
