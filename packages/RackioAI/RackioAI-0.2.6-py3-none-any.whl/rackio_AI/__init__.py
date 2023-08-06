d326f3a40198213d91f5e7563a65a11f66348e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:15 +0000

    Add package RackioAI/0.2.6/RackioAI-0.2.6-py3-none-any.whl

---
+++ packages/RackioAI/RackioAI-0.2.6-py3-none-any.whl/rackio_AI/__init__.py
@@ -0,0 +1,14 @@
+from .api import *
+from .managers import *
+from .models import *
+from .preprocessing import *
+from .data_analysis import *
+from .readers import *
+from ._singleton import Singleton
+from .core import *
+from .data import *
+from .pipeline import *
+from .utils import *
+
+
+RackioAI = RackioAI()
