a5587fb870d9dfd61d477f4194542cbdf8d6559f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:36 +0000

    Add package searcHPV/1.0.0a0/searcHPV-1.0.0a0-py3.7.egg

---
+++ packages/searcHPV/searcHPV-1.0.0a0-py3.7.egg/searcHPV/hpv_fusion.py
@@ -0,0 +1,23 @@
+import subprocess
+import os
+from searcHPV.generate_call_fusion_virus import *
+from searcHPV.selection_contig_call_virus_insertion import *
+
+##############
+#Fuction: run pipeline for hpv fusion points calling
+#humRef: human reference genome
+#virRef: virus reference genome
+#out_dir: output directory for seacHPV
+def hpv_fusion(humRef,virRef,out_dir):
+    script_mapHg = mapToHgRef(out_dir,humRef)
+    script_mapVir = mapToVirRef(out_dir,virRef)
+    os.system(f'chmod +x {script_mapHg}')
+    subprocess.call(script_mapHg)
+    os.system(f'chmod +x {script_mapVir}')
+    subprocess.call(script_mapVir)
+
+    count_sr(out_dir)
+    contigDic = read_mapping_info(out_dir)
+    combinedContigDic = cal_hpv_ins(contigDic,out_dir)
+    selectedAllContig = select_contig(combinedContigDic)
+    write_to_file(selectedAllContig,out_dir)
