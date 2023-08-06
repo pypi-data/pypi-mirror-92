f2653448f3730817a1bcfab4412985c7d0136a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:36 +0000

    Add package rdf-fingerprinter/0.2.9/rdf_fingerprinter-0.2.9-py3-none-any.whl

---
+++ packages/rdf-fingerprinter/rdf_fingerprinter-0.2.9-py3-none-any.whl/fingerprint/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# __init__.py
+# Date:  15/08/2019
+# Author: Eugeniu Costetchi
+# Email: costezki.eugen@gmail.com
+
+import logging.config
+
+__docformat__ = "restructuredtext en"
+
+__version__ = "0.2.9"
+__date__ = "2021-01-21"
+
+logging.config.fileConfig('logging.conf')
---
+++ packages/rdf-fingerprinter/rdf_fingerprinter-0.2.9-py3-none-any.whl/fingerprint/service_layer/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# __init__.py
+# Date:  15/10/2020
+# Author: Eugeniu Costetchi
+# Email: costezki.eugen@gmail.com 
+
+""" """
