b2c02c7dac3ec22736fb02cca924caafe40c716b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:18 +0000

    Add package erikunicamp-myutils/0.0.31/erikunicamp-myutils-0.0.31.tar.gz

---
+++ packages/erikunicamp-myutils/erikunicamp-myutils-0.0.31.tar.gz/erikunicamp-myutils-0.0.31/myutils/graph.py
@@ -0,0 +1,60 @@
+import os
+import numpy as np
+import igraph
+from myutils import geo
+
+R = 6371000
+
+##########################################################
+def add_lengths(g, x, y):
+    """Add length to the edges, assuming each vertex has a location"""
+
+    for i, e in enumerate(g.es()):
+        lon1, lat1 = g.vs[e.source][x], g.vs[e.source][y]
+        lon2, lat2 = g.vs[e.target][x], g.vs[e.target][y]
+        g.es[i]['length'] = geo.haversine([lon1, lat1], [lon2, lat2])
+    return g
+
+##########################################################
+def simplify_graphml(graphpath, directed=True, simplify=True):
+    """Get largest connected component from @graphatph and add weights
+    According to the params @undirected, @simplify, convert to
+    undirected and/or remove multiple edges and self-loops.
+    If the original graph has x,y attributes, we also compute the length"""
+
+    g = igraph.Graph.Read(graphpath)
+    if simplify: g.simplify(combine_edges='first')
+    if not directed: g.to_undirected()
+    g = g.components(mode='weak').giant()
+
+    if ('x' in g.vertex_attributes()) or ('lon' in g.vertex_attributes()):
+        if 'x' in g.vertex_attributes(): x = 'x'; y = 'y';
+        else: x = 'lon'; y = 'lat';
+
+        g.vs[x] = [float(xx) for xx in g.vs[x]]
+        g.vs[y] = [float(yy) for yy in g.vs[y]]
+
+        g = add_lengths(g, x, y)
+
+    g.vs['origid'] = list(range(g.vcount()))
+
+    return g
+
+##########################################################
+def plot_graph(gin, plotpath, shppath=''):
+    """Plot the graph @gin and export to @plotpath and, if provided, plot
+    the border given by @shppath"""
+    if type(gin) == str:
+        g = simplify_graphml(gin, directed=False, simplify=True)
+    else:
+        g = gin
+
+    vcoords = np.array([(x, y) for x, y in zip(g.vs['x'], g.vs['y'])])
+
+    ecoords = []
+    for e in g.es:
+        ecoords.append([ [float(g.vs[e.source]['x']), float(g.vs[e.source]['y'])],
+                [float(g.vs[e.target]['x']), float(g.vs[e.target]['y'])], ])
+
+    import myutils.plot
+    myutils.plot.plot_graph(vcoords, ecoords, plotpath, shppath)
