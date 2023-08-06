ff60d1e946d99b60adf3cc759500e85e6f14a166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:24 +0000

    Add package simple-manga-downloader/1.12.0/simple_manga_downloader-1.12.0-py3-none-any.whl

---
+++ packages/simple-manga-downloader/simple_manga_downloader-1.12.0-py3-none-any.whl/simple_manga_downloader/modules/__init__.py
@@ -0,0 +1,39 @@
+"""The module that handles importing the manga modules and changing their attributes."""
+from .manga import BaseManga
+# Modules need to be imported so they get registered, order matters
+from .mangadex_org import Mangadex
+from .mangakakalot_com import Mangakakalot  # noqa: F401
+from .manganelo_com import Manganelo  # noqa: F401
+from .mangatown_com import Mangatown  # noqa: F401
+
+
+def match_module(link, title):
+    """Initialize the proper module"""
+    module = BaseManga.find_matching_module(link)
+    if module is False:
+        return False
+    else:
+        return module(link, title=title)
+
+
+def set_mangadex_language(lang_code):
+    """Changes the mangadex language code for all instances"""
+    Mangadex.lang_code = lang_code
+
+
+def set_md_at_home(flag):
+    """Sets the MD@Home setting"""
+    Mangadex.md_at_home = flag
+
+
+def toggle_check_only():
+    """Toggles check_only for all modules"""
+    BaseManga.check_only = True
+
+
+def set_download_directory(path):
+    """
+    Sets the download directory for all of the modules
+    path = pathlib Path object
+    """
+    BaseManga.directory = path
