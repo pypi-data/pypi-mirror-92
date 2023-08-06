8479f0fa406525cdebfb7e2874152b17e8941d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:50 +0000

    Add package edc-identifier/0.3.0/edc_identifier-0.3.0-py3-none-any.whl

---
+++ packages/edc-identifier/edc_identifier-0.3.0-py3-none-any.whl/edc_identifier/tests/test_simple_identifier.py
@@ -0,0 +1,20 @@
+from django.test import TestCase, tag
+
+from ..simple_identifier import SimpleIdentifier
+from edc_identifier.simple_identifier import SimpleUniqueIdentifier
+from edc_identifier.models import IdentifierModel
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class TestSimpleIdentifier(TestCase):
+    def test_simple(self):
+        obj = SimpleIdentifier()
+        obj.identifier
+
+    def test_simple_unique(self):
+        obj = SimpleUniqueIdentifier()
+        self.assertIsNotNone(obj.identifier)
+        try:
+            IdentifierModel.objects.get(identifier=obj.identifier)
+        except ObjectDoesNotExist:
+            self.fail("Identifier not add to history")
