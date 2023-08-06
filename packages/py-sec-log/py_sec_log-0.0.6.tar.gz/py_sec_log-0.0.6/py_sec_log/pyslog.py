3d0d615681faca9f42322f8543a938d2012e8cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package py-sec-log/0.0.6/py_sec_log-0.0.6.tar.gz

---
+++ packages/py-sec-log/py_sec_log-0.0.6.tar.gz/py_sec_log-0.0.6/py_sec_log/pyslog.py
@@ -0,0 +1,78 @@
+import requests, os, sys
+from licensing import methods
+from licensing.methods import Helpers
+import hashlib
+import smtplib, ssl
+import random, string
+class login:
+    k=Helpers.GetMachineCode()
+    ke=k.encode('utf-8')
+    ey=hashlib.md5(ke)
+    global key
+    key=ey.hexdigest()
+    def user_id():
+       Helpers.GetMachineCode()
+    def auth(auth_id):
+        if auth_id=='':
+            print('Please Enter auth_id')
+            sys.exit()
+        else:
+            global r
+        r=requests.get('https://pastebin.com/'+auth_id)
+    def verify(username, password):
+        if username=='' and password=='' or username=='' or password=='':
+            print('Need Username And Password For Login!')
+            sys.exit()
+        else:
+            pass
+        ins=username+password
+        sec=ins.encode('utf-8')
+        secu=hashlib.md5(sec)
+        info=secu.hexdigest()
+        if info in r.text:
+            if key in r.text:
+                pass
+            else:
+                print('Unexpected Error!')
+                sys.exit()
+        else:
+            print('Invalid Username Or Password!')
+            sys.exit()
+class verify:
+  def email(auth_id, email, ConformationCode:
+    rag=requests.get('https://pastebin.com/'+auth_id)
+    info=rag
+    sec=ins.encode('utf-8')
+    secu=hashlib.md5(sec)
+    info=secu.hexdigest()
+    if info in rag.text:
+      pass
+    else:
+      print('E-mail Not Registered')
+      sys.exit()
+    sec="1234567890"
+    code = []
+    for x in range(6):
+    	code.append(random.choice(sec))
+      code=''.join(code)
+      port = 587  # For starttls
+      smtp_server = "smtp.gmail.com"
+      sender_email = "sendotpnoreply@gmail.com"
+      password = "otp@send"
+      message = """\
+      Subject: No-Reply
+
+      Hi, Your Conformation Code Is """+code
+
+      context = ssl.create_default_context()
+      with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, email, message) 
+        if ConformationCode==code:
+          pass
+        else:
+	        print('Invalid Conformation Code')
+	        sys.exit()
