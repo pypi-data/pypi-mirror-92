8c8ba1822c3140e02c051a22b81f89ad1e3e9d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:49 +0000

    Add package octadocs/0.0.20/octadocs-0.0.20.tar.gz

---
+++ packages/octadocs/octadocs-0.0.20.tar.gz/octadocs-0.0.20/octadocs/octiron/context.py
@@ -0,0 +1,15 @@
+from deepmerge import always_merger
+
+from octadocs.octiron.types import Context
+
+
+def merge(first: Context, second: Context) -> Context:
+    """
+    Merge two contexts into one.
+
+    Second context can override the first.
+    """
+    return always_merger.merge(
+        base=first,
+        nxt=second,
+    )
