2c36be56f1c4a24c10c707a36b41b8eb4f84065e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:08 +0000

    Add package invenio-theme-tugraz/1.5.0/invenio_theme_tugraz-1.5.0-py2.py3-none-any.whl

---
+++ packages/invenio-theme-tugraz/invenio_theme_tugraz-1.5.0-py2.py3-none-any.whl/invenio_theme_tugraz/webpack.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 TUGRAZ.
+#
+# invenio-theme-tugraz  is free software.
+
+"""JS/CSS Webpack bundles for theme."""
+
+from invenio_assets.webpack import WebpackThemeBundle
+
+theme = WebpackThemeBundle(
+    __name__,
+    "assets",
+    default="semantic-ui",
+    themes={
+        "semantic-ui": dict(
+            entry={
+                "invenio-theme-tugraz-theme": "./less/invenio_theme_tugraz/theme.less",
+                "invenio-theme-tugraz-js": "./js/invenio_theme_tugraz/theme.js",
+            },
+            dependencies={},
+        )
+    },
+)
