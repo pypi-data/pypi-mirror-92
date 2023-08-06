97cc96f7ba5f12c9a7a7491a3406d0d20400662f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:13 +0000

    Add package pyDataverse/0.3.0/pyDataverse-0.3.0-py3-none-any.whl

---
+++ packages/pyDataverse/pyDataverse-0.3.0-py3-none-any.whl/pyDataverse/__init__.py
@@ -0,0 +1,20 @@
+"""Find out more at https://github.com/GDCC/pyDataverse.
+
+Copyright 2019 Stefan Kasberger
+
+Licensed under the MIT License.
+"""
+from __future__ import absolute_import
+
+from requests.packages import urllib3
+
+urllib3.disable_warnings()  # noqa
+
+__author__ = "Stefan Kasberger"
+__email__ = "stefan.kasberger@univie.ac.at"
+__copyright__ = "Copyright (c) 2019 Stefan Kasberger"
+__license__ = "MIT License"
+__version__ = "0.3.0"
+__url__ = "https://github.com/GDCC/pyDataverse"
+__download_url__ = "https://pypi.python.org/pypi/pyDataverse"
+__description__ = "A Python module for Dataverse."
