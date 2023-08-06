25fcb23cc7c3e6c3d024167f0c4a7ef86f44be91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:11 +0000

    Add package gittools/0.5.4/gittools-0.5.4-py3-none-any.whl

---
+++ packages/gittools/gittools-0.5.4-py3-none-any.whl/gittools/__init__.py
@@ -0,0 +1,13 @@
+"""Tools for using GIT in python, based on gitpython."""
+
+from .gittools import DirtyRepo, NotInTree
+from .gittools import current_commit_hash, path_status, module_status
+from .gittools import repo_tags, path_in_tree
+from .gittools import save_metadata
+
+# from importlib.metadata import version
+from importlib_metadata import version
+
+__author__ = 'Olivier Vincent'
+__version__ = version('gittools')
+__license__ = '3-Caluse BSD'
