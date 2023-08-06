7874abecd6326566d2672a9c0813f7c79beec2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:21 +0000

    Add package caffoa/0.2.2/caffoa-0.2.2.tar.gz

---
+++ packages/caffoa/caffoa-0.2.2.tar.gz/caffoa-0.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='caffoa',
+    version='0.2.2',
+    packages=['caffoa'],
+    url='https://github.com/claasd/caffoa',
+    license='MIT',
+    author='Claas Diederichs',
+    author_email='',
+    description='Create Azure Functions From Open Api (for C#)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "prance",
+        "openapi-spec-validator"
+    ]
+)
