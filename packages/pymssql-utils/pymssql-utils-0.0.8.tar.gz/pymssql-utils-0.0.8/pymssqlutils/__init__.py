1e4d18acd97f24da8c43a29cacaa9fce175315d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:19 +0000

    Add package pymssql-utils/0.0.8/pymssql-utils-0.0.8.tar.gz

---
+++ packages/pymssql-utils/pymssql-utils-0.0.8.tar.gz/pymssql-utils-0.0.8/pymssqlutils/__init__.py
@@ -0,0 +1,10 @@
+from .databaseresult import DatabaseResult, DatabaseError
+from .methods import (
+    execute,
+    execute_batched,
+    execute_many,
+    query,
+    to_sql_list,
+    model_to_values,
+    substitute_parameters,
+)
