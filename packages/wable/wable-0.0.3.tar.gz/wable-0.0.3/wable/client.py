b909cf28833d4366cdcd6ecb1db728f64e41de33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package wable/0.0.3/wable-0.0.3.tar.gz

---
+++ packages/wable/wable-0.0.3.tar.gz/wable-0.0.3/wable/client.py
@@ -0,0 +1,23 @@
+import requests
+"""
+A client to communicate with https://wable.org api.
+"""
+
+
+class Wable:
+    auth_token = None
+
+
+def getserial():
+    # Extract serial from cpuinfo file
+    cpuserial = "0000000000000000"
+    try:
+        f = open('/proc/cpuinfo', 'r')
+        for line in f:
+            if line[0:6] == 'Serial':
+                cpuserial = line[10:26]
+        f.close()
+    except:
+        cpuserial = "ERROR000000000"
+
+    return cpuserial
