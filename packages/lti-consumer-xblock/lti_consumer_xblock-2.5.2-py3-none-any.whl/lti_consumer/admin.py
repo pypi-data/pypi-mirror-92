e524f54c5a7c9a736d2cffb5e3714c87cfd1d657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package lti-consumer-xblock/2.5.2/lti_consumer_xblock-2.5.2-py3-none-any.whl

---
+++ packages/lti-consumer-xblock/lti_consumer_xblock-2.5.2-py3-none-any.whl/lti_consumer/admin.py
@@ -0,0 +1,19 @@
+"""
+Admin views for LTI related models.
+"""
+from django.contrib import admin
+from lti_consumer.models import LtiAgsLineItem, LtiConfiguration, LtiAgsScore
+
+
+class LtiConfigurationAdmin(admin.ModelAdmin):
+    """
+    Admin view for LtiConfiguration models.
+
+    Makes the location field read-only to avoid issues.
+    """
+    readonly_fields = ('location', 'config_id')
+
+
+admin.site.register(LtiConfiguration, LtiConfigurationAdmin)
+admin.site.register(LtiAgsLineItem)
+admin.site.register(LtiAgsScore)
---
+++ packages/lti-consumer-xblock/lti_consumer_xblock-2.5.2-py3-none-any.whl/lti_consumer/apps.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+lti_consumer Django application initialization.
+"""
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+
+class LTIConsumerApp(AppConfig):
+    """
+    Configuration for the lti_consumer Django application.
+    """
+
+    name = 'lti_consumer'
+
+    # Set LMS urls for LTI endpoints
+    # Urls are under /api/lti_consumer/
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': 'lti_consumer',
+                'regex': '^api/',
+                'relative_path': 'plugin.urls',
+            }
+        }
+    }
+
+    def ready(self):
+        # pylint: disable=unused-import,import-outside-toplevel
+        import lti_consumer.signals
