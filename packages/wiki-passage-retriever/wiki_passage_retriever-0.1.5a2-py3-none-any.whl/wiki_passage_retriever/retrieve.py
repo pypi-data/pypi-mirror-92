c380d38e32aad933a645a45fe7177fb8227593fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:02 +0000

    Add package wiki-passage-retriever/0.1.5a2/wiki_passage_retriever-0.1.5a2-py3-none-any.whl

---
+++ packages/wiki-passage-retriever/wiki_passage_retriever-0.1.5a2-py3-none-any.whl/wiki_passage_retriever/retrieve.py
@@ -0,0 +1,57 @@
+from transformers import DPRReader
+from transformers.models.dpr import DPRReaderOutput
+from .dpr_tokenizer import MyDPRReaderTokenizer
+from .utils import retrieve_wiki_page
+from typing import List, Union, Tuple, Dict
+import torch
+from numpy import ndarray
+
+
+__all__ = ['get_most_relevant_spans_from_wiki', 'get_most_relevant_passages']
+
+
+def process_with_dpr_reader(passages: List[str], titles: Union[List[str], str], question: str) -> Tuple[MyDPRReaderTokenizer, Dict[str, List[List[int]]], DPRReaderOutput]:
+    if isinstance(titles, str):
+        return process_with_dpr_reader(passages, [titles] * len(passages), question)
+
+    with torch.no_grad():
+        tokenizer = MyDPRReaderTokenizer.from_pretrained('facebook/dpr-reader-single-nq-base')
+        model = DPRReader.from_pretrained('facebook/dpr-reader-single-nq-base')
+        encoded_inputs = tokenizer(
+            questions=question,
+            titles=titles,
+            texts=passages,
+            return_tensors='pt',
+            truncation=False,
+            padding=True
+        )
+        return tokenizer, encoded_inputs, model(**encoded_inputs)
+
+
+def get_relevance_scores(passages: List[str], titles: Union[List[str], str], question: str) -> ndarray:
+    """
+    Given a list of passages, a list of corresponding titles (or a single title if all passages are in the same article), and a question,
+    returns the relevance score of the passages with respect to the question.
+    """
+    _, _, outputs = process_with_dpr_reader(passages, titles, question)
+    return outputs.relevance_logits.numpy()
+
+
+def get_most_relevant_spans(passages: List[str], titles: Union[List[str], str], question: str, top_k: int=1) -> List[str]:
+    tokenizer, encoded_inputs, outputs = process_with_dpr_reader(passages, titles, question)
+    return [span.text for span in tokenizer.decode_best_spans(encoded_inputs, outputs, num_spans=top_k)]
+
+
+def get_most_relevant_spans_from_wiki(search_query: str, question: str, top_k: int=1) -> List[str]:
+    return get_most_relevant_spans(retrieve_wiki_page(search_query), search_query, question, top_k)
+
+
+def get_most_relevant_passages(search_query: str, question: str, top_k: int=1) -> List[str]:
+    passages = retrieve_wiki_page(search_query)
+    relevance_scores = get_relevance_scores(passages, search_query, question)
+
+    # get top_k relevance scores
+    # (based on https://stackoverflow.com/questions/6910641/how-do-i-get-indices-of-n-maximum-values-in-a-numpy-array)
+    top_k_ind = relevance_scores.argsort()[-top_k:][::-1]
+    return [passages[ind] for ind in top_k_ind]
+
---
+++ packages/wiki-passage-retriever/wiki_passage_retriever-0.1.5a2-py3-none-any.whl/wiki_passage_retriever/utils.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+from typing import List, Callable
+import nltk
+from nltk.tokenize import sent_tokenize
+import itertools
+
+
+__all__ = ['retrieve_wiki_page']
+
+
+MIN_SENTENCE_LENGTH = 4  # filter sentences with less than 4 characters (likely to be noise)
+
+
+def remove_citation(paragraph: str) -> str:
+    """Remove all citations (numbers in side square brackets) in paragraph"""
+    return re.sub(r'\[\d+\]', '', paragraph)
+
+
+def remove_new_line(paragraph: str) -> str:
+    return paragraph.replace("\n", "")
+
+
+def compose_fns(functions: List[Callable]) -> Callable:
+    def ret(input):
+        for fn in functions:
+            input = fn(input)
+
+        return input
+
+    return ret
+
+
+def retrieve_page_content(url: str) -> List[str]:
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
+
+    nltk.download('punkt')  # make sure that the tokenizer is downloaded
+
+    paragraphs = list(map(lambda p: p.getText(), soup.find_all('p')))
+    sentences = itertools.chain.from_iterable(map(sent_tokenize, paragraphs))
+    preprocess_fn = compose_fns([remove_citation, remove_new_line])
+
+    # preprocess sentences, stripping starting and trailing spaces, and then filter out too short sentences
+    return list(filter(lambda sent: len(sent) > MIN_SENTENCE_LENGTH, map(lambda sent: preprocess_fn(sent).strip(), sentences)))
+
+
+def retrieve_wiki_page(query: str) -> List[str]:
+    """Searches wikpedia for a query, gets the first result, then extracts all the paragraphs."""
+    session = requests.Session()
+    api_url = "https://en.wikipedia.org/w/api.php"
+
+    params = {
+        "action": "query",
+        "format": "json",
+        "list": "search",
+        "srsearch": query
+    }
+
+    response = session.get(url=api_url, params=params)
+    data = response.json()
+
+    # only get the first result
+    page_id = data['query']['search'][0]['pageid']
+    return retrieve_page_content("https://en.wikipedia.org/?curid={}".format(page_id))
