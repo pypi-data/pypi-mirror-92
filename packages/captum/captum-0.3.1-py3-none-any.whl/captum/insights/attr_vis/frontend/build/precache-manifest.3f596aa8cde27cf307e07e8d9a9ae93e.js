686a26cd735a6fb74010fbaee0fd3148cef69950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:12 +0000

    Add package captum/0.3.1/captum-0.3.1-py3-none-any.whl

---
+++ packages/captum/captum-0.3.1-py3-none-any.whl/captum/insights/attr_vis/frontend/build/precache-manifest.3f596aa8cde27cf307e07e8d9a9ae93e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b39d48a38c390f26d41af4e94fb09cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b41a79a3b8de027ca61c",
+    "url": "./static/css/main.6b86a595.chunk.css"
+  },
+  {
+    "revision": "8035fe1254ff6fa4676c",
+    "url": "./static/js/2.92863798.chunk.js"
+  },
+  {
+    "revision": "eba67c0545743a32d131866b1f69e0ab",
+    "url": "./static/js/2.92863798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41a79a3b8de027ca61c",
+    "url": "./static/js/main.d33271aa.chunk.js"
+  },
+  {
+    "revision": "aba697584663a7cb794c",
+    "url": "./static/js/runtime-main.c3c9eabf.js"
+  }
+]);
