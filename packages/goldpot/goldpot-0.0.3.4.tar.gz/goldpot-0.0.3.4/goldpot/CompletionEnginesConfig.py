6913fbc1d42c79c2bf679d94a330fde6a0e240a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:42 +0000

    Add package goldpot/0.0.3.4/goldpot-0.0.3.4.tar.gz

---
+++ packages/goldpot/goldpot-0.0.3.4.tar.gz/goldpot-0.0.3.4/goldpot/CompletionEnginesConfig.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass, field
+from functools import reduce
+from typing import Iterable, List, Optional, Dict
+from pathlib import Path
+
+from dataclasses_json import DataClassJsonMixin
+from yamldataclassconfig import YamlDataClassConfig
+
+
+@dataclass
+class CompletionEngineConfig(DataClassJsonMixin):
+    engine: str
+    max_tokens: int
+    temperature: float
+    top_p: float
+    n: int
+    stream: bool
+    logprobs: Optional[int]
+    echo: bool
+    stop: Optional[List[str]]
+    presence_penalty: float
+    frequency_penalty: float
+    best_of: int
+    #logit_bias: Optional[Dict[str, int]]
+
+    def __post_init__(self):
+        if not isinstance(self.engine, str):
+            self.engine = str(self.engine)
+
+
+@dataclass
+class CompletionEnginesConfigReader(YamlDataClassConfig):
+    configs: List[Dict[str, CompletionEngineConfig]] = field(
+        default=None,
+        metadata={'dataclasses_json': {'mm_field': List[Dict[str, CompletionEngineConfig]]}})
+
+    def read(self, path: str):
+        self.load(Path(path))
+        self.configs = reduce(lambda d1, d2: {**d1, **d2}, self.configs) # TODO: curerntly using Python 3.8, >=3.9 has d1|d2
+        return self
+
+    def get(self, handle: str):
+        return self.configs.get(handle, None)
+
---
+++ packages/goldpot/goldpot-0.0.3.4.tar.gz/goldpot-0.0.3.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README_PyPI.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="goldpot",
+    version="0.0.3.4",
+    author="Sebastian Senge",
+    author_email="ssenge.public@gmail.com",
+    description="A thin convenience layer on top of OpenAI's GPT-3 Python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ssenge/Goldpot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.7',
+)
