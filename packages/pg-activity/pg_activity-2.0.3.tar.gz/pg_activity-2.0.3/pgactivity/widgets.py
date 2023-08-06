22c522c421d54447d39ae0012974fe28e74614a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:15 +0000

    Add package pg-activity/2.0.3/pg_activity-2.0.3.tar.gz

---
+++ packages/pg-activity/pg_activity-2.0.3.tar.gz/pg_activity-2.0.3/pgactivity/widgets.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from blessed import Terminal
+
+
+def boxed(
+    term: Terminal,
+    content: str,
+    *,
+    border_color: str = "white",
+    center: bool = False,
+    width: Optional[int] = None,
+) -> str:
+    border_width = term.length(content) + 2
+    border_formatter = getattr(term, border_color)
+    lines = [
+        border_formatter("┌" + "─" * border_width + "┐"),
+        " ".join([border_formatter("│") + term.normal, content, border_formatter("│")]),
+        border_formatter("└" + "─" * border_width + "┘") + term.normal,
+    ]
+    if center:
+        if width is None:
+            width = term.width
+        lines = [term.center(line, width=width) for line in lines]
+    return "\n".join(lines)
