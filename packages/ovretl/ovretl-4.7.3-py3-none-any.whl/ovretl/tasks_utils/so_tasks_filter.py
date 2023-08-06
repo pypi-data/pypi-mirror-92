d35435f9993bae6ac2a1a19f3c2542d5ba06473b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:36 +0000

    Add package ovretl/4.7.3/ovretl-4.7.3-py3-none-any.whl

---
+++ packages/ovretl/ovretl-4.7.3-py3-none-any.whl/ovretl/tasks_utils/so_tasks_filter.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def so_tasks_filter(tasks_df: pd.DataFrame) -> pd.DataFrame:
+    so_tasks_df = tasks_df.loc[tasks_df["so_id"].notnull(), :]
+    so_tasks_df.loc[:, "created_at"] = so_tasks_df["created_at"].dt.date
+    so_tasks_df.loc[:, "updated_at"] = so_tasks_df["updated_at"].dt.date
+    so_tasks_df.loc[:, "due_date"] = so_tasks_df["due_date"].dt.date
+    return so_tasks_df
