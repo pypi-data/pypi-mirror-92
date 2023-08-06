56836b4df980a24b43d3d70a55952f9ca040d5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:56 +0000

    Add package polidoro-cli/1.0.1/polidoro-cli-1.0.1.tar.gz

---
+++ packages/polidoro-cli/polidoro-cli-1.0.1.tar.gz/polidoro-cli-1.0.1/polidoro_cli/main.py
@@ -0,0 +1,53 @@
+# TODO
+# bash completion
+import glob
+import os
+from subprocess import CalledProcessError
+
+from polidoro_argument import ArgumentParser
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append("/mnt/c/Users/heito/IdeaProjects/cli/")
+
+from polidoro_cli.clis.cli_utils import load_environment_variables, CONFIG_FILE, LOCAL_ENV_FILE, change_to_clis_dir
+
+load_environment_variables(CONFIG_FILE)
+load_environment_variables(LOCAL_ENV_FILE)
+
+VERSION = '1.0.1'
+
+
+def load_clis():
+    cur_dir = os.getcwd()
+    change_to_clis_dir()
+    for d in os.listdir():
+        if os.path.isdir(d) and not d.startswith('__'):
+            try:
+                os.chdir(d)
+                for file in glob.glob('*.py'):
+                    __import__('polidoro_cli.clis.%s.%s' % (d, file.replace('.py', '')))
+            except SystemExit as e:
+                print(e)
+        change_to_clis_dir()
+    os.chdir(cur_dir)
+
+
+def main():
+    # Load all the CLIs
+    load_clis()
+
+    try:
+        ArgumentParser(version=VERSION).parse_args()
+    except CalledProcessError as error:
+        return error.returncode
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    os.environ['CLI_PATH'] = os.path.dirname(os.path.realpath(__file__))
+    if os.environ.get('OS', None) == 'Windows_NT':
+        os.environ['CLI_PATH'] = os.environ['CLI_PATH'].replace('/mnt/c', 'C:').replace('/', '\\')
+
+    main()
