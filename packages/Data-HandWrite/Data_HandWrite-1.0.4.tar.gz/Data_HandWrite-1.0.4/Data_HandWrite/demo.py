c2356a455a4675836c8b7c7f626758e61ff9ae4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:12 +0000

    Add package Data-HandWrite/1.0.4/Data_HandWrite-1.0.4.tar.gz

---
+++ packages/Data-HandWrite/Data_HandWrite-1.0.4.tar.gz/Data_HandWrite-1.0.4/Data_HandWrite/demo.py
@@ -0,0 +1,31 @@
+import Data_HandWrite
+import numpy as np
+#__init__.dowloadPaintToolKitAndExecute()
+#X_train,Y_train,X_test,Y_test = __init__.LoadDataFromWeb()
+#X_train,Y_train = __init__.pickSample(X_train,Y_train,60)
+
+Data_HandWrite.downloadData()
+X_train,Y_train = Data_HandWrite.LoadData('train')
+X_test,Y_test = Data_HandWrite.LoadData('test')
+
+#x = np.array([X_test[0]])
+#x=np.reshape(X_test[0],(1,-1))# same effect as upper one
+#print(np.shape(x))
+#print(np.shape(x))
+#print(np.shape(X_test))
+'''
+from sklearn.svm import SVC
+model = SVC(decision_function_shape='ovo',gamma=0.004,probability=True)
+model.fit(X_train,Y_train)
+result = model.predict(X_test)
+print(model.score(X_test,Y_test))
+
+for i in range(len(result)):
+    print(Y_test[i],'==>',result[i])
+
+x = __init__.LoadImgFromFile('test/1_100_0000.jpg')
+result = model.predict_log_proba(x)
+print(result)
+result = model.predict(x)
+print(result)
+'''
