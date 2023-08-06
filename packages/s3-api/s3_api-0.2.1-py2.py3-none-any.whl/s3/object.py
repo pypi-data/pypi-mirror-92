222f3c2812993d5f055016fa7c96a7c89aca6a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:13 +0000

    Add package s3-api/0.2.1/s3_api-0.2.1-py2.py3-none-any.whl

---
+++ packages/s3-api/s3_api-0.2.1-py2.py3-none-any.whl/s3/object.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Amazon S3 API
+# Copyright (c) 2008-2020 Hive Solutions Lda.
+#
+# This file is part of Hive Amazon S3 API.
+#
+# Hive Amazon S3 API is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Amazon S3 API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Amazon S3 API. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2020 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import appier
+
+class ObjectAPI(object):
+
+    def create_object(self, bucket, name, data, sha256 = None):
+        url = self.bucket_url % bucket + "%s" % name
+        contents = self.put(
+            url,
+            data = data,
+            sign = True,
+            sha256 = sha256 or self._content_sha256(data = data)
+        )
+        return contents
+
+    def build_url_object(self, bucket, name):
+        return self.bucket_url % bucket + "%s" % name
+
+    def create_file_object(self, bucket, name, path, sha256 = None):
+        return self.create_object(
+            bucket,
+            name,
+            data = appier.file_g(path),
+            sha256 = sha256 or self._content_sha256(appier.file_g(path))
+        )
