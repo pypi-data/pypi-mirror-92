f18c78df68de24bf7d5196463c980434362e1989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:35 +0000

    Add package ANF-Feed/0.0.1.dev1/ANF-Feed-0.0.1.dev1.tar.gz

---
+++ packages/ANF-Feed/ANF-Feed-0.0.1.dev1.tar.gz/ANF-Feed-0.0.1.dev1/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+# Set Up Meta Data
+REQUIRED = ['feedparser', 'QtPy', 'QDarkStyle']
+
+with open('README.rst', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='ANF-Feed',
+    version='0.0.1.dev1',
+    author='m1ghtfr3e',
+    description='Read ANF Feeds',
+    keywords='anf, feed, rss',
+    long_description=long_description,
+    url='https://github.com/m1ghtfr3e/ANF-Feed-Reader',
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED,
+    include_package_data = True,
+    data_files=[
+        ('', ['assets/*'])
+    ],
+    entry_points={
+        'console_scripts':[
+            'anfrss = anfrss.gui.guiapp:run'
+        ]
+    },
+    license='GPLv3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Development Status :: 1 - Planning',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+        ],
+    python_requires='>=3',
+    project_urls={
+        'Source': 'https://github.com/m1ghtfr3e/ANF-Feed-Reader',
+        'Bug Reports': 'https://github.com/m1ghtfr3e/ANF-Feed-Reader/issues'
+    }
+)
