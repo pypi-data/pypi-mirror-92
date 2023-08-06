f980d26e63c5dba0a7e86f15d323448610861504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:39 +0000

    Add package eliteapi/0.3/eliteapi-0.3.tar.gz

---
+++ packages/eliteapi/eliteapi-0.3.tar.gz/eliteapi-0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='eliteapi',
+    packages=[''],
+    package_dir={'': 'src'},
+    version='0.3',
+    license='MIT',
+    description='eliteapi is a Python wrapper for Elite Creative allowing you to easly interact with the API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Kappa',
+    author_email='f.cappetti.05@gmail.com',
+    url='https://github.com/KappaOnGit/eliteapi',
+    download_url='https://github.com/FraKappa/eliteapi/archive/v_02.tar.gz',
+    keywords=['eliteapi', 'elite'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
