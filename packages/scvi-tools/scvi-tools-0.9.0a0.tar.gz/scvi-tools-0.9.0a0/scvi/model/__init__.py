6d40b9c45b34a74ace490c60bc56114bafec736a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:02 +0000

    Add package scvi-tools/0.9.0a0/scvi-tools-0.9.0a0.tar.gz

---
+++ packages/scvi-tools/scvi-tools-0.9.0a0.tar.gz/scvi-tools-0.9.0a0/scvi/model/__init__.py
@@ -0,0 +1,13 @@
+from ._autozi import AUTOZI
+from ._linear_scvi import LinearSCVI
+from ._scanvi import SCANVI
+from ._scvi import SCVI
+from ._totalvi import TOTALVI
+
+__all__ = [
+    "SCVI",
+    "TOTALVI",
+    "LinearSCVI",
+    "AUTOZI",
+    "SCANVI",
+]
