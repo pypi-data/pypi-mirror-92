903a9c8d26cc8cab8d56cf72ba56e35ddb29c275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:26 +0000

    Add package pubtatortool/0.2/pubtatortool-0.2.tar.gz

---
+++ packages/pubtatortool/pubtatortool-0.2.tar.gz/pubtatortool-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pubtatortool",
+    version="0.2",
+    author="Guilhem Piat",
+    author_email="guilhem.piat@protonmail.com",
+    description="A package for loading and manipulating PubTator files "
+                "as Python objects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gpiat/PubTator-Parsing-Tool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
