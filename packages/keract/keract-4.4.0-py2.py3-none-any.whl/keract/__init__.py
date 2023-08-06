c47463d7d48306a804c5919d2640e94fb53a6def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:19 +0000

    Add package keract/4.4.0/keract-4.4.0-py2.py3-none-any.whl

---
+++ packages/keract/keract-4.4.0-py2.py3-none-any.whl/keract/__init__.py
@@ -0,0 +1,17 @@
+import importlib
+
+tf_spec = importlib.util.find_spec("tensorflow")
+if tf_spec is None:
+    raise ImportError("No valid tensorflow installation found. Please install "
+                      "tensorflow>=2.0 or tensorflow-gpu>=2.0")
+
+from keract.keract import display_activations  # noqa
+from keract.keract import display_gradients_of_trainable_weights  # noqa
+from keract.keract import display_heatmaps  # noqa
+from keract.keract import get_activations  # noqa
+from keract.keract import get_gradients_of_activations  # noqa
+from keract.keract import get_gradients_of_trainable_weights  # noqa
+from keract.keract import load_activations_from_json_file  # noqa
+from keract.keract import persist_to_json_file  # noqa
+
+__version__ = '4.4.0'
