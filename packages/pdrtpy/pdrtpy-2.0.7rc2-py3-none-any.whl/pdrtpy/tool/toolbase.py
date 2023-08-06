9aca68288b3ebc6b32856776d4ed66669dcfb9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:12 +0000

    Add package pdrtpy/2.0.7rc2/pdrtpy-2.0.7rc2-py3-none-any.whl

---
+++ packages/pdrtpy/pdrtpy-2.0.7rc2-py3-none-any.whl/pdrtpy/tool/toolbase.py
@@ -0,0 +1,14 @@
+
+class ToolBase(object):
+    """ Base class object for PDR Toolbox tools.  This class implements a simple 
+        interface with a run method.  Tools will generally do some set up
+        such as reading in observational data before run() can be invoked.
+    """
+    def __init__(self):
+        # most tools will have their own plotter
+        self._plotter = None
+
+    def run(self):
+        """Runs the tool. Each subclass Tool must implement its own run() method.
+        """
+        pass
