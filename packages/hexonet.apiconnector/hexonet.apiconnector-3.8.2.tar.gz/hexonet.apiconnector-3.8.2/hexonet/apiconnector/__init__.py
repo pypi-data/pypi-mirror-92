14fc2235350ed6b8d920ea46e9e9d2e6cbf8f27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:54 +0000

    Add package hexonet.apiconnector/3.8.2/hexonet.apiconnector-3.8.2.tar.gz

---
+++ packages/hexonet.apiconnector/hexonet.apiconnector-3.8.2.tar.gz/hexonet.apiconnector-3.8.2/hexonet/apiconnector/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    hexonet.apiconnector
+    ~~~~~~~~~~~~~~~~~~~~
+    A connector library for the insanely fast HEXONET Backend API.
+    :copyright: © 2018 by HEXONET GmbH.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "3.8.2"
+name = "hexonet.apiconnector"
