592cb5f83fd482597f4359f04fb12ec40889e336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:20 +0000

    Add package sortify/1.1/sortify-1.1.tar.gz

---
+++ packages/sortify/sortify-1.1.tar.gz/sortify-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sortify',
+    version='1.1',
+    description='A Python package containing implementation of various sorting algorithms.',
+    packages=['sortify'],
+    author='Aman Sharma',
+    url = 'https://github.com/amansharma2910/Sortify',
+    author_email='amansharma2910@gmail.com',
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+)
