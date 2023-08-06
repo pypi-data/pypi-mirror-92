6fe44ed512a2297d09ce1110838dd4a8dd99e3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:59 +0000

    Add package exkaldi/1.3.4/exkaldi-1.3.4.tar.gz

---
+++ packages/exkaldi/exkaldi-1.3.4.tar.gz/exkaldi-1.3.4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup,find_packages
+import glob
+import os
+import subprocess
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+#with open("requirement.txt") as fr:
+#    requirement = fr.readlines()
+
+def read_version_info():
+    cmd = 'cd exkaldi && python3 -c "import version; print(version.info.version)"'
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode != 0:
+        raise Exception("Failed to detect ExKaldi version.\n"+err.decode())
+    else:
+        return out.decode().strip().split("\n")[-1].strip()
+
+setup(
+    name="exkaldi",
+    version=read_version_info(),
+    author="Wang Yu",
+    author_email="wangyu@alps-lab.org",
+    description="ExKaldi Automatic Speech Recognition Toolkit",
+    long_description=long_description,
+    long_description_content_type=os.path.join("text","markdown"),
+    url="https://github.com/wangyu09/exkaldi",
+    packages=find_packages(),
+    data_files = [
+            (os.path.join("exkaldisrc","tools"), glob.glob( os.path.join("tools","*")))
+        ],
+    install_requires=["numpy>=1.16", "kenlm>=0.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
