9ef43589f9907b93c96f05e9d419838a4f59c543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:09 +0000

    Add package samy-distributions/0.1/samy_distributions-0.1.tar.gz

---
+++ packages/samy-distributions/samy_distributions-0.1.tar.gz/samy_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='samy_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['samy_distributions'],
+      zip_safe=False)
+
+
