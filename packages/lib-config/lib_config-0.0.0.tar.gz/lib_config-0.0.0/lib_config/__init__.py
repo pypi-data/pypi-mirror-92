25dc509627aac178529389c961d3f65bf1fec5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:42 +0000

    Add package lib-config/0.0.0/lib_config-0.0.0.tar.gz

---
+++ packages/lib-config/lib_config-0.0.0.tar.gz/lib_config-0.0.0/lib_config/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""This package contains config helper functions"""
+
+__author__ = "Justin Furuness"
+__credits__ = ["Justin Furuness"]
+__Lisence__ = "BSD"
+__maintainer__ = "Justin Furuness"
+__email__ = "jfuruness@gmail.com"
+__status__ = "Development"
+
+from .config import Config
