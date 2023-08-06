ef13d543bacbef6b2afe25c5d7cfbba5d02cfe91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:20 +0000

    Add package moncashify/0.3.0/moncashify-0.3.0.tar.gz

---
+++ packages/moncashify/moncashify-0.3.0.tar.gz/moncashify-0.3.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name="moncashify",
+	version="0.3.0",
+	author="Lub Lorry Lamysère",
+	author_email="lemayzeur@code9haiti.com",
+	description="Python SDK to make API requests and handle API responses from the MonCash API",
+	long_description=long_description,
+	long_description_content_type='text/x-rst',
+	url="https://lemayzeur.github.io/moncashify_sdk",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+)
