3c211f1ec33de0ad09af18d298cb54134c855f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:47 +0000

    Add package model1/1.0.2.5/model1-1.0.2.5.tar.gz

---
+++ packages/model1/model1-1.0.2.5.tar.gz/model1-1.0.2.5/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="model1",
+    version="1.0.2.5",
+    keywords=("model1", "xxx"),
+    description="model1 sdk",
+    long_description="model1 sdk for python",
+    license="MIT Licence",
+
+    url="",
+    author="skyeye",
+    author_email="skyeye@gmail.com",
+
+    packages=find_packages(exclude=['dist']),
+    # packages=find_packages("model2"),
+    # package_dir={'':'model2'},
+    include_package_data=True,
+    platforms="any",
+    install_requires=['testext>=1.0.0.1'],
+    # data_files=[('model1/conf/validator', ['goods.json'])],
+    data_files=[('model1/conf/validator', ['model1/conf/validator/*.json'])],
+
+    scripts=[],
+    # entry_points={
+    #     'console_scripts': [
+    #         'test = test.help:main'
+    #     ]
+    # }
+)
