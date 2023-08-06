d6433785f0285d0e3357ff1ff2a62c821807a8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:49 +0000

    Add package django-stamdata3/0.10/django-stamdata3-0.10.tar.gz

---
+++ packages/django-stamdata3/django-stamdata3-0.10.tar.gz/django-stamdata3-0.10/employee_info/urls/browser.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from ..views import browser as views
+
+app_name = 'employee_info_browser'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('resource', views.resource, name='resource'),
+    path('organisation', views.organisation, name='organisation'),
+]
