d1e420c9e4602559916ff437320090c29e3d895f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:06 +0000

    Add package viberbot/0.1.4/viberbot-0.1.4-py2.7.egg

---
+++ packages/viberbot/viberbot-0.1.4-py2.7.egg/viberbot/api/api.py
@@ -0,0 +1,76 @@
+import hashlib
+import hmac
+import json
+import logging
+
+from viberbot.api.consts import VIBER_BOT_API_URL
+from viberbot.api.viber_requests import create_request
+from viberbot.api.api_request_sender import ApiRequestSender
+from viberbot.api.message_sender import MessageSender
+
+
+class Api(object):
+	def __init__(self, bot_configuration):
+		self._logger = logging.getLogger('viber.bot.api')
+		self._bot_configuration = bot_configuration
+		self._request_sender = ApiRequestSender(self._logger, VIBER_BOT_API_URL, bot_configuration)
+		self._message_sender = MessageSender(self._logger, self._request_sender, bot_configuration)
+
+	def get_name(self):
+		return self._bot_configuration.get_name()
+
+	def get_avatar(self):
+		return self._bot_configuration.get_avatar()
+
+	def set_webhook(self, url, webhook_events=None):
+		self._logger.debug(u"setting webhook to url: {0}".format(url))
+		return self._request_sender.set_webhook(url, webhook_events)
+
+	def unset_webhook(self):
+		self._logger.debug("unsetting webhook")
+		return self._request_sender.set_webhook('')
+
+	def get_online(self, ids):
+		return self._request_sender.get_online(ids)
+
+
+	def get_account_info(self):
+		self._logger.debug("requesting account info")
+		account_info = self._request_sender.get_account_info()
+		self._logger.debug(u"received account info: {0}".format(account_info))
+		return account_info
+
+	def verify_signature(self, request_data, signature):
+		return signature == self._calculate_message_signature(request_data)
+
+	def parse_request(self, request_data):
+		self._logger.debug("parsing request")
+		request_dict = json.loads(request_data, encoding='utf-16')
+		request = create_request(request_dict)
+		self._logger.debug(u"parsed request={0}".format(request))
+		return request
+
+	def send_messages(self, to, messages):
+		"""
+		:param to: Viber user id
+		:param messages: list of Message objects to be sent
+		:return: list of tokens of the sent messages
+		"""
+		self._logger.debug("going to send messages: {0}, to: {1}".format(messages, to))
+		if not isinstance(messages, list):
+			messages = [messages]
+
+		sent_messages_tokens = []
+
+		for message in messages:
+			token = self._message_sender.send_message(to, self._bot_configuration.get_name(), self._bot_configuration.get_avatar(), message)
+			sent_messages_tokens.append(token)
+
+		return sent_messages_tokens
+
+	def _calculate_message_signature(self, message):
+		return hmac.new(bytes(self._bot_configuration.get_auth_token().encode('ascii')),
+						msg=message,
+						digestmod=hashlib.sha256)\
+			.hexdigest()
+
---
+++ packages/viberbot/viberbot-0.1.4-py2.7.egg/viberbot/api/api_request_sender.py
@@ -0,0 +1,69 @@
+import requests
+from requests import RequestException
+import traceback
+from viberbot.api.consts import BOT_API_ENDPOINT
+import json
+
+
+class ApiRequestSender(object):
+	def __init__(self, logger, viber_bot_api_url, bot_configuration):
+		self._logger = logger
+		self._viber_bot_api_url = viber_bot_api_url
+		self._bot_configuration = bot_configuration
+
+	def set_webhook(self, url, webhook_events=None):
+		payload = {
+			'auth_token': self._bot_configuration.get_auth_token(),
+			'url': url,
+		}
+
+		if webhook_events is not None:
+			if not isinstance(webhook_events, list):
+				webhook_events = [webhook_events]
+			payload['event_types'] = webhook_events
+
+		result = self.post_request(
+			endpoint=BOT_API_ENDPOINT.SET_WEBHOOK,
+			payload=json.dumps(payload))
+
+		if not result['status'] == 0:
+			raise Exception(u"failed with status: {0}, message: {1}".format(result['status'], result['status_message']))
+
+	def get_account_info(self):
+		payload = {
+			'auth_token': self._bot_configuration.get_auth_token()
+		}
+		return self.post_request(
+			endpoint=BOT_API_ENDPOINT.GET_ACCOUNT_INFO,
+			payload=json.dumps(payload))
+
+	def post_request(self, endpoint, payload):
+		try:
+			response = requests.post(self._viber_bot_api_url + '/' + endpoint, data=payload)
+			response.raise_for_status()
+			return json.loads(response.text)
+		except RequestException as e:
+			self._logger.error(u"failed to post request to endpoint={0}, with payload={1}. error is: {2}"
+							   .format(endpoint, payload, traceback.format_exc()))
+			raise e
+		except Exception as ex:
+			self._logger.error(u"unexpected Exception while trying to post request. error is: {0}"
+							   .format(traceback.format_exc()))
+			raise ex
+
+	def get_online_status(self, ids=[]):
+		if ids is None or not isinstance(ids, list) or len(ids) == 0:
+			raise Exception("missing parameter ids, should be a list of viber memberIds")
+
+		payload = {
+			'auth_token': self._bot_configuration.get_auth_token(),
+			'ids': ids
+		}
+		result = self.post_request(
+			endpoint=BOT_API_ENDPOINT.GET_ONLINE,
+			payload=json.dumps(payload))
+
+		if not result['status'] == 0:
+			raise Exception(u"failed with status: {0}, message: {1}".format(result['status'], result['status_message']))
+
+		return result
