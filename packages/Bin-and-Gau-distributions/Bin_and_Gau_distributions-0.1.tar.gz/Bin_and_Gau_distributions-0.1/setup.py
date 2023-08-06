47715ea94e537356d3d5c11c500d45bb1396229c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:30 +0000

    Add package Bin-and-Gau-distributions/0.1/Bin_and_Gau_distributions-0.1.tar.gz

---
+++ packages/Bin-and-Gau-distributions/Bin_and_Gau_distributions-0.1.tar.gz/Bin_and_Gau_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Bin_and_Gau_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Bin_and_Gau_distributions'],
+      zip_safe=False)
