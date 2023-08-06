ec9fb04316b4921c531b577f4e3ed549a6b68c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package pyezviz/0.1.5.6/pyEzviz-0.1.5.6.tar.gz

---
+++ packages/pyezviz/pyEzviz-0.1.5.6.tar.gz/pyEzviz-0.1.5.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name='pyEzviz',
+    version="0.1.5.6",
+    license='Apache Software License',
+    author='Pierre Ourdouille',
+    author_email='baqs@users.github.com',
+    description='Pilot your Ezviz cameras',
+    long_description="Pilot your Ezviz cameras with this module. Please view readme on github",
+    url='http://github.com/baqs/pyEzviz/',
+    packages=setuptools.find_packages(include=['pyezviz']),
+    setup_requires=[
+        'requests',
+        'setuptools'
+    ],
+    install_requires=[
+        'requests',
+        'fake_useragent',
+        'uuid',
+        'pandas'
+    ],
+    entry_points={
+    'console_scripts': [
+        'pyezviz = pyezviz.__main__:main'
+    ]
+    }
+)
