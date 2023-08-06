9292ffbaf74fce03afe709a91a68680f3e17293c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package mini-tic-tac-toe/0.0.2/mini-tic-tac-toe-0.0.2.tar.gz

---
+++ packages/mini-tic-tac-toe/mini-tic-tac-toe-0.0.2.tar.gz/mini-tic-tac-toe-0.0.2/README.md
@@ -0,0 +1,17 @@
+---
+__ Mini Tic Tac Toe __
+
+A mini game written in Python
+
+___
+
+How to use:
+
+```python
+from game import TicTacToe
+
+game = TicTacToe()
+
+game.play()
+
+```
---
+++ packages/mini-tic-tac-toe/mini-tic-tac-toe-0.0.2.tar.gz/mini-tic-tac-toe-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mini-tic-tac-toe",
+    version="0.0.2",
+    description="Library to play Tic Tac Toe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MesterLum/tic-tac-toe",
+    author="Eduardo Cuauhtémoc Paez Palafox",
+    author_email="mesterlum@hotmail.com",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    license='MIT',
+)
