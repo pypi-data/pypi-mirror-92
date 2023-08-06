7554ed8f8a60eca7dc0e073818f3bbcace0f50d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:31 +0000

    Add package galaxy-language-server/0.3.2/galaxy_language_server-0.3.2-py3-none-any.whl

---
+++ packages/galaxy-language-server/galaxy_language_server-0.3.2-py3-none-any.whl/galaxyls/features.py
@@ -0,0 +1,6 @@
+"""This module contains definitions for custom features supported
+   by the Galaxy Tools Language Server."""
+
+AUTO_CLOSE_TAGS = "galaxytools.completion.autoCloseTags"
+CMD_GENERATE_TEST = "galaxytools.generate.tests"
+CMD_GENERATE_COMMAND = "galaxytools.generate.command"
