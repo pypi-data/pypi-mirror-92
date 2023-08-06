25e9998d7ee256410ff0fc9e59c7c85a9450f6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:45 +0000

    Add package sjjcb-py-captcha-util/1.2.4/sjjcb-py-captcha-util-1.2.4.tar.gz

---
+++ packages/sjjcb-py-captcha-util/sjjcb-py-captcha-util-1.2.4.tar.gz/sjjcb-py-captcha-util-1.2.4/setup.py
@@ -0,0 +1,28 @@
+import sys
+
+# if sys.version_info < (3, 0):
+#     print(sys.stderr, "{}: need Python 3, 0 or later.".format(sys.argv[0]))
+#     print(sys.stderr, "Your Python is {}".format(sys.version))
+#     sys.exit(1)
+
+#with open('README.rst', 'r', encoding='utf8') as fh:
+#   long_description = fh.read()
+
+from setuptools import setup, find_packages
+
+setup(
+                 name='sjjcb-py-captcha-util',
+                 version='1.2.4',
+                 description='Public POM project: sjjcb-py-captcha-util ',
+                 author='zijian',
+                 license='Apache License 2.0',
+                 author_email='ah.fanjian@aisino.com',
+                 url='http://www.example.com',
+                 packages=['sjjcb-py-captcha-util'],
+                 zip_safe=False,
+                 include_package_data=True,
+                 classifiers=(
+                     "Programming Language :: Python :: 3",
+                     "Operating System :: OS Independent"
+                 )
+)
