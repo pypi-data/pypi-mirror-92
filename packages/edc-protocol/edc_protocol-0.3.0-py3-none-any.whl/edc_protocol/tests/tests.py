d7798659f9abc4257d9a0203fc2967eb8e2549bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:41 +0000

    Add package edc-protocol/0.3.0/edc_protocol-0.3.0-py3-none-any.whl

---
+++ packages/edc-protocol/edc_protocol-0.3.0-py3-none-any.whl/edc_protocol/tests/tests.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+from dateutil.relativedelta import relativedelta
+from django.test import override_settings, TestCase, tag
+from edc_protocol import Protocol
+from edc_utils import get_utcnow
+
+opendte = get_utcnow() - relativedelta(years=2)
+closedte = get_utcnow() + relativedelta(years=1)
+
+
+class TestProtocol(TestCase):
+    @override_settings(
+        EDC_PROTOCOL_STUDY_OPEN_DATETIME=get_utcnow() - relativedelta(years=2),
+        EDC_PROTOCOL_STUDY_CLOSE_DATETIME=get_utcnow() + relativedelta(years=1),
+    )
+    def test_protocol(self):
+        self.assertEqual(Protocol().study_open_datetime.date(), opendte.date())
+        self.assertEqual(Protocol().study_close_datetime.date(), closedte.date())
