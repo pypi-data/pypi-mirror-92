ac96239808210f819c69e39d39d0130c21471141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package axit/0.0.1/axit-0.0.1-py3-none-any.whl

---
+++ packages/axit/axit-0.0.1-py3-none-any.whl/axit/mod1.py
@@ -0,0 +1,16 @@
+
+
+
+
+def ins(v):
+    print("ins>>>")
+    print('>dir:')
+    print(dir(v))
+    print('>type:')
+    print(type(v))
+    print('>print:')
+    print(v)
+    print("ins<<<")
+
+
+
