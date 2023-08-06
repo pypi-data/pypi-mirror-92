61fb1af41ad066baff946689527cadc59873473a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:22 +0000

    Add package backups/0.2.24/backups-0.2.24-py2.py3-none-any.whl

---
+++ packages/backups/backups-0.2.24-py2.py3-none-any.whl/backups/notify.py
@@ -0,0 +1,45 @@
+import json
+import requests
+
+from .resolve import resolve
+
+
+def _size(context):
+  if context.compress is []:
+    return "-"
+
+  size = ""
+  for compression in context.compress:
+    size = f"{size}{compression['size']['human']}, "
+
+  size = f"{size[0:-2]}"
+  return size
+
+
+def notify_slack(config, context):
+  webhook = resolve(config.get("webhook"))
+  text    = f"Backup `{context.job}` completed with *{_size(context)}*"
+  channel = resolve(config.get("channel", "#backups"))
+
+  payload = {
+    "text": text,
+    "channel": channel,
+  }
+
+  res = requests.post(webhook,
+    data=json.dumps(payload),
+    headers={'Content-Type': 'application/json'}
+  )
+
+  if res.status_code != 200:
+    raise RuntimeError(
+      'Request to slack returned an error %s, the response is:\n%s'
+      % (res.status_code, res.text)
+    )
+
+  context.notify.append({
+    "type": "slack",
+    "text": text,
+    "status": res.status_code,
+    "channel": channel,
+  })
