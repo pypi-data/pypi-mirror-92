aced0dbfa719bb928a4d39f88c124ff529f5182f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:02 +0000

    Add package msrest/0.6.20/msrest-0.6.20.tar.gz

---
+++ packages/msrest/msrest-0.6.20.tar.gz/msrest-0.6.20/setup.py
@@ -0,0 +1,69 @@
+﻿# --------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the ""Software""), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+# --------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+setup(
+    name='msrest',
+    version='0.6.20',
+    author='Microsoft Corporation',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    url=("https://github.com/Azure/msrest-for-python"),
+    license='MIT License',
+    description='AutoRest swagger generator Python client runtime.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development'],
+    install_requires=[
+        "requests~=2.16",
+        "requests_oauthlib>=0.5.0",
+        "isodate>=0.6.0",
+        "certifi>=2017.4.17",
+    ],
+    include_package_data=True,
+    package_data={
+        'pytyped': ['py.typed'],
+    },
+    extras_require={
+        ":python_version<'3.4'": ['enum34>=1.0.4'],
+        ":python_version<'3.5'": ['typing'],
+        "async:python_version>='3.5'": [
+            'aiohttp>=3.0',
+            'aiodns'
+        ],
+    }
+)
