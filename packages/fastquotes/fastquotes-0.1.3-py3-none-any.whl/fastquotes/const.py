954bc4b8b58aa64a71b217a1b1f9e76a88d25363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:02 +0000

    Add package fastquotes/0.1.3/fastquotes-0.1.3-py3-none-any.whl

---
+++ packages/fastquotes/fastquotes-0.1.3-py3-none-any.whl/fastquotes/const.py
@@ -0,0 +1,16 @@
+import os
+
+SINA_BASE_URL = "http://hq.sinajs.cn/list="
+TENCENT_BASE_URL = "http://qt.gtimg.cn/q="
+REQ_CODES_NUM_MAX = 300
+HEADERS = {
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/54.0.2840.100 "
+        "Safari/537.36"
+    ),
+}
+TRADE_DAYS_FILE_PATH = os.path.realpath(
+    os.path.join(os.path.dirname(__file__), "static/trade_days.txt")
+)
