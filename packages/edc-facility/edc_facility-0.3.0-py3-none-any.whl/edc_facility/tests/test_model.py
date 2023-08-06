c93c3ca8fa6ed8c3e214af8d4f64cee29a2bb71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:41 +0000

    Add package edc-facility/0.3.0/edc_facility-0.3.0-py3-none-any.whl

---
+++ packages/edc-facility/edc_facility-0.3.0-py3-none-any.whl/edc_facility/tests/test_model.py
@@ -0,0 +1,11 @@
+from django.test import TestCase, tag
+from edc_facility.models import Holiday
+from edc_utils import get_utcnow
+
+
+class TestModel(TestCase):
+    def test_str(self):
+        obj = Holiday.objects.create(
+            country="botswana", local_date=get_utcnow().date(), name="holiday"
+        )
+        self.assertTrue(str(obj))
