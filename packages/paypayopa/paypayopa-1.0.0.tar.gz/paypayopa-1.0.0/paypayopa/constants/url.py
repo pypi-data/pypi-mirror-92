ab2e989a9803884e4624e31f619e6cca72a5d5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:51 +0000

    Add package paypayopa/1.0.0/paypayopa-1.0.0.tar.gz

---
+++ packages/paypayopa/paypayopa-1.0.0.tar.gz/paypayopa-1.0.0/paypayopa/constants/url.py
@@ -0,0 +1,10 @@
+class URL(object):
+    SANDBOX_BASE_URL = 'https://stg-api.sandbox.paypay.ne.jp'
+    PRODUCTION_BASE_URL = 'https://api.paypay.ne.jp'
+    RESOLVE = 'https://developer.paypay.ne.jp/develop/resolve'
+    CODE = "/v2/codes"
+    PAYMENT = "/v2/payments"
+    ACCOUNT_LINK = "/v1/qr/sessions"
+    PENDING_PAYMENT = "/v1/requestOrder"
+    USER_AUTH = "/v2/user/authorizations"
+    REFUNDS = "/v2/refunds"
