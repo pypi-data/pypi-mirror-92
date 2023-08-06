e71a864caf9146449472922d134cf25b6cfe7770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:18 +0000

    Add package visionscaper-pybase/0.3.2/visionscaper_pybase-0.3.2.tar.gz

---
+++ packages/visionscaper-pybase/visionscaper_pybase-0.3.2.tar.gz/visionscaper_pybase-0.3.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='visionscaper_pybase',
+    version='0.3.2',
+    packages=['basics',],
+    license='MIT license',
+    long_description=open('README.md').read(),
+)
