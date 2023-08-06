14f49527c164e5243ab8d753fa208d336f5f1039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:35 +0000

    Add package edc-ltfu/0.3.0/edc_ltfu-0.3.0-py3-none-any.whl

---
+++ packages/edc-ltfu/edc_ltfu-0.3.0-py3-none-any.whl/edc_ltfu/tests/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm
+from edc_crf.modelform_mixins import CrfModelFormMixin
+from edc_visit_tracking.form_validators import VisitMissedFormValidator
+
+from edc_metadata.tests.models import SubjectVisitMissed
+
+
+class SubjectVisitMissedForm(CrfModelFormMixin, ModelForm):
+
+    form_validator_cls = VisitMissedFormValidator
+
+    def validate_against_consent(self):
+        pass
+
+    class Meta:
+        model = SubjectVisitMissed
+        fields = "__all__"
