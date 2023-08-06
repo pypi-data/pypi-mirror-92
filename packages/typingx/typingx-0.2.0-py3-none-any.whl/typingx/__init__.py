719fbc085114d72b416836c2007412335979574f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:01 +0000

    Add package typingx/0.2.0/typingx-0.2.0-py3-none-any.whl

---
+++ packages/typingx/typingx-0.2.0-py3-none-any.whl/typingx/__init__.py
@@ -0,0 +1,39 @@
+# flake8: noqa
+from typing import *
+
+from .main import isinstancex
+from .types import Listx, Tuplex
+from .typing_compat import (
+    Literal,
+    NoneType,
+    TypedDict,
+    get_args,
+    get_origin,
+    is_literal,
+    is_typeddict,
+)
+
+__all__ = (
+    # main methods
+    "isinstancex",
+    # typing, typing_extensions or own backport
+    "Any",
+    "Dict",
+    "Literal",
+    "List",
+    "Listx",
+    "Mapping",
+    "Optional",
+    "NoneType",
+    "Union",
+    "Set",
+    "Sequence",
+    "Tuple",
+    "Tuplex",
+    "Type",
+    "TypedDict",
+    "get_args",
+    "get_origin",
+    "is_literal",
+    "is_typeddict",
+)
