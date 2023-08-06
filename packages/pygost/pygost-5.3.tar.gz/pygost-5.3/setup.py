be957c0c533b5f72d0fbcd2240b7205f8346e0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:58 +0000

    Add package pygost/5.3/pygost-5.3.tar.gz

---
+++ packages/pygost/pygost-5.3.tar.gz/pygost-5.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+version = open("VERSION", "rb").read().strip().decode("ascii")
+
+setup(
+    name="pygost",
+    version=version,
+    description="Pure Python GOST cryptographic functions library",
+    long_description=open("README", "rb").read().decode("utf-8"),
+    author="Sergey Matveev",
+    author_email="stargrave@stargrave.org",
+    url="http://www.pygost.cypherpunks.ru/",
+    license="GPLv3",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=["pygost", "pygost.asn1schemas"],
+    package_data={
+        "pygost": ["stubs/**/*.pyi"],
+    },
+    data_files=(
+        ("", (
+            "AUTHORS",
+            "COPYING",
+            "FAQ",
+            "INSTALL",
+            "NEWS",
+            "README",
+            "THANKS",
+            "VERSION",
+        )),
+    ),
+    tests_require=["pyderasn~=8.1"],
+)
