47bafd95ab81cfffd63906c8c850c1070e0db6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:47 +0000

    Add package nitrogen/2.0.dev1/nitrogen-2.0.dev1.tar.gz

---
+++ packages/nitrogen/nitrogen-2.0.dev1.tar.gz/nitrogen-2.0.dev1/nitrogen/coordsys/__init__.py
@@ -0,0 +1,22 @@
+"""
+nitrogen.coordsys
+-----------------
+
+This module implements the CoordSys base class,
+which is extended for all NITROGEN coordinate
+systems.
+
+"""
+
+from . import coordsys 
+from . import simple_builtins
+from . import zmat
+
+from .coordsys import *
+from .simple_builtins import *
+from .zmat import *
+
+__all__ = []
+__all__ += coordsys.__all__
+__all__ += simple_builtins.__all__
+__all__ += zmat.__all__
---
+++ packages/nitrogen/nitrogen-2.0.dev1.tar.gz/nitrogen-2.0.dev1/nitrogen/pes/library/__init__.py
@@ -0,0 +1,15 @@
+"""
+nitrogen.pes.library
+--------------------
+
+A collection of potential energy surfaces. These should
+be accessed via the :func:`nitrogen.pes.loadpes()` function.
+
+PES name        Description
+==============  ==================================================
+H2O_PJT1996     Water. Cartesian coordinates H--O--H.
+==============  ==================================================
+
+
+"""
+
