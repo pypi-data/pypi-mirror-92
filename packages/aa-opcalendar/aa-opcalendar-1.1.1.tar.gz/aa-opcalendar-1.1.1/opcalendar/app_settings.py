aefd843e8be2f448d3761d7e0b9882c5b2b0d48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:41 +0000

    Add package aa-opcalendar/1.1.1/aa-opcalendar-1.1.1.tar.gz

---
+++ packages/aa-opcalendar/aa-opcalendar-1.1.1.tar.gz/aa-opcalendar-1.1.1/opcalendar/app_settings.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from .utils import clean_setting
+import re
+
+def get_site_url():  # regex sso url
+    regex = r"^(.+)\/s.+"
+    matches = re.finditer(regex, settings.ESI_SSO_CALLBACK_URL, re.MULTILINE)
+    url = "http://"
+
+    for m in matches:
+        url = m.groups()[0] # first match
+
+    return url
+
+# Hard timeout for tasks in seconds to reduce task accumulation during outages
+OPCALENDAR_TASKS_TIME_LIMIT = clean_setting("OPCALENDAR_TASKS_TIME_LIMIT", 7200)
+
+# whether admins will get notifications about import events
+OPCALENDAR_ADMIN_NOTIFICATIONS_ENABLED = clean_setting(
+    "OPCALENDAR_ADMIN_NOTIFICATIONS_ENABLED", True
+)
+
+# whether we should send out discord notifications for imported fleets
+OPCALENDAR_NOTIFY_IMPORTS = clean_setting(
+    "OPCALENDAR_NOTIFY_IMPORTS", True
+)
+
+OPCALENDAR_EVE_UNI_URL = "https://portal.eveuniversity.org/api/getcalendar"
+OPCALENDAR_SPECTRE_URL = "https://www.spectre-fleet.space/engagement/events/rss"
