6a8cc2b14f3b253131944a0d91080f563a71c0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:57 +0000

    Add package vasppy/0.6.3.0/vasppy-0.6.3.0.tar.gz

---
+++ packages/vasppy/vasppy-0.6.3.0.tar.gz/vasppy-0.6.3.0/README.md
@@ -0,0 +1,22 @@
+# vasppy - a Python suite for manipulating VASP files
+
+[![DOI](https://zenodo.org/badge/17946870.svg)](https://zenodo.org/badge/latestdoi/17946870)
+[![PyPI version](https://badge.fury.io/py/vasppy.svg)](https://badge.fury.io/py/vasppy)
+[![Build Status](https://travis-ci.org/bjmorgan/vasppy.svg?branch=master)](https://travis-ci.org/bjmorgan/vasppy)
+[![Test Coverage](https://codeclimate.com/github/bjmorgan/vasppy/badges/coverage.svg)](https://codeclimate.com/github/bjmorgan/vasppy/coverage)
+[![Documentation Status](https://readthedocs.org/projects/vasppy/badge/?version=latest)](http://vasppy.readthedocs.io/en/latest/?badge=latest)
+
+`vasppy` is a suite of Python tools and scripts written in Python for manipulating and processing [VASP](https://www.vasp.at/) input and output files.
+
+## Tests
+
+Automated testing of the latest build happens [here](https://travis-ci.org/bjmorgan/vasppy).
+
+Manual tests can be run using
+```
+python3 -m unittest discover
+```
+
+## API documentation
+
+API documentation is hosted at [Read the Docs](http://vasppy.readthedocs.io/en/latest/).
---
+++ packages/vasppy/vasppy-0.6.3.0.tar.gz/vasppy-0.6.3.0/setup.py
@@ -0,0 +1,59 @@
+"""
+vasppy: Python utilities for working with VASP inputs and outputs.
+"""
+
+from setuptools import setup, find_packages
+from vasppy import __version__ as VERSION
+
+readme = 'README.md'
+long_description = open(readme).read()
+
+scripts = ['check_species',
+           'murnfit', 
+           'vasp_summary', 
+           'poscar_to_cif', 
+           'potcar_spec',
+           'effective_mass',
+           'fat_bands',
+           'pimaim_to_poscar',
+           'pimaim_to_xtl',
+           'poscar_sort',
+           'poscar_to_pimaim',
+           'poscar_to_xtl',
+           'proc_poscar',
+           'rotate_poscar',
+           'spacegroup',
+           'vasp_grid',
+           'xdatcar_to_disp',
+           'xdatcar_to_poscart',
+           'xdatcar_to_rdf']
+
+setup(
+    name='vasppy',
+    version=VERSION,
+    description='Python utilities for working with VASP inputs and outputs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Benjamin J. Morgan',
+    author_email='bjm42@bath.ac.uk',
+    url='https://github.com/bjmorgan/vasppy', 
+    download_url='https://github.com/bjmorgan/vasppy/archive/{}.tar.gz'.format(VERSION),
+    keywords=['vasp'], # keywords
+    packages=find_packages(exclude=['docs', 'tests*']),
+    package_data={'vasppy': ['data/*.yaml']},
+    entry_points={'console_scripts':[
+                      '{} = vasppy.scripts.{}:main'.format(s, s) for s in scripts]},
+    license='MIT',
+    install_requires=['monty',
+                      'numpy>=1.16.2',
+                      'pandas',
+                      'pymatgen',
+                      'PyYAML', 
+                      'coverage==4.3.4',
+                      'codeclimate-test-reporter',
+                      'fortranformat',
+                      'scipy>=1.4.1', 
+                      'tqdm',
+                      'lxml'],
+    python_requires='>=3.6'
+    )
