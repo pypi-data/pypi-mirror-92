bea39cff5a43d5d03a90ec2892de5c3b7727937e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:38 +0000

    Add package matrix-operations-for-the-basic-people/0.1/matrix_operations_for_the_basic_people-0.1.tar.gz

---
+++ packages/matrix-operations-for-the-basic-people/matrix_operations_for_the_basic_people-0.1.tar.gz/matrix_operations_for_the_basic_people-0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+setup(name='matrix_operations_for_the_basic_people',
+      version='0.1',
+      description='Basic matrix operations',
+      packages=['matrix_operations'],
+      author = 'Jimmy Lin',
+      author_email='jimmylinzhe@hotmail.com',
+      zip_safe = False)
+
