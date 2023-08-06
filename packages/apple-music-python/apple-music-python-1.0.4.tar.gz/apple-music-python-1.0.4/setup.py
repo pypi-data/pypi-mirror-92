107592094c1a07bda0e53fd588ac57cc072f672f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:43 +0000

    Add package apple-music-python/1.0.4/apple-music-python-1.0.4.tar.gz

---
+++ packages/apple-music-python/apple-music-python-1.0.4.tar.gz/apple-music-python-1.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='apple-music-python',
+    url='https://github.com/mpalazzolo/apple-music-python',
+    version='1.0.4',
+    packages=['applemusicpy'],
+    license='LICENSE.txt',
+    author='Matt Palazzolo',
+    author_email='mattpalazzolo@gmail.com',
+    description='A python wrapper for the Apple Music API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.20.1',
+        'pyjwt>=1.6.4',
+        'cryptography>=2.4.2'
+    ],
+)
