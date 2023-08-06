20a76c3a99912faa223dca4b173629c1f2390e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:15 +0000

    Add package vlivepy/0.1.1/vlivepy-0.1.1-py3-none-any.whl

---
+++ packages/vlivepy/vlivepy-0.1.1-py3-none-any.whl/vlivepy/utils.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# empty module to bind functions
+
+from .connections import postIdToVideoSeq, getVpdid2, getVodId, getUpcomingList
+from .controllers import dumpSession, loadSession
