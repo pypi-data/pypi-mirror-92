1528b4e41c7f01a046165bfc2749d8109c240311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package distributions3470/34.70/distributions3470-34.70.tar.gz

---
+++ packages/distributions3470/distributions3470-34.70.tar.gz/distributions3470-34.70/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions3470',
+      version='34.70',
+      description='Gaussian distributions',
+      packages=['distributions3470'],
+      zip_safe=False)
