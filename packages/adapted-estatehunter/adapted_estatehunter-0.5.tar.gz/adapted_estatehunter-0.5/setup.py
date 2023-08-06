6aad4737794877fa23b10eb20b549f35def9a84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:02 +0000

    Add package adapted-estatehunter/0.5/adapted_estatehunter-0.5.tar.gz

---
+++ packages/adapted-estatehunter/adapted_estatehunter-0.5.tar.gz/adapted_estatehunter-0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+  name='adapted_estatehunter',         # How you named your package folder (MyLib)
+  packages=['adapted_estatehunter'],   # Chose the same as "name"
+  version='0.5',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description='Testing',   # Give a short description about your library
+  author='Odd',                   # Type in your name
+  author_email='odd@adapted.no',      # Type in your E-Mail
+  keywords=['Testing'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'bugsnag',
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
