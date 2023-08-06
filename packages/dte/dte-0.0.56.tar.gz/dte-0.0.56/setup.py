26287a5bc03b4963b0f972a615f415de79819ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:33 +0000

    Add package dte/0.0.56/dte-0.0.56.tar.gz

---
+++ packages/dte/dte-0.0.56.tar.gz/dte-0.0.56/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import setuptools
+import os
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='dte',  
+     version='0.0.56',
+     author="Marcelo V. Rozanti",
+     author_email="mvrozanti@hotmail.com",
+     description="Date Time Expressions",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/mvrozanti/dte",
+     packages=setuptools.find_packages('dte'),
+     install_requires=[
+         'python-dateutil', 'ply'
+         ],
+     scripts=[
+         'dte/dte',
+         ],
+     classifiers=[
+         "Development Status :: 4 - Beta",
+         "Topic :: Artistic Software",
+         "Intended Audience :: Developers",
+         "Programming Language :: Python :: 3.4",
+         "Programming Language :: Python :: 3.5",
+         "Programming Language :: Python :: 3.6",
+         "Programming Language :: Python :: 3 :: Only",
+         ],
+ )
