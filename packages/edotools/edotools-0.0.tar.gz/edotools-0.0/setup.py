4ec2a216fdeeb0ec546e2102594454aeb1f1396b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:59 +0000

    Add package edotools/0.0/edotools-0.0.tar.gz

---
+++ packages/edotools/edotools-0.0.tar.gz/edotools-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='edotools',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['edotools'],
+      author_email='edvard88@inbox.ru',
+      zip_safe=False)
