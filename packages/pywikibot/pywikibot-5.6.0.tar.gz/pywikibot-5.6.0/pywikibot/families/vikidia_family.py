e062713a1165559d1c2f75c9f645d3fe42103b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:27 +0000

    Add package pywikibot/5.6.0/pywikibot-5.6.0.tar.gz

---
+++ packages/pywikibot/pywikibot-5.6.0.tar.gz/pywikibot-5.6.0/pywikibot/families/vikidia_family.py
@@ -0,0 +1,24 @@
+"""Family module for Vikidia."""
+#
+# (C) Pywikibot team, 2010-2020
+#
+# Distributed under the terms of the MIT license.
+#
+from pywikibot import family
+
+
+class Family(family.SubdomainFamily):
+
+    """Family class for Vikidia."""
+
+    name = 'vikidia'
+    domain = 'vikidia.org'
+
+    codes = ['ca', 'de', 'el', 'en', 'es', 'eu', 'fr', 'hy', 'it', 'ru', 'scn']
+
+    # Sites we want to edit but not count as real languages
+    test_codes = ['central', 'test']
+
+    def protocol(self, code):
+        """Return https as the protocol for this family."""
+        return 'https'
