81d5ffcca1af9db35bd10f809bbe7f81bd16eb1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:03 +0000

    Add package techmanpy/1.2/techmanpy-1.2.tar.gz

---
+++ packages/techmanpy/techmanpy-1.2.tar.gz/techmanpy-1.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import sys
+
+if sys.version_info[:2] < (3, 8): raise Exception("'techmanpy' requires Python >= 3.8")
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+   long_description = fh.read()
+
+setuptools.setup(
+   name='techmanpy',
+   version='1.2',
+   description='Communication driver for Techman robots',
+   long_description=long_description,
+   long_description_content_type='text/markdown',
+   author='Jules van der Toorn',
+   author_email='julesvandertoorn@gmail.com',
+   license='MIT',
+   classifiers=[
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.8',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: POSIX :: Linux',
+      'Intended Audience :: Science/Research',
+      'Natural Language :: English'
+   ],
+   keywords='techman driver manipulator robotics',
+   url='https://github.com/jvdtoorn/techmanpy',
+   project_urls={
+      'Source': 'https://github.com/jvdtoorn/techmanpy'
+   },
+   packages=setuptools.find_packages(),
+   package_dir={'': '.'},
+   python_requires='~=3.8',
+)
