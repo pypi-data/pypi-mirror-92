f5507b84da01950d7bc9fa4e93f436819b101858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:09 +0000

    Add package dbcli-mongo-redis/0.1.7/dbcli_mongo_redis-0.1.7.tar.gz

---
+++ packages/dbcli-mongo-redis/dbcli_mongo_redis-0.1.7.tar.gz/dbcli_mongo_redis-0.1.7/dbcli_mongo_redis/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='dbcli_mongo_redis',
+      version='0.1.7',
+      description='Mongo and redis cli',
+      url='https://github.com/orasul/dbcli',
+      packages=['dbcli_mongo_redis.mcli', 'dbcli_mongo_redis.rcli', 'dbcli_mongo_redis'],
+      author='Rasul Osmanov',
+      install_requires=[
+          'click',
+          'flatten_json',
+          'pymongo',
+          'redis'
+      ],
+      entry_points={
+          'console_scripts': [
+              'rcli = dbcli_mongo_redis.rcli.rcli:_main',
+              'mcli = dbcli_mongo_redis.mcli.mcli:_main',
+          ],
+      },
+      include_package_data=True)
