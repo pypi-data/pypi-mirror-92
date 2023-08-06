4f1c7ee633110ceee8f7915d6bfa2f182aa89c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:42 +0000

    Add package python-gvm/21.1.2/python_gvm-21.1.2-py3-none-any.whl

---
+++ packages/python-gvm/python_gvm-21.1.2-py3-none-any.whl/gvm/protocols/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 - 2019 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Package for supported Greenbone Protocol versions.
+
+Currently `GMP version 7`_, `GMP version 8`_, `GMP version 9`_ ,
+`GMP version 20.08`_ and `OSP version 1`_ are supported.
+
+.. _GMP version 7:
+    https://docs.greenbone.net/API/GMP/gmp-7.0.html
+.. _GMP version 8:
+    https://docs.greenbone.net/API/GMP/gmp-8.0.html
+.. _GMP version 9:
+    https://docs.greenbone.net/API/GMP/gmp-9.0.html
+.. _GMP version 20.08:
+    https://docs.greenbone.net/API/GMP/gmp-20.08.html
+.. _OSP version 1:
+    https://docs.greenbone.net/API/OSP/osp-1.2.html
+"""
