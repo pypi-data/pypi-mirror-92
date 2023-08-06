1d0d150748a5cfecef0ede922290ee2ef27e16a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:42 +0000

    Add package Pypterminals/0.0.1/Pypterminals-0.0.1.tar.gz

---
+++ packages/Pypterminals/Pypterminals-0.0.1.tar.gz/Pypterminals-0.0.1/pypterminals/__init__.py
@@ -0,0 +1,3 @@
+from .terminal import AUTHOR as author
+from .terminal import NAME as name
+from .terminal import VERSION as version
---
+++ packages/Pypterminals/Pypterminals-0.0.1.tar.gz/Pypterminals-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='Pypterminals',
+    version='0.0.1',
+    description='A graphic process terminal, to solve the process only one command terminal problem',
+    author='binn',
+    author_email='binn@pigwow.cn',
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3"],
+    )
