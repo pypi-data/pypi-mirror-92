fe1065c8df1b424d5d659f8fe540ed2812a17300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:35 +0000

    Add package dudesec/0.0.9/dudesec-0.0.9.tar.gz

---
+++ packages/dudesec/dudesec-0.0.9.tar.gz/dudesec-0.0.9/dudesec/dudesec.py
@@ -0,0 +1,23 @@
+## Importando librarys
+
+import socket
+import os
+import random
+from urllib.request import urlopen
+from ping3 import ping, verbose_ping
+from bs4 import BeautifulSoup
+from itertools import permutations
+import requests 
+import base64
+from random import choice
+import string
+
+## Importando functions
+
+from application_monitoring.backend import *
+from application_monitoring.frontend import *
+from attack_tools.bruteforce import *
+from criptography.base64 import *
+from data_synthesis.cpf import *
+from data_synthesis.password import *
+from vulnerability_scan.server_port import *
---
+++ packages/dudesec/dudesec-0.0.9.tar.gz/dudesec-0.0.9/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dudesec",
+    version="0.0.9",
+    author="Gustavo de Oliveira Rosa",
+    author_email="gustavoolrosa2019@gmail.com",
+    description="Library para monitoramento de sistemas e exploração de vulnerabilidades.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GustavoOliveiraRosa/DudSecPython",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'socket',
+        'os',
+        'random',
+        'urllib.request',
+        'ping3',
+        'bs4',
+        'itertools',
+        'requests',
+        'base64',
+        'random',
+        'string',
+    ],
+    python_requires='>=3.6',
+)
