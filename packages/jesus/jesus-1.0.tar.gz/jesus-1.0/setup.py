70c48b0e294b982e7579240ffcfeb75abcc3e5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:34 +0000

    Add package jesus/1.0/jesus-1.0.tar.gz

---
+++ packages/jesus/jesus-1.0.tar.gz/jesus-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="jesus",
+    packages=["jesus"],
+    version="1.0",
+    license="unlicense",
+    description="Turns everything into Wine.",
+    author="megahomyak",
+    author_email="g.megahomyak@gmail.com",
+    url="https://github.com/megahomyak/jesus",
+    download_url="https://github.com/megahomyak/jesus/archive/v1.0.tar.gz"
+)
