50a0d6dd608adb3c7ed554eb8a7f0ea7ce317d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package scurri-zipcode/0.1.1/scurri_zipcode-0.1.1.tar.gz

---
+++ packages/scurri-zipcode/scurri_zipcode-0.1.1.tar.gz/scurri_zipcode-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='scurri_zipcode',
+    packages=find_packages(include=['scurri_zipcode']),
+    version='0.1.1',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    description='Python library to validate zip codes in the UK for Scurri interview',
+    url="https://github.com/JuliColombo/scurri-zipcode",
+    author='Julieta Colombo',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests'
+)
