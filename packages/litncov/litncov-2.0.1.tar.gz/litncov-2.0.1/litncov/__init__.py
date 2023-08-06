bc667425d424855abceb0d0d1952de4ce57810cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:17 +0000

    Add package litncov/2.0.1/litncov-2.0.1.tar.gz

---
+++ packages/litncov/litncov-2.0.1.tar.gz/litncov-2.0.1/litncov/__init__.py
@@ -0,0 +1,16 @@
+# the platform url
+hostname = "http://hmgr.sec.lit.edu.cn/wms"
+
+# the main api
+endpoints = {
+    "login": hostname + "/healthyLogin",
+    "lastRecord": hostname + "/lastHealthyRecord",
+    "firstRecord": hostname + "/addHealthyRecord",
+    "secondRecord": hostname + "/addTwoHealthyRecord",
+    "thirdRecord": hostname + "/addThreeHealthyRecord",
+    "getInstructor": hostname + "/getInstructor",
+    "getFamilys": hostname + "/familys/personal",
+    "getTrips": hostname + "/trips/personal",
+    "getImportantCity": hostname + "/teamImportantCity",
+    "queryRecord": hostname + "/healthyRecordByUser",
+}
