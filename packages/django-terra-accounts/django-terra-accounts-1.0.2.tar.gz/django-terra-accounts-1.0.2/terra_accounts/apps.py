467758754f99638c4c9150d7f6b6f2438e7bad01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:27 +0000

    Add package django-terra-accounts/1.0.2/django-terra-accounts-1.0.2.tar.gz

---
+++ packages/django-terra-accounts/django-terra-accounts-1.0.2.tar.gz/django-terra-accounts-1.0.2/terra_accounts/apps.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from terra_settings.settings import TERRA_APPLIANCE_SETTINGS
+from rest_framework_jwt.settings import api_settings
+
+from .permissions_mixins import PermissionRegistrationMixin
+
+
+class AccountsConfig(PermissionRegistrationMixin, AppConfig):
+    name = 'terra_accounts'
+    verbose_name = 'Terra Accounts'
+
+    permissions = (
+        ('User', 'can_manage_users', 'Is able to create, delete, update users'),
+        ('UserGroup', 'can_manage_groups', 'Is able to create, delete, update groups'),
+    )
+
+    def ready(self):
+        # terra_accounts need to add jwt_delta key in terra-settings settings endpoint
+        TERRA_APPLIANCE_SETTINGS.setdefault('jwt_delta', api_settings.JWT_EXPIRATION_DELTA)
+        super().ready()
