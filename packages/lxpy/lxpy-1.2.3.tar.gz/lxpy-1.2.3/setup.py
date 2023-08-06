74f789fcd52f14f1e07c0ef8b18b7a41a83d01db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:04 +0000

    Add package lxpy/1.2.3/lxpy-1.2.3.tar.gz

---
+++ packages/lxpy/lxpy-1.2.3.tar.gz/lxpy-1.2.3/setup.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+# @Author  : lx
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'lxpy',
+    version = '1.2.3',
+    author = 'ying5338619',
+    author_email = '125066648@qq.com',
+    install_requires=['lxml'],
+    packages=find_packages(),
+    url='https://github.com/lixi5338619/lxpy.git',
+    description = 'Web crawler and data processing toolkit !',
+    long_description = open('readme.md','r',encoding='utf-8').read(),
+)
