b0db96c0cd37e5ccac68d955242967561279b80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:59 +0000

    Add package unidic-lite/1.0.8a1/unidic-lite-1.0.8a1.tar.gz

---
+++ packages/unidic-lite/unidic-lite-1.0.8a1.tar.gz/unidic-lite-1.0.8a1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from distutils.core import setup
+from io import open
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    README = f.read()
+
+setup(name='unidic-lite', 
+      version='1.0.8a1',
+      author="Paul O'Leary McCann",
+      author_email="polm@dampfkraft.com",
+      description="A small version of UniDic packaged for Python",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url="https://github.com/polm/unidic-lite",
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: Japanese",
+      ],
+      package_data={'unidic_lite': ['dicdir/*']}
+      )
