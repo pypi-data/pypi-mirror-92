d7c97aec33ff01afdad80c259c8b805476ab3814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:15 +0000

    Add package maxflow/0.0.1/maxflow-0.0.1.tar.gz

---
+++ packages/maxflow/maxflow-0.0.1.tar.gz/maxflow-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="maxflow", # Replace with your own username
+    version="0.0.1", # Will be updated to 0.1 when the first working version becomes ready.
+    author="Touqir Sajed",
+    author_email="shuhash6@gmail.com",
+    description="",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # python_requires='>=3.6',
+)
