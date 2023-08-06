a0c5cc8759d49779fb1b7522d31568c3db88d59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:47 +0000

    Add package adal/1.2.6/adal-1.2.6.tar.gz

---
+++ packages/adal/adal-1.2.6.tar.gz/adal-1.2.6/setup.py
@@ -0,0 +1,84 @@
+﻿#!/usr/bin/env python
+#------------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation.
+# All rights reserved.
+#
+# This code is licensed under the MIT License.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions :
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#------------------------------------------------------------------------------
+
+from setuptools import setup
+import re, io
+
+# setup.py shall not import adal
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+    io.open('adal/__init__.py', encoding='utf_8_sig').read()
+    ).group(1)
+
+# To build:
+# python setup.py sdist
+# python setup.py bdist_wheel
+#
+# To install:
+# python setup.py install
+#
+# To register (only needed once):
+# python setup.py register
+#
+# To upload:
+# python setup.py sdist upload
+# python setup.py bdist_wheel upload
+
+setup(
+    name='adal',
+    version=__version__,
+    description=('Note: This library is already replaced by MSAL Python, ' +
+                 'available here: https://pypi.org/project/msal/ .' +
+                 'ADAL Python remains available here as a legacy. ' +
+                 'The ADAL for Python library makes it easy for python ' +
+                 'application to authenticate to Azure Active Directory ' +
+                 '(AAD) in order to access AAD protected web resources.'),
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='nugetaad@microsoft.com',
+    url='https://github.com/AzureAD/azure-activedirectory-library-for-python',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=['adal'],
+    install_requires=[
+        'PyJWT>=1.0.0,<3',
+        'requests>=2.0.0,<3',
+        'python-dateutil>=2.1.0,<3',
+        'cryptography>=1.1.0'
+    ]
+)
