3b3a07a77837f477476060d0c60e3555a7de4a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:36 +0000

    Add package django-dnoticias-services/1.0.2/django_dnoticias_services-1.0.2.tar.gz

---
+++ packages/django-dnoticias-services/django_dnoticias_services-1.0.2.tar.gz/django_dnoticias_services-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="django_dnoticias_services",
+    version='1.0.2',
+    url="https://www.dnoticias.pt/",
+    author="Pedro Mendes",
+    author_email="pedro.trabalho.uma@gmail.com",
+    license="MIT",
+    install_requires=[
+        'requests',
+        'django'
+    ]
+)
