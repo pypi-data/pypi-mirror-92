7d9d6e12a3d776f68422f3e7caa3e53497ed296e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:07 +0000

    Add package reecapi/0.0.2/reecapi-0.0.2.tar.gz

---
+++ packages/reecapi/reecapi-0.0.2.tar.gz/reecapi-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="reecapi",
+    version="0.0.2",
+    author="Luis Gascó",
+    author_email="luisgascosanchez@gmail.com",
+    description="Library to access to the data offered by the 'Registro Español de Estudios Clínicos'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luisgasco/REECapi/",
+    install_requires=[
+   'requests>=2.22.0'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
