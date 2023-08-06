1ea6fbd6c2648331d45c47771b9fc35194c9d5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:03 +0000

    Add package cloudenv/0.1.0/cloudenv-0.1.0.tar.gz

---
+++ packages/cloudenv/cloudenv-0.1.0.tar.gz/cloudenv-0.1.0/README.md
@@ -0,0 +1,16 @@
+# cloudenv-python
+
+## How to Install
+
+$ pip install cloudenv
+
+## How to Use
+
+```
+import cloudenv
+my_var = cloudenv.get("AWS_SECRET_ACCESS_KEY")
+```
+
+## Learn More
+
+Visit CloudEnv.com
---
+++ packages/cloudenv/cloudenv-0.1.0.tar.gz/cloudenv-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cloudenv", # Replace with your own username
+    version="0.1.0",
+    author="Lucas Carlson",
+    author_email="lucas@carlson.net",
+    description="Keep Your Environmental Variables Secure And In Sync",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cloudenvhq/cloudenv-python/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
