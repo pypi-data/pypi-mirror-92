d78aea3fe858def17769b51e6f061a6028215b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:12 +0000

    Add package captum/0.3.1/captum-0.3.1.tar.gz

---
+++ packages/captum/captum-0.3.1.tar.gz/captum-0.3.1/captum/insights/attr_vis/frontend/build/precache-manifest.83fe3c268d0494a3e8c7599ab78d8ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c753934cd82a66f60b621ee9379c20e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de6b8efaf5788eb8db15",
+    "url": "./static/css/main.fac91593.chunk.css"
+  },
+  {
+    "revision": "aa07ddf3d73e8e21325d",
+    "url": "./static/js/2.c6c4604e.chunk.js"
+  },
+  {
+    "revision": "9f3e154b5a21336fa161f9ed4e97157c",
+    "url": "./static/js/2.c6c4604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6b8efaf5788eb8db15",
+    "url": "./static/js/main.b165e2a6.chunk.js"
+  },
+  {
+    "revision": "aba697584663a7cb794c",
+    "url": "./static/js/runtime-main.c3c9eabf.js"
+  }
+]);
