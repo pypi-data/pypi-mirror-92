b683d9be052a35c4509019cd652c2440e3390711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:09 +0000

    Add package af-licence-manager/2.0.3/af_licence_manager-2.0.3.tar.gz

---
+++ packages/af-licence-manager/af_licence_manager-2.0.3.tar.gz/af_licence_manager-2.0.3/af_licence_manager/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from af_licence_manager.api.arista_flow_service_api import AristaFlowServiceApi
+from af_licence_manager.api.licence_manager_api import LicenceManagerApi
+from af_licence_manager.api.default_api import DefaultApi
