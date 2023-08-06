c06b3a7d774e9849b9e7d1eadb9fb65de59cb0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:16 +0000

    Add package django-dnoticias-services/1.0.0/django_dnoticias_services-1.0.0.tar.gz

---
+++ packages/django-dnoticias-services/django_dnoticias_services-1.0.0.tar.gz/django_dnoticias_services-1.0.0/dnoticias_services/mail/campaign.py
@@ -0,0 +1,45 @@
+import requests
+from django.conf import settings
+from dnoticias_services.mail.base import BaseMail
+
+
+class BaseCampaign(BaseMail):
+    def __call__(self, template_uuid, brand_group_uuid, newsletter_uuid, title, subject, context=dict(), from_email=None, from_name=None, track_opens=None, track_clicks=None, api_key=None):
+        url = self.get_url()
+        _api_key = api_key or self.api_key
+        
+        response = requests.post(
+            url,
+            headers={
+                "Authorization" : "Api-Key {}".format(_api_key)
+            },
+            data={
+                "email" : email,
+                "template_uuid" : template_uuid,
+                "brand_group_uuid" : brand_group_uuid,
+                "subject" : subject,
+                "context" : context,
+                "from_email" : from_email,
+                "from_name" : from_name,
+                "track_opens" : track_opens,
+                "track_clicks" : track_clicks,
+            }
+        )
+        response.raise_for_status()
+        return response
+
+    def get_url(self):
+        return None
+
+class SendCampaign(BaseCampaign):
+    def get_url(self):
+        return settings.SEND_CAMPAIGN_API_URL
+
+class CreateCampaign(BaseCampaign):
+    def get_url(self):
+        return settings.CREATE_CAMPAIGN_API_URL
+
+send_campaign = SendCampaign()
+create_campaign = CreateCampaign()
+
+__all__ = ("send_campaign", "create_campaign")
