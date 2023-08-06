04542b50574d9b0b363206a4f8a5311527a17392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:14 +0000

    Add package random-utils/0.1.5/random_utils-0.1.5.tar.gz

---
+++ packages/random-utils/random_utils-0.1.5.tar.gz/random_utils-0.1.5/random_utils/datatypes/queue.py
@@ -0,0 +1,51 @@
+class Queue:
+    def __init__(self, *args):
+        self.arr = list(args)
+
+    def size(self):
+        return len(self)
+
+    def push(self, element):
+        self.arr.append(element)
+
+    def append(self, element):
+        self.push(element)
+
+    def pop(self):
+        return self.arr.pop(0)
+
+    def get_list(self):
+        return self.arr
+
+    def get_array(self):
+        return self.arr
+
+    def __getitem__(self, slice):
+        return self.arr[slice]
+
+    def __iter__(self):
+        return iter(self.arr)
+
+    def __add__(self, lst):
+        self.arr.extend(lst)
+
+    def __sub__(self, num):
+        for i in range(len(self.arr)):
+            self.arr[i] -= num
+
+    def __mul__(self, num, individually=True):
+        if individually:
+            for i in range(len(self.arr)):
+                self.arr[i] *= num
+        else:
+            self.arr *= num
+
+    def __div__(self, num):
+        for i in range(len(self.arr)):
+            self.arr[i] /= num
+
+    def __len__(self):
+        return len(self.arr)
+
+    def __repr__(self):
+        return "[" + ", ".join(list(map(str, self.arr))) + "]"
