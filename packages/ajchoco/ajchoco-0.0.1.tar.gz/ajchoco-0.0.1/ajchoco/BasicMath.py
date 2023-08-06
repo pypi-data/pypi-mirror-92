cac7d91a32b7977f2c95c0c779c977f7191a9b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:11 +0000

    Add package ajchoco/0.0.1/ajchoco-0.0.1.tar.gz

---
+++ packages/ajchoco/ajchoco-0.0.1.tar.gz/ajchoco-0.0.1/ajchoco/BasicMath.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 16:00:00 2021
+
+@author: AsheeshJanda
+"""
+
+class Arithmatic:
+    def add(self,a,b):
+        return(a+b)
+    def sub(self,a ,b):
+        return(a-b)
