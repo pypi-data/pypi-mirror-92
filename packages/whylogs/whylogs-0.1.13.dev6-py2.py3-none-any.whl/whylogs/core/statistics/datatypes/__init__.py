0570ce784fca934832819b4ba7804595cdb28774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:06 +0000

    Add package whylogs/0.1.13.dev6/whylogs-0.1.13.dev6-py2.py3-none-any.whl

---
+++ packages/whylogs/whylogs-0.1.13.dev6-py2.py3-none-any.whl/whylogs/core/statistics/datatypes/__init__.py
@@ -0,0 +1,7 @@
+"""
+Define classes for tracking statistics for various data types
+"""
+from .floattracker import *
+from .integertracker import *
+from .stringtracker import *
+from .variancetracker import *
