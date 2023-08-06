0d1dbdd42f53deb418a1551ca48de8121111353e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:23 +0000

    Add package Demo-DAWGI-HBM/0.1.0/Demo_DAWGI_HBM-0.1.0.tar.gz

---
+++ packages/Demo-DAWGI-HBM/Demo_DAWGI_HBM-0.1.0.tar.gz/Demo_DAWGI_HBM-0.1.0/Pleiades/__init__.py
@@ -0,0 +1,9 @@
+"""
+Demo_DAWGI_HBM
+
+Age-metallicity inference of a simple stellar population.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Jairo Andrés Alzate Trujillo'
+__credits__ = 'Instituto de Radioastronómia y Astrofísica, Universidad Nacional Autónoma de México'
