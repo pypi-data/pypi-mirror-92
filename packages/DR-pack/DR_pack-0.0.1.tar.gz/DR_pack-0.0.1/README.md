4fc326ddfd9bdd1581c24a823cb9669a570e96c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package DR-pack/0.0.1/DR_pack-0.0.1.tar.gz

---
+++ packages/DR-pack/DR_pack-0.0.1.tar.gz/DR_pack-0.0.1/README.md
@@ -0,0 +1,1 @@
+It is a demo package creaded for educational purpose.
---
+++ packages/DR-pack/DR_pack-0.0.1.tar.gz/DR_pack-0.0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 15:03:40 2021
+
+@author: MILIND
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DR_pack",
+    version="0.0.1",
+    author="Riddhi",
+    author_email="drpack@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Riddhi-28/DR_pack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
