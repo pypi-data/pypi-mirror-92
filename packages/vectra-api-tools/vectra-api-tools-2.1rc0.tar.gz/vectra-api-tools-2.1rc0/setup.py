a79a5ae39df9f77f7475ae13edf55b514b6e364a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:18 +0000

    Add package vectra-api-tools/2.1rc0/vectra-api-tools-2.1rc0.tar.gz

---
+++ packages/vectra-api-tools/vectra-api-tools-2.1rc0.tar.gz/vectra-api-tools-2.1rc0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_desc = fh.read()
+
+setup(
+    name='vectra-api-tools',
+    description='Vectra API client library',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    version='2.1rc0',
+    author='Vectra',
+    author_email='tme@vectra.ai',
+    url='https://github.com/vectranetworks/vectra_api_tools',
+    license='Apache 2.0',
+    package_dir={
+        'vat': 'modules'
+    },
+    packages=['vat'],
+    install_requires=['requests', 'pytz', 'cabby', 'stix'],
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities'
+    ]
+)
