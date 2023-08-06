2b77cba058aa71345c69b7ae6ff6850bef57e2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package ceshiliaxilianxi/1.0/ceshiliaxilianxi-1.0.tar.gz

---
+++ packages/ceshiliaxilianxi/ceshiliaxilianxi-1.0.tar.gz/ceshiliaxilianxi-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='ceshiliaxilianxi', # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='我自己的测试文件', #描述
+    author='郭从超', # 作者
+    author_email='1365334391@qq.com',
+    py_modules=['ceshi.mode','ceshi.mode1'] # 要发布的模块
+)
