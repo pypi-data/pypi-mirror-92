80854b096cdc2a3601578355d59e6659917bc650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:33 +0000

    Add package honeybee-vtk/0.6.1/honeybee-vtk-0.6.1.tar.gz

---
+++ packages/honeybee-vtk/honeybee-vtk-0.6.1.tar.gz/honeybee-vtk-0.6.1/README.md
@@ -0,0 +1,2 @@
+# honeybee-vtk
+🐝 VTK - Honeybee extension for translating HBJSON to VTK files.
---
+++ packages/honeybee-vtk/honeybee-vtk-0.6.1.tar.gz/honeybee-vtk-0.6.1/honeybee_vtk/cli.py
@@ -0,0 +1,77 @@
+"""Honeybee VTK command line interface."""
+import sys
+import pathlib
+
+import click
+from click.exceptions import ClickException
+
+from .writer import writer
+
+
+@click.group()
+def main():
+    """Honeybee VTK commands entry point."""
+    pass
+
+
+@main.command('translate')
+@click.argument('hbjson-file')
+@click.option(
+    '--name', '-n', help='Name of the output file. If not provided, the name of '
+    'input HBJSON file will be used.'
+)
+@click.option(
+    '--folder', '-f', help='Path to target folder.',
+    type=click.Path(exists=False, file_okay=False, resolve_path=True, dir_okay='True'),
+    default='.', show_default=True
+)
+@click.option(
+    '--file-type', '-t', type=click.Choice(['vtk', 'xml', 'html']), default='html',
+    help='Switch between VTK, XML, and HTML formats. Default is HTML.',
+    show_default=True
+)
+@click.option(
+    '--include-grids', '-eg', is_flag=True, default=False,
+    help='Exclude exporting grids.', show_default=True
+)
+@click.option(
+    '--include-vectors', '-ev', is_flag=True, default=False,
+    help='Exclude exporting vector arrows.', show_default=True
+)
+@click.option(
+    '--include-points', '-ep', is_flag=True, default=False,
+    help='Exclude exporting grid points.', show_default=True
+)
+@click.option(
+    '--show-html', '--show', '-sh', is_flag=True, default=False,
+    help='Open the generated HTML file in a browser', show_default=True
+)
+def translate_recipe(
+        hbjson_file, name, folder, file_type, include_grids, include_vectors,
+        include_points, show_html
+    ):
+    """Translate a HBJSON file to several VTK, XML, or HTML file.
+
+    The output file is either a zipped file that contains all the generated VTK/XML files 
+    or an HTML file.
+
+    \b
+    Args:
+        hbjson-file: Path to input HBJSON file.
+
+    """
+
+    folder = pathlib.Path(folder)
+    folder.mkdir(exist_ok=True)
+
+    try:
+        output_file = writer(
+            hbjson_file, target_folder=folder, file_name=name,
+            include_grids=include_grids, include_vectors=include_vectors,
+            include_points=include_points, writer=file_type, open_html=show_html
+        )
+    except Exception as e:
+        raise ClickException(f'Translation to VTK failed:\n{e}')
+    else:
+        print(f'Success: {output_file}', file=sys.stderr)
+        return sys.exit(0)
