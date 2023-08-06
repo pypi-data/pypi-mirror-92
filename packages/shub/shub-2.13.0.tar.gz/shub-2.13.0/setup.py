60c34a06c6d823cc35293fe65cd886fd1a570d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:14 +0000

    Add package shub/2.13.0/shub-2.13.0.tar.gz

---
+++ packages/shub/shub-2.13.0.tar.gz/shub-2.13.0/setup.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+import io
+import os
+from setuptools import setup, find_packages
+
+
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'shub', '__init__.py'),
+             mode='r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+
+setup(
+    name='shub',
+    version='2.13.0',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    url=about['DOCS_LINK'],
+    description='Scrapinghub Command Line Client',
+    long_description=open('README.rst').read(),
+    author='Scrapinghub',
+    author_email='info@scrapinghub.com',
+    maintainer='Scrapinghub',
+    maintainer_email='info@scrapinghub.com',
+    license='BSD',
+    entry_points={
+        'console_scripts': ['shub = shub.tool:cli']
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'click==7.0',
+        'docker',
+        'pip',
+        'PyYAML',
+        'retrying',
+        'requests',
+        'scrapinghub>=2.3.1',
+        'six>=1.7.0',
+        'tqdm==4.55.1',
+        'toml',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
