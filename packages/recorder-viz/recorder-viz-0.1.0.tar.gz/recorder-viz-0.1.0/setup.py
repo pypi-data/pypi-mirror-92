b69b87f8e86e4bb231917a23286a1f3224afe5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:49 +0000

    Add package recorder-viz/0.1.0/recorder-viz-0.1.0.tar.gz

---
+++ packages/recorder-viz/recorder-viz-0.1.0.tar.gz/recorder-viz-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+c_reader_module = setuptools.Extension('recorder_viz/librreader',
+                                        ['lib/reader.c'], include_dirs=['lib'])
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="recorder-viz",                # Package name, e.g., pip install recorder-viz
+    version="0.1.0",
+    author="Chen Wang",
+    author_email="wangvsa@gmail.com",
+    description="Utilities for processing Recorder traces",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wangvsa/recorder-viz",
+    packages=['recorder_viz', 'lib'],   # package for import: after installaion, import recorder_viz
+    package_data = {'lib': ['*.h']},    # *.h by default will not be copied, we use this to ship it.
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: University of Illinois/NCSA Open Source License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+    ext_modules=[c_reader_module],
+)
