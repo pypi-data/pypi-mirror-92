c5ebd89c57ea422ea3e89a5fa081cbef03940a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:59 +0000

    Add package hit-starter-on-fhir/0.1.3/hit-starter-on-fhir-0.1.3.tar.gz

---
+++ packages/hit-starter-on-fhir/hit-starter-on-fhir-0.1.3.tar.gz/hit-starter-on-fhir-0.1.3/README.rst
@@ -0,0 +1,33 @@
+===================
+HIT Starter on FHIR
+===================
+
+.. image:: https://img.shields.io/pypi/v/hit-starter-on-fhir.svg
+        :target: https://pypi.python.org/pypi/hit-starter-on-fhir
+
+.. image:: https://img.shields.io/travis/walkIT-nl/hit-starter-on-fhir.svg
+        :target: https://travis-ci.com/walkIT-nl/hit-starter-on-fhir
+
+.. image:: https://readthedocs.org/projects/hit-starter-on-fhir/badge/?version=latest
+        :target: https://hit-starter-on-fhir.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+Health IT starter kit providing auditing, authn and authz using FHIR and OpenID Connect
+
+* Free software: MIT license
+* Documentation: https://hit-starter-on-fhir.readthedocs.io.
+
+
+Features
+--------
+
+* Audit client using FHIR AuditEvent
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/hit-starter-on-fhir/hit-starter-on-fhir-0.1.3.tar.gz/hit-starter-on-fhir-0.1.3/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['requests', 'dataclasses', 'fhir.resources==6.0.0']
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', 'responses']
+
+setup(
+    author="WalkIT",
+    author_email='code@walkit.nl',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description=" Health IT starter kit providing auditing, authn and authz using FHIR and OpenID Connect",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='hit-starter-on-fhir',
+    name='hit-starter-on-fhir',
+    packages=find_packages(include=['fhir_starter', 'fhir_starter.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/walkIT-nl/hit-starter-on-fhir',
+    version='0.1.3',
+    zip_safe=False,
+)
