108ed7e43839a4cfe17643cfb3516de8326c65aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:03 +0000

    Add package hermeskit/1.0.0/hermeskit-1.0.0.tar.gz

---
+++ packages/hermeskit/hermeskit-1.0.0.tar.gz/hermeskit-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='hermeskit', 
+      author='HuynhThanhQuan',
+      author_email='quanht6@onemount.com',
+      maintainer='HuynhThanhQuan',
+      maintainer_email='quanht6@onemount.com',
+      description='Credit Scoring Python package',
+      license='MIT',
+      version='1.0.0', 
+      python_requires=">=3.7",
+      url='https://vinid-team.atlassian.net/wiki/spaces/1/pages/677551034/0.+About+Hermes',
+      keywords=['CreditScoring', 'Hermes', '1MG', 'CDO'],
+      packages=find_packages())
