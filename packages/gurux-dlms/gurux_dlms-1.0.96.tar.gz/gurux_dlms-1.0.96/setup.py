0f9e4d69fd9ac450623f83900f260b096463e14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:30 +0000

    Add package gurux-dlms/1.0.96/gurux_dlms-1.0.96.tar.gz

---
+++ packages/gurux-dlms/gurux_dlms-1.0.96.tar.gz/gurux_dlms-1.0.96/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gurux_dlms",
+    version="1.0.96",
+    author="Gurux Ltd",
+    author_email="gurux@gurux.fi",
+    description="Gurux DLMS library for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gurux/gurux.dlms.python",
+    packages=setuptools.find_packages(),
+    package_data={'gurux_dlms': ['OBISCodes.txt', 'India.txt', 'Italy.txt', 'SaudiArabia.txt']},
+    license='GPLv2',
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
