4ca50da1318065c1284fbbba34896500574e23bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:21 +0000

    Add package sixgill-clients/0.2.0/sixgill-clients-0.2.0.tar.gz

---
+++ packages/sixgill-clients/sixgill-clients-0.2.0.tar.gz/sixgill-clients-0.2.0/sixgill/sixgill_utils.py
@@ -0,0 +1,27 @@
+import logging
+
+
+def streamify(batch_func):
+    def streamify_wrapper(*args, **kwargs):
+        batch = batch_func(*args, **kwargs)
+        while batch:
+            for indicator in batch:
+                yield indicator
+            batch = batch_func(*args, **kwargs)
+
+    return streamify_wrapper
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter("[%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]")
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+def is_indicator(indicator):
+    return "indicator" == indicator.get("type", "")
