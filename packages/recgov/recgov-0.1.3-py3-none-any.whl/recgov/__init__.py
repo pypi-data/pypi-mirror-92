f8f3b5ee71ae1ee54195cde72b56e415a05f6290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:27 +0000

    Add package recgov/0.1.3/recgov-0.1.3-py3-none-any.whl

---
+++ packages/recgov/recgov-0.1.3-py3-none-any.whl/recgov/__init__.py
@@ -0,0 +1,5 @@
+# This is the official version source.  Bump it before releases.
+__version__="0.1.3"
+
+from .availability import Availability
+from .campsites import Campsite, CampsiteSet, get_campsites
