25c34d57fd79075ae66019831e242f82b65df7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:04 +0000

    Add package motorisedcameratracking/0.4.1.dev18/motorisedcameratracking-0.4.1.dev18-cp37-cp37m-linux_armv7l.whl

---
+++ packages/motorisedcameratracking/motorisedcameratracking-0.4.1.dev18-cp37-cp37m-linux_armv7l.whl/motorisedcameratracking/Config.py
@@ -0,0 +1,26 @@
+import pkg_resources
+class Config():
+    """Hello"""
+    timeUnit=None
+    
+
+
+    #xMotor=""
+    #yMotor=""
+    #Cameras=[RPICam]#store the list of classes to allow the user to create their own
+    #Motors=[]
+
+
+
+    def __init__(self,timeUnit):
+        """The __init__ method"""
+        self.timeUnit=timeUnit
+
+    def getFaceClassifiersPath(self):
+        return pkg_resources.resource_filename('motorisedcameratracking','data/haarcascade_frontalface_default.xml')
+
+        
+
+
+
+
