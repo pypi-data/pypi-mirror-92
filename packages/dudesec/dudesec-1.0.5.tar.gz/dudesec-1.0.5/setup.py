2284855571806fcec984107c0830ea27507ad025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package dudesec/1.0.5/dudesec-1.0.5.tar.gz

---
+++ packages/dudesec/dudesec-1.0.5.tar.gz/dudesec-1.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dudesec",
+    version="1.0.5",
+    author="Gustavo de Oliveira Rosa",
+    author_email="gustavoolrosa2019@gmail.com",
+    description="Library para monitoramento de sistemas e exploração de vulnerabilidades.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GustavoOliveiraRosa/DudSecPython",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'socket',
+        'urllib.request',
+        'ping3',
+        'bs4',
+        'requests',
+        'random',
+        'string',
+    ],
+    python_requires='>=3.6',
+)
