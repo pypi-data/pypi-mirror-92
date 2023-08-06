992fc8db9e0f2f61f2b226c2841ed62615da673e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:18 +0000

    Add package python-gvm/21.1.3/python_gvm-21.1.3-py3-none-any.whl

---
+++ packages/python-gvm/python_gvm-21.1.3-py3-none-any.whl/gvm/protocols/gmp.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019-2020 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Module for communication with gvmd
+"""
+from typing import Any, Optional, Callable, Union
+
+from gvm.errors import GvmError
+
+from gvm.protocols.base import GvmProtocol, GvmConnection
+
+from gvm.protocols.gmpv7 import Gmp as Gmpv7
+from gvm.protocols.gmpv8 import Gmp as Gmpv8
+from gvm.protocols.gmpv9 import Gmp as Gmpv9
+from gvm.protocols.gmpv208 import Gmp as Gmpv208
+from gvm.protocols.gmpv214 import Gmp as Gmpv214
+
+from gvm.transforms import EtreeCheckCommandTransform
+
+from gvm.xml import XmlCommand
+
+SUPPORTED_GMP_VERSIONS = Union[  # pylint: disable=invalid-name
+    Gmpv7, Gmpv8, Gmpv9, Gmpv208, Gmpv214
+]
+
+
+class Gmp(GvmProtocol):
+    """Dynamically select supported GMP protocol of the remote manager daemon.
+
+    Must be used as a `Context Manager
+    <https://docs.python.org/3/reference/datamodel.html#context-managers>`_
+
+    Example:
+
+        .. code-block:: python
+
+            from gvm.protocols.gmp import Gmp
+
+            with Gmp(connection) as gmp:
+                # gmp can be an instance of gvm.protocols.gmpv7.Gmp,
+                # gvm.protocols.gmpv8.Gmp or gvm.protocols.gmpv9.Gmp depending
+                # on the supported GMP version of the remote manager daemon
+                resp = gmp.get_tasks()
+
+    Attributes:
+        connection: Connection to use to talk with the remote daemon. See
+            :mod:`gvm.connections` for possible connection types.
+        transform: Optional transform `callable`_ to convert response data.
+            After each request the callable gets passed the plain response data
+            which can be used to check the data and/or conversion into different
+            representations like a xml dom.
+
+            See :mod:`gvm.transforms` for existing transforms.
+
+    .. _callable:
+        https://docs.python.org/3/library/functions.html#callable
+    """
+
+    def __init__(
+        self,
+        connection: GvmConnection,
+        *,
+        transform: Optional[Callable[[str], Any]] = None,
+    ):
+        super().__init__(connection, transform=EtreeCheckCommandTransform())
+        self._gmp_transform = transform
+
+    def determine_remote_gmp_version(self) -> str:
+        """Determine the supported GMP version of the remote daemon"""
+        self.connect()
+        resp = self._send_xml_command(XmlCommand("get_version"))
+        self.disconnect()
+
+        version_el = resp.find('version')
+        if version_el is None:
+            raise GvmError(
+                'Invalid response from manager daemon while requesting the '
+                'version information.'
+            )
+
+        return version_el.text
+
+    def determine_supported_gmp(self) -> SUPPORTED_GMP_VERSIONS:
+        """Determine supported GMP version of the remote daemon and return a
+        corresponding Gmp class instance
+        """
+        version = self.determine_remote_gmp_version()
+        major_version = int(version.split('.')[0])
+        if major_version == 7:
+            gmp_class = Gmpv7
+        elif major_version == 8:
+            gmp_class = Gmpv8
+        elif major_version == 9:
+            gmp_class = Gmpv9
+        elif major_version == 20:
+            gmp_class = Gmpv208
+        elif major_version >= 21:
+            gmp_class = Gmpv214
+        else:
+            raise GvmError(
+                'Remote manager daemon uses an unsupported version of GMP. '
+                'The GMP version was {}.'.format(version)
+            )
+
+        return gmp_class(self._connection, transform=self._gmp_transform)
+
+    def __enter__(self):
+        gmp = self.determine_supported_gmp()
+
+        gmp.connect()
+
+        return gmp
---
+++ packages/python-gvm/python_gvm-21.1.3-py3-none-any.whl/gvm/xml.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 - 2019 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+from typing import Optional
+from io import IOBase
+
+import defusedxml.lxml as secET
+
+from defusedxml import DefusedXmlException
+from lxml import etree
+
+from gvm.errors import GvmError
+
+
+def create_parser():
+    # recover => try hard to parse through broken XML
+    # huge_tree => disable security restrictions and support very deep trees and
+    #              very long text content (for get_reports)
+    return etree.XMLParser(encoding="utf-8", recover=True, huge_tree=True)
+
+
+class XmlCommandElement:
+    def __init__(self, element):
+        self._element = element
+
+    def add_element(
+        self,
+        name: str,
+        text: Optional[str] = None,
+        *,
+        attrs: Optional[dict] = None,
+    ):
+        node = etree.SubElement(self._element, name, attrib=attrs)
+        node.text = text
+        return XmlCommandElement(node)
+
+    def set_attribute(self, name: str, value: str):
+        self._element.set(name, value)
+
+    def set_attributes(self, attrs: dict):
+        """Set several attributes at once.
+
+        Arguments:
+            attrs (dict): Attributes to be set on the element
+        """
+        for key, value in attrs.items():
+            self._element.set(key, value)
+
+    def append_xml_str(self, xml_text: str):
+        """Append a xml element in string format."""
+        node = secET.fromstring(xml_text)
+        self._element.append(node)
+
+    def to_string(self) -> str:
+        return etree.tostring(self._element).decode("utf-8")
+
+    def __str__(self):
+        return self.to_string()
+
+
+class XmlCommand(XmlCommandElement):
+    def __init__(self, name):
+        super().__init__(etree.Element(name))
+
+
+def pretty_print(xml, file: IOBase = sys.stdout):
+    """Prints beautiful XML-Code
+
+    This function gets a string containing the xml, an object of
+    List[lxml.etree.Element] or directly a lxml element.
+
+    Print it with good readable format.
+
+    Arguments:
+        xml (str, List[lxml.etree.Element] or lxml.etree.Element):
+            xml as string,
+            List[lxml.etree.Element] or directly a lxml element.
+        file:
+            A IOBase type. Can be a File, StringIO, ...
+
+    """
+    if not isinstance(file, IOBase):
+        raise TypeError(
+            'Type needs to be from IOBase, not {}.'.format(type(file))
+        ) from None
+
+    if isinstance(xml, list):
+        for item in xml:
+            if etree.iselement(item):
+                file.write(
+                    etree.tostring(item, pretty_print=True).decode(
+                        sys.getdefaultencoding() + '\n'
+                    )
+                )
+            else:
+                file.write(item + '\n')
+    elif etree.iselement(xml):
+        file.write(
+            etree.tostring(xml, pretty_print=True).decode(
+                sys.getdefaultencoding() + '\n'
+            )
+        )
+    elif isinstance(xml, str):
+        tree = secET.fromstring(xml)
+        file.write(
+            etree.tostring(tree, pretty_print=True).decode(
+                sys.getdefaultencoding() + '\n'
+            )
+        )
+
+
+def validate_xml_string(xml_string: str):
+    """Checks if the passed string contains valid XML
+
+    Raises a GvmError if the XML is invalid. Otherwise the function just
+    returns.
+
+    Arguments:
+        xml_string: XML string to validate
+
+    Raises:
+        GvmError: The xml string did contain invalid XML
+
+    """
+    try:
+        secET.fromstring(xml_string)
+    except (DefusedXmlException, etree.LxmlError) as e:
+        raise GvmError("Invalid XML", e) from e
