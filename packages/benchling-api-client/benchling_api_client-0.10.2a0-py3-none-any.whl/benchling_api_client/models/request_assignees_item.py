b03d55b6c967ab52c968bcff4d2d9a80b186daa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:26 +0000

    Add package benchling-api-client/0.10.2a0/benchling_api_client-0.10.2a0-py3-none-any.whl

---
+++ packages/benchling-api-client/benchling_api_client-0.10.2a0-py3-none-any.whl/benchling_api_client/models/request_assignees_item.py
@@ -0,0 +1,60 @@
+from typing import Any, Dict, List, Type, TypeVar, Union, cast
+
+import attr
+
+from ..models.team_summary import TeamSummary
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="RequestAssigneesItem")
+
+
+@attr.s(auto_attribs=True)
+class RequestAssigneesItem:
+    """  """
+
+    team: Union[Unset, TeamSummary] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        team: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.team, Unset):
+            team = self.team.to_dict()
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if team is not UNSET:
+            field_dict["team"] = team
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        team: Union[Unset, TeamSummary] = UNSET
+        _team = d.pop("team", UNSET)
+        if _team is not None and not isinstance(_team, Unset):
+            team = TeamSummary.from_dict(cast(Dict[str, Any], _team))
+
+        request_assignees_item = cls(
+            team=team,
+        )
+
+        request_assignees_item.additional_properties = d
+        return request_assignees_item
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
