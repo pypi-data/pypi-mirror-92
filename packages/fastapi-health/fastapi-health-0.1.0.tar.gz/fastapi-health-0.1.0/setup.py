003f3a2147ef50cdcaa83fbcbfca98c1fa4b635f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:46 +0000

    Add package fastapi-health/0.1.0/fastapi-health-0.1.0.tar.gz

---
+++ packages/fastapi-health/fastapi-health-0.1.0.tar.gz/fastapi-health-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_health']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'fastapi-health',
+    'version': '0.1.0',
+    'description': 'Heath check on FastAPI applications.',
+    'long_description': '<h1 align="center">\n    <strong>fastapi-health</strong>\n</h1>\n<p align="center">\n    <a href="https://github.com/Kludex/fastapi-health" target="_blank">\n        <img src="https://img.shields.io/github/last-commit/Kludex/fastapi-health" alt="Latest Commit">\n    </a>\n        <img src="https://img.shields.io/github/workflow/status/Kludex/fastapi-health/Test">\n        <img src="https://img.shields.io/codecov/c/github/Kludex/fastapi-health">\n    <br />\n    <a href="https://pypi.org/project/fastapi-health" target="_blank">\n        <img src="https://img.shields.io/pypi/v/fastapi-health" alt="Package version">\n    </a>\n    <img src="https://img.shields.io/pypi/pyversions/fastapi-health">\n    <img src="https://img.shields.io/github/license/Kludex/fastapi-health">\n</p>\n\n\n## Installation\n\n``` bash\npip install fastapi-health\n```\n\n## License\n\nThis project is licensed under the terms of the MIT license.\n',
+    'author': 'Marcelo Trylesinski',
+    'author_email': 'marcelotryle@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Kludex/fastapi-health',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
