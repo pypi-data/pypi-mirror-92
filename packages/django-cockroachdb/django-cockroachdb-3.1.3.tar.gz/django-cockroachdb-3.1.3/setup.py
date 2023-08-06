33477cd1826b80630f94dfa96d77f4d7308aec67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:39 +0000

    Add package django-cockroachdb/3.1.3/django-cockroachdb-3.1.3.tar.gz

---
+++ packages/django-cockroachdb/django-cockroachdb-3.1.3.tar.gz/django-cockroachdb-3.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django-cockroachdb',
+    version=__import__('django_cockroachdb').__version__,
+    python_requires='>=3.6',
+    url='https://github.com/cockroachdb/django-cockroachdb',
+    maintainer='Cockroach Labs',
+    maintainer_email='python@cockroachlabs.com',
+    description='Django backend for CockroachDB',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Framework :: Django :: 3.1',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    project_urls={
+        'Source': 'https://github.com/cockroachdb/django-cockroachdb',
+        'Tracker': 'https://github.com/cockroachdb/django-cockroachdb/issues',
+    },
+)
