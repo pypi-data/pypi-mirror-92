e4fb7163bd74d92eb994b9fa5717545b93110d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:13 +0000

    Add package MayReader/0.0.4/MayReader-0.0.4.tar.gz

---
+++ packages/MayReader/MayReader-0.0.4.tar.gz/MayReader-0.0.4/MayReader/MayReader.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 14:04:52 2021
+
+@author: Cicko-PC
+"""
+
+from os.path import exists
+
+def isComment(line):
+    """
+    This function returns true if arument is comment
+
+    Parameters
+    ----------
+    line : Strig value that contains one line of tekst.
+
+    Returns
+    -------
+    bool. True if argument is comment.
+    
+    """
+    return (line[0]=="/" and line[1]=="/")
+
+def isNode(line):
+    """
+    Check if line is part of Node in ma file. If node starts with string "createNode"
+
+    Parameters
+    ----------
+    line : Strig value that contains one line of tekst.
+
+    Returns
+    -------
+    bool. True if line starts with "createNode"
+
+    """
+    
+    return line[0:10]=="createNode"
+
+
+class MayReader:
+    """
+    This class is used fore simple reading of maya ascii files.
+    """
+    def __init__(self, file):
+        self.file = file
+        self.nodes = []
+        self.parse()
+        
+    
+    def parse(self):
+        """
+        Parsing throw and getting important informations from maya ascii file
+        Important informations are Nodes with informations of position, name and uid
+        also looking to information about type of node
+        Everything else is ignored
+
+        Returns
+        -------
+        None.
+
+        """
+        if (not exists(self.file)): 
+            return
+        self.f = open(self.file, 'r')
+        while(True):
+            line = self.f.readline()
+            if (len(line) == 0): break
+            if (isComment(line) or not isNode(line)): continue
+            if (line.split()[1] == "transform"): self.addNode()
+            
+            
+    def addNode(self):
+        """
+        Adding one node to list attribute nodes if all conditions are satisfied.
+
+        Returns
+        -------
+        None.
+
+        """
+        number = 0
+        while (True):
+            lineSplit = self.f.readline().replace('"', '').split()
+            if (lineSplit[0] == "setAttr" and lineSplit[1] == '.t'):
+                pos = (float(lineSplit[-4]), float(lineSplit[-3]), float(lineSplit[-2]))
+                number |= 1 
+            if (lineSplit[0] == "createNode" and lineSplit[1] == "mesh"):
+                name = lineSplit[3]
+                number |= 2
+            if (lineSplit[0] == "rename" and lineSplit[1] == "-uid"):
+                uid = lineSplit[2]
+                number |= 4
+            if (number == 7): 
+                self.nodes.append({"name": name, "position": pos, "uid": uid[:-1]})
+                break
+            if (lineSplit[0] == "createNode" and not lineSplit[1] == "mesh"):
+                if (lineSplit[1] == "transform"): self.addNode()
+                break
+                
+        
+            
+    def getMashObjects(self):
+        """
+        This method returns list of dicts for mash objects
+
+        Returns
+        -------
+        List of dict.
+
+        """
+        return self.nodes
+        
+    
+        
+        
+
+
---
+++ packages/MayReader/MayReader-0.0.4.tar.gz/MayReader-0.0.4/test/test_MayReader.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 17:22:11 2021
+
+@author: Cicko-PC
+"""
+
+import unittest
+from MayReader.MayReader import MayReader
+
+
+class TestMayReader(unittest.TestCase):
+    
+    def test_isComment(self):
+        self.assertTrue(MayReader.isComment("//This is comment"))
+        self.assertFalse(MayReader.isComment("/This is not a comment"))
+               
+        
+    def test_isNode(self):
+        self.assertTrue(MayReader.isNode("createNode transform -s -n"))
+        self.assertFalse(MayReader.isNode("/createNode transform -s -n"))
+        
+    def test_outputWithWrongInputFiile(self):
+        mr = MayReader.MayReader("notRealFile.txt")
+        self.assertFalse(mr.getMashObjects())
+        
+    def test_outputWithProperInput(self):
+        mr = MayReader.MayReader("example_file_1.ma")
+        dict1 = {'name': 'SphereShape', 'position': (1.0, 1.0, -6.0)
+                 , 'uid': '13C79C5C-402E-63DE-D388-53B66872D238'}
+        self.assertDictEqual(dict1, mr.getMashObjects()[0])
+        dict2 =  {'name': 'CubeShape', 'position': (-3.0, 2.0, 2.0)
+                  , 'uid': '8690CE34-4E5E-1275-5747-F8A21428D67B'}
+        self.assertDictEqual(dict2, mr.getMashObjects()[1])
+        dict3 = {'name': 'CylinderShape', 'position': (0.0, 0.0, 5.0)
+                 , 'uid': 'A2B6EB5B-47BF-1D5D-EE78-DAA62EB25ABE'}
+        self.assertDictEqual(dict3, mr.getMashObjects()[2])
