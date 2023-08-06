dfc2bd11af420780efeef16d857de5ed7e43540b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:34 +0000

    Add package fancy-trainer/0.0.7/fancy-trainer-0.0.7.tar.gz

---
+++ packages/fancy-trainer/fancy-trainer-0.0.7.tar.gz/fancy-trainer-0.0.7/fancy/trainer/model/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "Checkpoint",
+    "CheckpointPlotterBase", "CheckpointPlotter",
+]
+
+from .checkpoint import Checkpoint
+from .checkpoint_plotter_base import CheckpointPlotterBase
+from .checkpoint_plotter import CheckpointPlotter
