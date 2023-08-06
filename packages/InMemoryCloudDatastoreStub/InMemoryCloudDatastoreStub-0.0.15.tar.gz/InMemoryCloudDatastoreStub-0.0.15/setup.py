402e85a8fd389bafded0a7a8f1e68ee3949fa13a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:04 +0000

    Add package InMemoryCloudDatastoreStub/0.0.15/InMemoryCloudDatastoreStub-0.0.15.tar.gz

---
+++ packages/InMemoryCloudDatastoreStub/InMemoryCloudDatastoreStub-0.0.15.tar.gz/InMemoryCloudDatastoreStub-0.0.15/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="InMemoryCloudDatastoreStub",
+    version="0.0.15",
+    description="An in-memory stub implementation of Google Cloud Datastore for use in unit tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phil-lopreiato/google-cloud-datastore-stub/",
+    packages=setuptools.find_packages(),
+    python_requires=">=3",
+    install_requires=["google-cloud-ndb > 1.2.1"],
+)
---
+++ packages/InMemoryCloudDatastoreStub/InMemoryCloudDatastoreStub-0.0.15.tar.gz/InMemoryCloudDatastoreStub-0.0.15/tests/models.py
@@ -0,0 +1,18 @@
+from google.cloud import ndb
+
+
+class SimpleModel(ndb.Model):
+    str_prop = ndb.StringProperty()
+    int_prop = ndb.IntegerProperty()
+
+
+class ChildModel(ndb.Model):
+    str_prop = ndb.StringProperty()
+
+
+class RepeatedPropertyModel(ndb.Model):
+    int_props = ndb.IntegerProperty(repeated=True)
+
+
+class KeyPropertyModel(ndb.Model):
+    model_ref = ndb.KeyProperty(kind=SimpleModel)
