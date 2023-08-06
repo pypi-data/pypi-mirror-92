2e2f57cca0339185588c4342e7f65068abecc074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:13 +0000

    Add package econuy/0.18.6/econuy-0.18.6.tar.gz

---
+++ packages/econuy/econuy-0.18.6.tar.gz/econuy-0.18.6/econuy/utils/datasets.py
@@ -0,0 +1,83 @@
+original = {"naccounts": "Cuentas nacionales (demanda precios constantes, "
+                         "oferta precios constantes, oferta índice real, "
+                         "oferta índice real desestacionalizado, oferta "
+                         "precios corrientes, PBI precios corrientes)",
+            "industrial_production": "Producción industrial",
+            "electricity": "Ventas de energía eléctrica por sector",
+            "gasoline": "Ventas de nafta por departamento",
+            "diesel": "Ventas de gasoil por departamento",
+            "cattle": "Faena de bovinos",
+            "milk": "Remisión de leche a planta",
+            "cement": "Ventas de cemento",
+            "cpi": "Índice de precios al consumidor",
+            "nxr_monthly": "Tipo de cambio interbancario mensual",
+            "nxr_daily": "Tipo de cambio interbancario diario",
+            "balance": "Resultado fiscal (SPC, NFPS, GC-BPS, ANCAP, ANTEL, "
+                       "UTE, OSE, empresas públicas)",
+            "taxes": "Recaudación impositiva",
+            "public_debt": "Deuda pública (global, no monetario y BCU)",
+            "labor": "Tasa de actividad, empleo y desempleo",
+            "hours": "Horas promedio trabajadas por sector",
+            "wages": "Salarios nominales totales, sector público y privado",
+            "real_wages": "Salarios reales totales, sector público y privado",
+            "trade": "Comercio internacional de bienes (exportaciones e "
+                     "importaciones por origen, destino, sector, valor, "
+                     "volumen y precio)",
+            "rxr_official": "Tipos de cambio reales BCU",
+            "reserves": "Reservas internacionales",
+            "reserves_changes": "Cambio en las reservas internacionales del BCU",
+            "deposits": "Depósitos en el sistema bancario",
+            "credit": "Créditos del sistema bancario al sector no financiero",
+            "interest_rates": "Tasas de interés activas y pasivas",
+            "call": "Tasa call a 1 día",
+            "sovereign_risk": "Spread de bonos uruguayos respecto de bonos "
+                              "norteamericanos",
+            "household_income": "Ingreso medio de los hogares sin VL ni "
+                                "aguinaldo",
+            "capita_income": "Ingreso medio per cápita sin VL ni aguinaldo",
+            "consumer_confidence": "Índice de confianza de los consumidores"}
+
+custom = {"core_industrial": "Producción industrial total, sin refinería y "
+                             "núcleo",
+          "cpi_measures": "índice de precios transable, no transable, "
+                          "subyacente, residual y Winsorized 5%",
+          "balance_fss": "Resultado fiscal (SPC, NFPS y GC-BPS, ajustado y "
+                         "no ajustado por el FSS",
+          "net_public_debt": "Deuda neta excluyendo encajes",
+          "rates_people": "Tasas de actividad, empleo y desempleo, incluyendo "
+                          "localidades de más de 5 mil personas, y cantidad "
+                          "de personas",
+          "net_trade": "Balanza comercial por país",
+          "terms_of_trade": "Términos de intercambio",
+          "commodity_index": "Índice de precios de materias primas",
+          "rxr_custom": "Tipos de cambio reales vs. EE.UU., Argentina y "
+                        "Brasil",
+          "bonds": "Rendimiento de bonos soberanos en USD, UI y pesos",
+          "global_gdp": "PBI real desestacionalizado de Estados Unidos,"
+                        "Unión Europea, Japón y China",
+          "global_stocks": "Índices bursátiles (S&P 500, Euronext 100, Nikkei"
+                           "225 y Shanghai Composite)",
+          "global_policy_rates": "Tasas de política monetaria de Estados "
+                                 "Unidos, Eurozona, Japón y China",
+          "global_long_rates": "Tasas de bonos soberanos a 10 años de Estados "
+                               "Unidos, Alemania, Francia, Italia, España, "
+                               "Reino unido, Japón y China",
+          "global_nxr": "Cotización de monedas de Estados Unidos "
+                               "(dollar index), USDUER, USDJPY y USDCNY",
+          "regional_gdp": "PBI real desestacionalizado de Argentina y Brasil",
+          "regional_monthly_gdp": "PBI mensual real desestacionalizado de "
+                                  "Argentina y Brasil",
+          "regional_cpi": "Índice de precios al consumo de Argentina y Brasil",
+          "regional_embi_spreads": "EMBI spread de Argentina, Brasil y "
+                                   "EMBI Global",
+          "regional_embi_yields": "EMBI yield de Argentina, Brasil y "
+                                   "EMBI Global",
+          "regional_nxr": "Tipo de cambio de Argentina (oficial y "
+                          "paralelo) y Brasil",
+          "regional_policy_rates": "Tasas de política monetaria de Argentina "
+                                   "y Brasil",
+          "regional_stocks": "Índices bursátiles (MERVAL y BOVESPA) "
+                             "en dólares",
+          "regional_rxr": "Tipo de cambio real de Argentina y Brasil "
+                          "vis-à-vis EE.UU."
+          }
