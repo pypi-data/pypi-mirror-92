6a1dc1db8278eb46bb94a4d7adef7833c930cddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:46 +0000

    Add package nw-connections/1.1.3/nw_connections-1.1.3.tar.gz

---
+++ packages/nw-connections/nw_connections-1.1.3.tar.gz/nw_connections-1.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+required = [
+    'boto3>=1.9',
+    'requests>=2,<3',
+    'requests-cache>=0.4,<1',
+    'PyJWT>=1.7.1,<2',
+    'requests-jwt>=0.4,<1',
+    'simplejson>=3',
+]
+
+# Get current version
+with open("CURRENT_VERSION.txt") as f:
+    current_version = f.read().strip()
+
+setup(name='nw_connections',
+      version=current_version,
+      description='Shared Marple python modules',
+      url='http://github.com/marple-newsrobot/newsworthy-py-connections',
+      author='Journalism Robotics Stockholm',
+      author_email='contact@newsworthy.se',
+      license='MIT',
+      packages=['nw_connections'],
+      include_package_data=True,
+      install_requires=required,
+      zip_safe=False)
