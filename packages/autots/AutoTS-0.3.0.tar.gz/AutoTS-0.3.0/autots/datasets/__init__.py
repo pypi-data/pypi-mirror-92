1fa8988863f10531010a0e4034dce447d32a5f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:03 +0000

    Add package autots/0.3.0/AutoTS-0.3.0.tar.gz

---
+++ packages/autots/AutoTS-0.3.0.tar.gz/AutoTS-0.3.0/autots/datasets/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tools for Importing Sample Data
+"""
+from autots.datasets._base import load_daily
+from autots.datasets._base import load_monthly
+from autots.datasets._base import load_yearly
+from autots.datasets._base import load_hourly
+from autots.datasets._base import load_weekly
+
+__all__ = ['load_daily', 'load_monthly', 'load_yearly', 'load_hourly', 'load_weekly']
