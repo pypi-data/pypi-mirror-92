33fd6491cbeb424a153219e80ea8b6228f321a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:07 +0000

    Add package roverqaz-simple-api/0.1.0/roverqaz_simple_api-0.1.0.tar.gz

---
+++ packages/roverqaz-simple-api/roverqaz_simple_api-0.1.0.tar.gz/roverqaz_simple_api-0.1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="roverqaz_simple_api",
+    version="0.1.0",
+    author="Adil Idrissov",
+    author_email="idrissov.adil@gmail.com",
+    url="https://gitlab.com/roverqaz/test-task-python",
+    description="A simple api package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.7',
+)
