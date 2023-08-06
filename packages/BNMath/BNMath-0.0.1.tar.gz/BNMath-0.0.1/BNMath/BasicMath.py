6d50100c8f3a13a62a776e9ac8759c7e72bfba0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:10 +0000

    Add package BNMath/0.0.1/BNMath-0.0.1.tar.gz

---
+++ packages/BNMath/BNMath-0.0.1.tar.gz/BNMath-0.0.1/BNMath/BasicMath.py
@@ -0,0 +1,14 @@
+class mlbnm:
+    def ml1():
+        print(f"import csv\n")
+
+        print(f"with open('tennis.csv', 'r') as f: \n reader = csv.reader(f) \n your_list = list(reader)\n h = [['0', '0', '0', '0', '0', '0']]\nfor i in your_list:\n")
+        print(f"if i[-1] == Yes:\n j = 0\n for x in i:\n if x != Yes:\n if x != h[0][j] and h[0][j] == '0':\nh[0][j] = x\nelif x != h[0][j] and h[0][j] != '0':\nh[0][j] = '?'\n")
+        print(f"else:\npass\nj=j+1\nprint(h)")
+
+
+
+    
+        
+        
+            
