be9e38766a18f2bbf238df01f92d1122fa78cc70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:12 +0000

    Add package my-happy-flow/0.0.49/my_happy_flow-0.0.49.tar.gz

---
+++ packages/my-happy-flow/my_happy_flow-0.0.49.tar.gz/my_happy_flow-0.0.49/src/my_happy_flow/version.py
@@ -0,0 +1,8 @@
+def get_versions():
+    return {
+        'version': "0.0.49"
+    }
+
+def get_version():
+    """Get the short version string for this project."""
+    return get_versions()["version"]
