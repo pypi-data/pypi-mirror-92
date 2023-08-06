bae0296e9f59a04b460c3e37702f2caabfa5e232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:27 +0000

    Add package trepan2/1.2.3/trepan2-1.2.3-py2.7.egg

---
+++ packages/trepan2/trepan2-1.2.3-py2.7.egg/trepan/processor/cmdbreak.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2009, 2010, 2015, 2017-2018 Rocky Bernstein
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import inspect
+import pyficache
+from trepan import misc as Mmisc
+from trepan.processor.parse.semantics import build_bp_expr
+from trepan.processor.parse.parser import LocationError
+from trepan.processor.parse.scanner import ScannerError
+from trepan.processor.location import resolve_location
+
+def set_break(cmd_obj, func, filename, lineno, condition, temporary,
+              args, force=False):
+    if lineno is None:
+        part1 = ("I don't understand '%s' as a line number, function name,"
+                 % ' '.join(args[1:]))
+        msg = Mmisc.wrapped_lines(part1, "or file/module plus line number.",
+                                  cmd_obj.settings['width'])
+        cmd_obj.errmsg(msg)
+        return False
+    if filename is None:
+        filename = cmd_obj.proc.curframe.f_code.co_filename
+        filename = cmd_obj.core.canonic(filename)
+        pass
+    if func is None:
+        ok_linenos = pyficache.trace_line_numbers(filename)
+        if not ok_linenos or lineno not in ok_linenos:
+            part1 = ('File %s' % cmd_obj.core.filename(filename))
+            msg = Mmisc.wrapped_lines(part1,
+                                      "is not stoppable at line %d." %
+                                      lineno, cmd_obj.settings['width'])
+            cmd_obj.errmsg(msg)
+            if force:
+                cmd_obj.msg("Breakpoint set although it may never be reached")
+            else:
+                return False
+        pass
+    bp =  cmd_obj.core.bpmgr.add_breakpoint(filename, lineno, temporary,
+                                         condition, func)
+    if func and inspect.isfunction(func):
+        cmd_obj.msg('Breakpoint %d set on calling function %s()'
+                 % (bp.number, func.func_name))
+        part1 = 'Currently this is line %d of file'  % lineno
+        msg = Mmisc.wrapped_lines(part1, cmd_obj.core.filename(filename),
+                                  cmd_obj.settings['width'])
+    else:
+        part1 = ( 'Breakpoint %d set at line %d of file'
+                  % (bp.number, lineno))
+        msg = Mmisc.wrapped_lines(part1, cmd_obj.core.filename(filename),
+                                  cmd_obj.settings['width'])
+        pass
+    cmd_obj.msg(msg)
+    return True
+
+INVALID_PARSE_BREAK = (None, None, None, None)
+def parse_break_cmd(proc, args):
+    if proc.current_command is None:
+        proc.errmsg("Internal error")
+        return INVALID_PARSE_BREAK
+
+    text = proc.current_command[len(args[0])+1:]
+    if len(args) > 1 and args[1] == 'if':
+        location = '.'
+        condition = text[text.find('if ')+3:]
+    elif text == '':
+        location = '.'
+        condition  = None
+    else:
+        try:
+            bp_expr   = build_bp_expr(text)
+        except LocationError as e:
+            proc.errmsg("Error in parsing breakpoint expression at or around:")
+            proc.errmsg(e.text)
+            proc.errmsg(e.text_cursor)
+            return INVALID_PARSE_BREAK
+        except ScannerError as e:
+            proc.errmsg("Lexical error in parsing breakpoint expression at or around:")
+            proc.errmsg(e.text)
+            proc.errmsg(e.text_cursor)
+            return INVALID_PARSE_BREAK
+
+        location  = bp_expr.location
+        condition = bp_expr.condition
+
+    location = resolve_location(proc, location)
+    if location:
+        return location.method, location.path, location.line_number, condition
+    else:
+        return INVALID_PARSE_BREAK
+
+
+# Demo it
+if __name__=='__main__':
+    from trepan.processor.command import mock as Mmock
+    from trepan.processor.cmdproc import CommandProcessor
+    import sys
+    d = Mmock.MockDebugger()
+    cmdproc = CommandProcessor(d.core)
+    # print '-' * 10
+    # print_source_line(sys.stdout.write, 100, 'source_line_test.py')
+    # print '-' * 10
+    cmdproc.frame = sys._getframe()
+    cmdproc.setup()
+    for cmd in (
+            # "break '''c:\\tmp\\foo.bat''':1",
+            # 'break """/Users/My Documents/foo.py""":2',
+            # "break",
+            # "break 10",
+            # "break if True",
+            # "break cmdproc.py:5",
+            # "break set_break()",
+            "break 4 if i == 5",
+            # "break cmdproc.setup()",
+            ):
+        args = cmd.split(' ')
+        cmdproc.current_command = cmd
+        print(parse_break_cmd(cmdproc, args))
+    pass
---
+++ packages/trepan2/trepan2-1.2.3-py2.7.egg/trepan/processor/command/break.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2009-2010, 2013-2015, 2017-2018 Rocky Bernstein
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+
+# Our local modules
+from trepan.processor.command import base_cmd as Mbase_cmd
+from trepan.processor import cmdbreak as Mcmdbreak
+from trepan.processor import complete as Mcomplete
+
+
+class BreakCommand(Mbase_cmd.DebuggerCommand):
+    """**break** [*location*] [if *condition*]]
+
+Sets a breakpoint, i.e. stopping point just before the
+execution of the instruction specified by *location*.
+
+Without arguments or an empty *location*, the breakpoint is set at the
+current stopped location.
+
+See `help syntax location` for detailed information on a location.
+
+If the word `if` is given after *location*, subsequent arguments given
+Without arguments or an empty *location*, the breakpoint is set
+the current stopped location.
+
+Normally we only allow stopping at lines that we think are
+stoppable. If the command has a `!` suffix, force the breakpoint anyway.
+
+Examples:
+---------
+
+   break                # Break where we are current stopped at
+   break if i < j       # Break at current line if i < j
+   break 10             # Break on line 10 of the file we are
+                        # currently stopped at
+   break! 10            # Break where we are current stopped at, even if
+                        # we don't think line 10 is stoppable
+   break os.path.join() # Break in function os.path.join
+   break x[i].fn()      # break in function specified by x[i].fn
+   break x[i].fn() if x # break in function specified by x[i].fn
+                        # if x is set
+   break os.path:45     # Break on line 45 file holding module os.path
+   break myfile.py:2    # Break on line 2 of myfile.py
+   break myfile.py:2 if i < j # Same as above but only if i < j
+   break "foo's.py":1"  # One way to specify path with a quote
+   break 'c:\\foo.bat':1      # One way to specify a Windows file name,
+   break '/My Docs/foo.py':1  # One way to specify path with blanks in it
+
+See also:
+---------
+
+`info break`, `tbreak`, `condition` and `help syntax location`."""
+
+    aliases       = ('b', 'break!', 'b!')
+    category      = 'breakpoints'
+    min_args      = 0
+    max_args      = None
+    name          = os.path.basename(__file__).split('.')[0]
+    need_stack    = True
+    short_help    = 'Set breakpoint at specified line or function'
+
+    complete= Mcomplete.complete_break_linenumber
+
+    def run(self, args):
+        force = True if args[0][-1] == '!' else False
+        (func, filename, lineno,
+         condition) = Mcmdbreak.parse_break_cmd(self.proc, args)
+        if not (func == None and filename == None):
+            Mcmdbreak.set_break(self, func, filename, lineno, condition,
+                                False, args, force=force)
+        return
+
+# # Demo it
+# if __name__ == '__main__':
+#     import sys
+#     from trepan import debugger as Mdebugger
+#     d = Mdebugger.Debugger()
+#     cmd = BreakCommand(d.core.processor)
+#     cmd.proc.frame = sys._getframe()
+#     cmd.proc.setup()
+
+#     def foo():
+#         return 'bar'
+
+#     for c in (
+#             ('break', 'os.path:10'),
+#             ('break', 'os.path.join()'),
+#             ('break', 'foo()', 'if', 'True'),
+#             ('break', 'os.path:10', 'if', 'True'),
+#             ('break',),
+#             ('break', 'cmd.run()'),
+#             ('break', '10'),
+#             ('break', __file__ + ':10'),
+#             ('break', 'foo()')):
+#         cmd.proc.current_command = ' '.join(c)
+#         cmd.run(c)
+#     pass
