bfc21920cb1ff5d7113ab6c05246b153aff3518f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:15 +0000

    Add package servz/0.0.0.0/servz-0.0.0.0.tar.gz

---
+++ packages/servz/servz-0.0.0.0.tar.gz/servz-0.0.0.0/README.md
@@ -0,0 +1,35 @@
+<!---
+# Modifications © 2020 Hashmap, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+-->
+
+# Servz
+##  Machine Learning Model Serving
+
+This library is a pre-alpha right now
+
+The intent of this package is to provide a machine learning deplpoyment layer for model predictions.
+
+## structure
+````
+/base  - common library
+/core - core files for servz
+/orchestration_artifact_builder - 
+/orchestration_artifact_deployer - 
+/packager - 
+/pipeline - loading and validation of serving pipeline
+/server_templates - artifacts for artifact builder to construct endpoints
+/tests - unit tests and e2e tests
+
+```
---
+++ packages/servz/servz-0.0.0.0.tar.gz/servz-0.0.0.0/servz/packager/model_core.py
@@ -0,0 +1,36 @@
+# Copyright © 2020 Hashmap, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import servz.orchestration_artifact_builder.gcloud as gcs
+
+
+class ModelCore:
+
+    def __init__(self, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls, **kwargs):
+        """
+            Return the results of the model callback.
+
+            Args:
+                flows: dictionary of all active flows
+
+            Returns: model prediction
+
+        """
+        #  TODO:  some kind of factory using providah to generate the client from config params
+        model = gcs.fetch_model(kwargs)
+        return model.predict()
