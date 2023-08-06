c663094d13a5b5d93d1781f29a28e9048857925c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:47 +0000

    Add package webshotapi/1.0.4/webshotapi-1.0.4.tar.gz

---
+++ packages/webshotapi/webshotapi-1.0.4.tar.gz/webshotapi-1.0.4/webshotapi/__init__.py
@@ -0,0 +1,6 @@
+from .version import __version__
+from .ApiClient import ApiClient
+from .ApiException import ApiException
+from .RequestClient import RequestClient
+from .Project import Project
+from .RESTResponse import RESTResponse
