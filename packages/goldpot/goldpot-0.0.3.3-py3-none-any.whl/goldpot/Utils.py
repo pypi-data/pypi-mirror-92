6421e4a1a9dce790a9e9c0de28e52f38889b8cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:23 +0000

    Add package goldpot/0.0.3.3/goldpot-0.0.3.3-py3-none-any.whl

---
+++ packages/goldpot/goldpot-0.0.3.3-py3-none-any.whl/goldpot/Utils.py
@@ -0,0 +1,25 @@
+import itertools
+import sys
+from itertools import takewhile, accumulate
+from typing import Iterable
+
+
+class Splitter:
+    # TODO: more general (2d) knapsack opt approach
+    # TODO: tail rec for current implementation
+
+    @staticmethod
+    def len_chunk_split(items: Iterable[str], max_len: int = sys.maxsize, max_chunks: int = sys.maxsize) -> Iterable[str]:
+
+        def chunks(iterable, n):
+            for i in range(0, len(iterable), n):
+                yield iterable[i:i+n]
+
+        def rec(items, res: Iterable[str] = [], pending=True):
+            if len(items) == 0 or not pending:
+                return res
+            else:
+                split_n = len(list(takewhile(lambda cum_len: not cum_len > max_len, accumulate([len(s) for s in items]))))
+                return rec(items[split_n:], res + [items[:split_n]], split_n > 0)
+
+        return [list(chunks(split, max_chunks)) for split in rec(items) if len(split) > 0]
