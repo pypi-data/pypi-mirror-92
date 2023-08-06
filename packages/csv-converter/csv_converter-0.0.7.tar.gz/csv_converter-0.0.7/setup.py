0ca2ac9f4c771e06aa802a1eeedcf212ec4f7579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:36 +0000

    Add package csv-converter/0.0.7/csv_converter-0.0.7.tar.gz

---
+++ packages/csv-converter/csv_converter-0.0.7.tar.gz/csv_converter-0.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csv_converter",
+    version="0.0.7",
+    author="JWDT",
+    author_email="pypi@zephyr.ltd.uk",
+    description="Tool to convert CSV files based on JSON config.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JWDT/csv_converter",
+    packages='.',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
