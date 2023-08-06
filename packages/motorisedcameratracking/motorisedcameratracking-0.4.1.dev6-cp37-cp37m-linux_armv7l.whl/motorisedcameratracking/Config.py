bdc6b560bb13e4601670243ec6181987fb77bd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:05 +0000

    Add package motorisedcameratracking/0.4.1.dev6/motorisedcameratracking-0.4.1.dev6-cp37-cp37m-linux_armv7l.whl

---
+++ packages/motorisedcameratracking/motorisedcameratracking-0.4.1.dev6-cp37-cp37m-linux_armv7l.whl/motorisedcameratracking/Config.py
@@ -0,0 +1,21 @@
+
+class Config():
+    """Hello"""
+    timeUnit=None
+    
+
+
+    #xMotor=""
+    #yMotor=""
+    #Cameras=[RPICam]#store the list of classes to allow the user to create their own
+    #Motors=[]
+
+
+
+    def __init__(self,timeUnit):
+        """The __init__ method"""
+        self.timeUnit=timeUnit
+
+
+
+
