230498d9879cf947d224364f60cd5b9209562565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:04 +0000

    Add package are/1.0.0/are-1.0.0.tar.gz

---
+++ packages/are/are-1.0.0.tar.gz/are-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="are",
+    version="1.0.0",
+    packages=["are",],
+    install_requires=["reiter", "nfa",],
+    license="MIT",
+    url="https://github.com/reity/are",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Library for defining and working with abstract " +\
+                "regular expressions that work with any symbol type.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
