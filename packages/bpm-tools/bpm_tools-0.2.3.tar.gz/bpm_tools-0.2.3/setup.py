d88132f16578e9769edfd4f2b430022e2f1498cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:36 +0000

    Add package bpm-tools/0.2.3/bpm_tools-0.2.3.tar.gz

---
+++ packages/bpm-tools/bpm_tools-0.2.3.tar.gz/bpm_tools-0.2.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bpm_tools", # Replace with your own username
+    version="0.2.3",
+    author="Cameron Tarbotton",
+    #author_email="author@example.com",
+    description="Shared functions for BPM data pipelines.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
