b945638bdd6d739456b7a7f8adfaf03e13351ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:03 +0000

    Add package stressrunner/1.1.0/stressrunner-1.1.0.tar.gz

---
+++ packages/stressrunner/stressrunner-1.1.0.tar.gz/stressrunner-1.1.0/stressrunner/test/test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@file  : test.py
+@Time  : 2020/11/17 10:33
+@Author: Tao.Xu
+@Email : tao.xu2008@outlook.com
+"""
+
+import unittest
+from stressrunner import StressRunner
+
+
+class UnitTestCase(unittest.TestCase):
+
+    def test_1(self):
+        self.assertTrue('1')
+        print("---- test1 test1 test1 test1 test1 ...")
+
+    def test_2(self):
+        import random
+        self.assertEqual(1, random.choice(range(3)))
+        print("---- test2 test2 test2 test2 test2 ...")
+
+    def test_3(self):
+        self.assertTrue('1')
+        print("---- test3 test3 test3 test3 test3 ...")
+
+
+def teardown():
+    print("teardown teardown teardown ...")
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(UnitTestCase)
+    runner = StressRunner(iteration=3)
+    runner.test_desc = 'stress unittest'
+    runner.test_version = '1.2.3'
+    runner.report_title = 'report-1.2.3'
+    runner.test_env = {
+        'sss': 1
+    }
+    runner.run(suite)
+    teardown()
+    # runner.send_mail()
