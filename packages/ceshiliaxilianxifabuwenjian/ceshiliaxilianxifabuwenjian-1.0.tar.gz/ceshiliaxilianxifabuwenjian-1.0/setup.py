c9e1713504fd36004283fe8b8a3f5786f0fa05d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:39 +0000

    Add package ceshiliaxilianxifabuwenjian/1.0/ceshiliaxilianxifabuwenjian-1.0.tar.gz

---
+++ packages/ceshiliaxilianxifabuwenjian/ceshiliaxilianxifabuwenjian-1.0.tar.gz/ceshiliaxilianxifabuwenjian-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='ceshiliaxilianxifabuwenjian', # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='我自己的测试文件', #描述
+    author='郭从超', # 作者
+    author_email='1365334391@qq.com',
+    py_modules=['fabuwenjian.mode','fabuwenjian.mode1'] # 要发布的模块
+)
