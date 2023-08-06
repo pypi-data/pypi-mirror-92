d58f80a0041006d380799009810ae509e609a3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:52 +0000

    Add package mopinion/0.0.2/mopinion-0.0.2.tar.gz

---
+++ packages/mopinion/mopinion-0.0.2.tar.gz/mopinion-0.0.2/CHANGES.rst
@@ -0,0 +1,24 @@
+Changelog of mopinion-python-api
+===================================================
+
+
+0.0.3 (unreleased)
+-------------------
+
+
+0.0.2 (2021-01-20)
+-------------------
+
+- Fixed in documentation.
+
+- Removed travis ci. Integrated GitHub Actions.
+
+- Added coverage into GitHub actions.
+
+0.0.1 (2021-01-20)
+-------------------
+
+- Initial project structure.
+- Client implementation.
+- Test suite.
+- Request examples.
---
+++ packages/mopinion/mopinion-0.0.2.tar.gz/mopinion-0.0.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import os
+
+version = "0.0.2"
+
+long_description = "\n\n".join([open("README.rst").read(), open("CHANGES.rst").read()])
+
+install_requires = ["requests"]
+
+# emulate "--no-deps" on the readthedocs build (there is no way to specify this
+# behaviour in the .readthedocs.yml)
+if os.environ.get("READTHEDOCS") == "True":
+    install_requires = []
+
+
+tests_require = ["pytest"]
+
+setup(
+    name="mopinion",
+    version=version,
+    description="Client library for the Mopinion Data API",
+    long_description=long_description,
+    # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["mopinion"],
+    author="Mopinion",
+    author_email="",
+    url="https://github.com/mopinion/mopinion-python-api",
+    license="MIT License",
+    packages=["mopinion_client"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    python_requires=">=3.7",
+    extras_require={"test": tests_require},
+    entry_points={"console_scripts": []},
+)
