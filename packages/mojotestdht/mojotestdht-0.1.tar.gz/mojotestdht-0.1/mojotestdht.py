338cd3f182a173a919eaf54aac5591699bf1c34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:06 +0000

    Add package mojotestdht/0.1/mojotestdht-0.1.tar.gz

---
+++ packages/mojotestdht/mojotestdht-0.1.tar.gz/mojotestdht-0.1/mojotestdht.py
@@ -0,0 +1,51 @@
+from machine import Pin
+
+
+import dht 
+import time 
+
+
+
+
+
+class Main:
+	"""docstring for ClassName"""
+	def __init__(self, pin= None, type = 11):
+
+		self.pin = pin 
+		self.is_output = True 
+		self.is_input = False
+		self.type = type 
+
+
+
+	def measure_data(self):
+		sensor_pin = Pin(self.pin, Pin.IN, Pin.PULL_UP)
+
+		if self.type==11:
+			
+			sensor = dht.DHT11(sensor_pin)
+			sensor.measure()
+			return {'temprature':sensor.temprature(), 'humidity': sensor.humidity()}
+
+		else:
+			sensor = dht.DHT22(sensor_pin)
+			sensor.measure()
+			return {'temprature':sensor.temprature(), 'humidity': sensor.humidity()}
+
+
+	def input(self):
+		return is_input
+
+
+	def output(self):
+		return self.measure_data()
+
+
+
+
+
+
+
+		
+		
