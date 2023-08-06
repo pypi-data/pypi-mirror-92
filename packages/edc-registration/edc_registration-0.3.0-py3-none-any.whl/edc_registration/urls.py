d26edee14cc3936966b3ed0b8ba034c9be8398fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:42 +0000

    Add package edc-registration/0.3.0/edc_registration-0.3.0-py3-none-any.whl

---
+++ packages/edc-registration/edc_registration-0.3.0-py3-none-any.whl/edc_registration/urls.py
@@ -0,0 +1,11 @@
+from django.views.generic.base import RedirectView
+from django.urls.conf import path
+
+from .admin_site import edc_registration_admin
+
+app_name = "edc_registration"
+
+urlpatterns = [
+    path("admin/", edc_registration_admin.urls),
+    path("", RedirectView.as_view(url="/edc_registration/admin/"), name="home_url"),
+]
