65be259937cf8cde5168cc52a24bd88d14dc75e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:28 +0000

    Add package XX/3.2.23/XX-3.2.23-py2.py3-none-any.whl

---
+++ packages/XX/XX-3.2.23-py2.py3-none-any.whl/XX/Scrapy/DM/ProxyDM.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import base64
+import random
+
+from logzero import logger
+
+import XX.DB.RedisHelper as Rh
+
+
+# redisIP池代理
+class IPPollingProxy(object):
+    # ping from all province, then get cdn ip
+    proxy_cdn = []
+
+    # crawl everyday, then check them
+    proxy_public = []
+
+    # You must be allowed within the range of IP
+    proxy_private = [
+        'proxy.scrapy.com:23088',
+    ]
+
+    # all proxy pool
+    proxy = proxy_public + proxy_private
+
+    def process_request(self, request, spider):
+        proxy_ip = random.choice(self.proxy)
+        if proxy_ip:
+            request.meta['proxy'] = "http://" + proxy_ip
+        return
+
+
+# 阿布云代理
+class AbuyunProxy(object):
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        cls.settings = crawler.settings
+        return cls()
+
+    def process_request(self, request, spider):
+        # 代理服务器
+        proxy_url = "http://http-dyn.abuyun.com:9020"
+        code = bytes((self.settings.get("PROXY_UN") + ":" + self.settings.get("PROXY_PWD")), "ascii")
+        proxyAuth = "Basic " + base64.urlsafe_b64encode(code).decode("utf8")
+        request.meta["proxy"] = proxy_url
+        request.headers["Proxy-Authorization"] = proxyAuth
+
+
+# 固定IP代理
+class IpProxy(object):
+
+    def __init__(self):
+        # 获取Redis连接
+        pass
+
+    def process_request(self, request, spider):
+        #  这是ip用例，如果想要实时更新，就可以从DB中获取
+        ips = ["49.85.6.229:39896", "49.85.6.229:39896", "49.85.6.229:39896"]
+        ip = random.choice(ips)
+        request.meta["proxy"] = ip
+
+
+# Goubanjia代理
+class Goubanjia(object):
+
+    def __init__(self):
+        # 获取Redis连接
+        pass
+
+    def process_request(self, request, spider):
+        #  这是ip用例，如果想要实时更新，就可以从DB中获取
+        ips = ["49.85.6.229:39896"]
+        ip = random.choice(ips)
+        request.meta["proxy"] = ip
+
+
+class MimvpProxy(object):
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        cls.settings = crawler.settings
+        return cls()
+
+    def __init__(self):
+        self.conn_redis = Rh.RedisHelper.get_redis_connect(self.settings.get("REDIS_HOST"),
+                                                           password=self.settings.get("REDIS_PWD"), db=9)
+
+    def process_request(self, request, spider):
+        ips = self.conn_redis.keys()
+        if ips:
+            request.meta["proxy"] = "http://c1259e45c76b:b7b9bd6b48@" + random.choice(ips)
+            logger.info("Proxy is " + str(request.meta["proxy"]))
+        else:
+            logger.info("No Proxy ip")
