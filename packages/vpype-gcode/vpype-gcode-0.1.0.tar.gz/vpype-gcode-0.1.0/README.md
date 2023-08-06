f3f11cd2394d1f858bc1924a455db413ee454775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:40 +0000

    Add package vpype-gcode/0.1.0/vpype-gcode-0.1.0.tar.gz

---
+++ packages/vpype-gcode/vpype-gcode-0.1.0.tar.gz/vpype-gcode-0.1.0/README.md
@@ -0,0 +1,234 @@
+# vpype-gcode
+Vpype plugin for gcode
+See: https://github.com/abey79/vpype
+
+
+Gcode vpype plugin. Write gcode files for the vpype pipeline.
+
+* `gwrite` write gcode geometries to disk
+
+
+# Installing
+`$ pip install vpype-gcode`
+
+# vpype gwrite --help
+
+```Usage: vpype gwrite [OPTIONS] FILENAME
+
+Options:
+  -v, --version TEXT    version to write
+  -h, --header TEXT     header to write
+  -m, --move TEXT       move to write
+  -l, --line TEXT       header to write
+  -b, --preblock TEXT   preblock to write
+  -B, --postblock TEXT  postblock to write
+  -c, --prelayer TEXT   prelayer to write
+  -C, --postlayer TEXT  postlayer to write
+  -h, --footer TEXT     header to write
+  -u, --unit TEXT       unit for coordinates (i.e. in or mm)
+  -x, --flip_x          flip_x from native
+  -y, --flip_y          flip_y from native
+  -r, --relative        use relative coordinates
+  --help                Show this message and exit.
+```
+
+# Versions
+```python
+        'ninja':
+            {
+                'header': 'G20\nG17\nG90\n',
+                'move': 'M380\nG00 X%.4f Y%.4f\nM381\n',
+                'line': 'G01 X%.4f Y%.4f\n',
+                'footer': 'M2\n',
+                'unit': 'mm'
+            },
+        'gcode':
+            {
+                'header': 'G20\nG17\nG90\n',
+                'move': 'G00 X%.4f Y%.4f\n',
+                'line': 'G01 X%.4f Y%.4f\n',
+                'footer': 'M2\n',
+                'unit': 'mm'
+            },
+        'gcode_relative':
+            {
+                'header': 'G20\nG17\nG91\n',
+                'move': 'G00 X%.4f Y%.4f\n',
+                'line': 'G01 X%.4f Y%.4f\n',
+                'footer': 'M2\n',
+                'relative': True,
+                'unit': 'mm'
+            }
+        'csv':
+            {
+                'header': "#Operation, X-value, Y-value\n",
+                'move': "Move, %f, %f\n",
+                'line': "Line-to, %f, %f\n"
+            },
+        'json':
+            {
+                'header': '{\n',
+                'footer': '}\n',
+                'prelayer': '\t"Layer": {\n',
+                'preblock': '\t\t"Block": [\n',
+                'move': '\t\t{\n\t\t\t"X": %d,\n\t\t\t"Y": %d\n\t\t}',
+                'line': ',\n\t\t{\n\t\t\t"X": %d,\n\t\t\t"Y": %d\n\t\t}',
+                'postblock': '\n\t\t],\n',
+                'postlayer': '\t},\n',
+            }
+```
+
+
+# Examples
+
+## Convert SVG -> GCode
+
+`vpype read butterfly.svg gwrite --version gcode butterfly.gcode`
+
+Loads up a svg and writes it in default gcode.
+
+`vpype begin grid -o 25 25 10 10 circle 0 0 100 end gwrite --version ninja -y --footer M99 test.gcode`
+
+Here we are creating a grid of circles then we are `gwrite` in version `ninja` with a flipped y-axis and a footer of M99 rather than our default footer in `ninja` that is `M2\n`
+
+Let's say our gcode is so different that it's an CSV file.
+
+`vpype begin grid -o 25 25 10 10 circle 0 0 100 end gwrite --header "#Operation, X-value, Y-value\n" --move "Move, %d, %d\n" --line "Line-to, %d, %d\n" test.csv`
+
+This produces:
+```csv
+#Operation, X-value, Y-value
+Move, 26, 0
+Line-to, 26, 0
+Line-to, 26, -1
+Line-to, 26, -2
+Line-to, 26, -3
+Line-to, 25, -4
+Line-to, 25, -5
+Line-to, 25, -6
+...
+```
+
+Giving it a simplier example:
+`vpype rect 0 0 100 100 gwrite --header "#Operation, X-value, Y-value\n" --move "Move, %d, %d\n" --line "Line-to, %d, %d\n"  test.csv`
+
+```csv
+#Operation, X-value, Y-value
+Move, 0, 0
+Line-to, 0, 100
+Line-to, 100, 100
+Line-to, 100, 0
+Line-to, 0, 0
+```
+
+This produces a plain text CSV file of the rectangle.
+
+Now using versions we could have done `--version csv` for this, but you can simply give the parts needed to perform your output.
+
+
+# Formatting
+
+The gwrite command gives you access to write to a variety of formats that fit the given outline. We're writing generic ascii. Since gcode can have more flavors than a Baskin Robbins™, it's best to simply draw broad strokes as to what ascii output should look like. Here we define the various elements without any regard to the gcode it will largely be producing.
+
+```
+<header>
+  <prelayer>
+    <preblock>
+      <move>
+      <line>
+      <line>
+      <line>
+      <line>
+    <postblock>
+    <preblock>
+      <move>
+      <line>
+      <line>
+      <line>
+      <line>
+    <postblock>
+    <preblock>
+      <move>
+      <line>
+      <line>
+      <line>
+      <line>
+    <postblock>
+ <postlayer>
+ <prelayer>
+    <preblock>
+      <move>
+      <line>
+      <line>
+      <line>
+      <line>
+    <postblock>
+    <preblock>
+      <move>
+      <line>
+      <line>
+      <line>
+      <line>
+    <postblock>
+    <preblock>
+      <move>
+      <line>
+      <line>
+      <line>
+      <line>
+    <postblock>
+ <postlayer>
+<footer>
+```
+
+This is the secret sauce of gwrite, it writes generic ascii which can be themed as functional gcode.
+
+For example if you write version `json`:
+```python
+        'json':
+            {
+                'header': '{\n',
+                'footer': '}\n',
+                'prelayer': '\t"Layer": {\n',
+                'preblock': '\t\t"Block": [\n',
+                'move': '\t\t{\n\t\t\t"X": %d,\n\t\t\t"Y": %d\n\t\t}',
+                'line': ',\n\t\t{\n\t\t\t"X": %d,\n\t\t\t"Y": %d\n\t\t}',
+                'postblock': '\n\t\t],\n',
+                'postlayer': '\t},\n',
+            }
+```
+
+Sending our rectangle to Json:
+
+`vpype rect 0 0 100 100 gwrite --version json test.json`
+
+```json
+{
+	"Layer": {
+		"Block": [
+		{
+			"X": 0,
+			"Y": 0
+		},
+		{
+			"X": 0,
+			"Y": 100
+		},
+		{
+			"X": 100,
+			"Y": 100
+		},
+		{
+			"X": 100,
+			"Y": 0
+		},
+		{
+			"X": 0,
+			"Y": 0
+		}
+		],
+	},
+}
+```
+
+Strictly speaking json shouldn't have the last 2 commas there, but it's for demonstration purposes.
---
+++ packages/vpype-gcode/vpype-gcode-0.1.0.tar.gz/vpype-gcode-0.1.0/vpype_gcode/gwrite.py
@@ -0,0 +1,278 @@
+import click
+import vpype as vp
+
+
+@click.command()
+@click.argument('filename', type=click.Path(exists=False))
+@click.option(
+    "-v",
+    "--version",
+    nargs=1,
+    default=None,
+    type=str,
+    help="version to write",
+)
+@click.option(
+    "-h",
+    "--header",
+    nargs=1,
+    default=None,
+    type=str,
+    help="header to write",
+)
+@click.option(
+    "-m",
+    "--move",
+    nargs=1,
+    default=None,
+    type=str,
+    help="move to write",
+)
+@click.option(
+    "-l",
+    "--line",
+    nargs=1,
+    default=None,
+    type=str,
+    help="header to write",
+)
+@click.option(
+    "-b",
+    "--preblock",
+    nargs=1,
+    default=None,
+    type=str,
+    help="preblock to write",
+)
+@click.option(
+    "-B",
+    "--postblock",
+    nargs=1,
+    default=None,
+    type=str,
+    help="postblock to write",
+)
+@click.option(
+    "-c",
+    "--prelayer",
+    nargs=1,
+    default=None,
+    type=str,
+    help="prelayer to write",
+)
+@click.option(
+    "-C",
+    "--postlayer",
+    nargs=1,
+    default=None,
+    type=str,
+    help="postlayer to write",
+)
+@click.option(
+    "-h",
+    "--footer",
+    nargs=1,
+    default=None,
+    type=str,
+    help="header to write",
+)
+@click.option(
+    "-u",
+    "--unit",
+    nargs=1,
+    default="px",
+    type=str,
+    help="unit for coordinates",
+)
+@click.option(
+    "-x",
+    "--flip_x",
+    is_flag=True,
+    nargs=1,
+    default=None,
+    type=bool,
+    help="flip_x from native",
+)
+@click.option(
+    "-y",
+    "--flip_y",
+    is_flag=True,
+    nargs=1,
+    default=None,
+    type=bool,
+    help="flip_y from native",
+)
+@click.option(
+    "-r",
+    "--relative",
+    is_flag=True,
+    nargs=1,
+    default=None,
+    type=bool,
+    help="use relative coordinates",
+)
+@vp.global_processor
+def gwrite(document: vp.Document, filename: str, version: str,
+           header: str,
+           move: str,
+           line: str,
+           preblock: str,
+           postblock: str,
+           prelayer: str,
+           postlayer: str,
+           footer: str,
+           unit: str,
+           flip_x: bool,
+           flip_y: bool,
+           relative: bool):
+    if version is None and header is None and move is None \
+            and line is None and preblock is None and postblock is None \
+            and prelayer is None and postlayer is None and footer is None:
+        version = "gcode"
+    writers = {
+        'ninja':
+            {
+                'header': 'G20\nG17\nG90\n',
+                'move': 'M380\nG00 X%.4f Y%.4f\nM381\n',
+                'line': 'G01 X%.4f Y%.4f\n',
+                'footer': 'M2\n',
+                'unit': 'mm'
+            },
+        'gcode':
+            {
+                'header': 'G20\nG17\nG90\n',
+                'move': 'G00 X%.4f Y%.4f\n',
+                'line': 'G01 X%.4f Y%.4f\n',
+                'footer': 'M2\n',
+                'unit': 'mm'
+            },
+        'gcode_relative':
+            {
+                'header': 'G20\nG17\nG91\n',
+                'move': 'G00 X%.4f Y%.4f\n',
+                'line': 'G01 X%.4f Y%.4f\n',
+                'footer': 'M2\n',
+                'relative': True,
+                'unit': 'mm'
+            },
+        'csv':
+            {
+                'header': "#Operation, X-value, Y-value\n",
+                'move': "Move, %f, %f\n",
+                'line': "Line-to, %f, %f\n"
+            },
+        'json':
+            {
+                'header': '{\n',
+                'footer': '}\n',
+                'prelayer': '\t"Layer": {\n',
+                'preblock': '\t\t"Block": [\n',
+                'move': '\t\t{\n\t\t\t"X": %d,\n\t\t\t"Y": %d\n\t\t}',
+                'line': ',\n\t\t{\n\t\t\t"X": %d,\n\t\t\t"Y": %d\n\t\t}',
+                'postblock': '\n\t\t],\n',
+                'postlayer': '\t},\n',
+            }  # Json is nearly validated has extra ',' on the last post-block and post-layer.
+
+    }
+    if version in writers:
+        writer = writers[version]
+        if 'header' in writer and header is None:
+            header = writer['header']
+        if 'move' in writer and move is None:
+            move = writer['move']
+        if 'line' in writer and line is None:
+            line = writer['line']
+        if 'preblock' in writer and preblock is None:
+            preblock = writer['preblock']
+        if 'postblock' in writer and postblock is None:
+            postblock = writer['postblock']
+        if 'prelayer' in writer and prelayer is None:
+            prelayer = writer['prelayer']
+        if 'postlayer' in writer and postlayer is None:
+            postlayer = writer['postlayer']
+        if 'footer' in writer and footer is None:
+            footer = writer['footer']
+        if 'unit' in writer and unit is None:
+            unit = writer['unit']
+        if 'flip_x' in writer and flip_x is None:
+            flip_x = writer['flip_x']
+        if 'flip_y' in writer and flip_y is None:
+            flip_y = writer['flip_y']
+        if 'relative' in writer and relative is None:
+            relative = writer['relative']
+    if header is not None:
+        header = header.replace('\\n', '\n')
+    if move is not None:
+        move = move.replace('\\n', '\n')
+    if line is not None:
+        line = line.replace('\\n', '\n')
+    if preblock is not None:
+        preblock = preblock.replace('\\n', '\n')
+    if postblock is not None:
+        postblock = postblock.replace('\\n', '\n')
+    if prelayer is not None:
+        prelayer = prelayer.replace('\\n', '\n')
+    if postlayer is not None:
+        postlayer = postlayer.replace('\\n', '\n')
+    if footer is not None:
+        footer = footer.replace('\\n', '\n')
+    if relative is None:
+        relative = False
+    if flip_x is None:
+        flip_x = False
+    if flip_y is None:
+        flip_y = False
+
+    scale = 1 / vp.convert_length(unit)
+
+    with open(filename, 'w') as f:
+        if header is not None:
+            f.write(header)
+        last_x = 0
+        last_y = 0
+        for layer in document.layers.values():
+            if prelayer is not None:
+                f.write(prelayer)
+            for p in layer:
+                m = p * scale
+                first = True
+                if preblock is not None:
+                    f.write(preblock)
+                for v in m:
+                    x = v.real
+                    if flip_x:
+                        x = -x
+                    y = v.imag
+                    if flip_y:
+                        y = -y
+                    if relative:
+                        dx = x - last_x
+                        dy = y - last_y
+                        if first:
+                            if move is not None:
+                                f.write(move % (dx, dy))
+                            first = False
+                        else:
+                            if line is not None:
+                                f.write(line % (dx, dy))
+                    else:
+                        if first:
+                            if move is not None:
+                                f.write(move % (x, y))
+                            first = False
+                        else:
+                            if line is not None:
+                                f.write(line % (x, y))
+                    last_x = x
+                    last_y = y
+                if postblock is not None:
+                    f.write(postblock)
+            if postlayer is not None:
+                f.write(postlayer)
+        if footer is not None:
+            f.write(footer)
+
+    return document
+
+
+gwrite.help_group = "Gcode"
