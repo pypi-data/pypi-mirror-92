e0a839014f1e74a66f9c3928ac1521d0357a22a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:49 +0000

    Add package display-wifi/1.0.0/display_wifi-1.0.0-py3-none-any.whl

---
+++ packages/display-wifi/display_wifi-1.0.0-py3-none-any.whl/wifi/__main__.py
@@ -0,0 +1,29 @@
+import subprocess
+
+
+def main(password=False):
+    if password:
+        data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+        profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+        for i in profiles:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode(
+                'utf-8').split('\n')
+            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+            try:
+                print("{:<30}|  {:<}".format(i, results[0]))
+            except:
+                print("{:<30}|  {:<}".format(i, ""))
+    else:
+        data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+        profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+        for i in profiles:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode(
+                'utf-8').split('\n')
+            try:
+                print("{:<30}".format(i, results[0]))
+            except:
+                print("{:<30}".format(i, ""))
+
+
+if __name__ == '__main__':
+    main()
