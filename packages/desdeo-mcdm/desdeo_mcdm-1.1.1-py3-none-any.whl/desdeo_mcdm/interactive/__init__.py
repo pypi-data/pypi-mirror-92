7ebd37a471e803c889b9540552ec53330c0799a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:35 +0000

    Add package desdeo-mcdm/1.1.1/desdeo_mcdm-1.1.1-py3-none-any.whl

---
+++ packages/desdeo-mcdm/desdeo_mcdm-1.1.1-py3-none-any.whl/desdeo_mcdm/interactive/__init__.py
@@ -0,0 +1,87 @@
+"""This module contains interactive methods and related requests implemented as classes.
+"""
+
+__all__ = [
+    "ENautilus",
+    "ENautilusException",
+    "ENautilusInitialRequest",
+    "ENautilusRequest",
+    "ENautilusStopRequest",
+    "validate_response",
+    "validate_preferences",
+    "validate_n2_preferences",
+    "validate_n_iterations",
+    "Nautilus",
+    "NautilusV2",
+    "NautilusException",
+    "NautilusInitialRequest",
+    "NautilusRequest",
+    "NautilusStopRequest",
+    "NautilusNavigator",
+    "NautilusNavigatorException",
+    "NautilusNavigatorRequest",
+    "NIMBUS",
+    "NimbusException",
+    "NimbusClassificationRequest",
+    "NimbusIntermediateSolutionsRequest",
+    "NimbusMostPreferredRequest",
+    "NimbusSaveRequest",
+    "NimbusStopRequest",
+    "ReferencePointMethod",
+    "RPMException",
+    "RPMInitialRequest",
+    "RPMRequest",
+    "RPMStopRequest",
+]
+
+from desdeo_mcdm.interactive.ENautilus import (
+    ENautilus,
+    ENautilusException,
+    ENautilusInitialRequest,
+    ENautilusRequest,
+    ENautilusStopRequest,
+)
+
+from desdeo_mcdm.interactive.Nautilus import (
+    validate_response,
+    validate_preferences,
+    validate_n_iterations,
+    Nautilus,
+    NautilusException,
+    NautilusInitialRequest,
+    NautilusRequest,
+    NautilusStopRequest
+)
+
+from desdeo_mcdm.interactive.NautilusV2 import (
+    validate_response,
+    validate_n2_preferences,
+    validate_n_iterations,
+    NautilusV2,
+    NautilusException,
+    NautilusInitialRequest,
+    NautilusRequest,
+    NautilusStopRequest
+)
+
+from desdeo_mcdm.interactive.NautilusNavigator import (
+    NautilusNavigator,
+    NautilusNavigatorException,
+    NautilusNavigatorRequest,
+)
+from desdeo_mcdm.interactive.NIMBUS import (
+    NIMBUS,
+    NimbusClassificationRequest,
+    NimbusException,
+    NimbusIntermediateSolutionsRequest,
+    NimbusMostPreferredRequest,
+    NimbusSaveRequest,
+    NimbusStopRequest,
+)
+from desdeo_mcdm.interactive.ReferencePointMethod import (
+    RPMException,
+    RPMInitialRequest,
+    RPMRequest,
+    RPMStopRequest,
+    ReferencePointMethod,
+)
