9231fb1ec19cbe2963491d688edec3031046c2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:36 +0000

    Add package model1/1.0.2.3/model1-1.0.2.3.tar.gz

---
+++ packages/model1/model1-1.0.2.3.tar.gz/model1-1.0.2.3/model1/order/model.py
@@ -0,0 +1,12 @@
+from ext import EXT_CONTEXT
+from sqlalchemy import Column, Integer, String
+
+
+class Order(EXT_CONTEXT["db"].Model):
+    __tablename__ = 'order'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(20))
+
+    def __init__(self, name):
+        self.name = name
