98667fb47049ac5545d446c4c6128cba40b76feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:10 +0000

    Add package cased/0.2.0/cased-0.2.0.tar.gz

---
+++ packages/cased/cased-0.2.0.tar.gz/cased-0.2.0/setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+setup(
+    name="cased",
+    version="0.2.0",
+    description="",
+    long_description="",
+    author="Cased",
+    author_email="support@cased.com",
+    url="https://github.com/cased/guard-client",
+    license="MIT",
+    keywords="cased api",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    zip_safe=False,
+    scripts=["cased-init", "cguard/cased"],
+    python_requires=">3.5",
+    install_requires=[
+        "requests",
+        "responses",
+        "mock",
+        "pytest >= 4.0c0",
+        "pytest-mock",
+        "pytest-xdist",
+        "flask",
+        "yaspin",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/cased/guard/issues",
+        "Documentation": "https://docs.cased.com/guard",
+        "Source Code": "https://github.com/cased/guard",
+    },
+)
