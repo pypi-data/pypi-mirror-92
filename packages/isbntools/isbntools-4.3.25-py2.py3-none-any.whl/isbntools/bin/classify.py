d3bdac0340bac7a6dd80bc3a3547b26a49dd2e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:07 +0000

    Add package isbntools/4.3.25/isbntools-4.3.25-py2.py3-none-any.whl

---
+++ packages/isbntools/isbntools-4.3.25-py2.py3-none-any.whl/isbntools/bin/classify.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from json import dumps
+from ..app import classify, quiet_errors
+
+PREFIX = 'isbn_'
+
+
+def usage(prefix=PREFIX):
+    print('Usage: %sclassify ISBN' % prefix)
+    return 1
+
+
+def main(isbn=None, prefix=PREFIX):
+    sys.excepthook = quiet_errors
+    try:
+        isbn = sys.argv[1] if not isbn else isbn[1]
+        data = classify(isbn)
+        if data:
+            print(dumps(data, indent=4))
+        else:
+            raise Exception('No data')
+    except:
+        usage(prefix)
