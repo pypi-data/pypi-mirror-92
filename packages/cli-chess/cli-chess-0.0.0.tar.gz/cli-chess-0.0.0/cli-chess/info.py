5e5d10068b84cb07b6eb74b436792c774bd01e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:45 +0000

    Add package cli-chess/0.0.0/cli-chess-0.0.0.tar.gz

---
+++ packages/cli-chess/cli-chess-0.0.0.tar.gz/cli-chess-0.0.0/cli-chess/info.py
@@ -0,0 +1,4 @@
+def print_info():
+    print('cli-chess is coming soon!')
+
+print_info()
---
+++ packages/cli-chess/cli-chess-0.0.0.tar.gz/cli-chess-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'cli-chess',
+    version = '0.0.0',
+    author = 'Trevor Bayless',
+    url = 'https://github.com/trevorbayless/cli-chess',
+    description = 'cli-chess is coming soon!',
+    py_modules = ['info'],
+    package_dir = {'': 'cli-chess'},
+)
+
