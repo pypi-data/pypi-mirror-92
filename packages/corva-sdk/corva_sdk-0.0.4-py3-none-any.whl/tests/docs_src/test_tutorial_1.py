23049143f0a36a8f6fb1a170b18d26fbce5cc244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:26 +0000

    Add package corva-sdk/0.0.4/corva_sdk-0.0.4-py3-none-any.whl

---
+++ packages/corva-sdk/corva_sdk-0.0.4-py3-none-any.whl/tests/docs_src/test_tutorial_1.py
@@ -0,0 +1,10 @@
+from docs_src import tutorial_1_hello_world
+
+
+def test_tutorial(settings):
+    event = (
+                '[{"records": [{"asset_id": 0, "company_id": 0, "version": 0, "collection": "", "data": {}}], '
+                '"metadata": {"app_stream_id": 0, "apps": {"%s": {"app_connection_id": 0, "app_version": 0}}}}]'
+            ) % settings.APP_KEY
+
+    tutorial_1_hello_world.lambda_handler(event, None)
