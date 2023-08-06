8cf14f475152be7dc9a46ff0e7b20096e1e56b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package netack/0.1.7/netack-0.1.7.tar.gz

---
+++ packages/netack/netack-0.1.7.tar.gz/netack-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: POSIX :: Linux',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+]
+
+setup(
+    name="netack",
+    version="0.1.7",
+    description="python library for basic network attacks",
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    author="Akash Sharma and Akshat Sharma",
+    License="MIT",
+    classifiers= classifiers,
+    keywords="network security",
+    packages=find_packages(),
+    install_requires=['scapy']
+)
