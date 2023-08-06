3f3271452fc77b88b282077f72e63d83439ab2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:38 +0000

    Add package mysql-server-has-gone-away/2.0.0/mysql_server_has_gone_away-2.0.0.tar.gz

---
+++ packages/mysql-server-has-gone-away/mysql_server_has_gone_away-2.0.0.tar.gz/mysql_server_has_gone_away-2.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mysql_server_has_gone_away",
+    version="2.0.0",
+    description="Django myslq backend that fixes issue with long living connection",
+    author='Andrew Koidan',
+    author_email='deathangel908@gmail.com',
+    license="MIT",
+    url='https://github.com/akoidan/MySQL-server-has-gone-away',
+    long_description=long_description,
+    packages=find_packages(),
+    long_description_content_type="text/markdown",
+)
