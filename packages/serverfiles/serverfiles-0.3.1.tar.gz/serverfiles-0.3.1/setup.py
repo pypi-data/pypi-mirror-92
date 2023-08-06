f3e085cf68f9894236e129ab155909d530c69726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:52 +0000

    Add package serverfiles/0.3.1/serverfiles-0.3.1.tar.gz

---
+++ packages/serverfiles/serverfiles-0.3.1.tar.gz/serverfiles-0.3.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='serverfiles',
+        description="An utility that accesses files on a HTTP server and stores them locally for reuse.",
+        author='Bioinformatics Laboratory, FRI UL',
+        author_email='marko.toplak@fri.uni-lj.si',
+        packages=["serverfiles"],
+        install_requires=[
+            'requests>=2.11.1',
+        ],
+        version='0.3.1',
+        zip_safe=False,
+        url="https://github.com/biolab/serverfiles",
+        test_suite="tests.suite"
+    )
