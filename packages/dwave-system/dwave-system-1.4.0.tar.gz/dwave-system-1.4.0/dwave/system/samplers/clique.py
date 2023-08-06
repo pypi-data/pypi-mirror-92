42a8a2b9ef0f6d8ee2dd988daed22bff9ca7ea43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:16 +0000

    Add package dwave-system/1.4.0/dwave-system-1.4.0.tar.gz

---
+++ packages/dwave-system/dwave-system-1.4.0.tar.gz/dwave-system-1.4.0/dwave/system/samplers/clique.py
@@ -0,0 +1,231 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import dimod
+import dwave_networkx as dnx
+
+from minorminer.busclique import find_clique_embedding, busgraph_cache
+
+from dwave.system.samplers.dwave_sampler import DWaveSampler
+
+__all__ = ['DWaveCliqueSampler']
+
+
+class DWaveCliqueSampler(dimod.Sampler):
+    """A sampler for solving clique binary quadratic models on the D-Wave system.
+
+    This sampler wraps
+    :func:`~minorminer.busclique.find_clique_embedding` to generate embeddings
+    with even chain length. These embeddings work well for dense
+    binary quadratic models. For sparse models, using
+    :class:`.EmbeddingComposite` with :class:`.DWaveSampler` is preferred.
+
+    Configuration such as :term:`solver` selection is similar to that of
+    :class:`.DWaveSampler`.
+
+    Args:
+        **config:
+            Keyword arguments, as accepted by :class:`.DWaveSampler`
+
+    Examples:
+        This example creates a BQM based on a 6-node clique (complete graph),
+        with random :math:`\pm 1` values assigned to nodes, and submits it to
+        a D-Wave system. Parameters for communication with the system, such
+        as its URL and an autentication token, are implicitly set in a
+        configuration file or as environment variables, as described in
+        `Configuring Access to D-Wave Solvers <https://docs.ocean.dwavesys.com/en/stable/overview/sapi.html>`_.
+
+        >>> from dwave.system import DWaveCliqueSampler
+        >>> import dimod
+        ...
+        >>> bqm = dimod.generators.ran_r(1, 6)
+        ...
+        >>> sampler = DWaveCliqueSampler()   # doctest: +SKIP
+        >>> sampler.largest_clique_size > 5  # doctest: +SKIP
+        True
+        >>> sampleset = sampler.sample(bqm, num_reads=100)   # doctest: +SKIP
+
+    """
+    def __init__(self, **config):
+
+        self.child = child = DWaveSampler(**config)
+
+        # do some topology checking
+        try:
+            topology_type = child.properties['topology']['type']
+            shape = child.properties['topology']['shape']
+        except KeyError:
+            raise ValueError("given sampler has unknown topology format")
+
+        # We need a networkx graph with certain properties. In the
+        # future it would be good for DWaveSampler to handle this.
+        # See https://github.com/dwavesystems/dimod/issues/647
+        if topology_type == 'chimera':
+            G = dnx.chimera_graph(*shape,
+                                  node_list=child.nodelist,
+                                  edge_list=child.edgelist,
+                                  )
+        elif topology_type == 'pegasus':
+            G = dnx.pegasus_graph(shape[0],
+                                  node_list=child.nodelist,
+                                  edge_list=child.edgelist,
+                                  )
+        else:
+            raise ValueError("unknown topology type")
+
+        self.target_graph = G
+
+        # get the energy range
+        try:
+            self.qpu_linear_range = child.properties['h_range']
+            self.qpu_quadratic_range = child.properties.get(
+                'extended_j_range', child.properties['j_range'])
+        except KeyError as err:
+            # for backwards compatibility with old software solvers
+            if child.solver.is_software:
+                self.qpu_linear_range = [-2, 2]
+                self.qpu_quadratic_range = [-1, 1]
+            else:
+                raise err
+
+    @property
+    def parameters(self):
+        try:
+            return self._parameters
+        except AttributeError:
+            pass
+
+        self._parameters = parameters = self.child.parameters.copy()
+
+        # this sampler handles scaling
+        parameters.pop('auto_scale', None)
+        parameters.pop('bias_range', None)
+        parameters.pop('quadratic_range', None)
+
+        return parameters
+
+    @property
+    def properties(self):
+        try:
+            return self._properties
+        except AttributeError:
+            pass
+
+        self._properties = dict(qpu_properties=self.child.properties)
+        return self.properties
+
+    @property
+    def largest_clique_size(self):
+        """The maximum number of variables that can be embedded.
+        """
+        return len(self.largest_clique())
+
+    def clique(self, variables):
+        """Return a clique embedding of the given size.
+
+        Args:
+            variables (int/collection):
+                Source variables. If an integer, the variables  embedded are
+                labelled `[0,n)`.
+
+        Returns:
+            dict: The clique embedding.
+        """
+        return find_clique_embedding(variables, self.target_graph)
+
+    def largest_clique(self):
+        """The clique embedding with the maximum number of source variables.
+
+        Returns:
+            dict: The clique embedding with the maximum number of source
+            variables.
+
+        """
+        return busgraph_cache(self.target_graph).largest_clique()
+
+    def sample(self, bqm, chain_strength=None, **kwargs):
+        """Sample from the specified binary quadratic model.
+
+        Args:
+            bqm (:class:`~dimod.BinaryQuadraticModel`):
+                Any binary quadratic model with up to
+                :attr:`.largest_clique_size` variables. This BQM is embedded
+                using a clique embedding.
+
+            chain_strength (float, optional):
+                The (relative) chain strength to use in the embedding. By
+                default a chain strength of `1.5sqrt(N)` where `N` is the size
+                of the largest clique, as returned by
+                :attr:`.largest_clique_size`.
+
+            **kwargs:
+                Optional keyword arguments for the sampling method, specified
+                per solver in :attr:`.parameters`.
+                D-Wave System Documentation's
+                `solver guide <https://docs.dwavesys.com/docs/latest/doc_solver_ref.html>`_
+                describes the parameters and properties supported on the D-Wave
+                system. Note that `auto_scale` is not supported by this
+                sampler, because it scales the problem as part of the embedding
+                process.
+
+        """
+
+        # some arguments should not be overwritten
+        if 'auto_scale' in kwargs:
+            raise TypeError("sample() got an unexpected keyword argument "
+                            "'auto_scale'")
+        if 'bias_range' in kwargs:
+            raise TypeError("sample() got an unexpected keyword argument "
+                            "'bias_range'")
+        if 'quadratic_range' in kwargs:
+            raise TypeError("sample() got an unexpected keyword argument "
+                            "'quadratic_range'")
+
+        # handle circular import. todo: fix
+        from dwave.system.composites.embedding import FixedEmbeddingComposite
+
+        # get the embedding
+        embedding = find_clique_embedding(bqm.variables, self.target_graph,
+                                          use_cache=True)
+
+        # returns an empty embedding when the BQM is too large
+        if not embedding and bqm.num_variables:
+            raise ValueError("Cannot embed given BQM (size {}), sampler can "
+                             "only handle problems of size {}".format(
+                                len(bqm.variables), self.largest_clique_size))
+
+        assert bqm.num_variables == len(embedding)  # sanity check
+
+        # scaling only make sense in Ising space
+        original_bqm = bqm
+
+        if bqm.vartype is not dimod.SPIN:
+            bqm = bqm.change_vartype(dimod.SPIN, inplace=False)
+
+        sampler = FixedEmbeddingComposite(
+            dimod.ScaleComposite(self.child),
+            embedding)
+
+        if 'auto_scale' in self.child.parameters:
+            kwargs['auto_scale'] = False
+
+        sampleset = sampler.sample(bqm,
+                                   bias_range=self.qpu_linear_range,
+                                   quadratic_range=self.qpu_quadratic_range,
+                                   chain_strength=chain_strength,
+                                   **kwargs
+                                   )
+
+        # change_vartype is non-blocking
+        return sampleset.change_vartype(original_bqm.vartype)
---
+++ packages/dwave-system/dwave-system-1.4.0.tar.gz/dwave-system-1.4.0/dwave/system/testing.py
@@ -0,0 +1,126 @@
+# Copyright 2018 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import collections
+import unittest.mock as mock
+from uuid import uuid4
+
+import dimod
+import dwave_networkx as dnx
+from tabu import TabuSampler
+import dwave.cloud.computation
+
+try:
+    from neal import SimulatedAnnealingSampler
+except ImportError:
+    from dimod import SimulatedAnnealingSampler
+
+import concurrent.futures
+import numpy as np
+
+
+C4 = dnx.chimera_graph(4, 4, 4)
+
+
+class MockDWaveSampler(dimod.Sampler, dimod.Structured):
+    """Mock sampler modeled after DWaveSampler that can be used for tests."""
+
+    nodelist = None
+    edgelist = None
+    properties = None
+    parameters = None
+
+    def __init__(self, broken_nodes=None, **config):
+        if broken_nodes is None:
+            self.nodelist = sorted(C4.nodes)
+            self.edgelist = sorted(tuple(sorted(edge)) for edge in C4.edges)
+        else:
+            self.nodelist = sorted(v for v in C4.nodes if v not in broken_nodes)
+            self.edgelist = sorted(tuple(sorted((u, v))) for u, v in C4.edges
+                                   if u not in broken_nodes and v not in broken_nodes)
+
+        # mark the sample kwargs
+        self.parameters = parameters = {}
+        parameters['num_reads'] = ['num_reads_range']
+        parameters['flux_biases'] = ['j_range']
+        parameters['label'] = []
+
+        # add the interesting properties manually
+        self.properties = properties = {}
+        properties['j_range'] = [-2.0, 1.0]
+        properties['h_range'] = [-2.0, 2.0]
+        properties['num_reads_range'] = [1, 10000]
+        properties['num_qubits'] = len(C4)
+        properties['category'] = 'qpu'
+        properties['quota_conversion_rate'] = 1
+        properties['topology'] = {'type': 'chimera', 'shape': [4, 4, 4]}
+        properties['chip_id'] = 'MockDWaveSampler'
+        properties['annealing_time_range'] = [1, 2000]
+        properties['num_qubits'] = len(self.nodelist)
+        properties['extended_j_range'] = [-2.0, 1.0]
+
+    @dimod.bqm_structured
+    def sample(self, bqm, num_reads=10, flux_biases=[], **kwargs):
+        # we are altering the bqm if flux_biases given
+
+        info = dict(problem_id=str(uuid4()))
+        label = kwargs.get('label')
+        if label is not None:
+            info.update(problem_label=label)
+
+        if not flux_biases:
+            ss = SimulatedAnnealingSampler().sample(bqm, num_reads=num_reads)
+            ss.info.update(info)
+            return ss
+
+        new_bqm = bqm.copy()
+
+        for v, fbo in enumerate(flux_biases):
+            self.flux_biases_flag = True
+            new_bqm.add_variable(v, 1000. * fbo)  # add the bias
+
+        response = SimulatedAnnealingSampler().sample(new_bqm, num_reads=num_reads)
+
+        # recalculate the energies with the old bqm
+        return dimod.SampleSet.from_samples_bqm([{v: sample[v] for v in bqm.variables}
+                                                 for sample in response.samples()],
+                                                bqm, info=info)
+
+class MockLeapHybridSolver:
+
+    properties = {'supported_problem_types': ['bqm'],
+                  'minimum_time_limit': [[1, 1.0], [1024, 1.0],
+                                         [4096, 10.0], [10000, 40.0]],
+                  'parameters': {'time_limit': None},
+                  'category': 'hybrid',
+                  'quota_conversion_rate': 1}
+
+    supported_problem_types = ['bqm']
+
+    def upload_bqm(self, bqm, **parameters):
+        bqm_adjarray = dimod.serialization.fileview.load(bqm)
+        future = concurrent.futures.Future()
+        future.set_result(bqm_adjarray)
+        return future
+
+    def sample_bqm(self, sapi_problem_id, time_limit):
+        #Workaround until TabuSampler supports C BQMs
+        bqm = dimod.BQM(sapi_problem_id.linear,
+                                    sapi_problem_id.quadratic,
+                                    sapi_problem_id.offset,
+                                    sapi_problem_id.vartype)
+        result = TabuSampler().sample(bqm, timeout=1000*int(time_limit))
+        future = dwave.cloud.computation.Future('fake_solver', None)
+        future._result = {'sampleset': result, 'problem_type': 'bqm'}
+        return future
