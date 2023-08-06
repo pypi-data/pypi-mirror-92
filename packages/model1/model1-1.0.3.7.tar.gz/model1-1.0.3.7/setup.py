d0dfe810c7b0d0fa5774a77c242e2e05c87db14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:07 +0000

    Add package model1/1.0.3.7/model1-1.0.3.7.tar.gz

---
+++ packages/model1/model1-1.0.3.7.tar.gz/model1-1.0.3.7/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="model1",
+    version="1.0.3.7",
+    keywords=("model1", "xxx"),
+    description="model1 sdk",
+    long_description="model1 sdk for python",
+    license="MIT Licence",
+
+    url="",
+    author="skyeye",
+    author_email="skyeye@gmail.com",
+
+    packages=find_packages(exclude=['dist']),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['skyext>=1.0.1.4'],
+    data_files=[('model1/conf/api', ['model1/conf/api/*.json'])],
+
+    scripts=[],
+    # entry_points={
+    #     'console_scripts': [
+    #         'test = test.help:main'
+    #     ]
+    # }
+)
