418043d27086a7cba65d6557c8a904e5ccac1a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:20 +0000

    Add package strawberry-graphql/0.44.8/strawberry-graphql-0.44.8.tar.gz

---
+++ packages/strawberry-graphql/strawberry-graphql-0.44.8.tar.gz/strawberry-graphql-0.44.8/strawberry/directive.py
@@ -0,0 +1,33 @@
+import dataclasses
+from typing import Callable, List, Optional
+
+from graphql import DirectiveLocation
+
+from .utils.str_converters import to_camel_case
+
+
+@dataclasses.dataclass
+class DirectiveDefinition:
+    name: str
+    resolver: Callable
+    locations: List[DirectiveLocation]
+    description: Optional[str] = None
+
+
+def directive(*, locations: List[DirectiveLocation], description=None, name=None):
+    def _wrap(f):
+        directive_name = name or to_camel_case(f.__name__)
+
+        f.directive_definition = DirectiveDefinition(
+            name=directive_name,
+            locations=locations,
+            description=description,
+            resolver=f,
+        )
+
+        return f
+
+    return _wrap
+
+
+__all__ = ["DirectiveLocation", "DirectiveDefinition", "directive"]
