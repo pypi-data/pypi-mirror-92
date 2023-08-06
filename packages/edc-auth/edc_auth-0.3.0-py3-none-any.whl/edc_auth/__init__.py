4cd22b389dc521176a30bab33813c41f74099005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:10 +0000

    Add package edc-auth/0.3.0/edc_auth-0.3.0-py3-none-any.whl

---
+++ packages/edc-auth/edc_auth-0.3.0-py3-none-any.whl/edc_auth/__init__.py
@@ -0,0 +1,28 @@
+from .codenames import data_manager, screening
+from .get_default_codenames_by_group import get_default_codenames_by_group
+from .group_names import (
+    ACCOUNT_MANAGER,
+    ADMINISTRATION,
+    AE,
+    AE_REVIEW,
+    AUDITOR,
+    CELERY_MANAGER,
+    CLINIC,
+    DATA_MANAGER,
+    DISPENSING,
+    SITE_DATA_MANAGER,
+    DATA_QUERY,
+    EVERYONE,
+    EXPORT,
+    LAB,
+    LAB_VIEW,
+    PHARMACY,
+    PII,
+    PII_VIEW,
+    RANDO,
+    REVIEW,
+    SCREENING,
+    TMG,
+    UNBLINDING_REQUESTORS,
+    UNBLINDING_REVIEWERS,
+)
