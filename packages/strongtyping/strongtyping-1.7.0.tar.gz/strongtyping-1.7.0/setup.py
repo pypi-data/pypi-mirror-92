fa28f743e64f11d86e2ec92e0abfbccd9830523f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:12 +0000

    Add package strongtyping/1.7.0/strongtyping-1.7.0.tar.gz

---
+++ packages/strongtyping/strongtyping-1.7.0.tar.gz/strongtyping-1.7.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@created: 30.04.20
+@author: felix
+"""
+import pathlib
+
+from setuptools import find_packages
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+packages = find_packages(exclude=['test_*', '*.tests'])
+
+setup(
+    name="strongtyping",
+    version="1.7.0",
+    description="Decorator which checks whether the function is called with the correct type of parameters",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FelixTheC/strongtyping",
+    author="FelixTheC",
+    author_email="fberndt87@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=packages,
+    python_requires='>=3.7',
+    include_package_data=True,
+)
+
