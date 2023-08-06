9d319f5787757d28c39954e6bbeed03c9a3a62a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package taxonomy-connector/1.3.2/taxonomy_connector-1.3.2-py3-none-any.whl

---
+++ packages/taxonomy-connector/taxonomy_connector-1.3.2-py3-none-any.whl/taxonomy/apps.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Initialization code for taxonomy application.
+"""
+from __future__ import unicode_literals
+
+import logging
+
+from django.apps import AppConfig
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TaxonomyConfig(AppConfig):
+    """
+    App configuration for taxonomy app.
+    """
+
+    name = 'taxonomy'
+
+    def ready(self):
+        """
+        Connect handlers to signals.
+        """
+        LOGGER.info('[TAXONOMY] App config is ready')
+        from .signals import handlers  # pylint: disable=unused-import,import-outside-toplevel
