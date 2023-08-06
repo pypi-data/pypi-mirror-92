915941439758c4707c8ccb0b649b9e1626e4cacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:57 +0000

    Add package coveo-example-library/0.0.5/coveo-example-library-0.0.5.tar.gz

---
+++ packages/coveo-example-library/coveo-example-library-0.0.5.tar.gz/coveo-example-library-0.0.5/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['coveo_example_library']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'coveo-example-library',
+    'version': '0.0.5',
+    'description': 'coveo-python-oss example library',
+    'long_description': None,
+    'author': 'Jonathan Piché',
+    'author_email': 'jpiche@coveo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
