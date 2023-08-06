54705d553ca83e3219dbdc2fc050f83de18fbc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:20 +0000

    Add package aifig/0.1.7/aifig-0.1.7.tar.gz

---
+++ packages/aifig/aifig-0.1.7.tar.gz/aifig-0.1.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='aifig',
+     version='0.1.7',
+     author="Sigve Rokenes",
+     author_email="me@evgiz.net",
+     description="A machine learning figure generation library",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/evgiz/aifig",
+     packages=["aifig"],
+     install_requires=[
+          'svgwrite'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent"
+     ],
+ )
