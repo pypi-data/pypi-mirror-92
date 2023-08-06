3a8cf1c51800f0a257bc888c28f1008c412023fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:44 +0000

    Add package testext/1.0.1.2/testext-1.0.1.2.tar.gz

---
+++ packages/testext/testext-1.0.1.2.tar.gz/testext-1.0.1.2/ext/redis/__init__.py
@@ -0,0 +1,19 @@
+import redis
+
+REDIS = None
+
+
+def create_redis(redis_url):
+    if not redis_url:
+        return
+
+    redis_host_port_db = str(redis_url).replace("redis://", "")
+    if redis_host_port_db.find("/"):
+        _host_port, _db = redis_host_port_db.split("/")
+        _host, _port = _host_port.split(":")
+    else:
+        _db = 0
+        _host, _port = redis_host_port_db.split(":")
+
+    _instance = redis.Redis(host=_host, port=_port, db=_db, decode_responses=True)
+    return _instance
---
+++ packages/testext/testext-1.0.1.2.tar.gz/testext-1.0.1.2/ext/utils/db_utils.py
@@ -0,0 +1,36 @@
+import datetime
+
+from sqlalchemy.orm import class_mapper
+from ext import EXT_CONTEXT
+
+
+def object_to_dict(obj):
+    columns = [column.key for column in class_mapper(obj.__class__).columns]
+    get_key_value = lambda c: (c, getattr(obj, c).isoformat()) if isinstance(getattr(obj, c), datetime) else (
+        c, getattr(obj, c))
+    return dict(map(get_key_value, columns))
+
+
+def query2dict(model_list):
+    if isinstance(model_list, list):  # 如果传入的参数是一个list类型的，说明是使用的all()的方式查询的
+        if isinstance(model_list[0], EXT_CONTEXT["db"].Model):  # 这种方式是获得的整个对象  相当于 select * from table
+            lst = []
+            for model in model_list:
+                dic = {}
+                for col in model.__table__.columns:
+                    dic[col.name] = getattr(model, col.name)
+                lst.append(dic)
+            return lst
+        else:  # 这种方式获得了数据库中的个别字段  相当于select id,name from table
+            lst = []
+            for result in model_list:  # 当以这种方式返回的时候，result中会有一个keys()的属性
+                lst.append([dict(zip(result.keys, r)) for r in result])
+            return lst
+    else:  # 不是list,说明是用的get() 或者 first()查询的，得到的结果是一个对象
+        if isinstance(model_list, EXT_CONTEXT["db"].Model):  # 这种方式是获得的整个对象  相当于 select * from table limit=1
+            dic = {}
+            for col in model_list.__table__.columns:
+                dic[col.name] = getattr(model_list, col.name)
+            return dic
+        else:  # 这种方式获得了数据库中的个别字段  相当于select id,name from table limit = 1
+            return dict(zip(model_list.keys(), model_list))
