89443629d8cf635a0f3ad89ce1a7ccb28326f5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:15 +0000

    Add package xxlGsc/0.1/xxlGsc-0.1-py3-none-any.whl

---
+++ packages/xxlGsc/xxlGsc-0.1-py3-none-any.whl/xxlGsc/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#/* *=+=+=+=+* *** *=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+#  * 文档信息: *** :~/WORKM/studyCode/pythonCode/pipInstallExample/gsc_package/xxlGsc/__init__.py
+#  * 版权声明: *** :(魎魍魅魑)MIT
+#  * 联络信箱: *** :guochaoxxl@163.com
+#  * 创建时间: *** :2021年01月27日的下午08:21
+#  * 文档用途: *** :Python趣味编程入门与实战
+#  * 作者信息: *** :guochaoxxl(http://cnblogs.com/guochaoxxl)
+#  * 修订时间: *** :2021年第04周 01月27日 星期三 下午08:21 (第027天)
+#  * 文件描述: *** :自行添加
+# * *+=+=+=+=* *** *+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+*/
+ 
+from __future__ import absolute_import
+from .xxlGsc import *
+
+name = "xxlGsc"
