b3ebc4369429e04398d101cf6ddbc05b5e328b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:35 +0000

    Add package nepali-roman/1.0.1/nepali_roman-1.0.1.tar.gz

---
+++ packages/nepali-roman/nepali_roman-1.0.1.tar.gz/nepali_roman-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nepali_roman',
+    version='1.0.1',
+    author="Suraj Regmi",
+    author_email="regmi125@gmail.com",
+    description="Convert Nepali text to roman English",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Suraj1127/nepali-roman",
+    packages=setuptools.find_packages(),
+    py_modules=["romanize"],
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
