e3c17501bccbc34b3e798938b624ce46e0ed74d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:16 +0000

    Add package xpress/8.8.7/xpress-8.8.7-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/xpress/xpress-8.8.7-cp36-cp36m-manylinux1_x86_64.whl/xpress/examples/example_infeasible.py
@@ -0,0 +1,50 @@
+#!/bin/env python
+
+from __future__ import print_function
+
+import xpress as xp
+
+minf = xp.problem("ex-infeas")
+
+x0 = xp.var()
+x1 = xp.var()
+x2 = xp.var(vartype=xp.binary)
+
+minf.addVariable(x0, x1, x2)
+
+c1 = x0 + 2 * x1 >= 1
+c2 = 2 * x0 + x1 >= 1
+c3 = x0 + x1 <= .5
+
+minf.addConstraint(c1, c2, c3)
+minf.solve()
+minf.iisall()
+print("there are ", minf.attributes.numiis, " iis's")
+
+miisrow = []
+miiscol = []
+constrainttype = []
+colbndtype = []
+duals = []
+rdcs = []
+isolationrows = []
+isolationcols = []
+
+# get data for the first IIS
+
+minf.getiisdata(1, miisrow, miiscol, constrainttype, colbndtype,
+                duals, rdcs, isolationrows, isolationcols)
+
+print("iis data:", miisrow, miiscol, constrainttype, colbndtype,
+      duals, rdcs, isolationrows, isolationcols)
+
+# Another way to check IIS isolations
+print("iis isolations:", minf.iisisolations(1))
+
+rowsizes = []
+colsizes = []
+suminfeas = []
+numinfeas = []
+
+print("iisstatus:", minf.iisstatus(rowsizes, colsizes, suminfeas, numinfeas))
+print("vectors:", rowsizes, colsizes, suminfeas, numinfeas)
