89abad6f42168856dee0da7cf253fd131e7c4f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package gatetools/0.9.14/gatetools-0.9.14.tar.gz

---
+++ packages/gatetools/gatetools-0.9.14.tar.gz/gatetools-0.9.14/gatetools/gate_helpers.py
@@ -0,0 +1,54 @@
+# -----------------------------------------------------------------------------
+#   Copyright (C): OpenGATE Collaboration
+#   This software is distributed under the terms
+#   of the GNU Lesser General  Public Licence (LGPL)
+#   See LICENSE.md for further details
+# -----------------------------------------------------------------------------
+
+import shutil
+import platform
+import subprocess
+import pkg_resources
+import os
+
+# -----------------------------------------------------------------------------
+def print_gate_info(short_verbose, cmd_gate_name = 'Gate'):
+    '''
+    Print information about 'Gate' executablea and system
+    '''
+
+    # get Gate path
+    path = shutil.which(cmd_gate_name)
+
+    # get version info
+    if path == None:
+        path = cmd_gate_name+' not found'
+        output = ''
+    else:
+        result = subprocess.run(['Gate', '--version'], stdout=subprocess.PIPE)
+        output = result.stdout.decode("utf-8").splitlines()[0]
+
+    if short_verbose:
+        print(path)
+        return
+
+    # get gatetools version
+    gtv = pkg_resources.get_distribution("gatetools").version
+
+    # get G4 env variables
+    g4_list = ['G4ABLADATA', 'G4ENSDFSTATEDATA', 'G4INCLDATA', 'G4LEDATA', 'G4LEVELGAMMADATA', 'G4NEUTRONHPDATA', 'G4PARTICLEXSDATA', 'G4PIIDATA', 'G4RADIOACTIVEDATA', 'G4REALSURFACEDATA', 'G4SAIDXSDATA']
+
+    print('Gate path:         ', path)
+    print('Version:           ', output)
+    print('Machine type:      ', platform.machine())
+    print('Hostname:          ', platform.node())
+    print('Platform:          ', platform.platform())
+    print('Processor:         ', platform.processor())
+    print('Python:            ', platform.python_version())
+    print('System:            ', platform.system())
+    print('GateTools version: ', gtv)
+    for g in g4_list:
+        try:
+            print('{:<19} {}'.format(g, os.environ[g]))
+        except:
+            print('{:<19} NOT FOUND'.format(g))
