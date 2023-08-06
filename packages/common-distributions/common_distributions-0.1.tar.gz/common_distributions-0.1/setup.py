c98128dfa8b659d4ce81a298d195543f5913c7d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:00 +0000

    Add package common-distributions/0.1/common_distributions-0.1.tar.gz

---
+++ packages/common-distributions/common_distributions-0.1.tar.gz/common_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='common_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['common_distributions'],
+      author = 'Chris Wang',
+      author_email = 'chris.wang92688@gmail.com',
+      zip_safe=False)
