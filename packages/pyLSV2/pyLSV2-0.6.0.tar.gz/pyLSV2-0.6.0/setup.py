34a946f153e3d962e5f0edc570be5a5e8ec788de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:56 +0000

    Add package pyLSV2/0.6.0/pyLSV2-0.6.0.tar.gz

---
+++ packages/pyLSV2/pyLSV2-0.6.0.tar.gz/pyLSV2-0.6.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+"""package configuration for pyLSV2"""
+from setuptools import find_packages, setup
+from pyLSV2 import __version__
+
+setup(
+    name='pyLSV2',
+    packages=find_packages(include=['pyLSV2',], exclude=['tests', 'data']),
+    version=__version__,
+    description='A pure Python3 implementation of the LSV2 protocol',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='drunsinn',
+    author_email='dr.unsinn@googlemail.com',
+    url='https://github.com/drunsinn/pyLSV2',
+    license='MIT',
+    install_requires=[],
+    scripts=['scripts/check_for_LSV2_commands.py', 'scripts/lsv2_demo.py'],
+    zip_safe=True,
+    keywords="LSV2 cnc communication transfer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: System :: Archiving"
+    ],
+
+)
