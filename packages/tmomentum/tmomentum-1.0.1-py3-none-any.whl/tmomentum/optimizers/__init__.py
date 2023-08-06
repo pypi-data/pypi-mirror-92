09eb2e4fbe21741b1b4f8fe44a99a18462d3e872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:27 +0000

    Add package tmomentum/1.0.1/tmomentum-1.0.1-py3-none-any.whl

---
+++ packages/tmomentum/tmomentum-1.0.1-py3-none-any.whl/tmomentum/optimizers/__init__.py
@@ -0,0 +1,12 @@
+from .tadam import TAdam
+from .tadamw import TAdamW
+from .tpadam import TPAdam
+from .tsadam import TSAdam
+from .tradam import TRAdam
+from .tradam import TPlainRAdam
+
+from .tadabound import TAdaBound
+from .tadabelief import TAdaBelief
+from .tlaprop import TLaProp
+from .tdiffgrad import TDiffGrad
+from .tyogi import TYogi
---
+++ packages/tmomentum/tmomentum-1.0.1-py3-none-any.whl/tmomentum/utils/__init__.py
@@ -0,0 +1,1 @@
+from .tmomentum import TMomentum 
