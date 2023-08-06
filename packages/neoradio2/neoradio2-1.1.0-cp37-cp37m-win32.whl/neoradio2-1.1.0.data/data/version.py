f046c21df0417cd241b3be87d441be7362e0c6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:07 +0000

    Add package neoradio2/1.1.0/neoradio2-1.1.0-cp37-cp37m-win32.whl

---
+++ packages/neoradio2/neoradio2-1.1.0-cp37-cp37m-win32.whl/neoradio2-1.1.0.data/data/version.py
@@ -0,0 +1,12 @@
+MAJOR = 1
+MINOR = 1
+MICRO = 0
+POST = None
+
+
+def _get_version_str():
+    if POST:
+        post = "-{}".format(POST)
+    else:
+        post = ""
+    return "{}.{}.{}{}".format(MAJOR, MINOR, MICRO, post)
