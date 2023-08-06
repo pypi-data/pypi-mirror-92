3d0b940e93c1664b74b17f6239c2eedadc36c6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:23 +0000

    Add package Your-Fav-DL-Framework/1/Your_Fav_DL_Framework-1.tar.gz

---
+++ packages/Your-Fav-DL-Framework/Your_Fav_DL_Framework-1.tar.gz/Your_Fav_DL_Framework-1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Your_Fav_DL_Framework", # Replace with your own username
+    version="1",
+    author="CSE_ASU_2021",
+    author_email="monairy@icloud.com",
+    description="Deep learning framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.',
+)
