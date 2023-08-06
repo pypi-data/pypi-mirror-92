c3612a95c6cff051ed40562b405eac39f569eb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:26 +0000

    Add package SUDP/1.1.1/SUDP-1.1.1.tar.gz

---
+++ packages/SUDP/SUDP-1.1.1.tar.gz/SUDP-1.1.1/SUDP/SUDP.py
@@ -0,0 +1,30 @@
+import socket
+class sUDPsocket():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.timeout = 5
+    def bind(self, addres):
+        self.sock.bind(addres)
+    def recvfrom(self, bytes):
+        msg , address = self.sock.recvfrom(bytes)
+        self.sock.sendto(("ok").encode("utf-8"),address)
+        return msg, address
+    def recv(self, bytes):
+        msg , address = self.sock.recvfrom(bytes)
+        self.sock.sendto(("ok").encode("utf-8"),address)
+        return msg
+    def sendto(self, msg, address):
+        self.sock.sendto(msg,address)
+        self.sock.settimeout(self.timeout)
+        try:
+            ok , addressok = self.sock.recvfrom(1024)
+        except:
+            return False
+        finally:
+            self.sock.settimeout(None)
+        if ok.decode("utf-8") == "ok" and addressok == address:
+            return True
+        elif addressok != address:
+            return "received a \"ok\" message from the wrong address"
+        else:
+            return False
