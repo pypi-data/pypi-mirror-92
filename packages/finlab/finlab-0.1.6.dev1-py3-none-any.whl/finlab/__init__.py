e7dd1b4fadbd263d85dc53422026689365229429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:50 +0000

    Add package finlab/0.1.6.dev1/finlab-0.1.6.dev1-py3-none-any.whl

---
+++ packages/finlab/finlab-0.1.6.dev1-py3-none-any.whl/finlab/__init__.py
@@ -0,0 +1,21 @@
+import os
+from .backtest import sim
+
+__version__ = '0.1.6.dev1'
+
+class Token:
+    def __init__(self, interaction=True):
+
+        if interaction and 'finlab_id_token' not in os.environ:
+            id_token = input('Please login to https://finlab-python.github.io/finlab/ and copy api token here: \n')
+            os.environ['finlab_id_token'] = id_token
+        pass
+
+    def set(self, token):
+        os.environ['finlab_id_token'] = token
+
+    def get(self):
+        if 'finlab_id_token' in os.environ:
+            return os.environ['finlab_id_token']
+        else:
+            return None
