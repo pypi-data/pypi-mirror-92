2b4696b41538fab1bacb61cbf90695487d0d33cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:01 +0000

    Add package dart-fss/0.3.9/dart-fss-0.3.9.tar.gz

---
+++ packages/dart-fss/dart-fss-0.3.9.tar.gz/dart-fss-0.3.9/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from os import path
+
+import versioneer
+
+NAME = 'dart-fss'
+
+INSTALL_REQUIRES = (
+    ['numpy>=1.17.0', 'pandas', 'requests', 'lxml', 'html5lib', 'tqdm', 'beautifulsoup4',
+     'isodate', 'arelle', 'fake-useragent', 'openpyxl', 'halo', 'xmltodict', 'appdirs']
+)
+
+
+with open(path.join('./', 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name=NAME,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Web-scraping http://dart.fss.or.kr',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Sungwoo Jo',
+    author_email='nonswing.z@gmail.com',
+    url='https://github.com/josw123/dart-fss',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    packages=find_packages(exclude=['dart_fss.tests']),
+    keywords=['fss', 'dart-fss', 'scrapping'],
+    python_requires='>=3.5',
+    package_data={},
+    install_requires=INSTALL_REQUIRES
+)
