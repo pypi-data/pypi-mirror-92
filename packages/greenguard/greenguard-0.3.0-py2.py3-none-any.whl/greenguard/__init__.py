024a3ce8af104927135e3606a29bc717208adc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:47 +0000

    Add package greenguard/0.3.0/greenguard-0.3.0-py2.py3-none-any.whl

---
+++ packages/greenguard/greenguard-0.3.0-py2.py3-none-any.whl/greenguard/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for GreenGuard."""
+
+__author__ = """MIT Data To AI Lab"""
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.3.0'
+
+import os
+
+from greenguard.pipeline import GreenGuardPipeline, get_pipelines
+
+_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+MLBLOCKS_PIPELINES = os.path.join(_BASE_PATH, 'pipelines')
+MLBLOCKS_PRIMITIVES = os.path.join(_BASE_PATH, 'primitives')
+
+
+__all__ = (
+    'GreenGuardPipeline',
+    'get_pipelines',
+)
