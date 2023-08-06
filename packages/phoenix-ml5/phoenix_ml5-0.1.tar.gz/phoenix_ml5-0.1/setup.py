bc43548cf16f214c60960984a9de0a44c3dd78d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:42 +0000

    Add package phoenix-ml5/0.1/phoenix_ml5-0.1.tar.gz

---
+++ packages/phoenix-ml5/phoenix_ml5-0.1.tar.gz/phoenix_ml5-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='phoenix_ml5',
+      version='0.1',
+      description='Machine Learning FrameWork',
+      packages=['phoenix_ml5'],
+      zip_safe=False)
+
+
