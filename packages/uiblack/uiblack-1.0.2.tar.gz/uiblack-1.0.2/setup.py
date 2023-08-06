929f22e3ef5f87a0f0750e150b45a62999e60256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:14 +0000

    Add package uiblack/1.0.2/uiblack-1.0.2.tar.gz

---
+++ packages/uiblack/uiblack-1.0.2.tar.gz/uiblack-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import os.path
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="uiblack",
+    version="1.0.2",
+    description="Streamlined cross-platform Textual UI",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlackburnHax/uiblack",
+    author="Brandon Blackburn",
+    author_email="contact@bhax.net",
+    license="Apache 2.0",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["terminal"],
+    include_package_data=True,
+    install_requires=[
+        "blessed"
+    ]
+)
