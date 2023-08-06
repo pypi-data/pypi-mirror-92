c90d6cb9bd1f3750e4c25671e18e775e0e69d15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:02 +0000

    Add package chaostoolkit-k6/0.1.3/chaostoolkit_k6-0.1.3-py3-none-any.whl

---
+++ packages/chaostoolkit-k6/chaostoolkit_k6-0.1.3-py3-none-any.whl/chaosk6/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+from logzero import logger
+from chaoslib.discovery.discover import (
+    discover_actions,
+    discover_probes,
+    initialize_discovery_result,
+)
+from chaoslib.types import Discovery, DiscoveredActivities, Secrets
+
+"""Top-level package for chaostoolkit-k6."""
+
+__all__ = ["discover"]
+__version__ = "0.1.3"
+
+
+def discover(discover_system: bool = True) -> Discovery:
+    """
+    Discover k6 capabilities offered by this extension.
+    """
+    logger.info("Discovering capabilities from chaostoolkit-k6")
+
+    discovery = initialize_discovery_result("chaostoolkit-k6", __version__, "k6")
+    discovery["activities"].extend(load_exported_activities())
+    return discovery
+
+
+def load_exported_activities() -> List[DiscoveredActivities]:
+    activities = []
+    activities.extend(discover_actions("chaosk6.actions"))
+    activities.extend(discover_probes("chaosk6.probes"))
+    return activities
