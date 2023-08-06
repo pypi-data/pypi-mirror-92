60029be3939b907262e6800a457235ffc3046ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:17 +0000

    Add package imgwrench/0.15.0/imgwrench-0.15.0-py2.py3-none-any.whl

---
+++ packages/imgwrench/imgwrench-0.15.0-py2.py3-none-any.whl/imgwrench/commands/crop.py
@@ -0,0 +1,54 @@
+"""Crop images to the given aspect ratio."""
+
+from math import floor, ceil
+
+import click
+
+from ..param import RATIO
+
+
+def crop(image, aspect_ratio):
+    """Crop images to the given aspect ratio."""
+    width, height = image.size[0], image.size[1]
+    long_side = max(width, height)
+    short_side = min(width, height)
+    actual_ratio = long_side / short_side
+    target_ratio = max(aspect_ratio, 1 / aspect_ratio)
+    # we assume long_side == width and short_side == height
+    # if not, we are going to switch later
+    if target_ratio > actual_ratio:  # need to crop short side
+        crop_pixels = round((1 - actual_ratio / target_ratio) * short_side)
+        left = 0
+        right = long_side
+        upper = floor(crop_pixels / 2)
+        lower = short_side - ceil(crop_pixels / 2)
+    else:  # need to crop long side
+        crop_pixels = round((1 - target_ratio / actual_ratio) * long_side)
+        left = floor(crop_pixels / 2)
+        right = long_side - ceil(crop_pixels / 2)
+        upper = 0
+        lower = short_side
+    if height > width:
+        upper, left = left, upper
+        lower, right = right, lower
+    return image.crop((left, upper, right, lower))
+
+
+@click.command(name="crop")
+@click.option(
+    "-a",
+    "--aspect-ratio",
+    type=RATIO,
+    default="3:2",
+    show_default=True,
+    help="aspect ratio to crop to",
+)
+def cli_crop(aspect_ratio):
+    """Crop images to the given aspect ratio."""
+    click.echo("Initializing crop with parameters {}".format(locals()))
+
+    def _crop(images):
+        for info, image in images:
+            yield info, crop(image, aspect_ratio)
+
+    return _crop
---
+++ packages/imgwrench/imgwrench-0.15.0-py2.py3-none-any.whl/tests/test_filmstrip.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `filmstrip` subcommand."""
+
+
+import unittest
+
+from click.testing import CliRunner
+
+from .utils import execute_and_test_output_images
+
+
+class TestFilmstrip(unittest.TestCase):
+    """Tests for `filmstrip` subcommand."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        self.runner = CliRunner()
+
+    def test_filmstrip_output(self):
+        """Test output of filmstrip command."""
+        execute_and_test_output_images(
+            self, self.runner, 6, 1, "rc_", ["filmstrip", "-s", 10]
+        )
