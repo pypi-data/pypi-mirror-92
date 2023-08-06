984dca5438858067fcb61d1714ef963f142a25f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:56 +0000

    Add package solari/0.0.2/solari-0.0.2-py3-none-any.whl

---
+++ packages/solari/solari-0.0.2-py3-none-any.whl/solari/stats/__init__.py
@@ -0,0 +1,28 @@
+from .champions_rate_stats import (
+    ChampionPickrate,
+    ChampionPickCount,
+    ChampionWinrate,
+    ChampionBanrate,
+    ChampionBanCount,
+    ChampionPresenceRate
+)
+
+from .champions_stats import (
+    ChampionGeneric,
+    ChampionGenericPerMin,
+    ChampionKDA,
+    ChampionKillParticipation
+)
+
+from .items_rate_stats import (
+    ItemPickrate,
+    ItemWinrate
+)
+
+from .player_rates import (
+    PlayerPickrate,
+    PlayerWinrate,
+    PlayerPickCount,
+    PlayerWins,
+    PlayerLosses
+)
