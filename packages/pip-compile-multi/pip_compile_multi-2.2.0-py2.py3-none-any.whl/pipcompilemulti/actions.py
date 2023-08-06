cc1298e84aa66545c54221059a5957a6c277cd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:42 +0000

    Add package pip-compile-multi/2.2.0/pip_compile_multi-2.2.0-py2.py3-none-any.whl

---
+++ packages/pip-compile-multi/pip_compile_multi-2.2.0-py2.py3-none-any.whl/pipcompilemulti/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""High level actions to be called from CLI"""
+
+from .discover import discover
+from .environment import Environment
+from .verify import generate_hash_comment
+from .features import FEATURES
+from .deduplicate import PackageDeduplicator
+
+
+def recompile():
+    """Compile requirements files for all environments."""
+    env_confs = discover(FEATURES.compose_input_file_path('*'))
+    FEATURES.on_discover(env_confs)
+    deduplicator = PackageDeduplicator()
+    deduplicator.on_discover(env_confs)
+    sink_path = FEATURES.sink_path()
+    if sink_path:
+        Environment(in_path=sink_path).create_lockfile()
+    compile_topologically(env_confs, deduplicator)
+
+
+def compile_topologically(env_confs, deduplicator):
+    """Compile environments in topological order of reference."""
+    for conf in env_confs:
+        if not FEATURES.included(conf['in_path']):
+            continue
+        env = Environment(in_path=conf['in_path'], deduplicator=deduplicator)
+        if env.maybe_create_lockfile():
+            # Only munge lockfile if it was written.
+            header_text = generate_hash_comment(env.infile) + FEATURES.get_header_text()
+            env.replace_header(header_text)
+            env.add_references(conf['refs'])
