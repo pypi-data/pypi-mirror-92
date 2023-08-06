63b18a0abb2fb3ec727d65d15824c14259c9dc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:48 +0000

    Add package frictionless/4.0.0a9/frictionless-4.0.0a9-py2.py3-none-any.whl

---
+++ packages/frictionless/frictionless-4.0.0a9-py2.py3-none-any.whl/frictionless/describe/resource.py
@@ -0,0 +1,26 @@
+from ..resource import Resource
+
+
+def describe_resource(source, *, expand=False, nostats=False, **options):
+    """Describe the given source as a resource
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless import describe_resource`
+
+    Parameters:
+        source (any): data source
+        expand? (bool): if `True` it will expand the metadata
+        nostats? (bool): if `True` it not infer resource's stats
+        **options (dict): Resource constructor options
+
+    Returns:
+        Resource: data resource
+
+    """
+    # TODO: move trusted to be autodetected inside resource
+    resource = Resource(source, trusted=True, **options)
+    resource.infer(stats=not nostats)
+    if expand:
+        resource.expand()
+    return resource
