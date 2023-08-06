f19310080ffacdf828eb19dee654bfdbe74933bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:37 +0000

    Add package UiComparePicRec/1.0.1/UiComparePicRec-1.0.1.tar.gz

---
+++ packages/UiComparePicRec/UiComparePicRec-1.0.1.tar.gz/UiComparePicRec-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="UiComparePicRec",
+    version="1.0.1",
+    author="Jishuang Li",
+    author_email="1103192570@qq.com",
+    description="Selenium plugin library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lijishuang/UI-Pic-Rec",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
+
