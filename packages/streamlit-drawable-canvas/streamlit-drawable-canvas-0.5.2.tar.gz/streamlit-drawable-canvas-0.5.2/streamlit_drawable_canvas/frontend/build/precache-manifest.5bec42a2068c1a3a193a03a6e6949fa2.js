4fc90170db23fede9d3cebb648985557f98e97be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:21 +0000

    Add package streamlit-drawable-canvas/0.5.2/streamlit-drawable-canvas-0.5.2.tar.gz

---
+++ packages/streamlit-drawable-canvas/streamlit-drawable-canvas-0.5.2.tar.gz/streamlit-drawable-canvas-0.5.2/streamlit_drawable_canvas/frontend/build/precache-manifest.5bec42a2068c1a3a193a03a6e6949fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbbfbcdfcc195c638ceb59d17ffa2be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07e33580bfcea5956d04",
+    "url": "./static/css/main.60965e18.chunk.css"
+  },
+  {
+    "revision": "0f1811d3738148fc7dcb",
+    "url": "./static/js/2.daa0d25c.chunk.js"
+  },
+  {
+    "revision": "349298fbb30f981c1395376b153ee9a6",
+    "url": "./static/js/2.daa0d25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e33580bfcea5956d04",
+    "url": "./static/js/main.79864132.chunk.js"
+  },
+  {
+    "revision": "77e61c67eb4f95e15891",
+    "url": "./static/js/runtime-main.ff285346.js"
+  }
+]);
