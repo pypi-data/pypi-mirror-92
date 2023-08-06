de15ee8e1de77692f80cf98dd5159acb5a6557dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:11 +0000

    Add package lrc-kit/0.1.5/lrc_kit-0.1.5.tar.gz

---
+++ packages/lrc-kit/lrc_kit-0.1.5.tar.gz/lrc_kit-0.1.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lrc_kit",
+    version="0.1.5",
+    author="Peter Stenger",
+    author_email="peter.promotions.stenger@gmail.com",
+    description="A library to search for LRC files online and parse their contents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reteps/lrc_kit",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'bs4',
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
