652b88cbbda4a3cfcc6b5b172dbbdda3f5d62321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:31 +0000

    Add package google-drive/0.3.2/google-drive-0.3.2.tar.gz

---
+++ packages/google-drive/google-drive-0.3.2.tar.gz/google-drive-0.3.2/googledrive/models.py
@@ -0,0 +1,59 @@
+import json
+from dataclasses import dataclass
+
+from googleapiclient.errors import HttpError
+
+@dataclass
+class GoogleFile:
+
+    EXPORT_TYPE_RTF                  = 'application/rtf'
+    EXPORT_TYPE_VND_OASIS            = 'application/vnd.oasis.opendocument.text'
+    EXPORT_TYPE_HTML                 = 'text/html'
+    EXPORT_TYPE_PDF                  = 'application/pdf'
+    EXPORT_TYPE_EPUB_ZIP             = 'application/epub+zip'
+    EXPORT_TYPE_ZIP                  = 'application/zip'
+    EXPORT_TYPE_VND_WORDPROCESSINGML = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    EXPORT_TYPE_PLAIN                = 'text/plain'
+
+    def __init__(
+            self,
+            name,
+            id,
+            parents,
+            mime_type,
+            export_links):
+        self.id = id
+        self.name = name
+        self.parents = parents
+        self.mime_type = mime_type
+        self.export_links = export_links
+
+    def get_export_link(self, export_type):
+        export_link = self.export_links[export_type] or ''
+        return export_link
+
+    def __str__(self):
+        return f'({self.id}, {self.name}, {self.mime_type})'
+
+@dataclass
+class GoogleApiClientHttpError:
+
+    def __init__(self, code, message, status, details, errors):
+        self.code = code
+        self.message = message
+        self.status = status
+        self.details = details
+        self.errors = errors
+
+class GoogleApiClientHttpErrorBuilder:
+
+    def from_http_error(self, http_error: HttpError):
+        error_reason = json.loads(http_error.content)
+        error = error_reason['error']
+        return GoogleApiClientHttpError(
+            error['code'],
+            error['message'],
+            error.get('status', ''),
+            error.get('details', []),
+            error.get('errors', [])
+        )
---
+++ packages/google-drive/google-drive-0.3.2.tar.gz/google-drive-0.3.2/setup.py
@@ -0,0 +1,51 @@
+import os
+import pathlib
+
+import pkg_resources
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def read_requirements(path):
+    with pathlib.Path(path).open() as requirements_txt:
+        return [
+            str(requirement)
+            for requirement
+            in pkg_resources.parse_requirements(requirements_txt)
+        ]
+
+
+requirements = read_requirements('requirements/prod.txt')
+extra_requirements_dev = read_requirements('requirements/dev.txt')
+
+
+setup(
+    name="google-drive",
+    version="0.3.2",
+    author="Eduardo Garcia",
+    author_email="garciaruiz.edu+google-drive-python@gmail.com",
+    maintainer="Eduardo García",
+    maintainer_email="garciaruiz.edu+google-drive-python@gmail.com",
+    description=("Library and cli to manage and interact with your Google Drive"),
+    license="Apache",
+    keywords="google drive",
+    url="https://github.com/eduardogr/google-drive-python",
+    packages=['googledrive'],
+    install_requires=requirements,
+    extras_require={
+        'dev': extra_requirements_dev
+    },
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    entry_points={
+        'console_scripts': ['google-drive = googledrive.cli:googledrive'],
+    },
+)
