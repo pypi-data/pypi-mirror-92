4b5baae90d18af3c60c987c04ccf040861b68102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:42 +0000

    Add package edc-fieldsets/0.3.0/edc_fieldsets-0.3.0-py3-none-any.whl

---
+++ packages/edc-fieldsets/edc_fieldsets-0.3.0-py3-none-any.whl/edc_fieldsets/__init__.py
@@ -0,0 +1,4 @@
+from .fieldlist import Remove, Insert, Fieldlist
+from .fieldset import Fieldset
+from .fieldsets import Fieldsets, FieldsetError
+from .fieldsets_modeladmin_mixin import FieldsetsModelAdminMixin
---
+++ packages/edc-fieldsets/edc_fieldsets-0.3.0-py3-none-any.whl/edc_fieldsets/fieldset.py
@@ -0,0 +1,7 @@
+class Fieldset:
+
+    """"A class to format a fieldset.
+    """
+
+    def __init__(self, *fields, section=None):
+        self.fieldset = (section, {"fields": fields})
