548b1d29bb9f4818ce390bbb3c390a2be25b2d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:36 +0000

    Add package InMemoryCloudDatastoreStub/0.0.14/InMemoryCloudDatastoreStub-0.0.14.tar.gz

---
+++ packages/InMemoryCloudDatastoreStub/InMemoryCloudDatastoreStub-0.0.14.tar.gz/InMemoryCloudDatastoreStub-0.0.14/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="InMemoryCloudDatastoreStub",
+    version="0.0.14",
+    description="An in-memory stub implementation of Google Cloud Datastore for use in unit tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phil-lopreiato/google-cloud-datastore-stub/",
+    packages=setuptools.find_packages(),
+    python_requires=">=3",
+    install_requires=["google-cloud-ndb > 1.2.1"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "black", "pyre-check", "flake8", "mypy"],
+)
