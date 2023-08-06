4ae6e78738d393104632122f45dd6ef80a0540ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:04 +0000

    Add package bootstraphistogram/0.6.0/bootstraphistogram-0.6.0-py3-none-any.whl

---
+++ packages/bootstraphistogram/bootstraphistogram-0.6.0-py3-none-any.whl/bootstraphistogram/bootstraphistogram.py
@@ -0,0 +1,232 @@
+from copy import copy, deepcopy
+from typing import Any, Optional, Tuple, Union
+
+import boost_histogram as bh
+import numpy as np
+
+
+class BootstrapHistogram:
+    """
+    A histogram with automatic Poission bootstrap resampling
+
+    The implementation is backed by boost :std:doc:`usage/histogram` (<https://github.com/scikit-hep/boost-histogram>) and
+    thus :py:class:`BoostrapHistogram` mimics the :py:class:`boost_histogram.Histogram` interface.
+
+    Parameters
+    ----------
+    *axes : boost_histogram.axis.Axis
+        Any number of :py:class:`boost_histogram.axis.Axis` objects that define the histogram binning. See <https://boost-histogram.readthedocs.io/en/latest/usage/axes.html>.
+    numsamples : int
+        The number of bootstrap samples. Increasing this number improves the accuracy of estimators derived from the
+        bootstrap samples, at the cost of increase memory and CPU usage.
+    rng : Union[int, np.random.Generator, None]
+        A numpy generator. If not provided, the numpy default from :py:func:`numpy.random.default_rng` will be used.
+    **kwargs : Any
+        Passed on to the :py:class:`boost_histogram.Histogram` constructor. :py:class:`numpy.ndarray`
+
+    Attributes
+    ----------
+    nominal : boost_histogram.Histogram
+        A histogram of the filled values, with no bootstrap samples.
+    samples : boost_histogram.Histogram
+        A histogram of the bootstrap samples. The last dimension corresponds to the bootstrap sample index and is
+        of size :py:attr:`BootstrapHistogram.numsamples`.
+    numsamples : int
+        Number of bootstrap re-samplings.
+    axes : Tuple[bh.axis.Axis, ...]
+        :py:class:`boost_histogram.axis.Axis` representing the histogram binning. The last dimension corresponds to the
+        bootstrap sample index.
+    """
+
+    def __init__(
+        self,
+        *axes: bh.axis.Axis,
+        numsamples: int = 1000,
+        rng: Union[int, np.random.Generator, None] = None,
+        **kwargs: Any,
+    ):
+        axeslist = list(axes)
+        self._nominal = bh.Histogram(*axeslist, **kwargs)
+        axeslist.append(bh.axis.Integer(0, numsamples))
+        self._random = np.random.default_rng(rng)
+        self._hist = bh.Histogram(*axeslist, **kwargs)
+
+    @property
+    def nominal(self) -> bh.Histogram:
+        return self._nominal
+
+    @property
+    def samples(self) -> bh.Histogram:
+        return self._hist
+
+    def mean(self, flow=False) -> np.ndarray:
+        """
+        Binned sample mean.
+
+        Returns
+        -------
+        numpy.ndarray
+            an array containing the mean value of all bootstrap samples for each bin in the histogram, .
+        """
+        return np.mean(self.view(flow=flow), axis=-1)
+
+    def std(self, flow=False) -> np.ndarray:
+        """
+        Binned sample standard deviation.
+
+        Returns
+        -------
+        numpy.ndarray
+            an array containing the standard deviation of all bootstrap sample values for each bin in the histogram, .
+        """
+        return np.std(self.view(flow=flow), axis=-1)
+
+    def percentile(
+        self, q: float, flow=False, interpolation: str = "linear"
+    ) -> np.ndarray:
+        """
+        Binned q-th percentile.
+
+        Parameters
+        ----------
+        q : float
+            The percentile, a number between 0 and 100 (inclusive).
+        interpolation : str
+            As :py:func:`numpy.percentile`.
+
+        Returns
+        -------
+        numpy.ndarray
+            an array containing the q-th percentile of all bootstrap samples for each bin in the histogram, .
+        """
+        return np.percentile(
+            self.view(flow=flow), q, axis=-1, interpolation=interpolation
+        )
+
+    @property
+    def numsamples(self) -> int:
+        return len(self.axes[-1])
+
+    @property
+    def axes(self) -> Tuple[bh.axis.Axis, ...]:
+        return self._hist.axes
+
+    def fill(
+        self,
+        *args: np.ndarray,
+        weight: Optional[np.ndarray] = None,
+        seed: Optional[np.ndarray] = None,
+        **kwargs: Any,
+    ) -> "BootstrapHistogram":
+        """
+        Fill the histogram with some values.
+
+
+        Parameters
+        ----------
+        *args : np.ndarray
+            An 1D array containing coordinates for each dimension of the histogram.
+        weight : Optional[np.ndarray]
+            Entry weights used to fill the histogram.
+        seed: Optional[np.ndarray]
+            Per-element seed. Overrides the Generator given in the constructor and
+            uses a pseudo-random number generator seeded by the given value.
+            This may be useful when filling multiple histograms with data that is not statistically independent
+            (where if may be desirable to seed the generator with a record ID).
+        **kwargs : Any
+            Passed on to :py:class:`boost_histogram.Histogram.fill`.
+
+        Returns
+        -------
+        self : BootstrapHistogram
+            Reference to this object. This is done to maintain consistency with `boost_histogram.Histogram`.
+        """
+        self._nominal.fill(*args, weight=weight, **kwargs)
+        hist = self._hist
+        shape = np.shape(args[0])
+        if seed is not None:
+            generators = np.array(
+                [np.random.Generator(np.random.PCG64(s)) for s in seed]
+            )
+        for index in range(self.numsamples):
+            if seed is None:
+                w = self._random.poisson(1.0, size=shape)
+            else:
+                w = np.fromiter(
+                    (r.poisson(1.0) for r in generators),
+                    dtype=np.float,
+                    count=len(generators),
+                )
+            if weight is not None:
+                w *= weight
+            hist.fill(*args, index, weight=w, **kwargs)
+        return self
+
+    def view(self, flow=False) -> Any:
+        """
+        Return a view of the underlying histogram bootstrap sample values.
+        """
+        return self._hist.view(flow=flow)
+
+    def __eq__(self, other: object) -> bool:
+        """
+        BootstrapHistogram's are considered to be equal if the data in their underlying histograms are equal.
+        """
+        return (
+            isinstance(other, BootstrapHistogram)
+            and self._hist == other._hist
+            and self._nominal == other._nominal
+        )
+
+    def __add__(self, other: "BootstrapHistogram") -> "BootstrapHistogram":
+        """
+        Add together the values of two bootstrap histograms together.
+
+        This is useful to merge results when parallellizing filling in a map-reduce pattern.
+        This histograms must have the same binning and the same number of bootstrap samples.
+        The merge histogram will have the same binning and the same number of bootstrap samples as the input
+        histograms.
+
+        Returns
+        -------
+        hist : BootstrapHistogram
+            A new instance of the summed histogram.
+        """
+        result = deepcopy(self)
+        result._hist += other._hist
+        result._nominal += other._nominal
+        return result
+
+    def __mul__(self, other: float):
+        result = deepcopy(self)
+        result._hist *= other
+        return result
+
+    def __truediv__(self, other: float):
+        result = deepcopy(self)
+        result._hist /= other
+        return result
+
+    def project(self, *args: int) -> "BootstrapHistogram":
+        """
+        Reduce histogram dimensionality but summing over some dimensions.
+        The bootstrap sample axis is always kept by the operation.
+
+        Parameters
+        ----------
+        *args: int
+            The dimensions to be kept.
+
+        Returns
+        -------
+        hist: BootstrapHistogram
+            a copy of the histogram with only axes in *args and the bootstrap sample axes.
+        """
+        result = copy(self)
+        arglist = list(args)
+        result._nominal = result._nominal.project(*arglist)
+        sampleaxis = len(self.axes) - 1
+        if sampleaxis not in arglist:
+            arglist.append(sampleaxis)
+        result._hist = result._hist.project(*arglist)
+        return result
---
+++ packages/bootstraphistogram/bootstraphistogram-0.6.0-py3-none-any.whl/bootstraphistogram/plot.py
@@ -0,0 +1,167 @@
+from typing import Any, Optional, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.axes import Axes as MplAxes
+
+from bootstraphistogram.bootstraphistogram import BootstrapHistogram
+
+_PERCENTILES_1SIGMA = (50.0 - 68.27 / 2.0, 50.0 + 68.27 / 2.0)
+_PERCENTILES_2SIGMA = (50.0 - 95.45 / 2.0, 50.0 - 95.45 / 2.0)
+
+_PERCENTILES_MEDIAN_AND_1SIGMA = (_PERCENTILES_1SIGMA[0], 50.0, _PERCENTILES_1SIGMA[1])
+_PERCENTILES_MEDIAN_AND_2SIGMA = (_PERCENTILES_2SIGMA[0], 50.0, _PERCENTILES_2SIGMA[1])
+
+
+class HistogramRankError(ValueError):
+    pass
+
+
+def _enforce1d(hist: BootstrapHistogram) -> None:
+    if hist.nominal.rank != 1:
+        raise HistogramRankError(
+            "this function only supports plotting 1D histograms. Try BoostrapHistogram.project to reduce inputs to 1D."
+        )
+
+
+def _getaxes(ax: Optional[MplAxes]):
+    if ax is None:
+        ax = plt.gca()
+    return ax
+
+
+def errorbar(
+    hist: BootstrapHistogram, ax: Optional[MplAxes] = None, **kwargs: Any
+) -> Any:
+    """
+    Plot the bootstrap sample mean and standard deviation.
+
+    Parameters
+    ----------
+    hist: bootstraphistogram.BootstrapHistogram
+        the :py:class:`bootstraphistogram.BootstrapHistogram` to plot.
+    ax: Optional[matplotlib.axes.Axes]
+        :py:class:`matplotlib.axes.Axes` to plot on.
+    **kwargs : Any
+        passed on to :py:meth:`matplotlib.axes.Axes.errorbar`
+
+    Returns
+    -------
+    mplerrorbarresult : Any
+        returns the result of call to :py:meth:`matplotlib.axes.Axes.errorbar`.
+    """
+    _enforce1d(hist)
+    ax = _getaxes(ax)
+    edges = hist.axes[0].edges
+    x = hist.axes[0].centers
+    xerr = [x - edges[:-1], edges[1:] - x]
+    y = hist.mean()
+    yerr = hist.std()
+    return ax.errorbar(x=x, y=y, xerr=xerr, yerr=yerr, **kwargs)
+
+
+def step(
+    hist: BootstrapHistogram,
+    percentile: Optional[float] = None,
+    ax: Optional[MplAxes] = None,
+    **kwargs: Any,
+) -> Any:
+    """
+    Plot a curve corresponding to the histogram bootstrap sample mean (or the given percentile).
+
+    Parameters
+    ----------
+    hist: bootstraphistogram.BootstrapHistogram
+        the :py:class:`bootstraphistogram.BootstrapHistogram` to plot.
+    percentile: Optional[float]
+        the sample percentile to plot. A number between 0 and 100. 50 corresponds to the median. If ``None``, the mean is plotted.
+    ax: Optional[matplotlib.axes.Axes]
+        :py:class:`matplotlib.axes.Axes` to plot on.
+    **kwargs : Any
+        passed on to :py:meth:`matplotlib.axes.Axes.step`
+
+    Returns
+    -------
+    mplstepresult : Any
+        returns the result of call to :py:meth:`matplotlib.axes.Axes.step`.
+    """
+    _enforce1d(hist)
+    ax = _getaxes(ax)
+    edges = hist.axes[0].edges
+    if percentile is not None:
+        Y = hist.percentile(percentile)
+    else:
+        Y = hist.mean()
+    return ax.step(edges, np.concatenate((Y, [Y[-1]])), where="post", **kwargs)
+
+
+def fill_between(
+    hist: BootstrapHistogram,
+    percentiles: Tuple[float, float] = _PERCENTILES_1SIGMA,
+    ax: Optional[MplAxes] = None,
+    **kwargs: Any,
+) -> Any:
+    """
+    Fill the area between two percentiles.
+
+    Parameters
+    ----------
+    hist: bootstraphistogram.BootstrapHistogram
+        the :py:class:`bootstraphistogram.BootstrapHistogram` to plot.
+    percentiles: Tuple[float, float]
+        upper and lower percentile bounds to fill. A pair of numbers between 0 and 100. Defaults to fill an equal-tailed 68.27% interval.
+    ax: Optional[matplotlib.axes.Axes]
+        :py:class:`matplotlib.axes.Axes` to plot on.
+    **kwargs : Any
+        passed on to :py:meth:`matplotlib.axes.Axes.fill_between`
+
+    Returns
+    -------
+    mplfillbetweenresult : Any
+        returns the result of call to :py:meth:`matplotlib.axes.Axes.fill_between`.
+    """
+    _enforce1d(hist)
+    ax = _getaxes(ax)
+    low, high = min(percentiles), max(percentiles)
+    X = hist.axes[0].edges
+    Y1 = hist.percentile(low)
+    Y2 = hist.percentile(high)
+    Y1 = np.concatenate((Y1, [Y1[-1]]))
+    Y2 = np.concatenate((Y2, [Y2[-1]]))
+    return ax.fill_between(x=X, y1=Y1, y2=Y2, step="post", **kwargs)
+
+
+def scatter(
+    hist: BootstrapHistogram, ax: Optional[MplAxes] = None, **kwargs: Any
+) -> Any:  #
+    """
+    Scatter plot of the bootstrap samples.
+
+    The scatter-point x-coordinate within a bin drawn from a uniform random distribution.
+
+    Parameters
+    ----------
+    hist: bootstraphistogram.BootstrapHistogram
+        the :py:class:`bootstraphistogram.BootstrapHistogram` to plot.
+    ax: Optional[matplotlib.axes.Axes]
+        :py:class:`matplotlib.axes.Axes` to plot on.
+    **kwargs : Any
+        passed on to :py:meth:`matplotlib.axes.Axes.fill_between`
+
+    Returns
+    -------
+    mplfillbetweenresult : Any
+        returns the result of call to :py:meth:`matplotlib.axes.Axes.scatter`.
+    """
+    _enforce1d(hist)
+    ax = _getaxes(ax)
+    X = []
+    Y = []
+    for binnum, binlow, binhigh in zip(
+        range(len(hist.axes[0])), hist.axes[0].edges[:-1], hist.axes[0].edges[1:]
+    ):
+        y = hist.view()[binnum]
+        x = np.random.uniform(low=binlow, high=binhigh, size=len(y))
+        X.append(x)
+        Y.append(y)
+    return ax.scatter(X, Y, **kwargs)
