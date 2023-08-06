25022d29e3857267a52c8149a2f92a3a5d336566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package python-amazon-sp-api/0.0.25/python-amazon-sp-api-0.0.25.tar.gz

---
+++ packages/python-amazon-sp-api/python-amazon-sp-api-0.0.25.tar.gz/python-amazon-sp-api-0.0.25/tests/api/orders/test_orders.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+from sp_api.api import Orders
+from sp_api.base import SellingApiException
+
+
+def test_get_orders():
+    try:
+        res = Orders().get_orders(CreatedAfter=(datetime.utcnow() - timedelta(days=1)).isoformat())
+        print(res.payload)  # json data
+    except SellingApiException as ex:
+        print(ex)
+
+
+def test_get_order_items():
+    print(Orders().get_order_items('112-4494115-9849003').payload)
+
+
+def test_get_order_address():
+    print(Orders().get_order_address('112-4494115-9849003').payload)
+
+
+def test_get_order_buyer_info():
+    print(Orders().get_order_buyer_info('112-4494115-9849003').payload)
+
+
+def test_get_order():
+    print(Orders().get_order('1asdf1-3654623').payload)
