c3b9cc7bdc4eeb79ab0a8ae2d69e0bc5516a4a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:23 +0000

    Add package filepattern/1.4.2/filepattern-1.4.2-py3-none-any.whl

---
+++ packages/filepattern/filepattern-1.4.2-py3-none-any.whl/filepattern/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, unicode_literals
+
+from .classes import FilePattern, VectorPattern
+
+from .functions import get_matching, get_regex,\
+                       parse_directory, parse_filename,\
+                       output_name, logger, VARIABLES, \
+                       infer_pattern
