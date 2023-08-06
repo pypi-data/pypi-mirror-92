ebf8266025ff16a427ba197b26d9e91ecc306eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:03 +0000

    Add package configconfig/0.5.0/configconfig-0.5.0-py3-none-any.whl

---
+++ packages/configconfig/configconfig-0.5.0-py3-none-any.whl/configconfig/metaclass.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+#
+#  metaclass.py
+"""
+Metaclass for configuration values.
+"""
+#
+#  Copyright © 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+#  OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+# stdlib
+from abc import abstractmethod
+from typing import TYPE_CHECKING, Any, Callable, Dict, Mapping, Optional, Type, cast
+
+# this package
+from configconfig.utils import basic_schema, get_json_type
+
+__all__ = ["ConfigVarMeta"]
+
+if TYPE_CHECKING:
+	# this package
+	from configconfig.configvar import ConfigVar
+
+
+class ConfigVarMeta(type):
+	"""
+	Metaclass for configuration values.
+	"""
+
+	dtype: Type
+	rtype: Type
+	required: bool
+	default: Any
+	validator: Callable
+	category: str
+	__name__: str
+
+	def __new__(cls, name: str, bases, dct: Dict):  # noqa: D102
+		x = cast("ConfigVar", super().__new__(cls, name, bases, dct))
+
+		def get(name, default):
+			return dct.get(name, getattr(x, name, default))
+
+		x.dtype = get("dtype", Any)
+
+		if "rtype" in dct:
+			x.rtype = dct["rtype"]
+		elif getattr(x, "rtype", Any) != Any:
+			pass
+		else:
+			x.rtype = x.dtype
+
+		x.required = get("required", False)
+		x.default = get("default", '')
+		x.validator = get("validator", lambda y: y)  # type: ignore
+		x.category = get("category", "other")
+		x.__name__ = dct.get("name", dct.get("__name__", x.__name__))
+
+		return x
+
+	def get_schema_entry(cls, schema: Optional[Dict] = None) -> Dict[str, Any]:
+		"""
+		Returns the JSON schema entry for this configuration value.
+
+		:param schema:
+
+		:return: Dictionary representation of the JSON schema.
+		"""
+
+		if schema is None:
+			schema = {
+					**basic_schema,
+					"properties": {},
+					"required": [],
+					}
+
+		dtype = get_json_type(cls.dtype)
+		if dtype is NotImplemented:
+			raise NotImplementedError(cls.__name__, cls.dtype)
+		else:
+			schema["properties"][cls.__name__] = dtype
+
+		if cls.required:
+			schema["required"].append(cls.__name__)
+
+		for line in (cls.__doc__ or '').split("\n\n"):
+			line = ' '.join([p.strip() for p in line.split('\n') if p.strip()])
+			if line:
+				schema["properties"][cls.__name__]["description"] = line
+				break
+
+		return schema
+
+	@property
+	def schema_entry(cls) -> Dict[str, Any]:  # noqa: D102
+		return cls.get_schema_entry()
+
+	def __call__(cls, raw_config_vars: Dict[str, Any]) -> Any:  # type: ignore  # noqa: D102
+		return cls.get(raw_config_vars)
+
+	@abstractmethod
+	def get(cls, raw_config_vars: Mapping[str, Any]):  # pragma: no cover  # noqa: D102
+		return NotImplemented
+
+	def __repr__(self) -> str:
+		return f"<ConfigVar {self.__name__!r}>"
---
+++ packages/configconfig/configconfig-0.5.0-py3-none-any.whl/configconfig/parser.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+#
+#  parser.py
+"""
+Configuration parser.
+"""
+#
+#  Copyright © 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+#  OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+# stdlib
+import tempfile
+from typing import Any, List, Mapping, MutableMapping
+
+# 3rd party
+from domdf_python_tools.paths import PathPlus
+from domdf_python_tools.typing import PathLike
+from ruamel.yaml import safe_load
+
+# this package
+from configconfig.metaclass import ConfigVarMeta
+from configconfig.utils import make_schema
+from configconfig.validator import validate_files
+
+__all__ = ["Parser"]
+
+
+class Parser:
+	"""
+	Base class for YAML configuration parsers.
+
+	Custom parsing steps for each configuration variable can be implemented with methods in the form:
+
+	.. code-block:: python
+
+
+		def visit_<configuration value name>(self, raw_config_vars: Dict[str, Any]) -> Any: ...
+
+	The method must return the value to set the configuration variable to,
+	or raise an error in the case of invalid input.
+
+	|
+
+	A final custom parsing step, useful when several values must be set at once,
+	may be implemented in the ``custom_parsing`` method:
+
+	.. code-block:: python
+
+		def custom_parsing(
+			self,
+			raw_config_vars: Mapping[str, Any],
+			parsed_config_vars: MutableMapping[str, Any],
+			filename: PathPlus,
+			) -> MutableMapping[str, Any]: ...
+
+	This takes the mapping of raw configuration variables,
+	the mapping of parsed variables (those set with the ``visit_<configuration value name>`` method),
+	and the configuration file name. The method must return the ``parsed_config_vars``.
+	"""
+
+	config_vars: List[ConfigVarMeta]
+
+	def __init__(self, allow_unknown_keys: bool = False):
+		self.allow_unknown_keys = allow_unknown_keys
+
+	def run(self, filename: PathLike):
+		"""
+		Parse configuration from the given file.
+
+		:param filename: The filename of the YAML configuration file.
+		"""
+
+		filename = PathPlus(filename)
+
+		if not filename.is_file():
+			raise FileNotFoundError(str(filename))
+
+		with tempfile.TemporaryDirectory() as tmpdir:
+			tmpdir_p = PathPlus(tmpdir)
+			schema_file = tmpdir_p / "schema.json"
+			schema = make_schema(*self.config_vars)
+			schema["additionalProperties"] = self.allow_unknown_keys
+			schema_file.dump_json(schema)
+			validate_files(schema_file, filename)
+
+		parsed_config_vars: MutableMapping[str, Any] = {}
+
+		with filename.open() as file:
+			raw_config_vars: Mapping[str, Any] = safe_load(file)
+
+		for var in self.config_vars:
+			parsed_config_vars[var.__name__] = getattr(self, f"visit_{var.__name__}", var.get)(raw_config_vars)
+
+		return self.custom_parsing(raw_config_vars, parsed_config_vars, filename)
+
+	def custom_parsing(
+			self,
+			raw_config_vars: Mapping[str, Any],
+			parsed_config_vars: MutableMapping[str, Any],
+			filename: PathPlus,
+			) -> MutableMapping[str, Any]:
+		"""
+		Custom parsing step.
+
+		:param raw_config_vars: Mapping of raw configuration values loaded from the YAML configuration file.
+		:param parsed_config_vars: Mapping of parsed configuration values.
+		:param filename: The filename of the YAML configuration file.
+		"""
+
+		return parsed_config_vars
