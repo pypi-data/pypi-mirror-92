e6ed5a16dd5d17c27a7be9b643a1837fd95d9bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:53 +0000

    Add package Datey/0.0.1/Datey-0.0.1.tar.gz

---
+++ packages/Datey/Datey-0.0.1.tar.gz/Datey-0.0.1/README.md
@@ -0,0 +1,7 @@
+This is a new module for use Persian,Gergorian and hijri Dates
+
+For Use:
+  Frist:
+    pip install persiantools
+  second:
+    pip install hijri_converter
---
+++ packages/Datey/Datey-0.0.1.tar.gz/Datey-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Datey",
+    version="0.0.1",
+    author="irakli googoochani",
+    author_email="irakligoogoochaniv@gmail.com",
+    description="Use module for your program",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/33f44",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
+
