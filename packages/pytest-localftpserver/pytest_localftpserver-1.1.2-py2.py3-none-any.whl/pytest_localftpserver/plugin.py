8f51cff9207f8463e745ac0973a97771cdf31a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:16 +0000

    Add package pytest-localftpserver/1.1.2/pytest_localftpserver-1.1.2-py2.py3-none-any.whl

---
+++ packages/pytest-localftpserver/pytest_localftpserver-1.1.2-py2.py3-none-any.whl/pytest_localftpserver/plugin.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function, absolute_import
+
+import pytest
+
+from .servers import PytestLocalFTPServer
+from .helper_functions import get_scope
+
+# uncomment the next line to log _option_validator for debugging
+# import logging
+# logging.basicConfig(filename='option_validator.log', level=logging.INFO)
+
+FIXTURE_SCOPE = get_scope()
+
+
+@pytest.fixture(scope=FIXTURE_SCOPE)
+def ftpserver(request):
+    """The returned ``ftpsever`` provides a threaded instance of
+    ``pyftpdlib.servers.FTPServer`` running on localhost.
+
+    For details on the usage and configuration  check out the docs at:
+    https://pytest-localftpserver.readthedocs.io/en/latest/usage.html
+
+    Yields
+    ------
+    ftpserver: FunctionalityWrapper
+        The type of `ftpserver` isn't actually `FunctionalityWrapper`,
+        but a subclass with `threading.Thread` or `multiprocessing.Process`
+        depending on the OS. But for autocomplete sake and since
+        `FunctionalityWrapper` holds all functionality, let's pretend that it is
+        `FunctionalityWrapper`.
+    """
+    server = PytestLocalFTPServer()
+    request.addfinalizer(server.stop)
+    return server
+
+
+@pytest.fixture(scope=FIXTURE_SCOPE)
+def ftpserver_TLS(request):
+    """The returned ``ftpsever_TSL`` provides a threaded instance of
+    ``pyftpdlib.servers.FTPServer`` using TSL and running on localhost.
+
+    For details on the usage and configuration check out the docs at:
+    https://pytest-localftpserver.readthedocs.io/en/latest/usage.html
+
+    Yields
+    ------
+    ftpserver: FunctionalityWrapper
+        The type of `ftpsever_TSL` isn't actually `FunctionalityWrapper`,
+        but a subclass with `threading.Thread` or `multiprocessing.Process`
+        depending on the OS. But for autocomplete sake and since
+        `FunctionalityWrapper` holds all functionality, let's pretend that it is
+        `FunctionalityWrapper`.
+    """
+    server = PytestLocalFTPServer(use_TLS=True)
+    request.addfinalizer(server.stop)
+    return server
