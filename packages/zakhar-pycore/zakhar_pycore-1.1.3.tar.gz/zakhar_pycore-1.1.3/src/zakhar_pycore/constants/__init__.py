bc2fa4b2fd2e5fc5fe56d4c077cd3d1e5cd6e679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:11 +0000

    Add package zakhar-pycore/1.1.3/zakhar_pycore-1.1.3.tar.gz

---
+++ packages/zakhar-pycore/zakhar_pycore-1.1.3.tar.gz/zakhar_pycore-1.1.3/src/zakhar_pycore/constants/__init__.py
@@ -0,0 +1,16 @@
+# *************************************************************************
+#
+# Copyright (c) 2021 Andrei Gramakov. All rights reserved.
+#
+# This file is licensed under the terms of the MIT license.
+# For a copy, see: https://opensource.org/licenses/MIT
+#
+# site:    https://agramakov.me
+# e-mail:  mail@agramakov.me
+#
+# *************************************************************************
+
+from .addr import ADDR
+from .cmd import CMD
+from .regs import REGS
+from .ros import ROS
