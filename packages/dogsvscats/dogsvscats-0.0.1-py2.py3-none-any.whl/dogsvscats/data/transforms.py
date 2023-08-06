c069fc7eadbddd0e5847c870eaac836ca9406f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:41 +0000

    Add package dogsvscats/0.0.1/dogsvscats-0.0.1-py2.py3-none-any.whl

---
+++ packages/dogsvscats/dogsvscats-0.0.1-py2.py3-none-any.whl/dogsvscats/data/transforms.py
@@ -0,0 +1,18 @@
+from torchvision import transforms
+
+train_tfs = transforms.Compose(
+    [
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
+
+val_tfs = transforms.Compose(
+    [
+        transforms.RandomResizedCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
