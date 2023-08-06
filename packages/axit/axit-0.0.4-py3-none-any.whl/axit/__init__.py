1afe78f73f25c288c4d238da453f8bbd660bd3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:55 +0000

    Add package axit/0.0.4/axit-0.0.4-py3-none-any.whl

---
+++ packages/axit/axit-0.0.4-py3-none-any.whl/axit/__init__.py
@@ -0,0 +1,6 @@
+
+
+
+from .mod1 import *
+
+
---
+++ packages/axit/axit-0.0.4-py3-none-any.whl/axit/mod1.py
@@ -0,0 +1,43 @@
+# this is axit wrapper file
+
+
+
+
+
+
+
+
+
+
+
+
+
+# https://stackoverflow.com/a/9728478/5944456
+import os
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+
+
+
+
+
+def ins(v):
+    print("ins>>>")
+    print('>dir:')
+    print(dir(v))
+    print('>type:')
+    print(type(v))
+    print('>print:')
+    print(v)
+    print("ins<<<")
+
+
+
