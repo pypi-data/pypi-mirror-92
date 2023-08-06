04f4f940ed4e47e3709912136a19708966a83b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:36 +0000

    Add package svolfit/0.0.5/svolfit-0.0.5-py3-none-any.whl

---
+++ packages/svolfit/svolfit-0.0.5-py3-none-any.whl/svolfit/analysis_timeseries.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from svolfit.gridanalysis import gridanalysis
+from svolfit.timeseries_utils.tsparamstats import tsparamstats
+
+def analysis_timeseries(NAME,filename,assetname,obswindow_start,obswindow_finish,windows,stride,NumProcesses, dt, model, method, modeloptions ):
+
+#TODO: only one asset series at a time now
+
+
+    print('Fitting model')
+    FILES=['SimPaths_'+NAME+'_'+model+'_'+method+'_'+assetname+'.csv']
+    try:
+        gridanalysis(NAME,[filename],[assetname],obswindow_start,obswindow_finish,windows,stride,NumProcesses, dt, model, method, modeloptions )
+    except:
+        print('path fits failed')
+
+    print('Calculating Statistics and Reporting.')
+    try:
+        tsparamstats(NAME+'_'+model+'_'+method,filename,assetname,obswindow_start,obswindow_finish)
+    except:
+        print('paramters stats calcs failed')
+    return
