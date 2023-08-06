926b75b7df046b363fbc311c63628b19570c65a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:47 +0000

    Add package autoBoostcamp/1.8/autoBoostcamp-1.8.tar.gz

---
+++ packages/autoBoostcamp/autoBoostcamp-1.8.tar.gz/autoBoostcamp-1.8/autoBoostcamp/__init__.py
@@ -0,0 +1,12 @@
+from .backend import doHi, doBye
+import datetime
+
+def do():
+    now = datetime.datetime.now()
+    now_hm = now.hour + 0.01 * now.minute
+    if 9.30 <= now_hm <= 10:  # 9시 30분 ~ 10시
+        doHi()
+        print(f"autoBoostcamp doHi run")
+    elif now_hm >= 19:  # 19시 ~
+        doBye()
+        print(f"autoBoostcamp doBye run")
