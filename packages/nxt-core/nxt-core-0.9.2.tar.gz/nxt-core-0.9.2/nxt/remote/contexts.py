f2f7f76410b6ebe29d8308ec292af66afc0e4b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:02 +0000

    Add package nxt-core/0.9.2/nxt-core-0.9.2.tar.gz

---
+++ packages/nxt-core/nxt-core-0.9.2.tar.gz/nxt-core-0.9.2/nxt/remote/contexts.py
@@ -0,0 +1,47 @@
+# Builtin
+import sys
+import os
+import logging
+from collections import namedtuple
+
+from nxt import nxt_io
+
+logger = logging.getLogger('nxt')
+
+REMOTE_CONTEXT_BUILTIN_NODE = '_remote_sub_graph'
+SUB_GRAPH_BUILTIN_NODE = '_sub_graph'
+REMOTE_CONTEXT_ATTR_NAME = '_context'
+RemoteContext = namedtuple('RemoteContext', ('name', 'exe', 'graph'))
+_CONTEXTS = []
+starter_contexts = {'maya':
+                        os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                     'maya_context.nxt'))}
+
+
+def register_context(context):
+    global _CONTEXTS
+    _CONTEXTS += [context]
+    logger.info('registered context: ' + context.name)
+
+
+def find_context_by_name(name):
+    for context in _CONTEXTS:
+        if context.name == name:
+            return context
+    return None
+
+
+def iter_context_names():
+    for context in _CONTEXTS:
+        yield context.name
+
+
+_context_graph = '${var}/_context.nxt'.format(var=nxt_io.BUILTIN_GRAPHS_ENV_VAR)
+PYTHON_CONTEXT = RemoteContext('python', sys.executable, _context_graph)
+register_context(PYTHON_CONTEXT)
+
+
+def get_current_context_exe_name():
+    exe_name = os.path.basename(sys.executable)
+    exe_name, _ = os.path.splitext(exe_name)
+    return exe_name
