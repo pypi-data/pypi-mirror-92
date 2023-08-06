af9ee56c37e3f2b288ee031f934a7a2ce3f79ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:04 +0000

    Add package noteblog/0.1.15/noteblog-0.1.15.tar.gz

---
+++ packages/noteblog/noteblog-0.1.15.tar.gz/noteblog-0.1.15/noteblog/typecho/__init__.py
@@ -0,0 +1,6 @@
+from .main import Typecho
+from .models import Attachment, Category, Comment, Page, Post
+
+name = 'pytypecho'
+
+__all__ = ['Typecho', 'Post', 'Page', 'Category', 'Attachment', 'Comment']
