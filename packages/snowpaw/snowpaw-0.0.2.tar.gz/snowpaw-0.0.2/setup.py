159c0d7fdc0f2b5cf28a2354bd784cc7997e653c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:22 +0000

    Add package snowpaw/0.0.2/snowpaw-0.0.2.tar.gz

---
+++ packages/snowpaw/snowpaw-0.0.2.tar.gz/snowpaw-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from io import open
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="snowpaw",
+    version="0.0.2",
+    author="Liu Jianwei",
+    author_email="tungliu@126.com",
+    description="A useful python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://lttpp.github.io/snowpaw/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
