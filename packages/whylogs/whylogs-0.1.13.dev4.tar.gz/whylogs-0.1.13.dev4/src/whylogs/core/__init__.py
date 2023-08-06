b76cf396652fa7fb688afbaa60ef72a8e64cc290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:30 +0000

    Add package whylogs/0.1.13.dev4/whylogs-0.1.13.dev4.tar.gz

---
+++ packages/whylogs/whylogs-0.1.13.dev4.tar.gz/whylogs-0.1.13.dev4/src/whylogs/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+This subpackage defines the core components of the whylogs python library.
+
+In particular, look at the `DatasetProfile` class
+"""
+from .columnprofile import ColumnProfile
+from .datasetprofile import DatasetProfile
+from .image_profiling import TrackImage, _METADATA_DEFAULT_ATTRIBUTES
