970f8f8838f71035c1cb0b12f05cddf834620698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:08 +0000

    Add package MayReader/0.0.2/MayReader-0.0.2.tar.gz

---
+++ packages/MayReader/MayReader-0.0.2.tar.gz/MayReader-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python',
+  'Programming Language :: Python :: 2',
+  'Programming Language :: Python :: 3',
+]
+ 
+setup(
+  name='MayReader',
+  version='0.0.2',
+  description='Reader for maya ascii files',
+  long_description=open('README.md').read(),
+  url='',  
+  author='Milan Mitrovic',
+  author_email='milan.mitrovic2111@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Reader', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
