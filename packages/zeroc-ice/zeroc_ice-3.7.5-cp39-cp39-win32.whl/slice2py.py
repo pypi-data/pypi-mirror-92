224d926a999e35122678aa61d3f217e36cc25640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:28 +0000

    Add package zeroc-ice/3.7.5/zeroc_ice-3.7.5-cp39-cp39-win32.whl

---
+++ packages/zeroc-ice/zeroc_ice-3.7.5-cp39-cp39-win32.whl/slice2py.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# Copyright (c) ZeroC, Inc. All rights reserved.
+#
+
+import sys
+import Ice
+import IcePy
+
+
+def main():
+    sliceDir = Ice.getSliceDir()
+    # Automatically add the slice dir.
+    if sliceDir is not None:
+        sys.argv.append('-I' + sliceDir)
+    val = IcePy.compile(sys.argv)
+    sys.exit(int(val))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
