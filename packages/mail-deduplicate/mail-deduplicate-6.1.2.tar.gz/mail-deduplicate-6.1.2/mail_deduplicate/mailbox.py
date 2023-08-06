a738ad4ec90bad5c3df071e83679bdd9e4ab6a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:19 +0000

    Add package mail-deduplicate/6.1.2/mail-deduplicate-6.1.2.tar.gz

---
+++ packages/mail-deduplicate/mail-deduplicate-6.1.2.tar.gz/mail-deduplicate-6.1.2/mail_deduplicate/mailbox.py
@@ -0,0 +1,207 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+# All Rights Reserved.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import inspect
+import mailbox
+from functools import partial
+from pathlib import Path
+
+from boltons.dictutils import FrozenDict
+from boltons.iterutils import flatten
+
+from . import logger
+from .colorize import choice_style
+from .mail import DedupMail
+
+""" Patch and tweak Python's standard librabry mailboxes constructors to set
+sane defaults. Also forces out our own message factories to add deduplication
+tools and utilities. """
+
+
+def build_box_constructors():
+    """Build our own mail constructors for each mailbox format.
+
+    Gather all constructors defined by the standard Python library and extend them
+    with our ``DedupMail`` class.
+    """
+    # Only keep subclasses of the ``mailbox.Mailbox`` interface, but the latter and
+    # all others starting with an underscore.
+    for _, klass in inspect.getmembers(mailbox, inspect.isclass):
+        if (
+            klass != mailbox.Mailbox
+            and not klass.__name__.startswith("_")
+            and issubclass(klass, mailbox.Mailbox)
+        ):
+            # Fetch the default factory for each mailbox type based on naming
+            # conventions.
+            message_klass = getattr(mailbox, f"{klass.__name__}Message")
+            assert issubclass(message_klass, mailbox.Message)
+
+            # Extend the default factory with DedupMail class.
+            factory_klass = type(
+                f"{klass.__name__}DedupMail",
+                (DedupMail, message_klass, object),
+                {
+                    "__doc__": f"Extend the default message factory for {klass} with "
+                    "our own ``DedupMail`` class to add deduplication utilities."
+                },
+            )
+
+            # Set our own custom factory and safety options to default constructor.
+            constructor = partial(klass, factory=factory_klass, create=False)
+
+            # Generates our own box_type_id for use in CLI parameters.
+            box_type_id = klass.__name__.lower()
+
+            yield box_type_id, constructor
+
+
+# Mapping between supported box type IDs and their constructors.
+BOX_TYPES = FrozenDict(build_box_constructors())
+
+
+# Categorize each box type into its structure type.
+BOX_STRUCTURES = FrozenDict(
+    {
+        "file": {"mbox", "mmdf", "babyl"},
+        "folder": {"maildir", "mh"},
+    }
+)
+# Check we did not forgot any box type.
+assert set(flatten(BOX_STRUCTURES.values())) == set(BOX_TYPES)
+
+
+# List of required sub-folders defining a properly structured maildir.
+MAILDIR_SUBDIRS = frozenset(("cur", "new", "tmp"))
+
+
+def autodetect_box_type(path):
+    """Auto-detect the format of the mailbox located at the provided path.
+
+    Returns a box type as indexed in the ``box_types`` dictionnary above.
+
+    If the path is a file, then it is considered as an ``mbox``. Else, if th
+    provided path is a folder and feature the expecteed sub-directories, it is
+    parsed as a ``maildir``.
+
+    Future finer autodetection heuristics should be implemented here. Some ideas:
+        * single mail from a maildir
+        * plain text mail content
+        * other mailbox formats supported in Python's std lib:
+            * ``MH``
+            * ``Babyl``
+            * ``MMDF``
+    """
+    box_type = None
+
+    # Validates folder is a maildir.
+    if path.is_dir():
+        for subdir in MAILDIR_SUBDIRS:
+            if not path.joinpath(subdir).is_dir():
+                raise ValueError(f"Missing sub-directory {subdir!r}")
+        box_type = "maildir"
+
+    # Validates folder is a mbox.
+    elif path.is_file():
+        box_type = "mbox"
+
+    if not box_type:
+        raise ValueError("Unrecognized mail source type.")
+
+    logger.info(f"{choice_style(box_type)} detected.")
+    return box_type
+
+
+def open_box(path, box_type=False, force_unlock=False):
+    """Open a mailbox.
+
+    Returns a list of boxes, one per sub-folder. All are locked, ready for operations.
+
+    If ``box_type`` is provided, forces the opening of the box in the specified format.
+    Defaults to (crude) autodetection.
+    """
+    logger.info(f"\nOpening {choice_style(path)} ...")
+    path = Path(path)
+    if not box_type:
+        box_type = autodetect_box_type(path)
+    else:
+        logger.warning(f"Forcing {box_type} format.")
+
+    constructor = BOX_TYPES[box_type]
+    # Do not allow the constructor to create a new mailbox if not found.
+    box = constructor(path, create=False)
+
+    return open_subfolders(box, force_unlock)
+
+
+def lock_box(box, force_unlock):
+    """Lock an opened box and allows for forced unlocking.
+
+    Returns the locked box.
+    """
+    try:
+        logger.debug("Locking box...")
+        box.lock()
+    except mailbox.ExternalClashError:
+        logger.error("Box already locked!")
+        # Remove the lock manually and re-lock.
+        if force_unlock:
+            logger.warning("Forcing removal of lock...")
+            # Forces internal metadata.
+            box._locked = True
+            box.unlock()
+            box.lock()
+        # Re-raise error.
+        else:
+            raise
+    logger.debug("Box opened.")
+    return box
+
+
+def open_subfolders(box, force_unlock):
+    """Browse recursively the subfolder tree of a box.
+
+    Returns a list of opened and locked boxes, each for one subfolder.
+    """
+    folder_list = [lock_box(box, force_unlock)]
+
+    # Skip box types not supporting subfolders.
+    if hasattr(box, "list_folders"):
+        for folder_id in box.list_folders():
+            logger.info(f"Opening subfolder {folder_id} ...")
+            folder_list += open_subfolders(box.get_folder(folder_id), force_unlock)
+    return folder_list
+
+
+def create_box(path, box_type=False):
+    """Creates a brand new box from scratch."""
+    assert isinstance(path, Path)
+    logger.info(
+        f"Creating new {choice_style(box_type)} box at {choice_style(str(path))} ..."
+    )
+
+    if path.exists():
+        raise FileExistsError(path)
+
+    constructor = BOX_TYPES[box_type]
+    # Allow the constructor to create a new mail box as we already double-checked
+    # beforehand it does not exist.
+    box = constructor(path, create=True)
+
+    logger.debug("Locking box...")
+    box.lock()
+    return box
---
+++ packages/mail-deduplicate/mail-deduplicate-6.1.2.tar.gz/mail-deduplicate-6.1.2/mail_deduplicate/tests/conftest.py
@@ -0,0 +1,211 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+# All Rights Reserved.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import random
+import string
+import sys
+from email.utils import formatdate as maildate
+from mailbox import Mailbox, Maildir, Message, mbox
+from textwrap import dedent, indent
+from uuid import uuid4
+
+import arrow
+import click
+import pytest
+from boltons.iterutils import flatten, same
+from boltons.tbutils import ExceptionInfo
+from click.testing import CliRunner
+
+from .. import CLI_NAME
+from ..cli import mdedup
+
+""" Fixtures, configuration and helpers for tests. """
+
+
+def is_windows():
+    """ Return `True` only if current platform is of the Windows family. """
+    return sys.platform in ["win32", "cygwin"]
+
+
+skip_windows = pytest.mark.skipif(is_windows(), reason="Skip Windows")
+""" Pytest mark to skip a test if it is run on a Windows system. """
+
+
+def print_cli_output(cmd, output):
+    """ Simulate CLI output. Used to print debug traces in test results. """
+    print("\n► {}".format(click.style(" ".join(cmd), fg="white")))
+    if output:
+        print(indent(output, "  "))
+
+
+@pytest.fixture
+def runner():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        yield runner
+
+
+@pytest.fixture
+def invoke(runner):
+    """ Executes Click's CLI, print output and return results. """
+
+    def _run(*args, color=False):
+        # We allow for nested iterables and None values as args for
+        # convenience. We just need to flatten and filters them out.
+        args = list(filter(None.__ne__, flatten(args)))
+        if args:
+            assert same(map(type, args), str)
+
+        result = runner.invoke(mdedup, args, color=color)
+
+        print_cli_output([CLI_NAME] + args, result.output)
+
+        # Print some more debug info.
+        print(result)
+        if result.exception:
+            print(ExceptionInfo.from_exc_info(*result.exc_info).get_formatted())
+
+        return result
+
+    return _run
+
+
+class MailFactory:
+
+    """Create fake mail messages to serve as unittest fixtures.
+
+    Help production of either random, customized or deterministic mail message.
+    """
+
+    def __init__(self, **custom_fields):
+        """Init the mail with custom fields.
+
+        You can bypass data normalization by passing the pre-formated date
+        string with ``date_rfc2822`` custom field instead of ``date``.
+        """
+        # Defaults fields values.
+        self.fields = {
+            "body": "Да, они летят.",
+            "date": arrow.utcnow(),
+            "date_rfc2822": None,
+            "message_id": "<201111231111.abcdef101@mail.nohost.com>",
+        }
+
+        # Check all custom fields are recognized and supported.
+        assert set(custom_fields).issubset(self.fields)
+
+        # Parse dates and normalize to Arrow instance.
+        if "date" in custom_fields:
+            custom_fields["date"] = arrow.get(custom_fields["date"])
+
+        # Update default values with custom ones.
+        self.fields.update(custom_fields)
+
+        # Derive RFC-2822 date from arrow object if not set.
+        if not self.fields.get("date_rfc2822"):
+            self.fields["date_rfc2822"] = maildate(self.fields["date"].float_timestamp)
+
+    def render(self):
+        """ Returns the full, rendered content of the mail. """
+        return dedent(
+            """\
+            Return-path: <none@nohost.com>
+            Envelope-to: me@host.com
+            Delivery-date: {date_rfc2822}
+            Received: from [11.11.11.11] (helo=nope.com)
+            \tby host.com with esmtp (Exim 4.80)
+            \t(envelope-from <noone@nohost.com>)
+            \tid 1CX8OJ-0014c9-Ii
+            \tfor me@host.com; {date_rfc2822}
+            Date: {date_rfc2822}
+            From: foo@bar.com
+            Message-Id: {message_id}
+            To: baz
+            Subject: A duplicate mail
+            Mime-Version: 1.0
+            Content-Length: 60
+            Content-Type: text/plain; charset="utf-8"
+            Content-Transfer-Encoding: 8bit
+            {body}""".format(
+                **self.fields
+            )
+        ).encode("utf-8")
+
+    def as_message(self):
+        """Returns the mail as an instance of ``mailbox.Message``."""
+        return Message(self.render())
+
+    @staticmethod
+    def random_string(length):
+        return "".join(random.choice(string.ascii_lowercase) for i in range(length))
+
+
+@pytest.fixture
+def make_box(tmp_path):
+    """A generic fixture to produce a temporary box of mails.
+
+    The mail container can be created in any format supported by Python standard
+    library, by the way of the ``box_type`` parameter. Supported values: only
+    ``Maildir`` and ``mbox`` for the moment.
+    """
+
+    def _make_box(box_type, mails=None):
+        """Create a fake maildir and populate it with mails."""
+        # Check parameters.
+        assert box_type in (Maildir, mbox)
+        assert issubclass(box_type, Mailbox)
+
+        if not mails:
+            mails = []
+        assert same(map(type, mails), MailFactory)
+
+        # Create the container under a random name and put all provided mails there.
+        box = box_type(tmp_path.joinpath(uuid4().hex), create=True)
+        box.lock()
+        for fake_mail in mails:
+            box.add(fake_mail.render())
+
+        box.close()
+        return box._path, box_type
+
+    return _make_box
+
+
+def check_box(box_path, box_type, content=None):
+    """Check the content of a mail box (in any of maildir of mbox format).
+
+    Does not use ``set()`` types internally to avoid silent deduplication.
+    Translates all mails provided to ``mailbox.Message`` instances to provide
+    fair comparison in a normalized space.
+    """
+    # Check provided parameters.
+    assert isinstance(box_path, str)
+    assert box_type in (Maildir, mbox)
+    assert not isinstance(content, set)
+    if content is None:
+        content = []
+    assert same(map(type, content), MailFactory)
+
+    # Compares the content of the box.
+    box = box_type(box_path, create=False)
+
+    # TODO: use a COunter to count occurrences
+
+    assert len(box) == len(content)
+    mails_found = sorted([str(m) for m in box])
+    assert sorted([str(m.as_message()) for m in content]) == mails_found
+    box.close()
