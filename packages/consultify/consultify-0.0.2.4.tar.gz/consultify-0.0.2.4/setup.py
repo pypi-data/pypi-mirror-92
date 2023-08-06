6e6a4a26a678b6c1e0b9a791f2cb672a94568d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:32 +0000

    Add package consultify/0.0.2.4/consultify-0.0.2.4.tar.gz

---
+++ packages/consultify/consultify-0.0.2.4.tar.gz/consultify-0.0.2.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="consultify", # Replace with your own username
+    version="0.0.2.4",
+    author="Ryan Yu",
+    author_email="ryu@mba2021.hbs.edu",
+    description="Turns analyses on Python into management consulting-style PowerPoint slides.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ryancarlyu/consultify",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required
+)
