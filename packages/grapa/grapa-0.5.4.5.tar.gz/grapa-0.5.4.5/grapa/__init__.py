e9773c27b6b0b0226fed51d6db7b2b7977e91107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:27 +0000

    Add package grapa/0.5.4.5/grapa-0.5.4.5.tar.gz

---
+++ packages/grapa/grapa-0.5.4.5.tar.gz/grapa-0.5.4.5/grapa/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 11 14:50:26 2017
+
+@author: Romain Carron
+Copyright (c) 2018, Empa, Laboratory for Thin Films and Photovoltaics, Romain Carron
+"""
+
+__version__ = '0.5.4.5'
+
+def grapa():
+    from grapa.GUI import buildUI
+    buildUI()
