311290392e550c7876c6426df8f5cec77a12f583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:07 +0000

    Add package scienco/0.4.0/scienco-0.4.0.tar.gz

---
+++ packages/scienco/scienco-0.4.0.tar.gz/scienco-0.4.0/scienco/indexes/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from scienco.indexes.processing import automated_readability_index
+from scienco.indexes.processing import coleman_liau_index
+from scienco.indexes.processing import compute_indexes
+from scienco.indexes.processing import flesch_reading_ease_score
+from scienco.types import Indexes
+
+__all__ = ("automated_readability_index", "coleman_liau_index", "compute_indexes", "flesch_reading_ease_score", "Indexes")
---
+++ packages/scienco/scienco-0.4.0.tar.gz/scienco-0.4.0/scienco/metrics/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from scienco.metrics.processing import compute_metrics
+from scienco.metrics.processing import sentences
+from scienco.metrics.processing import syllables
+from scienco.metrics.processing import words
+from scienco.types import Metrics
+
+__all__ = ("compute_metrics", "sentences", "syllables", "words", "Metrics")
