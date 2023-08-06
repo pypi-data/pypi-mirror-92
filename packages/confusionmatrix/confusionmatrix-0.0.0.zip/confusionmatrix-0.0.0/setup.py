93205ddc1c9bd315c6dc9773937ac9c806d4126a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:37 +0000

    Add package confusionmatrix/0.0.0/confusionmatrix-0.0.0.zip

---
+++ packages/confusionmatrix/confusionmatrix-0.0.0.zip/confusionmatrix-0.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md","r") as readme_file:
+    long_description = readme_file.read
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup(
+    name='confusionmatrix',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='DELL.SXO10',
+    author_email='nourankhaled1234@gmail.com',
+    description='Accuracy',
+    include_package_data=True
+)
