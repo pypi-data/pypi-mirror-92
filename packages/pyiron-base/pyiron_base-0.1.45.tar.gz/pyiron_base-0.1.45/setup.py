1b09d5b3a75fab07183b85798680cd0ff82a0a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:46 +0000

    Add package pyiron-base/0.1.45/pyiron_base-0.1.45.tar.gz

---
+++ packages/pyiron-base/pyiron_base-0.1.45.tar.gz/pyiron_base-0.1.45/setup.py
@@ -0,0 +1,55 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='pyiron_base',
+    version=versioneer.get_version(),
+    description='pyiron_base - an integrated development environment (IDE) for computational science.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_base',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='janssen@mpie.de',
+    license='BSD',
+
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8'],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'dill>=0.3.1.1',
+        'future>=0.18.2',
+        'gitpython>=3.1.0',
+        'h5io>=0.1.1',
+        'h5py>=2.10.0',
+        'numpy>=1.18.1',
+        'pandas>=1.0.1',
+        'pathlib2>=2.3.5',
+        'psutil>=5.7.0',
+        'pyfileindex>=0.0.4',
+        'pysqa>=0.0.13',
+        'sqlalchemy>=1.3.14',
+        'tables>=3.6.1',
+        'tqdm>=4.35.0'
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+
+    entry_points = {
+            "console_scripts": [
+                'pyiron=pyiron_base.cli:main'
+            ]
+    }
+    )
