f2b3bc7b6492cebab04db1d6375d79dd70c42fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:07 +0000

    Add package Orange3-Text/1.3.0/Orange3-Text-1.3.0.tar.gz

---
+++ packages/Orange3-Text/Orange3-Text-1.3.0.tar.gz/Orange3-Text-1.3.0/orangecontrib/text/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE3-TEXT SETUP.PY
+short_version = '1.3.0'
+version = '1.3.0'
+full_version = '1.3.0'
+git_revision = '6c9fafe1ee60e0ece6901fc527309e6b264e2b6a'
+release = True
+if not release:
+    version = full_version
+    short_version += ".dev"
