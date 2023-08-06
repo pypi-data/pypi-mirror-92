ac81a554f283e7eeb7e466c6ecb51e399acbbc70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:04 +0000

    Add package covid-env-biggreen-xprize/0.0.1/covid-env-biggreen-xprize-0.0.1.tar.gz

---
+++ packages/covid-env-biggreen-xprize/covid-env-biggreen-xprize-0.0.1.tar.gz/covid-env-biggreen-xprize-0.0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='covid-env-biggreen-xprize',
+      version='0.0.1',
+      install_requires=['gym','numpy', 'pandas', 'tensorflow']  # And any other dependencies foo needs
+)
