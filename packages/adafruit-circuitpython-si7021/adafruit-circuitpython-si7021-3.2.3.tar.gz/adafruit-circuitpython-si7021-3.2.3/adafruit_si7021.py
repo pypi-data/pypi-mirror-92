00a18233adbb428fff44bf2347ea1e81cdf26f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:20 +0000

    Add package adafruit-circuitpython-si7021/3.2.3/adafruit-circuitpython-si7021-3.2.3.tar.gz

---
+++ packages/adafruit-circuitpython-si7021/adafruit-circuitpython-si7021-3.2.3.tar.gz/adafruit-circuitpython-si7021-3.2.3/adafruit_si7021.py
@@ -0,0 +1,143 @@
+# SPDX-FileCopyrightText: 2017 Radomir Dopieralski for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+``adafruit_si7021``
+===================
+
+This is a CircuitPython driver for the SI7021 temperature and humidity sensor.
+
+* Author(s): Radomir Dopieralski
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+* Adafruit `Si7021 Temperature & Humidity Sensor Breakout Board
+  <https://www.adafruit.com/product/3251>`_ (Product ID: 3251)
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the ESP8622 and M0-based boards:
+  https://github.com/adafruit/circuitpython/releases
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+"""
+try:
+    import struct
+except ImportError:
+    import ustruct as struct
+
+from adafruit_bus_device.i2c_device import I2CDevice
+from micropython import const
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_SI7021.git"
+
+HUMIDITY = const(0xF5)
+TEMPERATURE = const(0xF3)
+_RESET = const(0xFE)
+_READ_USER1 = const(0xE7)
+_USER1_VAL = const(0x3A)
+
+
+def _crc(data):
+    crc = 0
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x80:
+                crc <<= 1
+                crc ^= 0x131
+            else:
+                crc <<= 1
+    return crc
+
+
+class SI7021:
+    """
+    A driver for the SI7021 temperature and humidity sensor.
+
+    :param i2c_bus: The `busio.I2C` object to use. This is the only required parameter.
+    :param int address: (optional) The I2C address of the device.
+    """
+
+    def __init__(self, i2c_bus, address=0x40):
+        self.i2c_device = I2CDevice(i2c_bus, address)
+        self._command(_RESET)
+        # Make sure the USER1 settings are correct.
+        while True:
+            # While restarting, the sensor doesn't respond to reads or writes.
+            try:
+                data = bytearray([_READ_USER1])
+                with self.i2c_device as i2c:
+                    i2c.write_then_readinto(data, data)
+                value = data[0]
+            except OSError:
+                pass
+            else:
+                break
+        if value != _USER1_VAL:
+            raise RuntimeError("bad USER1 register (%x!=%x)" % (value, _USER1_VAL))
+        self._measurement = 0
+
+    def _command(self, command):
+        with self.i2c_device as i2c:
+            i2c.write(struct.pack("B", command))
+
+    def _data(self):
+        data = bytearray(3)
+        data[0] = 0xFF
+        while True:
+            # While busy, the sensor doesn't respond to reads.
+            try:
+                with self.i2c_device as i2c:
+                    i2c.readinto(data)
+            except OSError:
+                pass
+            else:
+                if data[0] != 0xFF:  # Check if read succeeded.
+                    break
+        value, checksum = struct.unpack(">HB", data)
+        if checksum != _crc(data[:2]):
+            raise ValueError("CRC mismatch")
+        return value
+
+    @property
+    def relative_humidity(self):
+        """The measured relative humidity in percent."""
+        self.start_measurement(HUMIDITY)
+        value = self._data()
+        self._measurement = 0
+        return min(100.0, value * 125.0 / 65536.0 - 6.0)
+
+    @property
+    def temperature(self):
+        """The measured temperature in degrees Celcius."""
+        self.start_measurement(TEMPERATURE)
+        value = self._data()
+        self._measurement = 0
+        return value * 175.72 / 65536.0 - 46.85
+
+    def start_measurement(self, what):
+        """
+        Starts a measurement.
+
+        Starts a measurement of either ``HUMIDITY`` or ``TEMPERATURE``
+        depending on the ``what`` argument. Returns immediately, and the
+        result of the measurement can be retrieved with the
+        ``temperature`` and ``relative_humidity`` properties. This way it
+        will take much less time.
+
+        This can be useful if you want to start the measurement, but don't
+        want the call to block until the measurement is ready -- for instance,
+        when you are doing other things at the same time.
+        """
+        if what not in (HUMIDITY, TEMPERATURE):
+            raise ValueError()
+        if not self._measurement:
+            self._command(what)
+        elif self._measurement != what:
+            raise RuntimeError("other measurement in progress")
+        self._measurement = what
