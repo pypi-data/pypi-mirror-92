b5988a91929864ae5fb872d49e044ff3b82f0963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:14 +0000

    Add package memspectrum/1.0.0/memspectrum-1.0.0.tar.gz

---
+++ packages/memspectrum/memspectrum-1.0.0.tar.gz/memspectrum-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+#setuptools.find_packages()
+
+setup(
+    name='memspectrum',
+    version='1.0.0',
+    author='Alessandro Martini, Walter Del Pozzo',
+    author_email='martini.alessandr@gmail.com,walter.delpozzo@ligo.org',
+    packages=['memspectrum'],
+    package_dir = {'memspectrum':'./memspectrum'},
+    url="https://github.com/martini-alessandro/Maximum-Entropy-Spectrum/",
+    license='CC by 4.0',
+    description='maximum entropy spectral analysis',
+    long_description=readme(),
+    include_package_data = True,
+#    package_data={'memspectrum': get_package_data()},
+    install_requires=[
+        "numpy >= 1.16.4",
+    ],
+	long_description_content_type = 'text/x-rst'
+)
+
