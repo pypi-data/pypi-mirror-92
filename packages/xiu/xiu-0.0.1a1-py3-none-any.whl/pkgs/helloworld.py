c8f3f2197576d638aa606131020fe88d9fd1a66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:14 +0000

    Add package xiu/0.0.1a1/xiu-0.0.1a1-py3-none-any.whl

---
+++ packages/xiu/xiu-0.0.1a1-py3-none-any.whl/pkgs/helloworld.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import json
+import time
+import signal
+from typing import List
+from pprint import pprint
+
+
+def say_hello():
+    print("Hello world")
+
---
+++ packages/xiu/xiu-0.0.1a1-py3-none-any.whl/xiu.py
@@ -0,0 +1,57 @@
+# coding:utf-8
+import os
+import sys
+import json
+import time
+import signal
+import inspect
+from typing import List
+from pprint import pprint
+from logger import Logger
+
+
+# =========================================================== display
+def p(s):
+    print(s)
+
+
+def pp(d: dict):
+    pprint(d)
+
+
+def sleep(countdown: int):
+    time.sleep(countdown)
+
+
+# =========================================================== IO
+def jsonread(file_name: str) -> dict:
+    res = {}
+    with open(file_name, 'r') as f:
+        res = json.loads(f.read())
+    return res
+
+
+def jsonwrite(d: dict, file_name: str):
+    json.dump(d, open(file_name, 'w'), ensure_ascii=False, indent=2)
+
+logger = Logger()
+# =========================================================== Decorator
+def set_timeout(countdown: int, callback=print):
+    def decorator(func):
+        def handle(signum, frame):
+            raise RuntimeError
+
+        def wrapper(*args, **kwargs):
+            try:
+                signal.signal(signal.SIGALRM, handle)
+                signal.alarm(countdown)  # set countdown
+                r = func(*args, **kwargs)
+                signal.alarm(0)  # close alarm
+                return r
+            except RuntimeError as e:
+                print(e)
+                callback()
+
+        return wrapper
+
+    return decorator
