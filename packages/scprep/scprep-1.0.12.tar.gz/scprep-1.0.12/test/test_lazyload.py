d2fd88227660c4293fc147855b1a298cfc528293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package scprep/1.0.12/scprep-1.0.12.tar.gz

---
+++ packages/scprep/scprep-1.0.12.tar.gz/scprep-1.0.12/test/test_lazyload.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+import scprep
+import sys
+from tools import data
+
+
+def test_lazyload():
+    proc = subprocess.Popen(
+        ["nose2", "--quiet", "_test_lazyload"],
+        cwd=os.path.join(data._get_root_dir(), "test"),
+        stderr=subprocess.PIPE,
+    )
+    return_code = proc.wait()
+    try:
+        assert return_code == 0
+    except AssertionError:
+        lines = proc.stderr.read().decode().split("\n")
+        lines = lines[4:-6]
+        raise AssertionError("\n".join(lines))
+    finally:
+        proc.stderr.close()
+
+
+def test_builtins():
+    for module in scprep._lazyload._importspec.keys():
+        if module == "anndata2ri" and sys.version_info[:2] < (3, 6):
+            continue
+        try:
+            del sys.modules[module]
+        except KeyError:
+            pass
+        assert (
+            getattr(scprep._lazyload, module).__class__ is scprep._lazyload.AliasModule
+        )
+        try:
+            getattr(scprep._lazyload, module).__version__
+        except AttributeError:
+            pass
+        assert getattr(scprep._lazyload, module).__class__ is type(scprep)
