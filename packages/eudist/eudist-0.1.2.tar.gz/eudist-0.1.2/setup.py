e438a0a8e4b1c8c9461176ee688900dcd9f4aff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package eudist/0.1.2/eudist-0.1.2.tar.gz

---
+++ packages/eudist/eudist-0.1.2.tar.gz/eudist-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import Extension, setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    # Ignore error, as otherwise setuptools doesn't know we need cython
+    def cythonize(*args, **kwargs):
+        pass
+
+
+sourcefiles = ["eudist.pyx", "eudist_cpp.cxx"]
+
+extensions = [Extension("eudist", sourcefiles)]
+
+setup(
+    name="eudist",
+    version="0.1.2",
+    ext_modules=cythonize(
+        extensions,
+        language_level=3,
+        compiler_directives=dict(binding=True, embedsignature=True),
+    ),
+    setup_requires=["setuptools", "cython"],
+    install_requires=["cython"],
+    license="GPL",
+)
