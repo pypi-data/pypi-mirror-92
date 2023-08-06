05462c809582ae1c0fe3073427f8754c807274fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:28 +0000

    Add package tsia/0.1.12/tsia-0.1.12-py3-none-any.whl

---
+++ packages/tsia/tsia-0.1.12-py3-none-any.whl/tsia/__init__.py
@@ -0,0 +1,17 @@
+"""Time series image analysis module for Python.
+tsia is a Python package for time series analysis through images. It
+aims to enable advanced diagnostic tools for time series through imaging
+and visualization capabilities.
+"""
+
+from .plot import plot
+from .markov import mtf as markov
+from .network_graph import ng as network_graph
+from .utils import tools as utils
+from .diagnostics import histograms
+from .diagnostics import profiling
+
+__version__ = '0.1.12'
+__all__ = [
+    'markov', 'network_graph', 'plot', 'utils', 'histograms', 'profiling'
+]
