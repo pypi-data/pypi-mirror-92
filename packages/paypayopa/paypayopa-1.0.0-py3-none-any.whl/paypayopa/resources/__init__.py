e6665507bace7778af080fa8d9a04a3093d81c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:51 +0000

    Add package paypayopa/1.0.0/paypayopa-1.0.0-py3-none-any.whl

---
+++ packages/paypayopa/paypayopa-1.0.0-py3-none-any.whl/paypayopa/resources/__init__.py
@@ -0,0 +1,15 @@
+from .code import Code
+from .payment import Payment
+from .account import Account
+from .preauth import Preauth
+from .pending import Pending
+from .user import User
+
+__all__ = [
+        'Code',
+        'Payment',
+        'Account',
+        'Preauth',
+        'Pending',
+        'User',
+]
