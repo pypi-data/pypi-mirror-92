a954e14cb9b73ea0576512345bee770c1ce3cb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:18 +0000

    Add package polimorfo/0.9.52/polimorfo-0.9.52.tar.gz

---
+++ packages/polimorfo/polimorfo-0.9.52.tar.gz/polimorfo-0.9.52/polimorfo/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for polimorfo."""
+
+__author__ = """Fabio Fumarola"""
+__email__ = "fabiofumarola@gmail.com"
+__version__ = "__version__ = '0.9.52'"
+__cache_dir__ = "~/.polimorfo"
