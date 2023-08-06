aaebb76347b87bbdb85233a9a7b647552096213f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:01 +0000

    Add package swear-provention/0.0.7/swear_provention-0.0.7.tar.gz

---
+++ packages/swear-provention/swear_provention-0.0.7.tar.gz/swear_provention-0.0.7/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Ethan Brammah",
+    author_email="",
+    name='swear_provention',
+    license="MIT",
+    description='a library that removes swear words from strings',
+    version='v0.0.7',
+    long_description=README,
+    url='https://github.com/Dragon445/swear_provention',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    
+    
+)
