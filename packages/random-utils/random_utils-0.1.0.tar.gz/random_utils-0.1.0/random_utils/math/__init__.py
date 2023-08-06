48f4db7e8eeb7a0af4a3585c1c8b98852b2e31bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:41 +0000

    Add package random-utils/0.1.0/random_utils-0.1.0.tar.gz

---
+++ packages/random-utils/random_utils-0.1.0.tar.gz/random_utils-0.1.0/random_utils/math/__init__.py
@@ -0,0 +1,23 @@
+from functools import reduce
+from .vector import Vector
+
+
+def is_prime(num):
+    if num == 2 or num == 3: return True
+    if num < 2 or num % 2 == 0: return False
+    if num < 9: return True
+    if num % 3 == 0: return False
+    r = int(num**0.5)
+    f = 5
+    while f <= r:
+        if num % f == 0: return False
+        if num % (f+2) == 0: return False
+        f += 6
+    return True
+
+
+def find_factors(num, sort=True):
+    step = 2 if num % 2 else 1
+    factors = ([i, num // i] for i in range(1, int(num ** 0.5) + 1, step) if num % i == 0)
+    rv = set(reduce(list.__add__, factors))
+    return sorted(rv) if sort else rv
