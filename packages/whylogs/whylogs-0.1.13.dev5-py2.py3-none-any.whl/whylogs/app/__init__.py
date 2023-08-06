1929839f40909f9d051287ec72f7494259fe32ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:05 +0000

    Add package whylogs/0.1.13.dev5/whylogs-0.1.13.dev5-py2.py3-none-any.whl

---
+++ packages/whylogs/whylogs-0.1.13.dev5-py2.py3-none-any.whl/whylogs/app/__init__.py
@@ -0,0 +1,8 @@
+"""
+The whylogs client application API
+"""
+from .config import load_config
+from .session import SessionConfig
+from .writers import WriterConfig
+from .logger import Logger
+from .session import Session
---
+++ packages/whylogs/whylogs-0.1.13.dev5-py2.py3-none-any.whl/whylogs/core/statistics/__init__.py
@@ -0,0 +1,7 @@
+"""
+Define classes for tracking statistics
+"""
+from .counterstracker import CountersTracker
+from .numbertracker import NumberTracker
+from .schematracker import SchemaTracker
+from .thetasketch import ThetaSketch
