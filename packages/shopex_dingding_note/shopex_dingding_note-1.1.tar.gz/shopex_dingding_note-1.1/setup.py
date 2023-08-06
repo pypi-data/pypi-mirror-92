b6efabf2028784b404f2f5236847b029c87eabb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:46 +0000

    Add package shopex_dingding_note/1.1/shopex_dingding_note-1.1.tar.gz

---
+++ packages/shopex_dingding_note/shopex_dingding_note-1.1.tar.gz/shopex_dingding_note-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shopex_dingding_note",
+    version=1.1,
+    description="ShopEx dingding notify for python",
+    long_description_content_type=open("README.md").read(),
+    packages=find_packages(),
+    author="ShopEx",
+    author_email="xuhongtao@shopex.cn",
+    license="LGPL",
+    url="https://github.com/magic119/shopex_dingding_note.git",
+    platforms=["README.md"],
+    python_requires=">=3.7.0",
+)
