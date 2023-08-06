caccff3c5a61a70052ac9dbd73ec5d4734243a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:27 +0000

    Add package transpak/0.2.1/transpak-0.2.1.tar.gz

---
+++ packages/transpak/transpak-0.2.1.tar.gz/transpak-0.2.1/transpak/__init__.py
@@ -0,0 +1,6 @@
+""" Binary packaging and compression library for PQM-800 transient data """
+__version__ = "0.2.1"
+
+from .pak_lib import pack
+from .pak_lib import unpack
+from .pak_lib import unpack_file
