fb348d6ce68d303d156d7d3f7cff96f9c822f3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:04 +0000

    Add package machinevision-toolbox-python/0.5.2/machinevision-toolbox-python-0.5.2.tar.gz

---
+++ packages/machinevision-toolbox-python/machinevision-toolbox-python-0.5.2.tar.gz/machinevision-toolbox-python-0.5.2/machinevisiontoolbox/__init__.py
@@ -0,0 +1,5 @@
+# classes
+from machinevisiontoolbox.Image import Image
+from machinevisiontoolbox.blobs import Blob
+from machinevisiontoolbox.features2d import *
+from machinevisiontoolbox.Camera import *
---
+++ packages/machinevision-toolbox-python/machinevision-toolbox-python-0.5.2.tar.gz/machinevision-toolbox-python-0.5.2/machinevisiontoolbox/reshape.py
@@ -0,0 +1,13 @@
+    """
+    hcat
+    vcat
+    grid(shape=(), list or *pos)
+
+    scale(factor, width, height)
+    rotate
+    reduce(factor, width, height)
+    warp
+    affinemap
+
+    pad(halign="<^>", valign="^v-", align="<|>v^-, width=, height=)
+    """
