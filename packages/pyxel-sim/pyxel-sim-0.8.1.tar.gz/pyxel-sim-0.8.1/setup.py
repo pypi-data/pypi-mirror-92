15de2e63471f8d8a30c1826cab722f1100a987bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:36 +0000

    Add package pyxel-sim/0.8.1/pyxel-sim-0.8.1.tar.gz

---
+++ packages/pyxel-sim/pyxel-sim-0.8.1.tar.gz/pyxel-sim-0.8.1/setup.py
@@ -0,0 +1,10 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup
+
+setup()
