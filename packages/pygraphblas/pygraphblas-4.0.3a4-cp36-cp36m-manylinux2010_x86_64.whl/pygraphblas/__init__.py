ecd71bf4792927f1deeb344f196d94fa98d87a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:05 +0000

    Add package pygraphblas/4.0.3a4/pygraphblas-4.0.3a4-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/pygraphblas/pygraphblas-4.0.3a4-cp36-cp36m-manylinux2010_x86_64.whl/pygraphblas/__init__.py
@@ -0,0 +1,186 @@
+"""pygraphblas is a python extension that bridges [The GraphBLAS
+API](http://graphblas.org) with the [Python](https://python.org)
+programming language.  It uses the
+[CFFI](https://cffi.readthedocs.io/en/latest/) library to wrap the low
+level GraphBLAS API and provides high level `pygraphblas.matrix.Matrix` and
+`pygraphblas.Vector` Python types that make GraphBLAS simple and easy.
+
+See the [Github README](https://github.com/Graphegon/pygraphblas) for
+details on how to install pygraphblas. Once installed, the library can
+be imported for use:
+
+>>> from pygraphblas import *
+
+The core idea of the GraphBLAS is the mathematical duality between a
+graph and a `pygraphblas.matrix.Matrix`.  As illustrated here, a graph can be
+expressed as a `pygraphblas.matrix.Matrix` and vice versa.
+
+`pygraphblas.matrix.Matrix` is the primary object of The GraphBLAS API.
+There are many ways to contstruct them, but a simple approach is to
+provide three lists of data, the first are are lists of the row and
+column positions that define the begining and end of a graph edge, and
+the third list is the weight for that edge:
+
+>>> I = [0, 0, 1, 1, 2, 3, 3, 4, 5, 6, 6, 6]
+>>> J = [1, 3, 4, 6, 5, 0, 2, 5, 2, 2, 3, 4]
+>>> V = [True for _ in range(len(I))]
+>>> M = Matrix.from_lists(I, J, V)
+>>> print(M)
+      0  1  2  3  4  5  6
+  0|     t     t         |  0
+  1|              t     t|  1
+  2|                 t   |  2
+  3|  t     t            |  3
+  4|                 t   |  4
+  5|        t            |  5
+  6|        t  t  t      |  6
+      0  1  2  3  4  5  6
+>>> from pygraphblas.gviz import draw_graph
+>>> g = draw_graph(M, show_weight=False, 
+...     filename='/docs/imgs/Matrix_from_lists2')
+
+![Matrix_from_lists2.png](../imgs/Matrix_from_lists2.png)
+
+GraphBLAS is a sparse [Linear
+Algebra](https://en.wikipedia.org/wiki/Linear_algebra) API optimized
+for processing graphs encoded as sparse matrices and vectors.  In
+addition to common real/integer `pygraphblas.matrix.Matrix` algebra
+operations, GraphBLAS supports over a thousand different
+[Semiring](https://en.wikipedia.org/wiki/Semiring) algebra operations,
+that can be used as basic building blocks to implement a wide variety
+of graph algorithms. See
+[Applications](https://en.wikipedia.org/wiki/Semiring#Applications)
+from Wikipedia for some specific examples.
+
+The core operation of Linear Algebra is [Matrix
+Multiplication](https://en.wikipedia.org/wiki/Matrix_multiplication).
+In this GraphBLAS duality, this is an operation along the edges of a
+graph from nodes to their adjacenct neighbors, taking one step in a
+[Breadth First
+Search](https://en.wikipedia.org/wiki/Breadth-first_search) across the
+graph:
+
+
+
+pygraphblas leverages the expertise in the field of sparse matrix
+programming by [The GraphBLAS Forum](http://graphblas.org) and uses
+the
+[SuiteSparse:GraphBLAS](http://faculty.cse.tamu.edu/davis/GraphBLAS.html)
+API implementation. SuiteSparse:GraphBLAS is brought to us by the work
+of [Dr. Tim Davis](http://faculty.cse.tamu.edu/davis/welcome.html),
+professor in the Department of Computer Science and Engineering at
+Texas A&M University.  [News and
+information](http://faculty.cse.tamu.edu/davis/news.html) can provide
+you with a lot more background information.
+
+"""
+
+from .base import (
+    lib,
+    ffi,
+    GxB_INDEX_MAX,
+    GxB_IMPLEMENTATION,
+    GxB_SPEC,
+    options_get,
+    options_set,
+)
+
+lib.LAGraph_init()
+
+from .matrix import Matrix
+from .vector import Vector
+from .scalar import Scalar
+from .semiring import build_semirings
+from .binaryop import build_binaryops
+from .unaryop import build_unaryops
+from .monoid import build_monoids
+
+build_semirings()
+build_binaryops()
+build_unaryops()
+build_monoids()
+
+from .types import (
+    FP64,
+    FP32,
+    FC64,
+    FC32,
+    INT64,
+    INT32,
+    INT16,
+    INT8,
+    UINT64,
+    UINT32,
+    UINT16,
+    UINT8,
+    BOOL,
+)
+
+from . import semiring
+from . import binaryop
+from . import unaryop
+from . import monoid
+from . import descriptor
+from . import gviz
+
+__all__ = [
+    "lib",
+    "ffi",
+    "GxB_INDEX_MAX",
+    "GxB_IMPLEMENTATION",
+    "GxB_SPEC",
+    "options_set",
+    "options_get",
+    "Matrix",
+    "Vector",
+    "Scalar",
+    "FP64",
+    "FP32",
+    "FC64",
+    "FC32",
+    "INT64",
+    "INT32",
+    "INT16",
+    "INT8",
+    "UINT64",
+    "UINT32",
+    "UINT16",
+    "UINT8",
+    "BOOL",
+    "monoid",
+    "unaryop",
+    "binaryop",
+    "semiring",
+    "types",
+    "descriptor",
+    "gviz",
+]
+
+ffi = ffi  # global assign hack to fool pdoc
+""" CFFI C Foreign Function Interface.  For internal low-level use only. """
+
+lib = lib
+""" Raw interface to SuiteSparse library. For internal low-level use only.  """
+
+GxB_INDEX_MAX = GxB_INDEX_MAX
+"""Maximum key size for SuiteSparse, defaults to `2**60`."""
+
+GxB_IMPLEMENTATION = GxB_IMPLEMENTATION
+""" Tuple containing GxB_IMPLEMENTATION (MAJOR, MINOR, SUB) """
+
+GxB_SPEC = GxB_SPEC
+""" Tuple containing GxB_SPEC (MAJOR, MINOR, SUB) """
+
+
+def run_doctests():
+    import sys, doctest
+
+    this = sys.modules[__name__]
+    for mod in (this, matrix, base):
+        doctest.testmod(mod, optionflags=doctest.ELLIPSIS)
+
+
+__pdoc__ = {
+    "base": False,
+    "build": False,
+}
---
+++ packages/pygraphblas/pygraphblas-4.0.3a4-cp36-cp36m-manylinux2010_x86_64.whl/pygraphblas/monoid.py
@@ -0,0 +1,112 @@
+"""Contains all automatically generated Monoids from CFFI.
+
+The scalar addition of conventional matrix multiplication is replaced
+with a *monoid*.  A monoid is an associative and commutative binary
+operator `z=f(x,y)` where all three domains are the same (the
+types of `x`, `y`, and `z`), and where the operator has
+an identity value `id` such that `f(x,id)=f(id,x)=x`.
+Performing matrix multiplication with a semiring uses a monoid in
+place of the `add` operator, scalar addition being just one of many
+possible monoids.  The identity value of addition is zero, since
+$x+0=0+x=x$.  GraphBLAS includes many built-in operators suitable for
+use as a monoid: min (with an identity value of positive infinity),
+max (whose identity is negative infinity), add (identity is zero),
+multiply (with an identity of one), four logical operators: AND, OR,
+exclusive-OR, and Boolean equality (XNOR), four bitwise operators
+(AND, OR, XOR, and XNOR), and the ANY operator.  User-created monoids
+can be defined with any associative and commutative operator that has
+an identity value.
+"""
+
+__all__ = ["Monoid", "AutoMonoid", "current_monoid"]
+
+import os
+import sys
+import re
+import contextvars
+from itertools import chain
+from collections import defaultdict
+
+from .base import lib, ffi, _check
+from .binaryop import BinaryOp
+from . import types
+
+current_monoid = contextvars.ContextVar("current_monoid")
+
+
+class Monoid:
+
+    _auto_monoids = defaultdict(dict)
+
+    __slots__ = ("name", "monoid", "token", "op", "type")
+
+    def __init__(self, op, typ, monoid, udt=None, boolean=False):
+        self.monoid = monoid
+        self.__class__._auto_monoids[op + "_MONOID"][
+            types.Type.gb_from_name(typ)
+        ] = monoid
+        cls = getattr(types, typ, None)
+        if cls is not None:
+            setattr(cls, op + "_MONOID", self)
+        self.op = op
+        self.type = typ
+        self.name = "_".join((op, typ, "monoid"))
+        self.token = None
+
+    def __enter__(self):
+        self.token = current_monoid.set(self)
+        return self
+
+    def __exit__(self, *errors):
+        current_monoid.reset(self.token)
+        return False
+
+    def get_monoid(self, left=None, right=None):
+        return self.monoid
+
+
+class AutoMonoid(Monoid):
+    def __init__(self, name):
+        self.name = name
+        self.token = None
+
+    def get_monoid(self, left=None, right=None):
+        typ = types.promote(left, right)
+        return Monoid._auto_monoids[self.name][typ.gb_type]
+
+
+gxb_monoid_re = re.compile(
+    "^GxB_(MIN|MAX|PLUS|TIMES|ANY|BOR|BAND|BXOR|BXNOR)_"
+    "(UINT8|UINT16|UINT32|UINT64|INT8|INT16|INT32|INT64|FP32|FP64)_MONOID$"
+)
+
+grb_monoid_re = re.compile(
+    "^GrB_(MIN|MAX|PLUS|TIMES)_MONOID_"
+    "(UINT8|UINT16|UINT32|UINT64|INT8|INT16|INT32|INT64|FP32|FP64)$"
+)
+
+pure_bool_re = re.compile("^GxB_(ANY|LOR|LAND|LXOR|LXNOR|EQ)_(BOOL)_MONOID$")
+pure_bool_re_v13 = re.compile("^GrB_(LOR|LAND|LXOR|LXNOR)_MONOID_(BOOL)$")
+
+
+def monoid_group(reg):
+    srs = []
+    for n in filter(None, [reg.match(i) for i in dir(lib)]):
+        op, typ = n.groups()
+        m = Monoid(op, typ, getattr(lib, n.string))
+        srs.append(m)
+    return srs
+
+
+def build_monoids():
+    this = sys.modules[__name__]
+    for r in chain(
+        monoid_group(gxb_monoid_re),
+        monoid_group(grb_monoid_re),
+        monoid_group(pure_bool_re),
+        monoid_group(pure_bool_re_v13),
+    ):
+        setattr(this, r.name, r)
+    for name in Monoid._auto_monoids:
+        bo = AutoMonoid(name)
+        setattr(this, name, bo)
