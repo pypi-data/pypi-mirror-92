b363a8f1650dcdaf70edd3c89dbf00e26cc0215c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:21 +0000

    Add package biothings-explorer/0.0.1/biothings_explorer-0.0.1.tar.gz

---
+++ packages/biothings-explorer/biothings_explorer-0.0.1.tar.gz/biothings_explorer-0.0.1/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "jupyter",
+    "ipykernel",
+    "ipython",
+    "notebook",
+    "networkx==2.4",
+    "jsonpath-rw>=1.4.0",
+    "requests>=2.21.0",
+    "graphviz>=0.11.1",
+    "aiohttp",
+    "pandas",
+    "pyyaml",
+    "nest_asyncio",
+]
+
+
+setup(
+    name="biothings_explorer",
+    version="0.0.1",
+    author="Jiwen Xin, Chunlei Wu",
+    author_email="cwu@scripps.edu",
+    description="Python Client for BioThings Explorer",
+    license="BSD",
+    keywords="schema biothings",
+    url="https://github.com/biothings/biothings_explorer",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"": ["data/smartapi_local_specs.json"]},
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=install_requires,
+    dependency_links=["git+https://github.com/mmayers12/data_tools.git#egg=data_tools"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
