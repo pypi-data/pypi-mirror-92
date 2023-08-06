430e96d6b33eb9f7749e9a5773fb433125d29dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:18 +0000

    Add package hysteresis/1.0.1/hysteresis-1.0.1.tar.gz

---
+++ packages/hysteresis/hysteresis-1.0.1.tar.gz/hysteresis-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hysteresis",
+    version="1.0.1",
+    author="Christian Slotboom",
+    author_email="cslotboom@yahoo.com",
+    description="Postprocessing tools for OpenSeesPy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cslotboom/Hysteresis",
+    packages=setuptools.find_packages(),
+    # package_dir = {'': 'Hysteresis'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
