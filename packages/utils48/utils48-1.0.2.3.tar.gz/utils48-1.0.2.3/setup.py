297430d6ac3521392633eb597b32acaa6e93d9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:46 +0000

    Add package utils48/1.0.2.3/utils48-1.0.2.3.tar.gz

---
+++ packages/utils48/utils48-1.0.2.3.tar.gz/utils48-1.0.2.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'utils48',         # How you named your package folder (MyLib)
+  packages = ['utils48'],   # Chose the same as "name"
+  version = "1.0.2.3",      # Start with a small number and increase it with every change you make
+  description = 'some utilities',   # Give a short description about your library
+  author = '48panda',                   # Type in your name
+  url = 'https://github.com/48panda48/48util',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/48panda48/48util/archive/1.0.2.3.tar.gz',    # I explain this later on
+  )
