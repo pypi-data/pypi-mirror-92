14ca3d16ec12e44668606097bc32a97013d9f26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:37 +0000

    Add package oarepo-micro-api/3.3.37/oarepo-micro-api-3.3.37.tar.gz

---
+++ packages/oarepo-micro-api/oarepo-micro-api-3.3.37.tar.gz/oarepo-micro-api-3.3.37/oarepo_micro_api/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CESNET.
+#
+# OARepo Micro API is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""OARepo Micro API."""
+
+from __future__ import absolute_import, print_function
+
+from .version import __version__
+
+__all__ = ('__version__', )
