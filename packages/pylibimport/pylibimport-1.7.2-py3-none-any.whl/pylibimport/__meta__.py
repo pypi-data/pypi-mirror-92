2887078b9cea034c57f11f4c9d717440744f8027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:11 +0000

    Add package pylibimport/1.7.2/pylibimport-1.7.2-py3-none-any.whl

---
+++ packages/pylibimport/pylibimport-1.7.2-py3-none-any.whl/pylibimport/__meta__.py
@@ -0,0 +1,6 @@
+name = 'pylibimport'
+version = '1.7.2'
+description = 'Python utility for importing packages with the same name, but different version.'
+url = 'https://github.com/justengel/pylibimport'
+author = 'Justin Engel'
+author_email = 'jtengel08@gmail.com'
