99b4eba10ec9643c2135ee5147a5a1d51ed1e5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:31 +0000

    Add package truecaser/0.0.3/truecaser-0.0.3.tar.gz

---
+++ packages/truecaser/truecaser-0.0.3.tar.gz/truecaser-0.0.3/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+	name="truecaser",
+	version="0.0.3",
+	description="Predict and restore word casing",
+	py_modules=["truecaser"],
+	package_dir={"": "src"},
+	classifiers=[
+		'Programming Language :: Python :: 3',
+	]
+)
+
+
