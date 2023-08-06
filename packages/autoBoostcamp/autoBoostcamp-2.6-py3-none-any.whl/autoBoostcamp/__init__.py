a3038a6dafdcf818a33fa18933565a939323ea31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package autoBoostcamp/2.6/autoBoostcamp-2.6-py3-none-any.whl

---
+++ packages/autoBoostcamp/autoBoostcamp-2.6-py3-none-any.whl/autoBoostcamp/__init__.py
@@ -0,0 +1,17 @@
+__all__ = ["backend"]
+
+from .backend import doHi, doBye, quick_start
+import datetime
+
+def do():
+    now = datetime.datetime.now()
+    if now.weekday() in [5,6]: # 토요일, 일요일
+        return
+
+    now_hm = now.hour + 0.01 * now.minute
+    if 9.40 <= now_hm <= 10.5:  # 9시 40분 ~ 10시5분
+        doHi()
+        print(f"autoBoostcamp doHi run")
+    elif now_hm >= 19:  # 19시 ~
+        doBye()
+        print(f"autoBoostcamp doBye run")
