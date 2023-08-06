da9f4568fe216279239f71bf2de18888772fc6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:28 +0000

    Add package kitsune.py/0.0.1a0/kitsune.py-0.0.1a0.tar.gz

---
+++ packages/kitsune.py/kitsune.py-0.0.1a0.tar.gz/kitsune.py-0.0.1a0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+from kitsune import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kitsune.py",
+    version=__version__,
+    author="frissyn",
+    author_email="author@example.com",
+    description="A simple and extensible Python API wrapper for Kitsu.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frissyn/kitsune",
+    packages=["kitsune"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires='>=3.6',
+)
