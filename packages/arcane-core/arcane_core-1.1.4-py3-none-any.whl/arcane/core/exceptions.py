49b56cf572a616f9651a293191ed231a7550d6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:47 +0000

    Add package arcane-core/1.1.4/arcane_core-1.1.4-py3-none-any.whl

---
+++ packages/arcane-core/arcane_core-1.1.4-py3-none-any.whl/arcane/core/exceptions.py
@@ -0,0 +1,42 @@
+from google.cloud import exceptions as _exceptions
+import requests  # Include in google-cloud-core
+
+
+GOOGLE_EXCEPTIONS_TO_RETRY = (
+    _exceptions.InternalServerError,
+    _exceptions.ServerError,
+    _exceptions.ServiceUnavailable,
+    _exceptions.GatewayTimeout,
+    requests.exceptions.ConnectionError,  # Use to make http call in google's client
+    ConnectionResetError,  # not a Google exception but can happen
+    BrokenPipeError,
+    ConnectionAbortedError
+)
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class IssueSpreadsheetDatabase(Error):
+    """Raised when there is an error with the spreadsheets database"""
+    def __init__(self, reason: str, message: str, *args):
+        self.reason = reason
+        self.message = message
+        super().__init__(message, reason, *args)
+
+
+class GCPBucketNotFound(Exception):
+    def __init__(self, bucket_name: str, project: str = ""):
+        print(f"Bucket {bucket_name} don't exist in project {project} ")
+
+
+class BadRequestError(Error):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class NotFoundError(Error):
+    def __init__(self, message):
+        super().__init__(message)
