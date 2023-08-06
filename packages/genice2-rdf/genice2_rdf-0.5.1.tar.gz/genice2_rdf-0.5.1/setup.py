67f653d20a885925b9c80ef0a6cc362d34aafac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:38 +0000

    Add package genice2-rdf/0.5.1/genice2_rdf-0.5.1.tar.gz

---
+++ packages/genice2-rdf/genice2_rdf-0.5.1.tar.gz/genice2_rdf-0.5.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup, Extension
+from setuptools import setup, Extension
+import os
+import codecs
+import re
+
+#Copied from wheel package
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.dirname(__file__), 'genice2_rdf', '__init__.py'),
+                 encoding='utf8') as version_file:
+    metadata = dict(re.findall(r"""__([a-z]+)__ = "([^"]+)""", version_file.read()))
+
+
+
+long_desc = "".join(open("README.md").readlines())
+
+
+setup(
+    name='genice2_rdf',
+    version=metadata['version'],
+    description='RDF format plugin for GenIce.',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ],
+    author='Masakazu Matsumoto',
+    author_email='vitroid@gmail.com',
+    url='https://github.com/vitroid/genice-rdf/',
+    keywords=['genice', 'RDF'],
+
+    packages=['genice2_rdf',
+              'genice2_rdf.formats',
+    ],
+
+    entry_points = {
+        'genice2_format': [
+            '_RDF    = genice2_rdf.formats._RDF',
+        ],
+    },
+    install_requires=['PairList>=0.2.3', 'GenIce2'],
+
+    license='MIT',
+)
