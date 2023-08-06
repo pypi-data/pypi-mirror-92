392c27b66db35314ee2074a7ece8830e9e5334ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:17 +0000

    Add package commonly-used-distributions/0.2/commonly-used-distributions-0.2.tar.gz

---
+++ packages/commonly-used-distributions/commonly-used-distributions-0.2.tar.gz/commonly-used-distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='commonly-used-distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['commonly-used-distributions'],
+      author = 'Chris Wang',
+      author_email = 'chris.wang92688@gmail.com',
+      zip_safe=False)
