46250c673c503cdcdb4c81e981ff80ab3f27e43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:52 +0000

    Add package dicom-client-python/0.2.14.dev787/dicom-client-python-0.2.14.dev787.tar.gz

---
+++ packages/dicom-client-python/dicom-client-python-0.2.14.dev787.tar.gz/dicom-client-python-0.2.14.dev787/tests/conftest.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from os import environ
+
+def pytest_addoption(parser):
+    parser.addoption("--auth_connection_info", default=environ.get("AUTH_CONNECTION_INFO"))
