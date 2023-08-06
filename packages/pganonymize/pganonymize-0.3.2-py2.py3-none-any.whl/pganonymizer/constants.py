0eef593512b1a2b015c530350bd20ce6c8b8a150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:37 +0000

    Add package pganonymize/0.3.2/pganonymize-0.3.2-py2.py3-none-any.whl

---
+++ packages/pganonymize/pganonymize-0.3.2-py2.py3-none-any.whl/pganonymizer/constants.py
@@ -0,0 +1,11 @@
+# Database arguments used for the CLI
+DATABASE_ARGS = ('dbname', 'user', 'password', 'host', 'port')
+
+# Default name for the primary key column
+DEFAULT_PRIMARY_KEY = 'id'
+
+# Delimiter used to buffer and import database data.
+COPY_DB_DELIMITER = '\x1f'
+
+# Filename of the default schema
+DEFAULT_SCHEMA_FILE = 'schema.yml'
