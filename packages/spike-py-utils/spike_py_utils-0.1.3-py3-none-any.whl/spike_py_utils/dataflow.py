9a8942413924647ffad5cbb7af871d3affb01104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:23 +0000

    Add package spike-py-utils/0.1.3/spike_py_utils-0.1.3-py3-none-any.whl

---
+++ packages/spike-py-utils/spike_py_utils-0.1.3-py3-none-any.whl/spike_py_utils/dataflow.py
@@ -0,0 +1,27 @@
+import numpy as np
+import time
+from functools import wraps
+
+def dummy_f2():
+    return 2.0*np.random.rand()
+
+
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    def deco_retry(f):
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck as e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+            return f(*args, **kwargs)
+        return f_retry  # true decorator
+    return deco_retry
