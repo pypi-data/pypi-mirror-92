46b70bc3d8ed6a5b370d47dec4df02b761b037e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:00 +0000

    Add package widowx-env/0.0.2/widowx_env-0.0.2.tar.gz

---
+++ packages/widowx-env/widowx_env-0.0.2.tar.gz/widowx_env-0.0.2/README.md
@@ -0,0 +1,21 @@
+# widowx_env
+
+widowx_env is a series of OpenAI Gym reinforcement learning environments. 
+It simulates a robotic arm (WidowX MK-II) with the Pybullet engine. The goal is to bring the end-effector as close as possible to the target position (represented as a green sphere).
+
+
+# Installation
+ 
+## Pip installation
+
+```bash
+pip install widowx-env
+```
+
+## Development installation
+
+```bash
+git clone https://github.com/PierreExeter/widowx_env
+cd widowx_env
+pip install --e .
+```
---
+++ packages/widowx-env/widowx_env-0.0.2.tar.gz/widowx_env-0.0.2/widowx_env/__init__.py
@@ -0,0 +1,100 @@
+""" Register Gym environments """
+
+
+from gym.envs.registration import register
+
+
+register(
+    id='widowx_reacher-v1',
+    entry_point='widowx_env.envs.widowx_reacher_v1:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v2',
+    entry_point='widowx_env.envs.widowx_reacher_v2:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v3',
+    entry_point='widowx_env.envs.widowx_reacher_v3:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v4',
+    entry_point='widowx_env.envs.widowx_reacher_v4:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v5',
+    entry_point='widowx_env.envs.widowx_reacher_v5:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v6',
+    entry_point='widowx_env.envs.widowx_reacher_v6:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v7',
+    entry_point='widowx_env.envs.widowx_reacher_v7:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v8',
+    entry_point='widowx_env.envs.widowx_reacher_v8:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v9',
+    entry_point='widowx_env.envs.widowx_reacher_v9:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v10',
+    entry_point='widowx_env.envs.widowx_reacher_v10:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v11',
+    entry_point='widowx_env.envs.widowx_reacher_v11:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v12',
+    entry_point='widowx_env.envs.widowx_reacher_v12:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v13',
+    entry_point='widowx_env.envs.widowx_reacher_v13:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v14',
+    entry_point='widowx_env.envs.widowx_reacher_v14:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v15',
+    entry_point='widowx_env.envs.widowx_reacher_v15:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v16',
+    entry_point='widowx_env.envs.widowx_reacher_v16:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v17',
+    entry_point='widowx_env.envs.widowx_reacher_v17:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v18',
+    entry_point='widowx_env.envs.widowx_reacher_v18:WidowxEnv',
+    max_episode_steps=100)
+
+register(
+    id='widowx_reacher-v19',
+    entry_point='widowx_env.envs.widowx_reacher_v19:WidowxEnv',
+    max_episode_steps=100)
