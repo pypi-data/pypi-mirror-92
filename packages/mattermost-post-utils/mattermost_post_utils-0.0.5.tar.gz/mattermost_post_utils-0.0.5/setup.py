f2dc8f86dab92bead77893dc33f62dc43b0e9b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package mattermost-post-utils/0.0.5/mattermost_post_utils-0.0.5.tar.gz

---
+++ packages/mattermost-post-utils/mattermost_post_utils-0.0.5.tar.gz/mattermost_post_utils-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mattermost_post_utils",
+    version="0.0.5",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.tech.orange/dtrs-security/devops/mattermost-utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
