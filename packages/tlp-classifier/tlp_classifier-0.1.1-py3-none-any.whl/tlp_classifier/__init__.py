165aa82f8cfa24ccb98bafc3ce2401e656882564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:27 +0000

    Add package tlp-classifier/0.1.1/tlp_classifier-0.1.1-py3-none-any.whl

---
+++ packages/tlp-classifier/tlp_classifier-0.1.1-py3-none-any.whl/tlp_classifier/__init__.py
@@ -0,0 +1,10 @@
+from .file_help import *
+from .complexity import *
+from .two_labels_classifier import *
+from .problem_set import *
+from .problem import *
+from .classifier import *
+from .generator import *
+
+# Package version
+__version__ = '0.1.0'
