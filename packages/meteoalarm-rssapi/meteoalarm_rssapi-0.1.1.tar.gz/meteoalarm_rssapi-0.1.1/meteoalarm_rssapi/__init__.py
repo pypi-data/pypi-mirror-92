a2977c6047f813e4aeff113e4c1eb562711aff44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:44 +0000

    Add package meteoalarm-rssapi/0.1.1/meteoalarm_rssapi-0.1.1.tar.gz

---
+++ packages/meteoalarm-rssapi/meteoalarm_rssapi-0.1.1.tar.gz/meteoalarm_rssapi-0.1.1/meteoalarm_rssapi/__init__.py
@@ -0,0 +1,10 @@
+from .parser import (
+    MeteoAlarm,
+    MeteoAlarmException,
+    MeteoAlarmUnrecognizedCountryError,
+    MeteoAlarmUnrecognizedRegionError,
+    MeteoAlarmParseError,
+    MeteoAlarmServiceError,
+)
+
+__version__ = "0.1.1"
