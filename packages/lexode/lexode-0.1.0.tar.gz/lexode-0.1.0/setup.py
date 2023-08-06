fe0a6d7ad0c21d4667ff4eee1acd445aa2e7a54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:20 +0000

    Add package lexode/0.1.0/lexode-0.1.0.tar.gz

---
+++ packages/lexode/lexode-0.1.0.tar.gz/lexode-0.1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="lexode",
+    version="0.1.0",
+    author="Amirouche Boubekki",
+    author_email="amirouche@hypermove.net",
+    url="https://github.com/amirouche/python-lexode",
+    description="Explore relational data",
+    long_description=read("README.md"),
+    py_modules=['lexode'],
+    zip_safe=False,
+    license="MIT",
+    install_requires=[],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 3",
+    ],
+)
