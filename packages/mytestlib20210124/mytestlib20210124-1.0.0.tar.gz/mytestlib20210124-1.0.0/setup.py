7ad18128d5e32843fb2e8a444fabb9af44fde60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package mytestlib20210124/1.0.0/mytestlib20210124-1.0.0.tar.gz

---
+++ packages/mytestlib20210124/mytestlib20210124-1.0.0.tar.gz/mytestlib20210124-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='mytestlib20210124',
+   version='1.0.0',
+   url='https://www.google.co.jp/',
+   author='kiyosima00',
+   author_email='kiyosima00@test.net',
+   description='test',
+   packages=find_packages()
+)
