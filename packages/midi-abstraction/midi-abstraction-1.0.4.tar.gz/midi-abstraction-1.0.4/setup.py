d128207e2e91a64bf16106e0635695697a900c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:14 +0000

    Add package midi-abstraction/1.0.4/midi-abstraction-1.0.4.tar.gz

---
+++ packages/midi-abstraction/midi-abstraction-1.0.4.tar.gz/midi-abstraction-1.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r', encoding = 'utf-8') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = 'midi-abstraction',
+	version = '1.0.4',
+	author = 'Marco Silva',
+	author_email = 'cyberrumor@gmail.com',
+	description = 'Abstract MIDI pitches into keys, chords, modes, notes, and octaves.',
+	long_description_content_type = 'text/markdown',
+	long_description = long_description,
+	url = 'https://github.com/cyberrumor/midi_abstraction',
+	packages = ['midi_abstraction'],
+	classifiers = [
+		'Programming Language :: Python :: 3.9',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Operating System :: OS Independent',
+	],
+	python_requires = '>=3.9',
+)
+
