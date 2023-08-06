d2a0340017eb040e8a45a54079e329079b72339e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:38 +0000

    Add package CC-distributions/0.1/CC_distributions-0.1.tar.gz

---
+++ packages/CC-distributions/CC_distributions-0.1.tar.gz/CC_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='CC_distributions',
+      version='0.1',
+      description='Gaussian * Binomial distributions',
+      packages=['CC_distributions'],
+      author='Cooper Chastain',
+      zip_safe=False)
