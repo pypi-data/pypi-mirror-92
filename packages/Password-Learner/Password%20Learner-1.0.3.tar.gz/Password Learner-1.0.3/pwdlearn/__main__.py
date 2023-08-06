73a29f17c79102ff2c6d8aa59feac792e6b3a5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:19 +0000

    Add package Password-Learner/1.0.3/Password%20Learner-1.0.3.tar.gz

---
+++ packages/Password-Learner/Password%20Learner-1.0.3.tar.gz/Password Learner-1.0.3/pwdlearn/__main__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from pwdgen.password import Password
+from pwdgen import PasswordGenerator as PGen
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Learn passwords!")
+parser.add_argument("--new", action="store_true", dest="create_new")
+parser.add_argument("--allowed-chars", dest="allowed_chars", default=None)
+parser.add_argument(
+    "-e", "--exclude-ambigous", dest="exclude_ambigous_chars", action="store_true"
+)
+parser.add_argument("-l", "--length", dest="length", default=16)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if not args.create_new:
+        try:
+            pwd = Password.load()
+        except IOError:
+            print("Please, specify tag --new to create a new password.")
+            exit(1)
+    else:
+        gen = PGen(
+            args.length,
+            allowed_chars=args.allowed_chars,
+            exclude_ambigous_chars=args.exclude_ambigous_chars,
+        )
+        pwd = gen.generate()
+
+    pwd.learn()
