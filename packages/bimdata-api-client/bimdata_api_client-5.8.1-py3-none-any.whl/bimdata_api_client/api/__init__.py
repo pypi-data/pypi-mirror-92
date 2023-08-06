be2f193e6b3775f10eab737dd03aafcbb36104e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:33 +0000

    Add package bimdata-api-client/5.8.1/bimdata_api_client-5.8.1-py3-none-any.whl

---
+++ packages/bimdata-api-client/bimdata_api_client-5.8.1-py3-none-any.whl/bimdata_api_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from bimdata_api_client.api.bcf_api import BcfApi
+from bimdata_api_client.api.checker_api import CheckerApi
+from bimdata_api_client.api.collaboration_api import CollaborationApi
+from bimdata_api_client.api.ifc_api import IfcApi
+from bimdata_api_client.api.sso_api import SsoApi
+from bimdata_api_client.api.webhook_api import WebhookApi
