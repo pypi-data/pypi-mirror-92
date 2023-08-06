a81766fb093b29f0fc0e89fddfae6ba479a4cef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:13 +0000

    Add package datamaps/1.1.2/datamaps-1.1.2.tar.gz

---
+++ packages/datamaps/datamaps-1.1.2.tar.gz/datamaps-1.1.2/README.md
@@ -0,0 +1,13 @@
+![Tests](https://github.com/hammerheadlemon/datamaps/workflows/Tests/badge.svg)
+
+This software is a CLI UI layer for
+[bcompiler-engine](https://github.com/hammerheadlemon/bcompiler-engine).  It
+allows for easy access to adapters available within `bcompiler-engine` and is
+intended to be packaged using a packaging application such as `PyInstaller` and
+thus making `bcompiler-engine` palatable to non-technical users, and users who
+do not have access to a Python installation.
+
+
+Details, including links to the downloadable version of this software are be
+found at [Datamaps - Twenty Four
+Software](https://www.datamaps.twentyfoursoftware.com).
---
+++ packages/datamaps/datamaps-1.1.2.tar.gz/datamaps-1.1.2/datamaps/tests/conftest.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import tempfile
+
+from pathlib import Path
+
+import pytest
+
+from engine.config import Config
+
+
+@pytest.fixture
+def master() -> Path:
+    return Path.cwd() / "datamaps" / "tests" / "resources" / "master.xlsx"
+
+
+@pytest.fixture
+def temp_input_dir() ->  Path:
+    pth = tempfile.mkdtemp()
+    yield pth
+
+
+@pytest.fixture
+def mock_config(monkeypatch):
+    monkeypatch.setattr(Config, "PLATFORM_DOCS_DIR", Path("/tmp/Documents/datamaps"))
+    monkeypatch.setattr(
+        Config, "DATAMAPS_LIBRARY_DATA_DIR", Path("/tmp/.local/share/datamaps-data")
+    )
+    monkeypatch.setattr(
+        Config, "DATAMAPS_LIBRARY_CONFIG_DIR", Path("/tmp/.config/datamaps-data")
+    )
+    monkeypatch.setattr(
+        Config,
+        "DATAMAPS_LIBRARY_CONFIG_FILE",
+        Path("/tmp/.config/datamaps-data/config.ini"),
+    )
+    yield Config
+    try:
+        shutil.rmtree(Config.DATAMAPS_LIBRARY_DATA_DIR)
+        shutil.rmtree(Config.DATAMAPS_LIBRARY_CONFIG_DIR)
+        shutil.rmtree(Config.PLATFORM_DOCS_DIR)
+    except FileNotFoundError:
+        pass
+
+
+@pytest.fixture
+def resource_dir():
+    return Path.cwd() / "datamaps" / "tests" / "resources"
