1c915484da5f3b007a21b64cc8c1c2713c27029c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:48 +0000

    Add package exos/0.6/exos-0.6-py2-none-any.whl

---
+++ packages/exos/exos-0.6-py2-none-any.whl/exos/io.py
@@ -0,0 +1,47 @@
+"""
+stateful functions
+"""
+
+__author__ = "Bruno Lange"
+__email__ = "blangeram@gmail.com"
+__license__ = "MIT"
+
+
+def each(accept, iterable, *args, **kwargs):
+    """
+    Applies the accept function to each of the elements in the iterable
+    collection.
+    """
+    if isinstance(accept, str):
+        methods = [getattr(item, accept) for item in iterable]
+        _ = [method(*args, **kwargs) for method in methods]
+        return
+
+    unpack = kwargs.get("_unpack", False)
+    for item in iterable:
+        _args = (item,) if not unpack else item
+        accept(*_args)
+
+
+def ueach(accept, iterable, *args, **kwargs):
+    """
+    Unpacks elements in the collection before applying the accept function.
+    """
+    kwargs["_unpack"] = True
+    each(accept, iterable, *args, **kwargs)
+
+
+def print_each(xs, prefix=""):
+    """
+    Prints each element in the collection given by xs.
+    """
+    return ueach(
+        lambda i, x: print("{}{}".format(prefix.format(i=i), x)), enumerate(xs)
+    )
+
+
+def peach(xs, prefix=""):
+    """
+    A shortcut to print_each
+    """
+    return print_each(xs, prefix=prefix)
