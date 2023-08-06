d9bdccdbd600b4c2740b9d3aa9f93c601eb21744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:51 +0000

    Add package quickbuild/0.2.0/quickbuild-0.2.0-py3-none-any.whl

---
+++ packages/quickbuild/quickbuild-0.2.0-py3-none-any.whl/quickbuild/core.pyi
@@ -0,0 +1,18 @@
+import abc
+from abc import ABC, abstractmethod
+from collections import namedtuple
+from quickbuild.endpoints.builds import Builds as Builds
+from quickbuild.exceptions import QBError as QBError, QBNotFoundError as QBNotFoundError, QBProcessingError as QBProcessingError
+from typing import Any, Callable, Optional
+
+Response = namedtuple('Response', ['status', 'body'])
+ServerVersion: Any
+
+class QuickBuild(ABC, metaclass=abc.ABCMeta):
+    builds: Any = ...
+    def __init__(self) -> None: ...
+    @abstractmethod
+    def close(self) -> None: ...
+    @abstractmethod
+    def request(self, callback: Callable, method: str, path: str, fcb: Optional[Callable]=..., **kwargs: Any) -> Any: ...
+    def get_version(self) -> ServerVersion: ...
