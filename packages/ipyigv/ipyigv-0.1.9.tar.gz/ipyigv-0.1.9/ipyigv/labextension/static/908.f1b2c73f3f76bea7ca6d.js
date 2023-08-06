0e421688221fa5fac6c71cbe968505105d00de2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:27 +0000

    Add package ipyigv/0.1.9/ipyigv-0.1.9.tar.gz

---
+++ packages/ipyigv/ipyigv-0.1.9.tar.gz/ipyigv-0.1.9/ipyigv/labextension/static/908.f1b2c73f3f76bea7ca6d.js
@@ -0,0 +1,1 @@
+(self.webpackChunkjupyter_igv=self.webpackChunkjupyter_igv||[]).push([[908],{908:(e,t,r)=>{var i=r(138),u=r(335);e.exports={id:"jupyter-igv",requires:[u.IJupyterWidgetRegistry],activate:function(e,t){t.registerWidget({name:"jupyter-igv",version:i.version,exports:i})},autoStart:!0}}}]);
