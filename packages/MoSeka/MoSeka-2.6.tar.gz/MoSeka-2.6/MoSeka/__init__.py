aea290f355555997d16b6c43b3fa9b58e25109de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:09 +0000

    Add package MoSeka/2.6/MoSeka-2.6.tar.gz

---
+++ packages/MoSeka/MoSeka-2.6.tar.gz/MoSeka-2.6/MoSeka/__init__.py
@@ -0,0 +1,18 @@
+from Utils import*
+from functional import *
+from layers import *
+
+from activations import *
+from losses import Loss
+from net import *
+from evaluation_matrix import*
+
+
+from Activation_util import *
+from utils_func import *
+from Convolution_util import *
+from Pooling_util import *
+from RBF_initial_weight import *
+from LayerObjects import *
+
+__all__ = ["Utils","functional","layers","activations","losses","net","evaluation_matrix","Activation_util","utils_func","Convolution_util","Pooling_util","RBF_initial_weight","LayerObjects"]
---
+++ packages/MoSeka/MoSeka-2.6.tar.gz/MoSeka-2.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup_args = dict(
+name = 'MoSeka',  # How you named your package folder (MyLib)
+version = '2.6',  # Start with a small number and increase it with every change you make
+license = 'MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+description = 'Deep learning framework',  # Give a short description about your library
+author = 'Mohamed elsayed',  # Type in your name
+packages=find_packages(),
+author_email = 'mohamedelsayed16397@gmail.com',  # Type in your E-Mail
+url = 'https://github.com/Muhamedelsayed/MNiST',  # Provide either the link to your github or to your website
+download_url = 'https://github.com/Muhamedelsayed/MNiST/archive/2.6.tar.gz',  # I explain this later on
+keywords = ['Lenet5', 'Framework', 'MNIST'],  # Keywords that define your package best
+include_package_data=True
+)
+install_requires = [  # I get to this in a second
+                       'numpy',
+                       'pandas',
+                       'matplotlib',
+                   ],
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
