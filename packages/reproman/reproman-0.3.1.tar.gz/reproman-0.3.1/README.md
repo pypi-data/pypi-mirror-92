dd25570777d50295d454e4092e91984a27ea063b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:10 +0000

    Add package reproman/0.3.1/reproman-0.3.1.tar.gz

---
+++ packages/reproman/reproman-0.3.1.tar.gz/reproman-0.3.1/README.md
@@ -0,0 +1,81 @@
+# ReproMan
+
+[![Supports python version](https://img.shields.io/pypi/pyversions/datalad)](https://pypi.org/project/datalad/)
+[![GitHub release](https://img.shields.io/github/release/ReproNim/reproman.svg)](https://GitHub.com/ReproNim/reproman/releases/)
+[![PyPI version fury.io](https://badge.fury.io/py/reproman.svg)](https://pypi.python.org/pypi/reproman/)
+[![Travis tests status](https://secure.travis-ci.org/ReproNim/reproman.png?branch=master)](https://travis-ci.org/ReproNim/reproman)
+[![codecov.io](https://codecov.io/github/ReproNim/reproman/coverage.svg?branch=master)](https://codecov.io/github/ReproNim/reproman?branch=master)
+[![Documentation](https://readthedocs.org/projects/reproman/badge/?version=latest)](https://reproman.readthedocs.io/en/latest/?badge=latest)
+
+
+ReproMan aims to simplify creation and management of computing environments
+in Neuroimaging.  While concentrating on Neuroimaging use-cases, it is
+by no means is limited to this field of science and tools will find
+utility in other fields as well.
+
+# Status
+
+ReproMan is under rapid development. While
+the code base is still growing the focus is increasingly shifting towards
+robust and safe operation with a sensible API. There has been no major public
+release yet, as organization and configuration are still subject of
+considerable reorganization and standardization. 
+
+
+See [CONTRIBUTING.md](CONTRIBUTING.md) if you are interested in
+internals and/or contributing to the project.
+
+# Installation
+
+ReproMan requires Python 3 (>= 3.6).
+
+## Debian-based systems
+
+On Debian-based systems we recommend to enable [NeuroDebian](http://neuro.debian.net)
+from which we will soon provide recent releases of ReproMan (as soon as
+there is something to release).  We will also provide backports of
+all necessary packages from that repository.
+
+## Other Linux'es, OSX (Windows yet TODO) via pip
+
+By default, installation via pip installs core functionality of reproman
+allowing for managing datasets etc.  Additional installation schemes
+are available, so you could provide enhanced installation via
+`pip install reproman[SCHEME]` where `SCHEME` could be
+
+- tests
+     to also install dependencies used by unit-tests battery of the reproman
+- full
+     to install all of possible dependencies.
+
+For installation through `pip` you would need some external dependencies
+not shipped from it (e.g. `docker`, `singularity`, etc.) for which please refer to
+the next section.  
+
+## Dependencies
+
+Our `setup.py` and corresponding packaging describes all necessary dependencies.
+On Debian-based systems we recommend to enable [NeuroDebian](http://neuro.debian.net)
+since we use it to provide backports of recent fixed external modules we
+depend upon.  Additionally, if you would
+like to develop and run our tests battery see [CONTRIBUTING.md](CONTRIBUTING.md)
+regarding additional dependencies.
+
+Later we will provide bundled installations of ReproMan across popular
+platforms.
+
+
+# License
+
+MIT/Expat
+
+
+# Disclaimer
+
+It is in a beta stage -- majority of the functionality is usable but
+Documentation and API enhancements is WiP to make it better.  Please do not be
+shy of filing an issue or a pull request. See [CONTRIBUTING.md](CONTRIBUTING.md)
+for the guidance.
+
+[Git]: https://git-scm.com
+[Git-annex]: http://git-annex.branchable.com
---
+++ packages/reproman/reproman-0.3.1.tar.gz/reproman-0.3.1/setup.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the ReproMan package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+import platform
+
+from os.path import sep as pathsep
+from os.path import join as opj
+from os.path import splitext
+from os.path import dirname
+
+from setuptools import findall
+from setuptools import setup, find_packages
+
+# manpage build imports
+from setup_support import BuildManPage
+from setup_support import BuildRSTExamplesFromScripts
+# from setup_support import BuildConfigInfo
+from setup_support import get_version
+
+
+def findsome(subdir, extensions):
+    """Find files under subdir having specified extensions
+
+    Leading directory (reproman) gets stripped
+    """
+    return [
+        f.split(pathsep, 1)[1] for f in findall(opj('reproman', subdir))
+        if splitext(f)[-1].lstrip('.') in extensions
+    ]
+
+# reproman version to be installed
+version = get_version()
+
+# Only recentish versions of find_packages support include
+# reproman_pkgs = find_packages('.', include=['reproman*'])
+# so we will filter manually for maximal compatibility
+reproman_pkgs = [pkg for pkg in find_packages('.') if pkg.startswith('reproman')]
+
+requires = {
+    'core': [
+        'appdirs',
+        'attrs>=16.3.0',
+        'etelemetry>=0.2.0',
+        'humanize',
+        'pyyaml',
+        'tqdm',
+        'fabric>=2.3.1',
+        'cryptography>=2.5',
+        'pytz',
+        'scp',
+        'pycrypto',
+        'pyOpenSSL==16.2.0',
+        'requests',
+        'reprozip; sys_platform=="linux" or sys_platform=="linux2"',
+        'rpaths',
+        'jinja2',
+    ],
+    'debian': [
+        'python-debian',
+        # on Debian systems will come via apt
+        #'python-apt>=0.8.9.1',
+        # Origin.codename was introduced in 0.8.9
+        # unfortunately not on pypi but needed by python-apt
+        #'http://archive.ubuntu.com/ubuntu/pool/main/p/python-apt/python-apt_0.9.3.5.tar.xz'
+        'chardet',  # python-debian misses dependency on it
+    ],
+    'datalad': [
+        # Drop the rc as soon as we can to avoid installing future
+        # rcs. See https://github.com/datalad/datalad-container/pull/74.
+        'datalad>=0.13.0rc2',
+        'datalad-container',
+    ],
+    'docker': [
+        'docker>=3.0.0',
+        'dockerpty',
+    ],
+    'aws': [
+        'boto3',
+    ],
+    'meta': [
+        'rdflib',
+    ],
+    'tests': [
+        'pytest>=3.3.0',
+    ]
+}
+
+# Target to ease testing installation.
+requires['full-except-datalad'] = [v for k, vs in requires.items()
+                                   for v in vs if k != "datalad"]
+requires['full'] = sum(list(requires.values()), [])
+
+
+# Now add additional ones useful for development
+requires.update({
+    'devel-docs': [
+        # used for converting README.md -> .rst for long_description
+        #  TODO: enable whenever we are ready! otherwise requires installation
+        #        of pandoc first via apt-get
+        # 'pypandoc',
+        # Documentation
+        'sphinx',
+        'sphinx-rtd-theme',
+    ],
+    'devel-utils': [
+        'line-profiler',
+        # necessary for accessing SecretStorage keyring (system wide Gnome
+        # keyring)  but not installable on travis, IIRC since it needs connectivity
+        # to the dbus whenever installed or smth like that, thus disabled here
+        # but you might need it
+        # 'dbus-python',
+    ],
+    'devel-neuroimaging': [
+        # Specifically needed for tests here (e.g. example scripts testing)
+        'nibabel',
+    ]
+})
+requires['devel'] = sum(list(requires.values()), [])
+
+
+# let's not build manpages and examples automatically (gh-896)
+# configure additional command for custom build steps
+#class DataladBuild(build_py):
+#    def run(self):
+#        self.run_command('build_manpage')
+#        self.run_command('build_examples')
+#        build_py.run(self)
+
+cmdclass = {
+    'build_manpage': BuildManPage,
+    # 'build_examples': BuildRSTExamplesFromScripts,
+    # 'build_cfginfo': BuildConfigInfo,
+    # 'build_py': DataladBuild
+}
+
+# PyPI doesn't render markdown yet. Workaround for a sane appearance
+# https://github.com/pypa/pypi-legacy/issues/148#issuecomment-227757822
+README = opj(dirname(__file__), 'README.md')
+try:
+    import pypandoc
+    long_description = pypandoc.convert(README, 'rst')
+except ImportError:
+    long_description = open(README).read()
+
+setup(
+    name="reproman",
+    author="The ReproMan Team and Contributors",
+    author_email="team@reproman.org",
+    version=version,
+    description="Neuroimaging Computational Environments Manager",
+    long_description=long_description,
+    packages=reproman_pkgs,
+    python_requires='>=3.6',
+    install_requires=requires['core'],
+    extras_require=requires,
+    entry_points={
+        'console_scripts': [
+            'reproman=reproman.cmdline.main:main',
+        ],
+    },
+    cmdclass=cmdclass,
+    package_data={
+        'reproman':
+            findsome(opj("distributions", "tests", "files"), {"yml", "yaml"}) +
+            findsome("examples", {"trig", "yml", "yaml"}) +
+            findsome(opj("formats", "tests", "files"), {"yml", "yaml"}) +
+            findsome(opj("interface"), {"shim"}) +
+            findsome(opj("interface", "tests"), {"yml", "yaml"}) +
+            findsome(opj("interface", "tests", "files"), {"yml", "yaml"}) +
+            findsome(opj("support", "jobs", "job_templates", "runscript"),
+                     {"sh"}) +
+            findsome(opj("support", "jobs", "job_templates", "runscript", "includes"),
+                     {"sh"}) +
+            findsome(opj("support", "jobs", "job_templates", "submission"),
+                     {"template"}) +
+            findsome(opj("support", "jobs", "job_templates", "cluster"),
+                     {"template"}) +
+            findsome(opj("tests", "files"), {"cfg"})
+    }
+)
