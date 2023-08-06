8eaa8823a318dd5e7844244697dcd9923f7f6231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:14 +0000

    Add package coopstarter-data/0.2.8/coopstarter_data-0.2.8-py3-none-any.whl

---
+++ packages/coopstarter-data/coopstarter_data-0.2.8-py3-none-any.whl/coopstarter_data/translation.py
@@ -0,0 +1,40 @@
+from modeltranslation.translator import register, translator, TranslationOptions
+from .models import Resource, Type, Format, LearningOutcome, Step, Field, SharingCriteria, Target
+
+# A translation option is a class that declares which fields of a model to translate
+# registering it with the translator creates duplicate columns for each language, in the database
+# (https://django-modeltranslation.readthedocs.io/en/latest/registration.html#changes-automatically-applied-to-the-model-class)
+
+class ResourceTranslationOptions(TranslationOptions):
+    fields = ('description', 'name')
+    # required_languages = ('en', 'fr')
+    # required_languages = {'de': ('title', 'text'), 'default': ('title',)} - fine-grained control
+
+@register(Type)
+class TypeTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(Field)
+class FieldTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(SharingCriteria)
+class SharingCriteriaTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(Format)
+class FormatTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(Target)
+class TargetTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(Step)
+class StepTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+@register(LearningOutcome)
+class LearningOutcomeTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
