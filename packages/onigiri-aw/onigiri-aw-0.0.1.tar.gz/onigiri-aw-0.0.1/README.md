d499aff8986dbc7390d6653dffc321e449797319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:47 +0000

    Add package onigiri-aw/0.0.1/onigiri-aw-0.0.1.tar.gz

---
+++ packages/onigiri-aw/onigiri-aw-0.0.1.tar.gz/onigiri-aw-0.0.1/README.md
@@ -0,0 +1,11 @@
+# 
+python sdk for 
+
+https://pypi.org/account/register/
+验证邮箱
+https://pypi.org/manage/account/
+Add API token
+
+pypi-AgEIcHlwaS5vcmcCJGIyNzYzMTdhLTM5ZDgtNGQzNC04NzMxLTJkODNiZjE3ZGE1OAACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgCXWQek40y9QA8FXHncUXsq6XIS8vM1jghyYMrLfPPAM
+python3 setup.py sdist bdist_wheel
+twine upload dist/*
---
+++ packages/onigiri-aw/onigiri-aw-0.0.1.tar.gz/onigiri-aw-0.0.1/setup.py
@@ -0,0 +1,66 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# setuptools.setup(
+#     name="yinhua-airworks",
+#     version="0.0.1",
+#     scripts=[],
+#     author="valuesimplex",
+#     description="yinhua-airworks-api sdk",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     packages=setuptools.find_packages(),
+#     python_requires=">=3.5",
+#     classifiers=[
+#         # 发展时期,常见的如下
+#         #   3 - Alpha
+#         #   4 - Beta
+#         #   5 - Production/Stable
+#         'Development Status :: 3 - Alpha',
+#         # 开发的目标用户
+#         'Intended Audience :: Developers',
+#         # 属于什么类型
+#         'Topic :: Software Development :: Build Tools',
+#         # 许可证信息
+#         'License :: OSI Approved :: MIT License',
+#         # 目标 Python 版本
+#         'Programming Language :: Python :: 3',
+#     ],
+#     install_requires=[
+#         'pandas',
+#         'requests'
+#     ]
+# )
+
+setuptools.setup(
+    name="onigiri-aw",
+    version="0.0.1",
+    scripts=[],
+    author="onigiri",
+    description="onigiri-api sdk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    classifiers=[
+        # 发展时期,常见的如下
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        # 开发的目标用户
+        'Intended Audience :: Developers',
+        # 属于什么类型
+        'Topic :: Software Development :: Build Tools',
+        # 许可证信息
+        'License :: OSI Approved :: MIT License',
+        # 目标 Python 版本
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'pandas',
+        'requests'
+    ]
+)
