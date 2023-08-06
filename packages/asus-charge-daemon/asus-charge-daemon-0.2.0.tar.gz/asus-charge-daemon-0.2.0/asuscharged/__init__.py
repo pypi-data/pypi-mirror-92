9bc7402c96d4f0867940c2101285e4ba7e0d3e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:46 +0000

    Add package asus-charge-daemon/0.2.0/asus-charge-daemon-0.2.0.tar.gz

---
+++ packages/asus-charge-daemon/asus-charge-daemon-0.2.0.tar.gz/asus-charge-daemon-0.2.0/asuscharged/__init__.py
@@ -0,0 +1,22 @@
+"""A daemon exposing D-Bus services for managing the charge level of recent
+ASUS notebooks.
+"""
+
+import logging
+import sys
+
+__version__ = "0.2.0"
+
+APP_NAME = "asuscharged"
+PACKAGE_NAME = "asus-charge-daemon"
+DBUS_NAME = "ca.cforrester.AsusChargeDaemon1"
+DBUS_PATH = "/ca/cforrester/AsusChargeDaemon1"
+
+if __debug__:
+    log_level = logging.DEBUG
+    log_format = "%(asctime)s [%(levelname)s] %(module)s(%(lineno)d) - %(message)s"
+else:
+    log_level = logging.WARNING
+    log_format = "[%(levelname)s] %(message)s"
+
+logging.basicConfig(stream=sys.stdout, level=log_level, format=log_format)
