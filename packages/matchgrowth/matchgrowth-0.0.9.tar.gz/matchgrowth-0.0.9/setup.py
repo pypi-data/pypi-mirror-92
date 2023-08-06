a313e9c95fa1e70332c0cc14f238e7c82c5927c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:15 +0000

    Add package matchgrowth/0.0.9/matchgrowth-0.0.9.tar.gz

---
+++ packages/matchgrowth/matchgrowth-0.0.9.tar.gz/matchgrowth-0.0.9/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="matchgrowth",
+    version="0.0.9",
+    author="Stefan Corneliu Petrea",
+    author_email="stefan@garage-coding.com",
+    description="Tool for estimating growth rates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://blog.garage-coding.com/",
+    packages=setuptools.find_packages(),
+    scripts=["match-growth.py"],
+    install_requires=[
+          'numpy>=1.19.4',
+          'scipy>=1.5.4',
+          'matplotlib>=3.3.3',
+          'sympy>=1.7.1',
+          'similaritymeasures>=0.4.4',
+    ],
+    extras_require={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
