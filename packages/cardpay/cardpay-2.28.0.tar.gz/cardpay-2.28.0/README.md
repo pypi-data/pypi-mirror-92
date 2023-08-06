900cbe1a6b390f26c9e5e424a3e7d91ce857ff12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:13 +0000

    Add package cardpay/2.28.0/cardpay-2.28.0.tar.gz

---
+++ packages/cardpay/cardpay-2.28.0.tar.gz/cardpay-2.28.0/README.md
@@ -0,0 +1,44 @@
+# Unlimint APIv3 Python SDK
+
+You can sign up for a Unlimint account at https://www.unlimint.com.
+
+## Getting Started
+
+Please follow the [installation](#installation) instruction and take a look at [usage examples](tests).
+
+
+## Requirements
+
+Python 3+
+
+## Installation & Usage
+### pip install
+
+If the python package is hosted on Github, you can install directly from Github
+
+```sh
+pip install git+https://github.com/cardpay/python-sdk-v3.git --upgrade
+```
+or
+
+```sh
+pip install 'cardpay>=2.28.0' --upgrade
+```
+
+Then import the package:
+```python
+from cardpay import *
+```
+
+### Setuptools
+
+Install via [Setuptools](http://pypi.python.org/pypi/setuptools).
+
+```sh
+python setup.py install --user
+```
+
+Then import the package:
+```python
+from cardpay import *
+```
---
+++ packages/cardpay/cardpay-2.28.0.tar.gz/cardpay-2.28.0/setup.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+import os
+from codecs import open
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+os.chdir(here)
+
+with open(os.path.join(here, "README.md"), "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setup(
+    name="cardpay",
+    version="2.28.0",
+    description="Unlimint APIv3 Python SDK",
+    author_email="",
+    url="https://github.com/cardpay/python-sdk-v3.git",
+    license="MIT",
+    keywords=["cardpay", "APIv3", "CardPay REST API"],
+    install_requires=[
+        "certifi>=2019.11.28",
+        "python-dateutil>=2.8.0",
+        "six>=1.14.0",
+        "urllib3>=1.25.8"
+    ],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    project_urls={
+        "Documentation": "https://integration.unlimint.com/",
+        "Source Code": "https://github.com/cardpay/python-sdk-v3",
+    }
+)
