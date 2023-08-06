87aa1bc26cff128e8a54132f5028ca1f8fca2bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:05 +0000

    Add package scikit-gstat/0.3.2/scikit-gstat-0.3.2.tar.gz

---
+++ packages/scikit-gstat/scikit-gstat-0.3.2.tar.gz/scikit-gstat-0.3.2/skgstat/__init__.py
@@ -0,0 +1,9 @@
+from .Variogram import Variogram
+from .DirectionalVariogram import DirectionalVariogram
+from .SpaceTimeVariogram import SpaceTimeVariogram
+from .Kriging import OrdinaryKriging
+from . import interfaces
+
+# set some stuff
+__version__ = '0.3.2'
+__author__ = 'Mirko Maelicke <mirko.maelicke@kit.edu>'
