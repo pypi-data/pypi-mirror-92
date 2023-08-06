fe7c85f00fc1b9b220527e9c184e8fe7535ad91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:37 +0000

    Add package staff-graded-xblock/1.5/staff_graded-xblock-1.5.tar.gz

---
+++ packages/staff-graded-xblock/staff_graded-xblock-1.5.tar.gz/staff_graded-xblock-1.5/setup.py
@@ -0,0 +1,72 @@
+"""Setup for Staff Graded XBlock."""
+
+import os
+from setuptools import setup
+
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+setup(
+    name='staff_graded-xblock',
+    version='1.5',
+    description='Staff Graded XBlock',   # TODO: write a better description.
+    license='AGPL v3',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+    ],
+    packages=[
+        'staff_graded',
+    ],
+    install_requires=load_requirements('requirements/base.in'),
+    entry_points={
+        'xblock.v1': [
+            'staffgradedxblock = staff_graded:StaffGradedXBlock',
+        ]
+    },
+    package_data=package_data("staff_graded", ["static", "public"]),
+)
