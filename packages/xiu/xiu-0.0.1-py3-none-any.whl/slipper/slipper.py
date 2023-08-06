9ced72e1a755701d1f05f254967ec6d17960e60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:58 +0000

    Add package xiu/0.0.1/xiu-0.0.1-py3-none-any.whl

---
+++ packages/xiu/xiu-0.0.1-py3-none-any.whl/slipper/slipper.py
@@ -0,0 +1,47 @@
+import os, sys, json
+import signal
+from typing import List
+from pprint import pprint
+
+
+# =========================================================== display
+def p(s):
+    print(s)
+
+
+def pp(d: dict):
+    pprint(d)
+
+
+# =========================================================== IO
+def jsonread(file_name: str) -> dict:
+    res = {}
+    with open(file_name, 'r') as f:
+        res = json.loads(f.read())
+    return res
+
+
+def jsonwrite(d: dict, file_name: str):
+    json.dump(d, open(file_name, 'w'), indent=2)
+
+
+# =========================================================== Decorator
+def set_timeout(countdown: int, callback=print):
+    def decorator(func):
+        def handle(signum, frame):
+            raise RuntimeError
+
+        def wrapper(*args, **kwargs):
+            try:
+                signal.signal(signal.SIGALRM, handle)
+                signal.alarm(countdown)  # set countdown
+                r = func(*args, **kwargs)
+                signal.alarm(0)  # close alarm
+                return r
+            except RuntimeError as e:
+                print(e)
+                callback()
+
+        return wrapper
+
+    return decorator
