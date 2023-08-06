e5226606c01f24aff187c97c3a719fd7634bf1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:57 +0000

    Add package sic/1.3.2/sic-1.3.2.tar.gz

---
+++ packages/sic/sic-1.3.2.tar.gz/sic-1.3.2/setup.py
@@ -0,0 +1,36 @@
+import sys
+from setuptools import setup
+
+ext_modules = None
+with open('README.md', mode='r', encoding='utf8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='sic',
+    version='1.3.2',
+    description='Utility for string normalization',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pgolo/sic',
+    author='Pavel Golovatenko-Abramov',
+    author_email='p.golovatenko@gmail.com',
+    packages=['sic'],
+    ext_modules=ext_modules,
+    include_package_data=True,
+    license='MIT',
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Linguistic',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
