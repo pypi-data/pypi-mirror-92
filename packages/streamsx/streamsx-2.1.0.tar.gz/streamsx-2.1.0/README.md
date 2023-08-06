031b592bfae71c8f88cde8561337a9dd335b3920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:37 +0000

    Add package streamsx/2.1.0/streamsx-2.1.0.tar.gz

---
+++ packages/streamsx/streamsx-2.1.0.tar.gz/streamsx-2.1.0/README.md
@@ -0,0 +1,14 @@
+# pypi.streamsx
+This is a step in allowing natural use of Streams for a Python developer.
+
+A project that will be registered with PyPi to allow 'pip install' of Python packages that support Python developers interacting with IBM Streams. 'pip install' is the standard mechanism for Python developers to download code.
+(See http://peterdowns.com/posts/first-time-with-pypi.html)
+
+Project supports:
+
+    Python Application API (from streamsx.topology)
+    Python Rest API (from streamsx.topology)
+
+Project would maintain copies of the files from other IBMStreams projects (or their releases), the main versions would remain in the source projects.
+
+A single PyPi package for IBM Streams Python code is used rather than multiple to simplify setup for Python developers.
---
+++ packages/streamsx/streamsx-2.1.0.tar.gz/streamsx-2.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import streamsx._streams._version
+version = streamsx._streams._version.__version__
+setup(
+  name = 'streamsx',
+  packages = ['streamsx', 'streamsx.spl', 'streamsx.topology', 'streamsx.scripts', 'streamsx._streams'],
+  include_package_data=True,
+  version = version,
+  description = 'IBM Streams Python Support',
+  long_description = open('DESC.txt').read(),
+  author = 'IBM Streams @ github.com',
+  author_email = 'hegermar@de.ibm.com',
+  license='Apache License - Version 2.0',
+  url = 'https://github.com/IBMStreams/pypi.streamsx',
+  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/' + version,
+  keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics'],
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+  install_requires=['requests', 'future', 'dill>=0.2.8.2,<0.3.1'],
+  entry_points = {
+        'console_scripts': [
+            'streamsx-runner=streamsx.scripts.runner:main',
+            'streams-service=streamsx.scripts.service:main',
+            'streamsx-service=streamsx.scripts.service:main',
+            'streamsx-info=streamsx.scripts.info:main',
+            'streamsx-streamtool=streamsx.scripts.streamtool:main',
+            'streamsx-sc=streamsx.scripts.sc:main',
+            'spl-python-extract=streamsx.scripts.extract:main'
+        ],
+  },
+)
