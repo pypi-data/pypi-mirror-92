f0d83edb2c2f9605a2d8efcff1d2c1afb53ed4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:18 +0000

    Add package vtt2text/0.0.9/vtt2text-0.0.9.tar.gz

---
+++ packages/vtt2text/vtt2text-0.0.9.tar.gz/vtt2text-0.0.9/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Author: anh-tuan.vu
+# @Date:   2021-01-27 10:29:59
+# @Last Modified by:   anh-tuan.vu
+# @Last Modified time: 2021-01-27 15:00:44
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "vtt2text",
+    version = "0.0.9",
+    author = "VU Anh Tuan",
+    author_email = "vuanhtuan1012@outlook.com",
+    description = "A small package to clean up the content of a subtitle file to text",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/vuanhtuan1012/vtt2text",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+)
