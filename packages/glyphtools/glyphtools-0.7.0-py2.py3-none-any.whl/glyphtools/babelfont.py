d6e0c5a9d0c3d787dd738ca872aa5940e1d33d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:14 +0000

    Add package glyphtools/0.7.0/glyphtools-0.7.0-py2.py3-none-any.whl

---
+++ packages/glyphtools/glyphtools-0.7.0-py2.py3-none-any.whl/glyphtools/babelfont.py
@@ -0,0 +1,33 @@
+def isbabelfont(font):
+    return hasattr(font, "_set_kerning")
+
+
+def get_glyph_metrics(font, glyphname):
+    g = font[glyphname]
+    metrics = {"width": g.width, "lsb": g.leftMargin, "rsb": g.rightMargin}
+    bounds = g.bounds
+    if bounds:
+        (metrics["xMin"], metrics["yMin"], metrics["xMax"], metrics["yMax"]) = g.bounds
+    else:
+        (metrics["xMin"], metrics["yMin"], metrics["xMax"], metrics["yMax"]) = (0,0,0,0)
+    metrics["rise"] = get_rise(g)
+    metrics["run"] = get_run(g)
+    metrics["fullwidth"] = metrics["xMax"] - metrics["xMin"]
+    return metrics
+
+
+def get_rise(glyph):
+    entry = [a.y for a in glyph.anchors if a.name == "entry"]
+    entry.append(0)  # In case there isn't one
+    exit = [a.y for a in glyph.anchors if a.name == "exit"]
+    exit.append(0)
+
+    return entry[0] - exit[0]
+
+def get_run(glyph):
+    entry = [a.x for a in glyph.anchors if a.name == "entry"]
+    entry.append(glyph.width)  # In case there isn't one
+    exit = [a.x for a in glyph.anchors if a.name == "exit"]
+    exit.append(0)
+
+    return entry[0] - exit[0]
