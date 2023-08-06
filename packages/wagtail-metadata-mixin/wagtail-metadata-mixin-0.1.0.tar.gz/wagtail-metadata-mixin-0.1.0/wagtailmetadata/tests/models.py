7ee8927070b53ca96fef5ff39e36bd7e3ec26f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:03 +0000

    Add package wagtail-metadata-mixin/0.1.0/wagtail-metadata-mixin-0.1.0.tar.gz

---
+++ packages/wagtail-metadata-mixin/wagtail-metadata-mixin-0.1.0.tar.gz/wagtail-metadata-mixin-0.1.0/wagtailmetadata/tests/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from wagtail.core.models import Page
+from wagtailmetadata.models import MetadataPageMixin
+
+
+class SimplePage(MetadataPageMixin, Page):
+    promote_panels = Page.promote_panels + MetadataPageMixin.panels
