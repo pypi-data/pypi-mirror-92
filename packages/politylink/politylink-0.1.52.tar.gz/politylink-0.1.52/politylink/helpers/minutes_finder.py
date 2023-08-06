86a313336ffb4207d6deea85ec83b024bc260b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:20 +0000

    Add package politylink/0.1.52/politylink-0.1.52.tar.gz

---
+++ packages/politylink/politylink-0.1.52.tar.gz/politylink-0.1.52/politylink/helpers/minutes_finder.py
@@ -0,0 +1,28 @@
+from sgqlc.operation import Operation
+
+from politylink.graphql.client import GraphQLClient
+from politylink.graphql.schema import Query, _MinutesFilter, _Neo4jDateTimeInput
+
+
+class MinutesFinder:
+    """
+    GraphQL based Minutes finder
+    """
+
+    def __init__(self, **kwargs):
+        self.client = GraphQLClient(**kwargs)
+
+    def find(self, text, dt=None):
+        op = Operation(Query)
+        minutes_filter = _MinutesFilter(None)
+        minutes_filter.name_contains = text
+        if dt:
+            minutes_filter.start_date_time = _Neo4jDateTimeInput(year=dt.year, month=dt.month, day=dt.day)
+        minutes = op.minutes(filter=minutes_filter)
+        minutes.id()
+        minutes.name()
+
+        data = self.client.endpoint(op)
+        GraphQLClient.validate_response_or_raise(data)
+        minutes = (op + data).minutes
+        return minutes
