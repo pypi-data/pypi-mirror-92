4a73ac47264c7142678ed8b0e72b713d38322716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:55 +0000

    Add package drf-openapi-tester/0.1.0/drf-openapi-tester-0.1.0.tar.gz

---
+++ packages/drf-openapi-tester/drf-openapi-tester-0.1.0.tar.gz/drf-openapi-tester-0.1.0/openapi_tester/case_testers.py
@@ -0,0 +1,34 @@
+from typing import Any, Callable
+
+from inflection import camelize, dasherize, underscore
+
+from openapi_tester.exceptions import CaseError
+
+
+def _create_tester(casing: str, handler: Callable[[Any], str]) -> Callable[[str], None]:
+    """ factory function for creating testers """
+
+    def tester(key: str) -> None:
+        stripped = key.strip()
+        if len(stripped) and not handler(stripped) == stripped:
+            raise CaseError(key=key, case=casing, expected=handler(key))
+
+    return tester
+
+
+def _camelize(s: str) -> str:
+    return camelize(underscore(s), False)
+
+
+def _pascalize(s: str) -> str:
+    return camelize(underscore(s))
+
+
+def _kebabize(s: str) -> str:
+    return dasherize(underscore(s))
+
+
+is_camel_case = _create_tester("camelCased", _camelize)
+is_kebab_case = _create_tester("kebab-cased", _kebabize)
+is_pascal_case = _create_tester("PascalCased", _pascalize)
+is_snake_case = _create_tester("snake_cased", underscore)
