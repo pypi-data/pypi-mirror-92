c5a567a75f7e609d94d55cb9ec0af560ed3abf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:53 +0000

    Add package filenames/0.1.2/filenames-0.1.2.tar.gz

---
+++ packages/filenames/filenames-0.1.2.tar.gz/filenames-0.1.2/filenames/__main__.py
@@ -0,0 +1,37 @@
+'''
+usage: filenames.py [-h] [--preview] cmd path old_key new_key
+
+positional arguments:
+  cmd         command to run [rename] or [copy]
+  path        path to directory containing files to rename
+  old_key     old key term to replace
+  new_key     new key term to replace old with
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --preview   Only Preview Changes!
+'''
+from filenames import *
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('cmd', help = 'command to run [rename] or [copy]')
+    ap.add_argument('path', help = 'path to directory containing files to rename')
+    ap.add_argument('old_key', help = 'old key term to replace')
+    ap.add_argument('new_key', help = 'new key term to replace old with')
+    ap.add_argument('--preview', default=False, action='store_true', help = 'Only Preview Changes!')
+    args = ap.parse_args()
+
+    cmd = args.cmd
+    path = args.path
+    old_key = args.old_key
+    new_key = args.new_key
+
+    if cmd == 'rename':
+        rename(path, old_key, new_key, preview=args.preview)
+    if cmd == 'copy':
+        copy(path, old_key, new_key, preview=args.preview)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/filenames/filenames-0.1.2.tar.gz/filenames-0.1.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='filenames',
+    version='0.1.2',
+    author='Teddy Katayama',
+    author_email='katayama@udel.edu',
+    description='Rename or Copy Files with Replacement Optinos.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kkatayama/filenames',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'filenames=filenames.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
