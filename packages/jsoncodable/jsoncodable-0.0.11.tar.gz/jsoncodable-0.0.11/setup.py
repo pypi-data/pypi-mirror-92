124e92b138d7042c9f67f7c89911456038c79992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:50 +0000

    Add package jsoncodable/0.0.11/jsoncodable-0.0.11.tar.gz

---
+++ packages/jsoncodable/jsoncodable-0.0.11.tar.gz/jsoncodable-0.0.11/setup.py
@@ -0,0 +1,26 @@
+import setuptools, os
+
+readme_path = os.path.join(os.getcwd(), "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'jsoncodable'
+
+setuptools.setup(
+    name="jsoncodable",
+    version="0.0.11",
+    author="Kristof",
+    description="to_dict",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/py_jsoncodable",
+    packages=setuptools.find_packages(),
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
