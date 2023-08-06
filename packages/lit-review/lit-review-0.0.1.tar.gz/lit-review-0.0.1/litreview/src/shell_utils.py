da336cd9a3676262dd487f78e07a25d1c511c267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:47 +0000

    Add package lit-review/0.0.1/lit-review-0.0.1.tar.gz

---
+++ packages/lit-review/lit-review-0.0.1.tar.gz/lit-review-0.0.1/litreview/src/shell_utils.py
@@ -0,0 +1,25 @@
+import subprocess
+import pathlib
+
+
+
+
+def run(command, shell=False, capture_output=True):
+    r = subprocess.run(command, shell=shell, capture_output=capture_output, check=True, universal_newlines=True)
+    return r.stdout
+
+def get_config_dir():
+    return run("echo $HOME", shell=True).replace("\n", "") + "/.config/litreview"
+
+
+def download_pdf(pdflink, filename):
+    filepath = f"/tmp/{filename}"
+    download = run(
+        ["wget", "--user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36", "-O", filepath, pdflink],
+        capture_output=False
+    )
+    return filepath
+
+
+def copy_file(filepath, newpath):
+    return run(["cp", filepath, newpath])
---
+++ packages/lit-review/lit-review-0.0.1.tar.gz/lit-review-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+# This call to setup() does all the work
+setup(
+    name="lit-review",
+    version="0.0.1",
+    description="Automate creation of Jekyll posts for literature reviews",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Velythyl/litreview",
+    author="Charlie Gauthier",
+    author_email="charlie.gauthier@umontreal.ca",
+    license="MIT",
+    packages=find_packages(exclude=("pdfs")),
+    package_data={"litreview": ["*.yaml", "*.md"]},
+    include_package_data=True,
+    install_requires=["pyyaml", "beautifulsoup4", "validators", "six", "soupsieve", "twine"],
+    entry_points={
+        "console_scripts": [
+            "litreview=litreview.main:main",
+        ]
+    },
+)
