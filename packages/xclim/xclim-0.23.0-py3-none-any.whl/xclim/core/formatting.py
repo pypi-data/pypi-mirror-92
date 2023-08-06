1514c9df4c43dcf7f6f0d38b57165d2b69a070b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:20 +0000

    Add package xclim/0.23.0/xclim-0.23.0-py3-none-any.whl

---
+++ packages/xclim/xclim-0.23.0-py3-none-any.whl/xclim/core/formatting.py
@@ -0,0 +1,330 @@
+# -*- coding: utf-8 -*-
+# noqa: D205,D400
+"""
+Formatting utilities for indicators
+===================================
+"""
+import datetime as dt
+import re
+import string
+from fnmatch import fnmatch
+from typing import Dict, Mapping, Optional, Sequence, Union
+
+import xarray as xr
+
+
+class AttrFormatter(string.Formatter):
+    """A formatter for frequently used attribute values.
+
+    See the doc of format_field() for more details.
+    """
+
+    def __init__(
+        self,
+        mapping: Mapping[str, Sequence[str]],
+        modifiers: Sequence[str],
+    ) -> None:
+        """Initialize the formatter.
+
+        Parameters
+        ----------
+        mapping : Mapping[str, Sequence[str]]
+            A mapping from values to their possible variations.
+        modifiers : Sequence[str]
+            The list of modifiers, must be the as long as the longest value of `mapping`.
+        """
+        super().__init__()
+        self.modifiers = modifiers
+        self.mapping = mapping
+
+    def format_field(self, value, format_spec):
+        """Format a value given a formatting spec.
+
+        If `format_spec` is in this Formatter's modifiers, the corresponding variation
+        of value is given. If `format_spec` is not specified but `value` is in the
+        mapping, the first variation is returned.
+
+        Examples
+        --------
+        Let's say the string "The dog is {adj1}, the goose is {adj2}" is to be translated
+        to french and that we know that possible values of `adj` are `nice` and `evil`.
+        In french, the genre of the noun changes the adjective (cat = chat is masculine,
+        and goose = oie is feminine) so we initialize the formatter as:
+
+        >>> fmt = AttrFormatter({'nice': ['beau', 'belle'], 'evil' : ['méchant', 'méchante']}, ['m', 'f'])
+        >>> fmt.format("Le chien est {adj1:m}, l'oie est {adj2:f}", adj1='nice', adj2='evil')
+        "Le chien est beau, l'oie est méchante"
+
+        The base values may be given using unix shell-like patterns:
+
+        >>> fmt = AttrFormatter({'AS-*': ['annuel', 'annuelle'], 'MS' : ['mensuel', 'mensuelle']}, ['m', 'f'])
+        >>> fmt.format("La moyenne {freq:f} est faite sur un échantillon {src_timestep:m}", freq='AS-JUL', src_timestep='MS')
+        'La moyenne annuelle est faite sur un échantillon mensuel'
+        """
+        baseval = self._match_value(value)
+        if baseval is not None and not format_spec:
+            return self.mapping[baseval][0]
+
+        if format_spec in self.modifiers:
+            if baseval is not None:
+                return self.mapping[baseval][self.modifiers.index(format_spec)]
+            raise ValueError(
+                f"No known mapping for string '{value}' with modifier '{format_spec}'"
+            )
+        return super().format_field(value, format_spec)
+
+    def _match_value(self, value):
+        if isinstance(value, str):
+            for mapval in self.mapping.keys():
+                if fnmatch(value, mapval):
+                    return mapval
+        return None
+
+
+# Tag mappings between keyword arguments and long-form text.
+default_formatter = AttrFormatter(
+    {
+        "YS": ["annual", "years"],
+        "AS-*": ["annual", "years"],
+        "MS": ["monthly", "months"],
+        "QS-*": ["seasonal", "seasons"],
+        "DJF": ["winter"],
+        "MAM": ["spring"],
+        "JJA": ["summer"],
+        "SON": ["fall"],
+        "norm": ["Normal"],
+        "m1": ["january"],
+        "m2": ["february"],
+        "m3": ["march"],
+        "m4": ["april"],
+        "m5": ["may"],
+        "m6": ["june"],
+        "m7": ["july"],
+        "m8": ["august"],
+        "m9": ["september"],
+        "m10": ["october"],
+        "m11": ["november"],
+        "m12": ["december"],
+    },
+    ["adj", "noun"],
+)
+
+
+def parse_doc(doc: str) -> Dict[str, str]:
+    """Crude regex parsing."""
+    if doc is None:
+        return dict()
+
+    out = dict()
+
+    sections = re.split(r"(\w+\s?\w+)\n\s+-{3,50}", doc)  # obj.__doc__.split('\n\n')
+    intro = sections.pop(0)
+    if intro:
+        intro_content = list(map(str.strip, intro.strip().split("\n\n")))
+        if len(intro_content) == 1:
+            out["title"] = intro_content[0]
+        elif len(intro_content) >= 2:
+            out["title"], abstract = intro_content[:2]
+            out["abstract"] = " ".join(map(str.strip, abstract.splitlines()))
+
+    for i in range(0, len(sections), 2):
+        header, content = sections[i : i + 2]
+
+        if header in ["Notes", "References"]:
+            out[header.lower()] = content.replace("\n    ", "\n").strip()
+        elif header == "Parameters":
+            out["parameters"] = _parse_parameters(content)
+        elif header == "Returns":
+            match = re.search(r"xarray\.DataArray\s*(.*)", content)
+            if match:
+                out["long_name"] = match.groups()[0]
+
+    return out
+
+
+def _parse_parameters(section):
+    """Parse the parameters section of a docstring into a dictionary mapping the parameter name to its description."""
+    curr_key = None
+    params = {}
+    for line in section.split("\n"):
+        if line.strip():
+            if line.startswith(" " * 6):  # description
+                s = " " if params[curr_key]["description"] else ""
+                params[curr_key]["description"] += s + line.strip()
+            elif line.startswith(" " * 4):  # param title
+                name, annot = line.split(":", maxsplit=1)
+                curr_key = name.strip()
+                params[curr_key] = {"description": ""}
+    return params
+
+
+def merge_attributes(
+    attribute: str,
+    *inputs_list: Union[xr.DataArray, xr.Dataset],
+    new_line: str = "\n",
+    missing_str: Optional[str] = None,
+    **inputs_kws: Union[xr.DataArray, xr.Dataset],
+):
+    r"""
+    Merge attributes from several DataArrays or Datasets.
+
+    If more than one input is given, its name (if available) is prepended as: "<input name> : <input attribute>".
+
+    Parameters
+    ----------
+    attribute : str
+      The attribute to merge.
+    inputs_list : Union[xr.DataArray, xr.Dataset]
+      The datasets or variables that were used to produce the new object. Inputs given that way will be prefixed by their `name` attribute if available.
+    new_line : str
+      The character to put between each instance of the attributes. Usually, in CF-conventions,
+      the history attributes uses '\\n' while cell_methods uses ' '.
+    missing_str : str
+      A string that is printed if an input doesn't have the attribute. Defaults to None, in which
+      case the input is simply skipped.
+    inputs_kws : Union[xr.DataArray, xr.Dataset]
+      Mapping from names to the datasets or variables that were used to produce the new object.
+      Inputs given that way will be prefixes by the passed name.
+
+    Returns
+    -------
+    str
+      The new attribute made from the combination of the ones from all the inputs.
+    """
+    inputs = []
+    for in_ds in inputs_list:
+        inputs.append((getattr(in_ds, "name", None), in_ds))
+    inputs += list(inputs_kws.items())
+
+    merged_attr = ""
+    for in_name, in_ds in inputs:
+        if attribute in in_ds.attrs or missing_str is not None:
+            if in_name is not None and len(inputs) > 1:
+                merged_attr += f"{in_name}: "
+            merged_attr += in_ds.attrs.get(
+                attribute, "" if in_name is None else missing_str
+            )
+            merged_attr += new_line
+
+    if len(new_line) > 0:
+        return merged_attr[: -len(new_line)]  # Remove the last added new_line
+    return merged_attr
+
+
+def update_history(
+    hist_str: str,
+    *inputs_list: Union[xr.DataArray, xr.Dataset],
+    new_name: Optional[str] = None,
+    **inputs_kws: Union[xr.DataArray, xr.Dataset],
+):
+    """Return an history string with the timestamped message and the combination of the history of all inputs.
+
+    The new history entry is formatted as "[<timestamp>] <new_name>: <hist_str> - xclim version: <xclim.__version__>."
+
+    Parameters
+    ----------
+    hist_str : str
+      The string describing what has been done on the data.
+    new_name : Optional[str]
+      The name of the newly created variable or dataset to prefix hist_msg.
+    *inputs_list : Union[xr.DataArray, xr.Dataset]
+      The datasets or variables that were used to produce the new object.
+      Inputs given that way will be prefixed by their "name" attribute if available.
+    **inputs_kws : Union[xr.DataArray, xr.Dataset]
+      Mapping from names to the datasets or variables that were used to produce the new object.
+      Inputs given that way will be prefixes by the passed name.
+
+    Returns
+    -------
+    str
+      The combine history of all inputs starting with `hist_str`.
+
+    See Also
+    --------
+    merge_attributes
+    """
+    from xclim import __version__  # pylint: disable=cyclic-import
+
+    merged_history = merge_attributes(
+        "history",
+        *inputs_list,
+        new_line="\n",
+        missing_str="",
+        **inputs_kws,
+    )
+    if len(merged_history) > 0 and not merged_history.endswith("\n"):
+        merged_history += "\n"
+    merged_history += f"[{dt.datetime.now():%Y-%m-%d %H:%M:%S}] {new_name or ''}: {hist_str} - xclim version: {__version__}."
+    return merged_history
+
+
+def update_cell_methods(attrs, new):
+    """Update cell methods attributes.
+
+    attrs : dict
+      Original data attributes.
+    new : str
+      Cell method to append to the original cell methods.
+
+
+    Returns
+    -------
+    str
+      Updated cell method.
+    """
+    cm = attrs.get("cell_methods", "")
+    return (cm + " " + new).strip()
+
+
+def prefix_attrs(source, keys, prefix):
+    """Rename some of the keys of a dictionary by adding a prefix.
+
+    Parameters
+    ----------
+    source : dict
+      Source dictionary, for example data attributes.
+    keys : sequence
+      Names of keys to prefix.
+    prefix : str
+      Prefix to prepend to keys.
+
+    Returns
+    -------
+    dict
+      Dictionary of attributes with some keys prefixed.
+    """
+    out = {}
+    for key, val in source.items():
+        if key in keys:
+            out[f"{prefix}{key}"] = val
+        else:
+            out[key] = val
+    return out
+
+
+def unprefix_attrs(source, keys, prefix):
+    """Remove prefix from keys in a dictionary.
+
+    Parameters
+    ----------
+    source : dict
+      Source dictionary, for example data attributes.
+    keys : sequence
+      Names of original keys for which prefix should be removed.
+    prefix : str
+      Prefix to remove from keys.
+
+    Returns
+    -------
+    dict
+      Dictionary of attributes whose keys were prefixed, with prefix removed.
+    """
+    out = {}
+    n = len(prefix)
+    for key, val in source.items():
+        k = key[n:]
+        if (k in keys) and key.startswith(prefix):
+            out[k] = val
+        elif key not in out:
+            out[key] = val
+    return out
---
+++ packages/xclim/xclim-0.23.0-py3-none-any.whl/xclim/core/units.py
@@ -0,0 +1,434 @@
+# -*- coding: utf-8 -*-
+# noqa: D205,D400
+"""
+Units handling submodule
+========================
+
+`Pint` is used to define the `units` `UnitRegistry` and `xclim.units.core` defines
+most unit handling methods.
+"""
+import re
+import warnings
+from inspect import signature
+from typing import Any, Callable, List, Optional, Union
+
+import pint.converters
+import pint.unit
+import xarray as xr
+from boltons.funcutils import wraps
+from packaging import version
+
+from .options import datacheck
+from .utils import ValidationError
+
+__all__ = [
+    "convert_units_to",
+    "declare_units",
+    "pint_multiply",
+    "pint2cfunits",
+    "units",
+    "units2pint",
+]
+
+
+units = pint.UnitRegistry(autoconvert_offset_to_baseunit=True)
+units.define(
+    pint.unit.UnitDefinition(
+        "percent", "%", ("pct",), pint.converters.ScaleConverter(0.01)
+    )
+)
+
+# Define commonly encountered units not defined by pint
+if version.parse(pint.__version__) >= version.parse("0.10"):
+    units.define("@alias degC = C = deg_C")
+    units.define("@alias degK = deg_K")
+    units.define("@alias day = d")
+    units.define("@alias hour = h")  # Not the Planck constant...
+    units.define(
+        "@alias degree = degrees_north = degrees_N = degreesN = degree_north = degree_N = degreeN"
+    )
+    units.define(
+        "@alias degree = degrees_east = degrees_E = degreesE = degree_east = degree_E = degreeE"
+    )
+
+else:
+    units.define("degC = kelvin; offset: 273.15 = celsius = C = deg_C")
+    units.define("d = day")
+    units.define("h = hour")
+    units.define(
+        "degrees_north = degree = degrees_N = degreesN = degree_north = degree_N "
+        "= degreeN"
+    )
+    units.define(
+        "degrees_east = degree = degrees_E = degreesE = degree_east = degree_E = degreeE"
+    )
+
+units.define("[speed] = [length] / [time]")
+
+# Default context.
+null = pint.Context("none")
+units.add_context(null)
+
+# Precipitation units. This is an artificial unit that we're using to verify that a given unit can be converted into
+# a precipitation unit. Ideally this could be checked through the `dimensionality`, but I can't get it to work.
+units.define("[precipitation] = [mass] / [length] ** 2 / [time]")
+units.define("mmday = 1000 kg / meter ** 2 / day")
+
+units.define("[discharge] = [length] ** 3 / [time]")
+units.define("cms = meter ** 3 / second")
+
+hydro = pint.Context("hydro")
+hydro.add_transformation(
+    "[mass] / [length]**2",
+    "[length]",
+    lambda ureg, x: x / (1000 * ureg.kg / ureg.m ** 3),
+)
+hydro.add_transformation(
+    "[mass] / [length]**2 / [time]",
+    "[length] / [time]",
+    lambda ureg, x: x / (1000 * ureg.kg / ureg.m ** 3),
+)
+hydro.add_transformation(
+    "[length] / [time]",
+    "[mass] / [length]**2 / [time]",
+    lambda ureg, x: x * (1000 * ureg.kg / ureg.m ** 3),
+)
+units.add_context(hydro)
+units.enable_contexts(hydro)
+
+# These are the changes that could be included in a units definition file.
+
+# degrees_north = degree = degrees_N = degreesN = degree_north = degree_N = degreeN
+# degrees_east = degree = degrees_E = degreesE = degree_east = degree_E = degreeE
+# degC = kelvin; offset: 273.15 = celsius = C
+# day = 24 * hour = d
+# @context hydro
+#     [mass] / [length]**2 -> [length]: value / 1000 / kg / m ** 3
+#     [mass] / [length]**2 / [time] -> [length] / [time] : value / 1000 / kg * m ** 3
+#     [length] / [time] -> [mass] / [length]**2 / [time] : value * 1000 * kg / m ** 3
+# @end
+
+
+def units2pint(value: Union[xr.DataArray, str]) -> pint.unit.UnitDefinition:
+    """Return the pint Unit for the DataArray units.
+
+    Parameters
+    ----------
+    value : Union[xr.DataArray, str]
+      Input data array or string representing a unit (with no magnitude).
+
+    Returns
+    -------
+    pint.unit.UnitDefinition
+      Units of the data array.
+
+    """
+
+    def _transform(s):
+        """Convert a CF-unit string to a pint expression."""
+        if s == "%":
+            return "percent"
+
+        return re.subn(r"([a-zA-Z]+)\^?(-?\d)", r"\g<1>**\g<2>", s)[0]
+
+    if isinstance(value, str):
+        unit = value
+    elif isinstance(value, xr.DataArray):
+        unit = value.attrs["units"]
+    elif isinstance(value, units.Quantity):
+        return value.units
+    else:
+        raise NotImplementedError(f"Value of type `{type(value)}` not supported.")
+
+    unit = unit.replace("%", "pct")
+    if unit == "1":
+        unit = ""
+
+    try:  # Pint compatible
+        return units.parse_units(unit)
+    except (
+        pint.UndefinedUnitError,
+        pint.DimensionalityError,
+        AttributeError,
+        TypeError,
+    ):  # Convert from CF-units to pint-compatible
+        return units.parse_units(_transform(unit))
+
+
+# Note: The pint library does not have a generic Unit or Quantity type at the moment. Using "Any" as a stand-in.
+def pint2cfunits(value: Any) -> str:
+    """Return a CF-Convention unit string from a `pint` unit.
+
+    Parameters
+    ----------
+    value : pint.UnitRegistry
+      Input unit.
+
+    Returns
+    -------
+    out : str
+      Units following CF-Convention.
+    """
+    # Print units using abbreviations (millimeter -> mm)
+    s = f"{value:~}"
+
+    # Search and replace patterns
+    pat = r"(?P<inverse>/ )?(?P<unit>\w+)(?: \*\* (?P<pow>\d))?"
+
+    def repl(m):
+        i, u, p = m.groups()
+        p = p or (1 if i else "")
+        neg = "-" if i else ("^" if p else "")
+
+        return f"{u}{neg}{p}"
+
+    out, n = re.subn(pat, repl, s)
+    return out.replace("percent", "%")
+
+
+def pint_multiply(da: xr.DataArray, q: Any, out_units: Optional[str] = None):
+    """Multiply xarray.DataArray by pint.Quantity.
+
+    Parameters
+    ----------
+    da : xr.DataArray
+      Input array.
+    q : pint.Quantity
+      Multiplicative factor.
+    out_units : Optional[str]
+      Units the output array should be converted into.
+    """
+    a = 1 * units2pint(da)
+    f = a * q.to_base_units()
+    if out_units:
+        f = f.to(out_units)
+    out = da * f.magnitude
+    out.attrs["units"] = pint2cfunits(f.units)
+    return out
+
+
+def str2pint(val: str):
+    """Convert a string to a pint.Quantity, splitting the magnitude and the units.
+
+    Parameters
+    ----------
+    val : str
+      A quantity in the form "[{magnitude} ]{units}", where magnitude is castable to a float and
+      units is understood by `units2pint`.
+
+    Returns
+    -------
+    pint.Quantity
+      Magnitude is 1 if no magnitude was present in the string.
+    """
+    mstr, *ustr = val.split(" ", maxsplit=1)
+    try:
+        if ustr:
+            return units.Quantity(float(mstr), units=units2pint(ustr[0]))
+        return units.Quantity(float(mstr))
+    except ValueError:
+        return units.Quantity(1, units2pint(val))
+
+
+def convert_units_to(
+    source: Union[str, xr.DataArray, Any],
+    target: Union[str, xr.DataArray, Any],
+    context: Optional[str] = None,
+):
+    """
+    Convert a mathematical expression into a value with the same units as a DataArray.
+
+    Parameters
+    ----------
+    source : Union[str, xr.DataArray, Any]
+      The value to be converted, e.g. '4C' or '1 mm/d'.
+    target : Union[str, xr.DataArray, Any]
+      Target array of values to which units must conform.
+    context : Optional[str]
+
+    Returns
+    -------
+    out
+      The source value converted to target's units.
+    """
+    # Target units
+    if isinstance(target, units.Unit):
+        tu = target
+    elif isinstance(target, (str, xr.DataArray)):
+        tu = units2pint(target)
+    else:
+        raise NotImplementedError
+
+    if isinstance(source, str):
+        q = str2pint(source)
+        # Return magnitude of converted quantity. This is going to fail if units are not compatible.
+        return q.to(tu).m
+
+    if isinstance(source, units.Quantity):
+        return source.to(tu).m
+
+    if isinstance(source, xr.DataArray):
+        fu = units2pint(source)
+        tu_u = pint2cfunits(tu)
+
+        if fu == tu:
+            # The units are the same, but the symbol may not be.
+            source.attrs["units"] = tu_u
+            return source
+
+        with units.context(context or "none"):
+            out = xr.DataArray(
+                data=units.convert(source.data, fu, tu),
+                coords=source.coords,
+                attrs=source.attrs,
+                name=source.name,
+            )
+            out.attrs["units"] = tu_u
+            return out
+
+    # TODO remove backwards compatibility of int/float thresholds after v1.0 release
+    if isinstance(source, (float, int)):
+        if context == "hydro":
+            fu = units.mm / units.day
+        else:
+            fu = units.degC
+        warnings.warn(
+            "Future versions of xclim will require explicit unit specifications.",
+            FutureWarning,
+            stacklevel=3,
+        )
+        return units.Quantity(source, units=fu).to(tu).m
+
+    raise NotImplementedError(f"Source of type `{type(source)}` is not supported.")
+
+
+@datacheck
+def check_units(val: Optional[Union[str, int, float]], dim: Optional[str]) -> None:
+    if dim is None or val is None:
+        return
+
+    if str(val).startswith("UNSET "):
+        warnings.warn(
+            "This index calculation will soon require user-specified thresholds.",
+            FutureWarning,
+            stacklevel=4,
+        )
+        val = str(val).replace("UNSET ", "")
+
+    # TODO remove backwards compatibility of int/float thresholds after v1.0 release
+    if isinstance(val, (int, float)):
+        return
+
+    expected = units.get_dimensionality(dim.replace("dimensionless", ""))
+    if isinstance(val, str):
+        val_dim = str2pint(val).dimensionality
+    else:  # a DataArray
+        val_dim = units2pint(val).dimensionality
+    if val_dim == expected:
+        return
+
+    # Check if there is a transformation available
+    start = pint.util.to_units_container(expected)
+    end = pint.util.to_units_container(val_dim)
+    graph = units._active_ctx.graph
+    if pint.util.find_shortest_path(graph, start, end):
+        return
+
+    if dim == "[precipitation]":
+        tu = "mmday"
+    elif dim == "[discharge]":
+        tu = "cms"
+    elif dim == "[length]":
+        tu = "m"
+    elif dim == "[speed]":
+        tu = "m s-1"
+    else:
+        raise NotImplementedError(f"Dimension `{dim}` is not supported.")
+
+    try:
+        units.Quantity(1, units=units2pint(val)).to(tu, "hydro")
+    except (pint.UndefinedUnitError, pint.DimensionalityError):
+        raise ValidationError(
+            f"Value's dimension `{val_dim}` does not match expected units `{expected}`."
+        )
+
+
+def declare_units(
+    out_units: Optional[Union[str, List[str]]],
+    check_output: bool = True,
+    **units_by_name,
+) -> Callable:
+    """Create a decorator to check units of function arguments.
+
+    The decorator checks that input and output values have units that are compatible with expected dimensions.
+
+    Parameters
+    ----------
+    out_units : str or Sequence[str]
+      The units of the output(s). If the indice outputs multiple DataArray, a sequence of string of the same length must be given.
+      Pass "" for unitless quantities.
+    check_output : bool
+      Set to False to skip the output units check.
+    units_by_name : Mapping[str, str]
+      Mapping from the input parameter names to their units or dimensionality ("[...]").
+
+    Examples
+    --------
+    In the following function definition:
+
+    .. code::
+
+       @declare_units("K", tas=["temperature"])
+       def func(tas):
+          ...
+
+    the decorator will check that `tas` has units of temperature (C, K, F) and that the output is in Kelvins.
+
+    """
+
+    def dec(func):
+        # Match the signature of the function to the arguments given to the decorator
+        sig = signature(func)
+        bound_units = sig.bind_partial(**units_by_name)
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Match all passed in value to their proper arguments so we can check units
+            bound_args = sig.bind(*args, **kwargs)
+            for name, val in bound_args.arguments.items():
+                check_units(val, bound_units.arguments.get(name, None))
+
+            out = func(*args, **kwargs)
+
+            def _check_out_units(out, out_units):
+                if "units" in out.attrs:
+                    # Check that output units dimensions match expectations, e.g. [temperature]
+                    if "[" in out_units:
+                        check_units(out, out_units)
+                    # Explicitly convert units if units are declared, e.g K
+                    else:
+                        out = convert_units_to(out, out_units)
+
+                # Otherwise, we impose the units if given.
+                elif "[" not in out_units:
+                    out.attrs["units"] = out_units
+
+                else:
+                    raise ValueError(
+                        "Output units are not propagated by computation nor specified by decorator."
+                    )
+                return out
+
+            if check_output:
+                if isinstance(out, tuple):
+                    out = tuple(
+                        _check_out_units(o, o_u) for o, o_u in zip(out, out_units)
+                    )
+                else:
+                    out = _check_out_units(out, out_units)
+
+            return out
+
+        return wrapper
+
+    return dec
