6d8904f7fadea6bad69d52154865b0f1c4636377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package nnio/0.1.5/nnio-0.1.5.tar.gz

---
+++ packages/nnio/nnio-0.1.5.tar.gz/nnio-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup, Extension
+
+setup(
+    name='nnio',
+    version='0.1.5',
+    description='Simple neural network inference on devices',
+    long_description='''
+    See more details at the project's homepage: https://github.com/FastSense/nnio
+    ''',
+    author='FastSense',
+    author_email='ruslan.baynazarov@fastsense.tech',
+    url='https://github.com/FastSense/nnio',
+    packages=['nnio', 'nnio.zoo', 'nnio.zoo.edgetpu', 'nnio.zoo.openvino', 'nnio.zoo.onnx'],
+    install_requires=[
+        'opencv-python'
+    ]
+)
