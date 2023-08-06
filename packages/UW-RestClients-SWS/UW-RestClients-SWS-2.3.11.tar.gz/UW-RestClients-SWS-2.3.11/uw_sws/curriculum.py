4adfb9a4fbd57fccf450e7881e64a778319a75d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:05 +0000

    Add package UW-RestClients-SWS/2.3.11/UW-RestClients-SWS-2.3.11.tar.gz

---
+++ packages/UW-RestClients-SWS/UW-RestClients-SWS-2.3.11.tar.gz/UW-RestClients-SWS-2.3.11/uw_sws/curriculum.py
@@ -0,0 +1,61 @@
+"""
+Interfacing with the Student Web Service, Curriculum Search Resource.
+"""
+import logging
+from urllib.parse import urlencode
+from uw_sws.models import Curriculum
+from uw_sws import get_resource
+
+
+logger = logging.getLogger(__name__)
+curriculum_search_url_prefix = "/student/v5/curriculum.json"
+
+
+def get_curricula_by_department(
+        department, future_terms=0, view_unpublished=False):
+    """
+    Returns a list of restclients.Curriculum models, for the passed
+    Department model.
+    """
+    if not isinstance(future_terms, int):
+        raise ValueError(future_terms)
+
+    if future_terms < 0 or future_terms > 2:
+        raise ValueError(future_terms)
+
+    view_unpublished = "true" if view_unpublished else "false"
+
+    url = "{}?{}".format(
+        curriculum_search_url_prefix,
+        urlencode([("department_abbreviation", department.label,),
+                   ("future_terms", future_terms,),
+                   ("view_unpublished", view_unpublished,)]))
+    return _json_to_curricula(get_resource(url))
+
+
+def get_curricula_by_term(term, view_unpublished=False):
+    """
+    Returns a list of restclients.Curriculum models, for the passed
+    Term model.
+    """
+    view_unpublished = "true" if view_unpublished else "false"
+    url = "{}?{}".format(
+        curriculum_search_url_prefix,
+        urlencode([
+                   ("quarter", term.quarter.lower(),),
+                   ("year", term.year,),
+                   ("view_unpublished", view_unpublished,)]))
+    return _json_to_curricula(get_resource(url))
+
+
+def _json_to_curricula(data):
+    curricula = []
+    for curr_data in data.get("Curricula", []):
+        curriculum = Curriculum()
+        curriculum.label = curr_data["CurriculumAbbreviation"]
+        curriculum.name = curr_data["CurriculumName"]
+        curriculum.full_name = curr_data["CurriculumFullName"]
+        curriculum.clean_fields()
+        curricula.append(curriculum)
+
+    return curricula
---
+++ packages/UW-RestClients-SWS/UW-RestClients-SWS-2.3.11.tar.gz/UW-RestClients-SWS-2.3.11/uw_sws/tests/test_financial.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from uw_sws.util import fdao_sws_override
+from uw_pws.util import fdao_pws_override
+from uw_sws.financial import get_account_balances_by_regid
+import datetime
+
+
+@fdao_pws_override
+@fdao_sws_override
+class SWSFinance(TestCase):
+    def test_financial_resource(self):
+        data = get_account_balances_by_regid(
+            "9136CCB8F66711D5BE060004AC494FFE")
+        self.assertEquals(data.tuition_accbalance, "12345.00")
+        self.assertEquals(data.pce_accbalance, "0.00")
+
+        data = get_account_balances_by_regid(
+            "FE36CCB8F66711D5BE060004AC494F31")
+        self.assertEquals(data.tuition_accbalance, "12345.00")
+        self.assertEquals(data.pce_accbalance, "345.00")
