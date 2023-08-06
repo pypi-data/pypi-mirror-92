517d94e4d7e55b773cc97a6342ad2f0417db716a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:50 +0000

    Add package typehints-checker/0.0.2/typehints_checker-0.0.2-py3-none-any.whl

---
+++ packages/typehints-checker/typehints_checker-0.0.2-py3-none-any.whl/typehints_checker/__init__.py
@@ -0,0 +1,40 @@
+r"""
+# typehints_checker
+[![fury](https://img.shields.io/pypi/v/typehints_checker.svg)](https://pypi.org/project/typehints_checker/)
+[![licence](https://img.shields.io/github/license/GoodManWEN/typehints_checker)](https://github.com/GoodManWEN/typehints_checker/blob/master/LICENSE)
+[![pyversions](https://img.shields.io/pypi/pyversions/typehints_checker.svg)](https://pypi.org/project/typehints_checker/)
+[![Publish](https://github.com/GoodManWEN/typehints_checker/workflows/Publish/badge.svg)](https://github.com/GoodManWEN/typehints_checker/actions?query=workflow:Publish)
+[![Build](https://github.com/GoodManWEN/typehints_checker/workflows/Build/badge.svg)](https://github.com/GoodManWEN/typehints_checker/actions?query=workflow:Build)
+[![Docs](https://readthedocs.org/projects/typehints_checker/badge/?version=latest)](https://readthedocs.org/projects/typehints_checker/)
+
+Some description.
+
+## Requirements
+- pipeit>=0.1.4
+
+## Install
+
+    pip install typehints_checker
+
+## Feature
+- Some feature.
+
+## Documentation
+https://typehints_checker.readthedocs.io
+
+## Example
+
+Some description.
+```Python3
+# Some code
+```
+
+A test of new branch
+"""
+__author__ = 'WEN (github.com/GoodManWEN)'
+__version__ = '0.0.2'
+
+from .utils import *
+
+__all__ = (
+)
