3d2b5e74acf48b1dfa9336d6eb22e6c681a4478b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:08 +0000

    Add package nebo-bot/0.0.6/nebo_bot-0.0.6-py3.9.egg

---
+++ packages/nebo-bot/nebo_bot-0.0.6-py3.9.egg/nebo_bot/command_line.py
@@ -0,0 +1,23 @@
+import argparse
+
+from loguru import logger
+
+from nebo_bot.core import BotCore
+
+
+def run():
+    parser = argparse.ArgumentParser(description='Nebo bot')
+    parser.add_argument('login', type=str, action='store',
+                        help='Login')
+    parser.add_argument('password', type=str, action='store',
+                        help='password')
+
+    args = parser.parse_args()
+    login = args.login
+    password = args.password
+    bot = BotCore()
+
+    if bot.login(login, password):
+        bot.run_works()
+    else:
+        logger.error('Fail login')
