ee1085a232fb9ed1819e01d5708b51fd3feab312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:27 +0000

    Add package salmon-lm/1.0.2a0/salmon-lm-1.0.2a0.tar.gz

---
+++ packages/salmon-lm/salmon-lm-1.0.2a0.tar.gz/salmon-lm-1.0.2a0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="salmon-lm",
+    version="1.0.2a",
+
+    description="A symbolic algebra based linear regression tool.",
+
+    url="https://github.com/ajboyd2/salmon",
+    download_url="https://github.com/ajboyd2/salmon/archive/v_100.tar.gz",
+
+    author="Alex Boyd",
+    author_email="alexjb@uci.edu",
+
+    license="GPLv3",
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+    
+    keywords='modeling symbolic regression',
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "scipy",
+    ],
+    packages=find_packages(exclude=("test",)),
+)
