fe664272d5352bf46ae95db9bf0dd57159a15edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:03 +0000

    Add package DodayUtils/0.0.59/DodayUtils-0.0.59-py3-none-any.whl

---
+++ packages/DodayUtils/DodayUtils-0.0.59-py3-none-any.whl/DodayUtils/DodayHttpClient.py
@@ -0,0 +1,41 @@
+#-*-coding:utf-8 -*-
+from DodayUtils._dwrappers import *
+import requests
+import urllib.parse
+
+class DodayHttpClient:
+	"""
+	This is the http client
+	class. 
+	"""
+	def __init__(self, **configs):
+		for key in configs:
+			if "_http_url" in key:
+				setattr(self, key, configs[key])
+
+	def __getitem__(self, key):
+		return getattr(self, key)
+		
+	def client_get(self, key, input_dict):
+		get_query = self[key]
+
+		# assign query list
+		query_list = ["?"]
+		for key in input_dict:
+			query_list.extend([str(key), "=", input_dict[key], "&"])
+
+		# concatenate together
+		get_query += "".join(query_list[:-1])
+
+		r = requests.get(get_query)
+		response = r.text 
+		return response
+
+	def client_post(self, key, input_dict):
+		post_query = self[key]
+
+		r = requests.post(post_query, json=input_dict)
+		response = r.text
+
+		return response
+
