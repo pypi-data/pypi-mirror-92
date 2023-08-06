9800240876a3abd781440c3c82e57ad40e92c1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:03 +0000

    Add package viber/0.1/viber-0.1-py2.7.egg

---
+++ packages/viber/viber-0.1-py2.7.egg/viber/api/event_type.py
@@ -0,0 +1,10 @@
+
+class EventType(object):
+	SEEN = 'seen'
+	CONVERSATION_STARTED = 'conversation_started'
+	DELIVERED = 'delivered'
+	MESSAGE = 'message'
+	SUBSCRIBED = 'subscribed'
+	UNSUBSCRIBED = 'unsubscribed'
+	FAILED = 'failed'
+	WEBHOOK = 'webhook'
