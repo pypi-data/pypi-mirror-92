726b74e5f1d73d2f2d3aafd78676591beba4be81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:41 +0000

    Add package spacy-nightly/3.0.0rc4/spacy_nightly-3.0.0rc4-cp38-cp38-win_amd64.whl

---
+++ packages/spacy-nightly/spacy_nightly-3.0.0rc4-cp38-cp38-win_amd64.whl/spacy/about.py
@@ -0,0 +1,7 @@
+# fmt: off
+__title__ = "spacy-nightly"
+__version__ = "3.0.0rc4"
+__download_url__ = "https://github.com/explosion/spacy-models/releases/download"
+__compatibility__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json"
+__projects__ = "https://github.com/explosion/projects"
+__projects_branch__ = "v3"
