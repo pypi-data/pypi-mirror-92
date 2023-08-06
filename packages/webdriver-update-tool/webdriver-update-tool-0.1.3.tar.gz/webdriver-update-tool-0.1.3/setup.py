dc8fed4ff875d470307158e5a7b69b65d271432d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:43 +0000

    Add package webdriver-update-tool/0.1.3/webdriver-update-tool-0.1.3.tar.gz

---
+++ packages/webdriver-update-tool/webdriver-update-tool-0.1.3.tar.gz/webdriver-update-tool-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README4PyPI.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webdriver-update-tool",
+    version="0.1.3",
+    author="cjz25",
+    description="A tool for updating the webdriver automatically",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    install_requires=[
+        'selenium >= 3.141.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    python_requires='>=3.5',
+)
