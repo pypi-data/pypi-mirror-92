0d4a5822659866b47cb134fefc5f63fdd93ac6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:19 +0000

    Add package HostHome-cli/1.4.2/HostHome-cli-1.4.2.tar.gz

---
+++ packages/HostHome-cli/HostHome-cli-1.4.2.tar.gz/HostHome-cli-1.4.2/hosthome/login.py
@@ -0,0 +1,34 @@
+import requests
+from getpass import getpass
+from termcolor import cprint
+
+import webbrowser, requests
+
+url = requests.get("https://raw.githubusercontent.com/HostHome-of/config/main/config.json").json()["url"]
+
+def login():
+    
+    print("\t\t\t\t\t"+"-"*33)
+    print("\t\t\t\t\t"+"|\t"+"                 \t|")
+    print("\t\t\t\t\t"+"|\t"+"Login en HostHome\t|")
+    print("\t\t\t\t\t"+"|\t"+"                 \t|")
+    print("\t\t\t\t\t"+"-"*33)
+
+    mail = input("Porfavor escribe tu email :: ")
+    psw = getpass("Escribe tu contraseña :: ")
+    
+    data = requests.post(f"{url}login?psw={psw}&mail={mail}").json()
+
+    # print(data)
+
+    if str(data) == "{}":
+        cprint("Esa cuenta no existe intentalo otra vez", "red")
+        si_no = input("\n¿Quieres crearte una? [s/n] :: ")
+        if si_no == "s":
+            webbrowser.open(f'{url}register', new=2)
+            return login()
+        else:
+            cprint("Veo que no", "red")
+            exit(0)
+
+    return data
