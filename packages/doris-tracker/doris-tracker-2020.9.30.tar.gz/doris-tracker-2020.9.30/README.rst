81ab7c816e1dc96004bdca0f603ebf52a5e58304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:17 +0000

    Add package doris-tracker/2020.9.30/doris-tracker-2020.9.30.tar.gz

---
+++ packages/doris-tracker/doris-tracker-2020.9.30.tar.gz/doris-tracker-2020.9.30/README.rst
@@ -0,0 +1,45 @@
+################################################################
+DORIS
+################################################################
+
+:Author: `Olivier Friard <http://www.di.unito.it/~friard>`_
+
+
+DORIS is an easy-to-use interactive object detection and tracking software with a graphical user interface.
+
+DORIS is a free and open-source software available for GNU/Linux, Windows and Mac OS.
+
+This project is at an early stage of development. Please report any bugs and suggestions using the GitHub issues tracker.
+
+DORIS web page: `http://www.boris.unito.it/pages/doris <http://www.boris.unito.it/pages/doris>`_
+
+This project was inspired by the **Tracktor project**: https://github.com/vivekhsridhar/tracktor https://besjournals.onlinelibrary.wiley.com/doi/abs/10.1111/2041-210X.13166
+
+Requirements
+========================================================================================
+
+* matplotlib
+* numpy
+* opencv-python-headless (>=4.0.0)
+* pandas
+* PyQt5
+* scikit-learn
+* scipy
+* sklearn
+
+
+User guide
+========================================================================================
+
+https://doris.readthedocs.io/en/latest/
+
+A video tutorial is available at https://www.youtube.com/watch?v=toSXiqsWRzY (no audio)
+
+
+Download binaries (executable versions for Linux, Windows and Mac OS)
+========================================================================================
+
+See the `Releases page <https://github.com/olivierfriard/DORIS/releases>`_
+
+
+
---
+++ packages/doris-tracker/doris-tracker-2020.9.30.tar.gz/doris-tracker-2020.9.30/setup.py
@@ -0,0 +1,49 @@
+# https://stackoverflow.com/questions/44977227/how-to-configure-main-py-init-py-and-setup-py-for-a-basic-package
+
+from setuptools import setup
+
+setup(
+   name='doris-tracker',
+   version=[x for x in open("doris/version.py","r").read().split("\n") if "__version__" in x][0].split(" = ")[1].replace('"', ''),
+   description='DORIS',
+   author='Olivier Friard',
+   author_email='olivier.friard@unito.it',
+   long_description=open("README_pip.rst", "r").read(),
+   #long_description_content_type="text/markdown",
+   url="http://www.boris.unito.it/pages/doris",
+   python_requires=">=3.6",
+   classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    packages=['doris'],  #same as name
+   
+    install_requires=[
+          "matplotlib",
+          "numpy==1.19.5",
+          "opencv-python-headless",
+          "pandas",
+          "pyqt5==5.15.2",
+          "scikit-learn",
+          "scipy",
+          "sklearn"
+      ],
+
+    package_data={
+    'doris': ['doris.ui', 'LICENSE.TXT', 'README.TXT'],
+     "": ["README.TXT", "LICENSE.TXT"],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'doris = doris:main',
+        ],
+    }
+ )
