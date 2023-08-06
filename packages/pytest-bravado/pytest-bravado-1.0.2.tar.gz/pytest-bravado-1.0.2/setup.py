caf97e9e5723fdb405f3063679a91e199e2da8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:53 +0000

    Add package pytest-bravado/1.0.2/pytest-bravado-1.0.2.tar.gz

---
+++ packages/pytest-bravado/pytest-bravado-1.0.2.tar.gz/pytest-bravado-1.0.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pytest_bravado']
+install_requires = \
+['bravado>=11.0.2,<12.0.0']
+
+entry_points = \
+{'pytest11': ['bravado = pytest_bravado']}
+
+setup_kwargs = {
+    'name': 'pytest-bravado',
+    'version': '1.0.2',
+    'description': 'Pytest-bravado automatically generates from OpenAPI specification client fixtures.',
+    'long_description': None,
+    'author': 'Viktor Kutepov',
+    'author_email': 'vkytepov@live.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/vkutepov/pytest-bravado',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
