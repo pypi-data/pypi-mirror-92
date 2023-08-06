c399c31d1cd6f8570ab10a240476dc7be95e9b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:05 +0000

    Add package MiniBrowser/0.1.0/MiniBrowser-0.1.0.tar.gz

---
+++ packages/MiniBrowser/MiniBrowser-0.1.0.tar.gz/MiniBrowser-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MiniBrowser",
+    version="0.1.0",
+    author="Hugo van de Kuilen from Hugo4IT.com",
+    author_email="hugo.vandekuilen1234567890@gmail.com",
+    description="An easy-to-use pygame User Interface Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hugo4IT/PythonEventSystem",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'PygameUILib',
+        'pygame'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
