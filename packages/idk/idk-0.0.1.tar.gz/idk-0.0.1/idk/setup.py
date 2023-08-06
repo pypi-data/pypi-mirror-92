9f4457ba3ceb5e104eafc653e71092a38ac93426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:05 +0000

    Add package idk/0.0.1/idk-0.0.1.tar.gz

---
+++ packages/idk/idk-0.0.1.tar.gz/idk-0.0.1/idk/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name="idk", 
+    version="0.0.1", 
+    author="", 
+    author_email="example@example.com", 
+    description="", 
+    long_description="",
+    long_description_content_type="text/markdown",install_requires=[],
+    url="https://github.com/", 
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
