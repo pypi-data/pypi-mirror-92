070198ed0e1c72c2d25bcd860082be1d4b86e3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package kora/0.9.17/kora-0.9.17.tar.gz

---
+++ packages/kora/kora-0.9.17.tar.gz/kora-0.9.17/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+requirements = [
+    "ipython",
+    "fastcore",
+]
+
+
+setuptools.setup(
+    name="kora", 
+    version="0.9.17",
+    author="Korakot Chaovavanich",
+    author_email="korakot@gmail.com",
+    description="Convenient tools for Colab",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/airesearch-in-th/kora",
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
