bfc977f77ae30ab64197157a2b8bdf4a40b02697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:22 +0000

    Add package argparseware/0.9.6.post2/argparseware-0.9.6.post2.tar.gz

---
+++ packages/argparseware/argparseware-0.9.6.post2.tar.gz/argparseware-0.9.6.post2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+# Optional dependencies
+extras_require = {
+    'config': [
+        'anyconfig>=0.9.1,<0.10',
+        'PyYAML>=3.12,<4',
+    ],
+    'wsgi': [
+        'Flask>=1.1.1,<2',
+        'gunicorn>=20.0.4,<21',
+        'gevent>=20.6.2,<21',
+    ],
+}
+
+# All dependencies
+extras_require['all'] = []
+for key in extras_require:
+    if key != 'all':
+        extras_require['all'] += extras_require[key]
+
+# Setup script
+setup(
+    extras_require=extras_require,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+)
