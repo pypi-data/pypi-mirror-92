f2f5791c2df68406a82355d2f444af47dd340eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:58 +0000

    Add package stingray-cli-core/2.1.4/stingray_cli_core-2.1.4.tar.gz

---
+++ packages/stingray-cli-core/stingray_cli_core-2.1.4.tar.gz/stingray_cli_core-2.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="stingray_cli_core",
+    version='2.1.4',
+    author="Stingray Technologies LLC",
+    description="Stingray core package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Swordfish-Security/stingray-cli-core",
+    packages=find_packages(),
+    author_email='stingray@appsec.global',
+    include_package_data=True,
+    install_requires=[
+         'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
