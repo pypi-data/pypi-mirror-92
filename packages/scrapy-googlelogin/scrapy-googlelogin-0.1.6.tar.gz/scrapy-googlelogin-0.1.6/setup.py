a516b67cd063b795d148a0e758c0ee28af60499f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:29 +0000

    Add package scrapy-googlelogin/0.1.6/scrapy-googlelogin-0.1.6.tar.gz

---
+++ packages/scrapy-googlelogin/scrapy-googlelogin-0.1.6.tar.gz/scrapy-googlelogin-0.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "scrapy-googlelogin",
+    version = "0.1.6",
+    keywords = ("pip", "datacanvas", "eds", "xiaoh"),
+    description = "google login downloader middleware for scrapy",
+    long_description = "google login downloader middleware for scrapy",
+    license = "MIT Licence",
+
+    url = "https://github.com/geek-dc/scrapy-googlelogin",
+    author = "derekchan",
+    author_email = "dchan0831@gmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['selenium', 'pyotp']
+)
+
+
