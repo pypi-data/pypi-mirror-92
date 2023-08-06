b55a6a9df31e4a84163a36646970bb1753b4fb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:16 +0000

    Add package axit/0.0.3/axit-0.0.3.tar.gz

---
+++ packages/axit/axit-0.0.3.tar.gz/axit-0.0.3/README.md
@@ -0,0 +1,22 @@
+# axit
+Axit means Ax it!  Aims for speedup develop efficient.
+
+
+# features
+- ins()  inspect a variable
+
+
+
+
+
+
+# releases
+
+## v0.0.3
+- integrate vcs
+
+## v0.0.2
+- test new version
+
+## v0.0.1
+- born at 2021-01-25
---
+++ packages/axit/axit-0.0.3.tar.gz/axit-0.0.3/axit/mod1.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+def info():
+	print('this is v0.0.3 version')
+
+
+def ins(v):
+    print("ins>>>")
+    print('>dir:')
+    print(dir(v))
+    print('>type:')
+    print(type(v))
+    print('>print:')
+    print(v)
+    print("ins<<<")
+
+
+
