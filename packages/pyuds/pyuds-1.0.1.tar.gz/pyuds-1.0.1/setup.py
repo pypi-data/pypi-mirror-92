cf184c4a2cf132f665d3a369d1d09d7a3ad96821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:12 +0000

    Add package pyuds/1.0.1/pyuds-1.0.1.tar.gz

---
+++ packages/pyuds/pyuds-1.0.1.tar.gz/pyuds-1.0.1/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 11 22:52:35 2019
+
+@author: levy.he
+"""
+
+'''
+    packages=find_packages(where='pyuds', exclude=['sample',
+                           'sample.*', 'GacDiagTest', 'DbMessage.*', 'DbMessage']),
+    packages=['pyuds',
+                'pyuds.PyUds',
+                'pyuds.PyUds.vxlapi',
+                'pyuds.Scripts',
+                'pyuds.TestCase',
+                'pyuds.TestCase.Report'
+                ],
+    packages=[  '.',
+                'PyUds',
+                'PyUds.vxlapi',
+                'Scripts',
+                'TestCase',
+                'TestCase.Report'
+              ],
+'''
+from setuptools import setup, find_packages
+
+setup(
+    name="pyuds",
+    version="1.0.1",
+    zip_safe=False,
+    description="python uds testcase",
+    long_description="python uds testcase",
+    license="MIT Licence",
+
+    url="https://github.com/LevyHe/PyUds",
+    author="levy.he",
+    author_email="levy.he@gmail.com",
+    packages=find_packages(include=['pyuds', 'pyuds.*', 'CddParser', 'ExcelParse', 'termcolor', 'pyps', 'pytty']),
+    package_data={'PyUds.bus.driver.vector': ['vxlapi_32bit.dll', 'vxlapi_64bit.dll'],
+                  'PyUds.TestCase.Report': ['r_template.template.html'],
+                  'PyUds.bus.driver.pcan': ['PCANBasic_nt_amd64_32bit.dll', 'PCANBasic_nt_amd64_64bit.dll']},
+    platforms="any",
+    install_requires=[
+        'pyserial',
+        'mako',
+        'pywin32',
+        'xlrd'
+    ],
+    python_requires=">=3.6",
+)
