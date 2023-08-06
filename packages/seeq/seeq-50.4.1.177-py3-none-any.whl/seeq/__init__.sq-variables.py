05351577bed53328b572d543a7964e8206332bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:56 +0000

    Add package seeq/50.4.1.177/seeq-50.4.1.177-py3-none-any.whl

---
+++ packages/seeq/seeq-50.4.1.177-py3-none-any.whl/seeq/__init__.sq-variables.py
@@ -0,0 +1,3 @@
+name = "seeq"
+__version__ = '%d.%d.%d.%d' % (int('$SQ(VERSION_MAJOR)'), int('$SQ(VERSION_MINOR)'), int('$SQ(VERSION_PATCH)'),
+                               int('$SQ(VERSION_PYPI)'))
