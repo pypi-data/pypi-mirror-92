921af00e3930788fe2fd24891ad3d25d16baeb0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:03 +0000

    Add package wagtail-metadata-mixin/0.1.0/wagtail_metadata_mixin-0.1.0-py3-none-any.whl

---
+++ packages/wagtail-metadata-mixin/wagtail_metadata_mixin-0.1.0-py3-none-any.whl/wagtailmetadata/models.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ImproperlyConfigured
+from django.db import models
+
+from meta import settings as meta_settings
+from meta.models import ModelMeta
+from wagtail.core.models import Site
+from wagtail.images import get_image_model_string
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+class MetadataMixin(ModelMeta):
+    context_meta_name = "meta"
+
+    object_type = None
+    schemaorg_type = None
+    custom_namespace = None
+
+    _metadata_default = {
+        "use_og": "use_og",
+        "use_twitter": "use_twitter",
+        "use_schemaorg": "use_schemaorg",
+        "use_title_tag": "use_title_tag",
+        "title": "get_meta_title",
+        "description": "get_meta_description",
+        "keywords": "get_meta_keywords",
+        "url": "get_meta_url",
+        "image": "get_meta_image",
+        "object_type": "get_meta_object_type",
+        "site_name": "get_meta_site_name",
+        "twitter_site": "get_meta_twitter_site",
+        "twitter_creator": "get_meta_twitter_creator",
+        "twitter_card": "get_meta_twitter_card",
+        "og_author": "get_author_url",
+        "og_publisher": meta_settings.FB_PUBLISHER,
+        "facebook_app_id": meta_settings.FB_APPID,
+        "fb_pages": meta_settings.FB_PAGES,
+        "locale": "get_meta_locale",
+        "schemaorg_type": "get_meta_schemaorg_type",
+        "custom_namespace": "get_meta_custom_namespace",
+        "get_domain": "get_domain",
+    }
+
+    @property
+    def use_og(self):
+        return meta_settings.USE_OG_PROPERTIES
+
+    @property
+    def use_twitter(self):
+        return meta_settings.USE_TWITTER_PROPERTIES
+
+    @property
+    def use_schemaorg(self):
+        return meta_settings.USE_SCHEMAORG_PROPERTIES
+
+    @property
+    def use_title_tag(self):
+        return meta_settings.USE_TITLE_TAG
+
+    def get_meta_title(self):
+        return False
+
+    def get_meta_description(self):
+        return False
+
+    def get_meta_keywords(self):
+        return []
+
+    def get_meta_url(self):
+        return False
+
+    def get_meta_image(self):
+        if bool(meta_settings.DEFAULT_IMAGE) is True:
+            return self.build_absolute_uri(meta_settings.DEFAULT_IMAGE)
+        return None
+
+    def get_meta_object_type(self):
+        return self.object_type or meta_settings.SITE_TYPE
+
+    def get_meta_schemaorg_type(self):
+        return self.schemaorg_type or meta_settings.SCHEMAORG_TYPE
+
+    def get_meta_site_name(self):
+        request = self.get_request()
+        site = getattr(request, "site", None)
+        if request and isinstance(site, Site):
+            if bool(request.site.site_name) is True:
+                return request.site.site_name
+
+        site = self.get_site()
+        if isinstance(site, Site):
+            if bool(site.site_name) is True:
+                return site.site_name
+
+        if request:
+            site = Site.find_for_request(request)
+            if isinstance(site, Site):
+                return site.site_name
+
+        return settings.WAGTAIL_SITE_NAME
+
+    def get_meta_twitter_site(self):
+        return meta_settings.TWITTER_SITE
+
+    def get_meta_twitter_creator(self):
+        return self.get_author_twitter()
+
+    def get_meta_twitter_card(self):
+        if self.get_meta_image() is not None:
+            return "summary_large_image"
+        return "summary"
+
+    def get_meta_locale(self):
+        return getattr(settings, "LANGUAGE_CODE", "en_US")
+
+    def get_meta_custom_namespace(self):
+        return self.custom_namespace or meta_settings.OG_NAMESPACES
+
+    def get_domain(self):
+        request = self.get_request()
+        if request and getattr(request, "site", None):
+            return request.site.hostname
+
+        site = self.get_site()
+        if site is not None:
+            if bool(site.hostname) is True:
+                return site.hostname
+
+        if not meta_settings.SITE_DOMAIN:
+            raise ImproperlyConfigured("META_SITE_DOMAIN is not set")
+
+        return meta_settings.SITE_DOMAIN
+
+    def get_author(self):
+        class Author(object):
+            fb_url = meta_settings.FB_AUTHOR_URL
+            twitter_profile = meta_settings.TWITTER_AUTHOR
+            schemaorg_profile = None
+
+            def get_full_name(self):  # pragma: no cover
+                return None
+
+        return Author()
+
+    def build_absolute_uri(self, url):
+        request = self.get_request()
+        if request is not None:
+            return request.build_absolute_uri(url)
+
+        if url.startswith("http"):
+            return url
+
+        site = self.get_site()
+        if site is not None:
+            return "%s%s" % (site.root_url, url if url.startswith("/") else "/" + url)
+
+        raise NotImplementedError
+
+    def get_context(self, request):
+        context = super(MetadataMixin, self).get_context(request)
+        context[self.context_meta_name] = self.as_meta(request)
+        return context
+
+
+class MetadataPageMixin(MetadataMixin, models.Model):
+
+    search_image = models.ForeignKey(
+        get_image_model_string(), null=True, blank=True, on_delete=models.SET_NULL, related_name="+"
+    )
+
+    panels = [ImageChooserPanel("search_image")]
+
+    _metadata = {
+        "published_time": "published_time",
+        "modified_time": "latest_revision_created_at",
+        "expiration_time": "expire_at",
+    }
+
+    class Meta:
+        abstract = True
+
+    @property
+    def published_time(self):
+        return self.go_live_at or self.first_published_at
+
+    def get_meta_title(self):
+        return self.seo_title or self.title
+
+    def get_meta_description(self):
+        return self.search_description
+
+    def get_meta_keywords(self):
+        return []
+
+    def get_meta_url(self):
+        return self.build_absolute_uri(self.url)
+
+    def get_meta_image(self):
+        if self.search_image is not None:
+            return self.build_absolute_uri(
+                self.search_image.get_rendition(getattr(settings, "META_SEARCH_IMAGE_RENDITION", "fill-800x450")).url
+            )
+        return super(MetadataPageMixin, self).get_meta_image()
+
+    def get_author(self):
+        author = super(MetadataPageMixin, self).get_author()
+        if hasattr(self, "owner") and isinstance(self.owner, get_user_model()):
+            author.get_full_name = self.owner.get_full_name
+        return author
---
+++ packages/wagtail-metadata-mixin/wagtail_metadata_mixin-0.1.0-py3-none-any.whl/wagtailmetadata/tests/settings.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = "DUMMY_SECRET_KEY"
+
+INTERNAL_IPS = []
+
+# Application definition
+
+PROJECT_APPS = ["wagtailmetadata.tests", "wagtailmetadata"]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "taggit",
+    "modelcluster",
+    "wagtail.core",
+    "wagtail.users",
+    "wagtail.images",
+    "meta",
+] + PROJECT_APPS
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "wagtail.core.middleware.SiteMiddleware",
+    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "tests", "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "wagtail.contrib.settings.context_processors.settings",
+            ]
+        },
+    }
+]
+
+ROOT_URLCONF = "wagtailmetadata.tests.urls"
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+MEDIA_URL = "/media/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
+
+STATIC_URL = "/static/"
+
+
+## Meta settings
+
+META_USE_OG_PROPERTIES = True
+META_USE_TWITTER_PROPERTIES = True
+
+
+## Wagtail settings
+
+WAGTAIL_SITE_NAME = "Test Site"
