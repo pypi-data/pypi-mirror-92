9778fd3865680b1af8947ed23278eca89749b85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:10 +0000

    Add package model2/1.0.4.1/model2-1.0.4.1.tar.gz

---
+++ packages/model2/model2-1.0.4.1.tar.gz/model2-1.0.4.1/model2/role/action.py
@@ -0,0 +1,37 @@
+from model2.role.main import RoleManager
+
+
+class RoleAction(object):
+    def get(self, **params):
+        if params:
+            for i in params:
+                print("find params===>key={0},value={1}".format(i, params[i]))
+
+        main = RoleManager()
+        return main.get(**params)
+
+    def post(self, **params):
+        if params:
+            for i in params:
+                print("find params===>key={0},value={1}".format(i, params[i]))
+
+        main = RoleManager()
+        return main.post(**params)
+
+
+    def delete(self, **params):
+        if params:
+            for i in params:
+                print("find params===>key={0},value={1}".format(i, params[i]))
+
+        main = RoleManager()
+        return main.delete(**params)
+
+
+    def put(self, **params):
+        if params:
+            for i in params:
+                print("find params===>key={0},value={1}".format(i, params[i]))
+
+        main = RoleManager()
+        return main.put(**params)
