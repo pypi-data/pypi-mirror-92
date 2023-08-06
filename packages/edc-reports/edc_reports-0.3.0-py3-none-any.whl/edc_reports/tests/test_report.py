580109f2499978f5d686c97a83def2164bf55532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:22 +0000

    Add package edc-reports/0.3.0/edc_reports-0.3.0-py3-none-any.whl

---
+++ packages/edc-reports/edc_reports-0.3.0-py3-none-any.whl/edc_reports/tests/test_report.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from uuid import uuid4
+
+from .my_report import MyReport
+
+
+class TestReport(TestCase):
+    def test_report(self):
+        report = MyReport(filename=f"{uuid4()}.pdf")
+        response = report.render()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("ReportLab Generated PDF document", str(response.content))
