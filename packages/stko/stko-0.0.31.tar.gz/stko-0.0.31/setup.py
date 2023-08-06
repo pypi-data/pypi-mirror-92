c21e9ca574ed75e0f088acf25c49f1ae3f8f079a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:49 +0000

    Add package stko/0.0.31/stko-0.0.31.tar.gz

---
+++ packages/stko/stko-0.0.31.tar.gz/stko-0.0.31/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stko",
+    version="0.0.31",
+    author="Steven Bennett, Andrew Tarzia",
+    author_email="s.bennett18@imperial.ac.uk",
+    description="Contains molecular optimizers and property calculators for use with stk.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JelfsMaterialsGroup/stko",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/stko/stko-0.0.31.tar.gz/stko-0.0.31/stko/optimizers/__init__.py
@@ -0,0 +1,6 @@
+from .optimizers import *  # noqa
+from .rdkit import *  # noqa
+from .gulp import *  # noqa
+from .collapser import *  # noqa
+from .xtb import *  # noqa
+from .macromodel import *  # noqa
