0fd1782d3ff411d2189b663e647d0ee5b31c8730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:27 +0000

    Add package PythonEventSystem/1.0.2/PythonEventSystem-1.0.2-py3-none-any.whl

---
+++ packages/PythonEventSystem/PythonEventSystem-1.0.2-py3-none-any.whl/PythonEventSystem/__init__.py
@@ -0,0 +1,14 @@
+# Super small event helper
+class Event:
+    def __init__(self):
+        self.subscribedListeners = []
+    def __call__(self, *args):
+        for listener in self.subscribedListeners:
+            listener(args)
+    def __iadd__(self, method):
+        self.subscribedListeners.append(method)
+        return self
+    def __isub__(self, method):
+        if method in self.subscribedListeners:
+            self.subscribedListeners.remove(method)
+        return self
