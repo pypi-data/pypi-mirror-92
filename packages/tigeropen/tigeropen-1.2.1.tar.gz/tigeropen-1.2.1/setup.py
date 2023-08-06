5c24940ca1bcbf2325f291b5a249f1b5ed68f100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:19 +0000

    Add package tigeropen/1.2.1/tigeropen-1.2.1.tar.gz

---
+++ packages/tigeropen/tigeropen-1.2.1.tar.gz/tigeropen-1.2.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018/9/16
+
+@author: gaoan
+"""
+from os import path
+from setuptools import find_packages, setup
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'requirements.txt')) as f:
+    install_requires = f.read()
+
+setup(
+    name='tigeropen',
+    version='1.2.1',
+    description='TigerBrokers Open API',
+    packages=find_packages(exclude=[]),
+    author='TigerBrokers',
+    author_email='openapi@tigerbrokers.com',
+    license='Apache License v2',
+    package_data={'': ['*.*']},
+    url='https://github.com/tigerbrokers/openapi-python-sdk',
+    platforms='any',
+    install_requires=install_requires,
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
