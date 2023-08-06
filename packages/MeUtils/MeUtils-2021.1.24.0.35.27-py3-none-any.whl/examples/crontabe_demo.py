7d1135b584c0585de27a86e403a3f36737302c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:18 +0000

    Add package MeUtils/2021.1.24.0.35.27/MeUtils-2021.1.24.0.35.27-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2021.1.24.0.35.27-py3-none-any.whl/examples/crontabe_demo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : crontabe_demo
+# @Time         : 2021/1/24 12:08 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : https://www.cnblogs.com/mysql-dba/p/13565057.html
+
+
+from meutils.crontab import CronTab
+
+# 增加一个crontab任务
+comment = 'comment' # crontab id
+with CronTab(user='yuanjie') as cron:
+    # cron.crons
+    job = cron.new(command='echo hello_world', comment=comment, pre_comment=True)
+    job.minute.every(1)
+
+# 删除一个crontab任务
+with CronTab(user='yuanjie') as cron:
+    jobs = cron.find_comment('job')
+    # cron.find_command('echo hello_world')
+    # cron.find_time('* * * * *')
+    for job in jobs:
+        cron.remove(job)
+
+# 修改任务
+with CronTab(user='yuanjie') as cron:
+    jobs = cron.find_comment('job')
+    for job in jobs:
+        job.set_command("python bakcup.py --port=3306")
+        # job.set_comment
+        # job.setall # 修改调度时间
