2ed433d63fc287d6f92165794534721396c649e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:01 +0000

    Add package edc-lab-dashboard/0.3.0/edc_lab_dashboard-0.3.0-py3-none-any.whl

---
+++ packages/edc-lab-dashboard/edc_lab_dashboard-0.3.0-py3-none-any.whl/edc_lab_dashboard/views/__init__.py
@@ -0,0 +1,24 @@
+from .action_views import (
+    ManageBoxItemView,
+    ProcessView,
+    ReceiveView,
+    VerifyBoxItemView,
+    PackView,
+    ManageManifestView,
+    ManifestView,
+    AliquotView,
+    RequisitionView,
+)
+from .home_view import HomeView
+from .listboard_views import (
+    AliquotListboardView,
+    ManageBoxListboardView,
+    ManifestListboardView,
+    PackListboardView,
+    ProcessListboardView,
+    ReceiveListboardView,
+    RequisitionListboardView,
+    ResultListboardView,
+    VerifyBoxListboardView,
+    ManageManifestListboardView,
+)
