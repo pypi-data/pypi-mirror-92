1f9ca89185be0bb84572348a79c73946178a931f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:05 +0000

    Add package my-happy-flow/0.0.2/my_happy_flow-0.0.2.tar.gz

---
+++ packages/my-happy-flow/my_happy_flow-0.0.2.tar.gz/my_happy_flow-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup, find_packages
+
+import versioneer
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+install_requires = [
+            'ray>=1.1.0',
+            'modin>=0.8.3'
+        ]
+
+from happy_python_utils import version
+
+setup(
+  name="my_happy_flow",
+  version=versioneer.get_version(),
+  description="My Happy Flow",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  license="A",
+  url="https://github.com/ggservice007/my-happy-flow",
+  author="ggservice007",
+  author_email="ggservice007@126.com",
+  packages=find_packages(include=["src"], exclude=[""]),
+  install_requires=install_requires,
+  include_package_data=True,
+  zip_safe=False
+)
